04f85f957f76a9c8e1112085c759170225330c13
nono303 <nono303@f19f5125-b473-6040-a4d2-80918b615817>
2018-01-03 14:56:22 +0000

    factorisation commandes x86 & x64
    git-svn-id: https://svn.nono303.net/build/msvc/httpd@2224 f19f5125-b473-6040-a4d2-80918b615817

--- x86.bat
+++ x86.bat
@@ -1,4 +1,11 @@
-SET PATH=C:\bin\nasm;c:\perl\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg32\bin
-call C:\msvc15\VC\Auxiliary\Build\vcvarsall.bat x86
+set ARCH=x86
 set perlbuild=VC-WIN32
+set CYGV=32
+
+rmdir /S /Q C:\httpd-sdk\build
+mklink /j C:\httpd-sdk\build C:\httpd-sdk\build_%ARCH%
+rmdir /S /Q C:\httpd-sdk\install
+mklink /j C:\httpd-sdk\install C:\httpd-sdk\install_%ARCH%
+SET PATH=C:\bin\nasm;c:\perl\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg%CYGV%\bin
+call C:\msvc15\VC\Auxiliary\Build\vcvarsall.bat %ARCH%
 cd /D C:\httpd-sdk
