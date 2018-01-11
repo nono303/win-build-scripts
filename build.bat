8a66a5e5087915f919b389fd4bfaf464fe61f735
nono303 <nono303@f19f5125-b473-6040-a4d2-80918b615817>
2018-01-11 09:45:05 +0000

    git-svn-id: https://svn.nono303.net/build/msvc/httpd@2314 f19f5125-b473-6040-a4d2-80918b615817

--- build.bat
+++ build.bat
@@ -121,14 +121,15 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\apr
 mkdir C:\httpd-sdk\build\apr
 cd /D C:\httpd-sdk\build\apr
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DMIN_WINDOWS_VER=0x0600 -DAPR_HAVE_IPV6=OFF -DAPR_INSTALL_PRIVATE_H=ON -DAPR_BUILD_TESTAPR=OFF -DINSTALL_PDB=OFF ..\..\src\apr
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DMIN_WINDOWS_VER=0x0601 -DAPR_HAVE_IPV6=OFF -DAPR_INSTALL_PRIVATE_H=ON -DAPR_BUILD_TESTAPR=OFF -DINSTALL_PDB=ON  ..\..\src\apr
+REM -DAPU_HAVE_CRYPTO=ON  pour trunk
 nmake /B /NOLOGO clean install
 
 cd ..
 rmdir /S /Q C:\httpd-sdk\build\apr-util
 mkdir C:\httpd-sdk\build\apr-util
 cd /D C:\httpd-sdk\build\apr-util
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DAPU_HAVE_CRYPTO=ON -DAPR_HAS_LDAP=ON -DAPR_BUILD_TESTAPR=OFF -DAPU_HAVE_ODBC=ON -DINSTALL_PDB=OFF -DOPENSSL_ROOT_DIR=C:\httpd-sdk\install\ ..\..\src\apr-util
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DAPR_HAS_LDAP=ON -DAPR_BUILD_TESTAPR=OFF -DAPU_HAVE_ODBC=ON -DINSTALL_PDB=OFF -DAPU_HAVE_CRYPTO=ON -DOPENSSL_ROOT_DIR=C:\httpd-sdk\install\ ..\..\src\apr-util
 nmake /B /NOLOGO clean install
 
 cd ..
@@ -138,12 +139,12 @@ cd /D C:\httpd-sdk\build\lua
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ..\..\src\lua
 nmake /B /NOLOGO clean install
 
-	REM -- OPTIMISATION CFLAGS KO...
 cd ..
 rmdir /S /Q C:\httpd-sdk\build\httpd
 mkdir C:\httpd-sdk\build\httpd
 cd /D C:\httpd-sdk\build\httpd
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DINSTALL_PDB=ON -DENABLE_MODULES=i -DINSTALL_MANUAL=OFF -DLIBXML2_ICONV_INCLUDE_DIR=C:/httpd-sdk/install/include -DLIBXML2_ICONV_LIBRARIES=C:/httpd-sdk/install/lib/iconv.lib -DZLIB_LIBRARIES=C:/httpd-sdk/install/lib/zlib.lib -DJANSSON_INCLUDE_DIR=C:/httpd-sdk/install/include -DJANSSON_LIBRARIES=C:/httpd-sdk/install/lib/jansson.lib -DOPENSSL_ROOT_DIR=C:\httpd-sdk\install\ -DENABLE_SOCACHE_DC=O -DENABLE_CHARSET_LITE=O ..\..\src\httpd
+REM -DEXTRA_COMPILE_FLAGS="/GL /GS- /Oy- /W0 /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox /wd9025" -DCMAKE_MODULE_LINKER_FLAGS=%EXTRA_LINK_FLAGS% -DCMAKE_SHARED_LINKER_FLAGS=%EXTRA_LINK_FLAGS% -DCMAKE_STATIC_LINKER_FLAGS=%EXTRA_LINK_FLAGS%
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/httpd_flags_%CMAKE_BUILD_TYPE%.sh
 nmake /B /NOLOGO clean install
 mt.exe -manifest C:\httpd-sdk\httpd.exe.manifest -outputresource:C:\httpd-sdk\install\bin\httpd.exe;1
--- httpd_flags.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/find /cygdrive/c/httpd-sdk/build/httpd -type f -name "build.make" -exec sed -i 's/\/machine:x64 \/INCREMENTAL:NO/\/machine:x64 \/LTCG \/NODEFAULTLIB:libcmt.lib \/OPT:ICF \/INCREMENTAL:NO/g' {} \;
-/usr/bin/find /cygdrive/c/httpd-sdk/build/httpd -type f -name "flags.make" -exec sed -i 's/RC_FLAGS = \/DWIN32/RC_FLAGS = \/nologo \/DWIN32/g' {} \;
-/usr/bin/find /cygdrive/c/httpd-sdk/build/httpd -type f -name "flags.make" -exec sed -i 's/\/W3 \/MD \/O2/\/GL \/GS- \/Oy- \/w \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox/g' {} \;
-
