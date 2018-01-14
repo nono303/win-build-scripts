95cb5e2f0cde7c90a6a929d5776ae1eb03341ff1
nono303 <nono303@f19f5125-b473-6040-a4d2-80918b615817>
2018-01-14 11:58:31 +0000

    git-svn-id: https://svn.nono303.net/build/msvc/httpd@2329 f19f5125-b473-6040-a4d2-80918b615817

--- build.bat
+++ build.bat
@@ -73,8 +73,9 @@ copy /Y C:\httpd-sdk\build\libiconv\libiconv.pdb C:\httpd-sdk\install\bin\libico
 
 cd /D C:\httpd-sdk\src\libxml2\win32\
 cscript configure.js compiler=nmakemsvc prefix=C:\httpd-sdk\install include=C:\httpd-sdk\install\include lib=C:\httpd-sdk\install\lib debug=no zlib=yes
-nmake /NOLOGO /A /B /f Makefile.msvc distclean install-libs
+nmake /NOLOGO /A /B /f Makefile.msvc clean install-libs
 copy /Y C:\httpd-sdk\src\libxml2\win32\bin.msvc\libxml2.pdb C:\httpd-sdk\install\bin\libxml2.pdb
+nmake /NOLOGO /A /B /f Makefile.msvc distclean
 
 cd ..
 rmdir /S /Q C:\httpd-sdk\build\pcre
