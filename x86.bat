033dfe0c20bf3617f97629af7bd4978e3b50ed88
nono303 <nono303@f19f5125-b473-6040-a4d2-80918b615817>
2018-02-21 15:19:16 +0000

    add git to path
    git-svn-id: https://svn.nono303.net/build/msvc/httpd@2453 f19f5125-b473-6040-a4d2-80918b615817

--- x86.bat
+++ x86.bat
@@ -11,6 +11,6 @@ rmdir /S /Q C:\httpd-sdk\build
 mklink /j C:\httpd-sdk\build C:\httpd-sdk\build_%ARCH%
 rmdir /S /Q C:\httpd-sdk\install
 mklink /j C:\httpd-sdk\install C:\httpd-sdk\install_%ARCH%
-SET PATH=C:\bin\nasm;c:\perl\bin;c:\perl\site\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg%CYGV%\bin
+SET PATH=C:\bin\nasm;c:\perl\bin;c:\perl\site\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg%CYGV%\bin;"C:\Program Files\Git\bin"
 call C:\msvc15\VC\Auxiliary\Build\vcvarsall.bat %ARCH%
 cd /D C:\httpd-sdk
