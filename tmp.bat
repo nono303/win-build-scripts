edb554fe1d416566f76bd857eb6c55d04471caa1
nono303 <nono303@f19f5125-b473-6040-a4d2-80918b615817>
2018-03-05 13:53:24 +0000

    git-svn-id: https://svn.nono303.net/build/msvc/httpd@2475 f19f5125-b473-6040-a4d2-80918b615817

--- tmp.bat
+++ tmp.bat
@@ -1,9 +1,17 @@
+REM http://uppod.ru/talk_2008
 cd ..
-rmdir /S /Q C:\httpd-sdk\build\httpd
-mkdir C:\httpd-sdk\build\httpd
-cd /D C:\httpd-sdk\build\httpd
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DINSTALL_PDB=ON -DENABLE_MODULES=i -DINSTALL_MANUAL=OFF -DLIBXML2_ICONV_INCLUDE_DIR=C:/httpd-sdk/install/include -DLIBXML2_ICONV_LIBRARIES=C:/httpd-sdk/install/lib/iconv.lib -DZLIB_LIBRARIES=C:/httpd-sdk/install/lib/zlib.lib -DJANSSON_INCLUDE_DIR=C:/httpd-sdk/install/include -DJANSSON_LIBRARIES=C:/httpd-sdk/install/lib/jansson.lib -DOPENSSL_ROOT_DIR=C:\httpd-sdk\install\ -DENABLE_SOCACHE_DC=O -DENABLE_CHARSET_LITE=O ..\..\src\httpd
-REM -DEXTRA_COMPILE_FLAGS="/GL /GS- /Oy- /W0 /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox /wd9025" -DCMAKE_MODULE_LINKER_FLAGS=%EXTRA_LINK_FLAGS% -DCMAKE_SHARED_LINKER_FLAGS=%EXTRA_LINK_FLAGS% -DCMAKE_STATIC_LINKER_FLAGS=%EXTRA_LINK_FLAGS%
-C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/httpd_flags_%CMAKE_BUILD_TYPE%.sh
-nmake /B /NOLOGO clean install
-mt.exe -manifest C:\httpd-sdk\httpd.exe.manifest -outputresource:C:\httpd-sdk\install\bin\httpd.exe;1
+rmdir /S /Q C:\httpd-sdk\build\mod_h264_streaming
+mkdir C:\httpd-sdk\build\mod_h264_streaming
+cd /D C:\httpd-sdk\build\mod_h264_streaming
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/mod_h264_streaming.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/output_mp4.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/output_bucket.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/moov.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/mp4_process.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/mp4_writer.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/mp4_reader.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 /GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox -I"C:/httpd-sdk/install/include" -I"C:/httpd-sdk/install/lib" -D BUILDING_H264_STREAMING /c /Fo C:/src/mod_h264_streaming/mp4_io.c
+link /nologo kernel32.lib "C:/httpd-sdk/install/lib/libhttpd.lib" "C:/httpd-sdk/install/lib/libapr-1.lib" "C:/httpd-sdk/install/lib/libaprutil-1.lib" -Dll /machine:x64 /LTCG /OPT:ICF /debug /out:mod_h264_streaming.so output_mp4.obj output_bucket.obj mp4_writer.obj mp4_reader.obj mp4_process.obj mp4_io.obj moov.obj mod_h264_streaming.obj
+copy /Y C:\httpd-sdk\build\mod_h264_streaming\mod_h264_streaming.so C:\httpd-sdk\install\modules\mod_h264_streaming.so
+copy /Y C:\httpd-sdk\build\mod_h264_streaming\mod_h264_streaming.pdb C:\httpd-sdk\install\modules\mod_h264_streaming.pdb
+cd /D C:\httpd-sdk\
