ba267a368ee4c5d0c64aaedc2ebd657f5652be4e
nono <nono@nono.com>
2018-10-07 18:49:39 +0200

    zlib with ASM support https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows

--- modules_bat/zlib.bat
+++ modules_bat/zlib.bat
@@ -1,9 +1,21 @@
-cd ..
-rmdir /S /Q C:\httpd-sdk\build\zlib
-mkdir C:\httpd-sdk\build\zlib
-cd /D C:\httpd-sdk\build\zlib
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -D%ZLIB_ASM%=1 ..\..\src\zlib
-nmake /B /NOLOGO clean install
-move /Y C:\src\zlib\zconf.h.included C:\src\zlib\zconf.h
-copy /Y C:\httpd-sdk\build\zlib\zlib.pdb c:\httpd-sdk\install\bin\zlib1.pdb
+REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
+
+mkdir C:\httpd-sdk\install\lib
+mkdir C:\httpd-sdk\install\bin
+mkdir C:\httpd-sdk\install\include
+cd /D C:\httpd-sdk\src\zlib
+nmake -f win32/Makefile.msc clean
+if %ARCH% == x86 (
+	nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF" OBJA="inffas32.obj match686.obj"
+)
+if %ARCH% == x64 (
+	nmake -f win32/Makefile.msc AS=ml64 LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj"
+)
+copy /Y C:\httpd-sdk\src\zlib\zlib.lib C:\httpd-sdk\install\lib\zlibstatic.lib
+copy /Y C:\httpd-sdk\src\zlib\zlib.pdb C:\httpd-sdk\install\lib\zlibstatic.pdb
+copy /Y C:\httpd-sdk\src\zlib\zlib1.dll c:\httpd-sdk\install\bin\zlib1.dll
+copy /Y C:\httpd-sdk\src\zlib\zlib1.pdb c:\httpd-sdk\install\bin\zlib1.pdb
+copy /Y C:\httpd-sdk\src\zlib\zdll.lib C:\httpd-sdk\install\lib\zlib.lib
+copy /Y C:\httpd-sdk\src\zlib\zlib.h C:\httpd-sdk\install\include\zlib.h
+copy /Y C:\src\zlib\zconf.h C:\httpd-sdk\install\include\zconf.h
 cd /D C:\httpd-sdk\modules_bat
--- x86.bat
+++ x86.bat
@@ -4,5 +4,4 @@ set perlbuild=VC-WIN32
 set archmsbuild=Win32
 set outmsbuild=Release
 set CYGV=32
-set ZLIB_ASM=ASM686
 call C:\httpd-sdk\xCommon.bat
