bc6c44093d1b96ceb3e1667451ff6a53fbf90627
nono <nono@nono.com>
2018-10-09 14:21:15 +0200

    subversion => module php => build all type for arch

--- phpsdk-local.bat
+++ phpsdk-local.bat
@@ -4,23 +4,11 @@ set path=%path%;%oldpath%
 cd /d C:\php72-sdk\
 
 set BUILDLIB=0
-set TS=0
 set LTCG=1
 set LOGNAME=C:\php72-sdk\silent-logs\%PHP_SDK_ARCH%_TS-%TS%_LTCG-%LTCG%_%Date:~-4%%Date:~-7,-5%%Date:~-10,-8%-%TIME:~-11,-9%%TIME:~-8,-6%%TIME:~-5,-3%.log
 
-if %TS% == 0 (
-	set ZTS=--disable-zts
-	set BUILDDIR=Release
-) else (
-	set BUILDDIR=Release_TS
-)
-if defined AVX (
-	set SED_AVX= \%AVX%
-	set outdir=-AVX
-)
-
-	REM ** uniquement sur init
-REM phpsdk_buildtree phpmaster
+REM ** uniquement sur init
+	REM phpsdk_buildtree phpmaster
 call phpsdk_deps -u -b 7.2 -a %PHP_SDK_ARCH% -d C:\php72-sdk\phpmaster\vc15\%PHP_SDK_ARCH%\deps -t vc15
 
 if %BUILDLIB% == 1 (
@@ -29,24 +17,23 @@ if %BUILDLIB% == 1 (
 	call %MODULE_BAT_DIR%libiconv-php.bat
 	call %MODULE_BAT_DIR%libpng-php.bat
 )
+set ZTS=--disable-zts
+set BUILDDIR=Release
 
-rmdir /S /Q C:\php72-sdk\phpmaster\vc15\%PHP_SDK_ARCH%\build%outdir%\%BUILDDIR%
-mkdir C:\php72-sdk\phpmaster\vc15\%PHP_SDK_ARCH%\build%outdir%\%BUILDDIR%
+set SED_AVX= /arch:AVX
+set outdir=-AVX
+	call C:\httpd-sdk\phpsdk-config_make.bat
+set SED_AVX=
+set outdir=
+	call C:\httpd-sdk\phpsdk-config_make.bat
+set BUILDDIR=Release_TS
+set ZTS=
 
-cd /d C:\php72-sdk\phpmaster\vc15\%PHP_SDK_ARCH%\php-src
-call buildconf
-call configure --with-mp=8 --enable-object-out-dir=../build%outdir%/ --disable-embed --disable-phpdbgs --disable-phpdbg --disable-cli-win32 --disable-test-ini --disable-debug --disable-debug-pack --disable-ipv6 --disable-phpdbg-webhelper --disable-crt-debug --disable-security-flags --without-enchant --without-imap --without-snmp --without-xmlrpc --without-xsl --without-gmp --without-wddx --without-libwebp --without-interbase --without-ldap --without-oci8 --without-pgsql --without-uncritical-warn-choke --enable-sockets --enable-mbstring --enable-exif --enable-memcache=shared --enable-pdo --enable-opcache --enable-soap --enable-fileinfo --enable-com-dotnet --enable-fd-setsize=2048 --enable-sanitizer --enable-intl --without-analyzer --with-curl --with-tidy --with-openssl --with-mysqli --with-pdo-mysql --with-bz2 --with-sqlite3 --with-pdo-sqlite --with-extra-includes="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Include\um";"C:\Program Files (x86)\Windows Kits\10\Include\10.0.17134.0\um" --with-extra-libs="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Lib\um\%PHP_SDK_ARCH%";"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\%PHP_SDK_ARCH%" --with-xdebug=shared %ZTS%
-if %LTCG% == 1 (
-	sed -i 's/LDFLAGS=\/nologo/LDFLAGS=\/nologo \/LTCG \/NODEFAULTLIB:libcmt.lib \/OPT:ICF/g' /cygdrive/c/src/php-src/Makefile
-	sed -i 's/ARFLAGS=\/nologo/ARFLAGS=\/nologo \/LTCG \/NODEFAULTLIB:libcmt.lib \/OPT:ICF/g' /cygdrive/c/src/php-src/Makefile
-	sed -i 's/CFLAGS=\/nologo/CFLAGS=\/nologo \/GL \/GS- \/Oy- \/w%SED_AVX%/g' /cygdrive/c/src/php-src/Makefile
-) else (
-	sed -i 's/LDFLAGS=\/nologo/LDFLAGS=\/nologo \/NODEFAULTLIB:libcmt.lib \/OPT:ICF/g' /cygdrive/c/src/php-src/Makefile
-	sed -i 's/ARFLAGS=\/nologo/ARFLAGS=\/nologo \/NODEFAULTLIB:libcmt.lib \/OPT:ICF/g' /cygdrive/c/src/php-src/Makefile
-	sed -i 's/CFLAGS=\/nologo/CFLAGS=\/nologo \/GS- \/Oy- \/w%SED_AVX%/g' /cygdrive/c/src/php-src/Makefile
-)
-sed -i 's/ \/W3 / /g' /cygdrive/c/src/php-src/Makefile
-sed -i 's/ \/wd4996 / /g' /cygdrive/c/src/php-src/Makefile
-nmake /B /NOLOGO
+set SED_AVX= /arch:AVX
+set outdir=-AVX
+	call C:\httpd-sdk\phpsdk-config_make.bat
+set SED_AVX=
+set outdir=
+	call C:\httpd-sdk\phpsdk-config_make.bat
 pause
 exit
--- subversion-build.bat
+++ subversion-build.bat
@@ -1,72 +1,7 @@
 mklink /H C:\httpd-sdk\install\lib\xml.lib C:\httpd-sdk\install\lib\expat.lib
 
-cd /D C:\httpd-sdk\src\sqlite-amalgamation
-rm -f *.exe *.obj *.pdb *.idb
-cl /nologo /GL /GS- /Oy- /w /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox shell.c sqlite3.c -Fesqlite3.exe
-
-cd /D C:\src\serf
-python c:/bin/scons/scons.py --clean
-git clean -fdx
-cd /D C:\httpd-sdk\src\serf\build
-python gen_def.py
-cd /D C:\src\serf
-	REM 1.3.9
-REM python c:/bin/scons/scons.py --no-cache MSVC_VERSION=14.1 TARGET_ARCH=%serfbuild% APR=C:/httpd-sdk/install/ APU=C:/httpd-sdk/install/ OPENSSL=C:/httpd-sdk/install/ / ZLIB=C:/httpd-sdk/install/
-	REM 2.0.0
-python c:/bin/scons/scons.py --no-cache MSVC_VERSION=14.1 TARGET_ARCH=%serfbuild% APR=C:/httpd-sdk/install/lib/ APU=C:/httpd-sdk/install/ OPENSSL=C:/httpd-sdk/install/ ZLIB=C:/httpd-sdk/install/lib/ BROTLI=C:/httpd-sdk/install/ EXPAT=C:/httpd-sdk/install/
-
-cd /D C:\src\subversion
-git clean -fdx
-mkdir C:\httpd-sdk\src\subversion\Release-%ARCH%
-mklink /J C:\httpd-sdk\src\subversion\Release C:\httpd-sdk\src\subversion\Release-%ARCH%
-rmdir /S /Q C:\httpd-sdk\src\subversion\build\win32\vcnet-vcproj
-python gen-make.py --release -t vcproj --with-jdk=C:\jdk8\%ARCH% --with-serf=C:\src\serf --with-sqlite=C:\httpd-sdk\src\sqlite-amalgamation --vsnet-version=2017 --with-openssl=C:\httpd-sdk\install --with-apr-util=C:\httpd-sdk\install --with-apr=C:\httpd-sdk\install --with-apr-iconv=C:\httpd-sdk\install --with-zlib=C:\httpd-sdk\install --with-apr_memcache=C:\httpd-sdk\install --with-httpd=C:\httpd-sdk\install 
-C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/subversion_vcxproj.sh
-"C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" subversion_vcnet.sln /nowarn:C4702 /nowarn:LNK4087 /nowarn:C4703 /nowarn:C4132 /nowarn:C4389 /nowarn:C4244 /nowarn:C4245 /nowarn:C4267 /nowarn:C4018 /nologo /m:8 /t:Clean;__ALL__;__JAVAHL__ /p:Configuration=Release /p:Platform=%archmsbuild%  /p:DebugSymbols=true /p:DebugType=None
-
-rmdir /S /Q C:\httpd-sdk\install\svn
-mkdir C:\httpd-sdk\install\svn
-
-for /f "tokens=*" %%G in ('dir C:\src\subversion\Release\*.exe /s/b') do (
-COPY %%~pG%%~nG.exe C:\httpd-sdk\install\svn\%%~nG.exe
-COPY %%~pG%%~nG.pdb C:\httpd-sdk\install\svn\%%~nG.pdb
-)
-for /f "tokens=*" %%G in ('dir C:\src\subversion\Release\*.dll /s/b') do (
-COPY %%~pG%%~nG.dll C:\httpd-sdk\install\svn\%%~nG.dll
-COPY %%~pG%%~nG.pdb C:\httpd-sdk\install\svn\%%~nG.pdb
-)
-for /f "tokens=*" %%G in ('dir C:\src\subversion\Release\*.so /s/b') do (
-COPY %%~pG%%~nG.so C:\httpd-sdk\install\svn\%%~nG.so
-COPY %%~pG%%~nG.pdb C:\httpd-sdk\install\svn\%%~nG.pdb
-)
-
-set DEPS=\deps\
-mkdir C:\httpd-sdk\install\svn%DEPS%
-if %ARCH% == x64 (
-	set sslarch=-x64
-)
-
-Copy /Y C:\httpd-sdk\install\bin\libssl-1_1%sslarch%.dll C:\httpd-sdk\install\svn%DEPS%libssl-1_1%sslarch%.dll
-Copy /Y C:\httpd-sdk\install\bin\libcrypto-1_1%sslarch%.dll C:\httpd-sdk\install\svn%DEPS%libcrypto-1_1%sslarch%.dll
-
-Copy /Y C:\httpd-sdk\install\bin\expat.dll C:\httpd-sdk\install\svn%DEPS%expat.dll
-Copy /Y C:\httpd-sdk\install\bin\libapr-1.dll C:\httpd-sdk\install\svn%DEPS%libapr-1.dll
-Copy /Y C:\httpd-sdk\install\bin\libapriconv-1.dll C:\httpd-sdk\install\svn%DEPS%libapriconv-1.dll
-Copy /Y C:\httpd-sdk\install\bin\libaprutil-1.dll C:\httpd-sdk\install\svn%DEPS%libaprutil-1.dll
-Copy /Y C:\httpd-sdk\install\bin\brotlienc.dll C:\httpd-sdk\install\svn%DEPS%brotlienc.dll
-Copy /Y C:\httpd-sdk\install\bin\brotlicommon.dll C:\httpd-sdk\install\svn%DEPS%brotlicommon.dll
-Copy /Y C:\httpd-sdk\install\bin\brotlidec.dll C:\httpd-sdk\install\svn%DEPS%brotlidec.dll
-Copy /Y C:\src\serf\libserf-2.dll C:\httpd-sdk\install\svn%DEPS%libserf-2.dll
-
-Copy /Y C:\httpd-sdk\install\bin\libssl-1_1%sslarch%.pdb C:\httpd-sdk\install\svn%DEPS%libssl-1_1%sslarch%.pdb
-Copy /Y C:\httpd-sdk\install\bin\expat.pdb C:\httpd-sdk\install\svn%DEPS%expat.pdb
-Copy /Y C:\httpd-sdk\install\bin\libapr-1.pdb C:\httpd-sdk\install\svn%DEPS%libapr-1.pdb
-Copy /Y C:\httpd-sdk\install\bin\libapriconv-1.pdb C:\httpd-sdk\install\svn%DEPS%libapriconv-1.pdb
-Copy /Y C:\httpd-sdk\install\bin\libaprutil-1.pdb C:\httpd-sdk\install\svn%DEPS%libaprutil-1.pdb
-Copy /Y C:\httpd-sdk\install\bin\libcrypto-1_1%sslarch%.pdb C:\httpd-sdk\install\svn%DEPS%libcrypto-1_1%sslarch%.pdb
-Copy /Y C:\httpd-sdk\install\bin\brotlienc.dll C:\httpd-sdk\install\svn%DEPS%brotlienc.pdb
-Copy /Y C:\httpd-sdk\install\bin\brotlicommon.dll C:\httpd-sdk\install\svn%DEPS%brotlicommon.pdb
-Copy /Y C:\httpd-sdk\install\bin\brotlidec.dll C:\httpd-sdk\install\svn%DEPS%brotlidec.pdb
-Copy /Y C:\src\serf\libserf-2.pdb C:\httpd-sdk\install\svn%DEPS%libserf-2.pdb
+call %MODULE_BAT_DIR%sqlite-svn.bat
+call %MODULE_BAT_DIR%serf-svn.bat
+call %MODULE_BAT_DIR%subversion-svn.bat
 
 cd /D C:\httpd-sdk
