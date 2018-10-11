9662733eb0b006933173fcbbe210e5dd45854f77
nono <nono@nono.com>
2018-10-11 11:35:30 +0200

    add avx build & install dir for httpd

--- xCommon.bat
+++ xCommon.bat
@@ -5,13 +5,16 @@ set CL=/MP
 
 REM AVX || SSE2 https://msdn.microsoft.com/fr-fr/library/7t5yh4fd.aspx
 set AVX=/arch:AVX
+if DEFINED AVX (
+	SET AVXB=-avx
+)
 set EXTCFLAGS=/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %AVX%
 
 set MODULE_BAT_DIR=C:\httpd-sdk\modules_bat\
 rmdir /S /Q C:\httpd-sdk\build
-mklink /j C:\httpd-sdk\build C:\httpd-sdk\build_%ARCH%
 rmdir /S /Q C:\httpd-sdk\install
-mklink /j C:\httpd-sdk\install C:\httpd-sdk\install_%ARCH%
+mklink /j C:\httpd-sdk\build C:\httpd-sdk\build_%ARCH%%AVXB%
+mklink /j C:\httpd-sdk\install C:\httpd-sdk\install_%ARCH%%AVXB%
 set PATH=c:\python27;C:\PROGRA~1\Git\bin;C:\bin\nasm;c:\perl\bin;c:\perl\site\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg%CYGV%\bin
 call C:\msvc15\VC\Auxiliary\Build\vcvarsall.bat %ARCH%
 cd /D C:\httpd-sdk
