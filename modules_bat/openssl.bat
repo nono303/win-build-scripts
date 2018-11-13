b51aeeb93a57f78f412397dc935e044a6f096d5c
nono <nono@nono.com>
2018-11-13 15:50:42 +0100

    correct sed x86 / x64

--- modules_bat/openssl.bat
+++ modules_bat/openssl.bat
@@ -1,7 +1,12 @@
 cd /D C:\httpd-sdk\src\openssl
 REM https://wiki.openssl.org/index.php/Compilation_and_Installation#Configure_Options
 perl Configure %perlbuild% --prefix=C:\httpd-sdk\install --openssldir=C:\httpd-sdk\install\conf --with-zlib-include=C:/httpd-sdk/install/include --with-zlib-lib=C:/httpd-sdk/install/lib/zlib.lib -no-unit-test -no-external-tests -no-ssl3 -no-weak-ssl-ciphers -no-tests -threads -DOPENSSL_USE_IPV6=0 -DOPENSSL_NO_HEARTBEATS -L"/OPT:ICF /LTCG" +"/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %AVX%"
-sed -i 's/\/O2 -threads +/\/wd4267 \/wd4244 /g' /cygdrive/c/src/openssl/makefile
+if %ARCH% == x86 (
+	sed -i 's/\/O2 \/WX \-threads +/\/wd4267 \/wd4244 /g' /cygdrive/c/src/openssl/makefile
+)
+if %ARCH% == x64 (
+	sed -i 's/\/O2 \-threads +/\/wd4267 \/wd4244 /g' /cygdrive/c/src/openssl/makefile
+)
 sed -i 's/\/nologo \/debug -L/\/nologo \/debug /g' /cygdrive/c/src/openssl/makefile
 sed -i 's/ARFLAGS= \/nologo/ARFLAGS= \/nologo \/LTCG/g' /cygdrive/c/src/openssl/makefile
 nmake %NMAKE_OPTS% clean install_sw
