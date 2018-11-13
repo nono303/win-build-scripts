361021f7a998ed43cc7e78442efa77db2fb40149
nono <nono@nono.com>
2018-11-13 15:51:20 +0100

    correct bugs for buildall

--- phpsdk-local.bat
+++ phpsdk-local.bat
@@ -3,16 +3,18 @@ call c:\httpd-sdk\%PHP_SDK_ARCH%.bat
 set path=%path%;%oldpath%
 cd /d C:\php72-sdk\
 
-set BUILDLIB=0
 set LTCG=1
-set BUILDALL=0
-set LOGNAME=C:\php72-sdk\silent-logs\%PHP_SDK_ARCH%_TS-%TS%_LTCG-%LTCG%_%Date:~-4%%Date:~-7,-5%%Date:~-10,-8%-%TIME:~-11,-9%%TIME:~-8,-6%%TIME:~-5,-3%.log
 
-REM ** uniquement sur init
-	REM phpsdk_buildtree phpmaster
+set BUILDALL=1
+set BUILDLIB=0
+set BUILDPROTOBUF=0
+
+REM ** uniquement sur init: phpsdk_buildtree phpmaster
 call phpsdk_deps -u -b 7.2 -a %PHP_SDK_ARCH% -d C:\php72-sdk\phpmaster\vc15\%PHP_SDK_ARCH%\deps -t vc15
 
-REM call %MODULE_BAT_DIR%protobuf-php.bat
+if %BUILDPROTOBUF% == 1 (
+	call %MODULE_BAT_DIR%protobuf-php.bat
+)
 if %BUILDLIB% == 1 (
 	call %MODULE_BAT_DIR%libxpm-php.bat
 	call %MODULE_BAT_DIR%freetype-php.bat
@@ -20,23 +22,29 @@ if %BUILDLIB% == 1 (
 	call %MODULE_BAT_DIR%libpng-php.bat
 )
 set ZTS=--disable-zts
+set TSNTS=nts
 set BUILDDIR=Release
 
 set SED_AVX= \/arch:AVX
-set outdir=-AVX
+set outdir=_avx
+	echo *** avx nts  ***
 	call C:\httpd-sdk\phpsdk-config_make.bat
 if %BUILDALL% == 1 (
 	set SED_AVX=
 	set outdir=
+		echo *** std nts  ***
 		call C:\httpd-sdk\phpsdk-config_make.bat
-	set BUILDDIR=Release_TS
+
 	set ZTS=
+	set TSNTS=ts
+	set BUILDDIR=Release_TS
+
+		echo *** std ts  ***
+		call C:\httpd-sdk\phpsdk-config_make.bat
 
 	set SED_AVX= \/arch:AVX
-	set outdir=-AVX
-		call C:\httpd-sdk\phpsdk-config_make.bat
-	set SED_AVX=
-	set outdir=
+	set outdir=_avx
+		echo *** avx ts  ***
 		call C:\httpd-sdk\phpsdk-config_make.bat
 )
 exit /B
