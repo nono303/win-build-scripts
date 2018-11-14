9a91938d6f7dc74d937fdb29e94b8773fde28c73
nono <nono@nono.com>
2018-11-14 19:50:07 +0100

    echo on one build dir cleaned on common

--- xCommon.bat
+++ xCommon.bat
@@ -20,10 +20,11 @@ set EXTCFLAGS=/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %
 
 set MODULE_BAT_DIR=C:\httpd-sdk\modules_bat\
 
-rmdir /S /Q C:\httpd-sdk\build
+cd /D C:\httpd-sdk\build
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 rmdir /S /Q C:\httpd-sdk\install
-mklink /J C:\httpd-sdk\build C:\httpd-sdk\build_%ARCH%%AVXB%
 mklink /J C:\httpd-sdk\install C:\httpd-sdk\install_%ARCH%%AVXB%
 set PATH=c:\python27;C:\PROGRA~1\Git\bin;C:\bin\nasm;c:\perl\bin;c:\perl\site\bin;c:\bin;C:\Windows\SysWOW64;C:\msvc15\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Windows\SysWOW64\wbem;c:\cyg%CYGV%\bin;C:\php72-sdk\bin\php
 call C:\msvc15\VC\Auxiliary\Build\vcvarsall.bat %ARCH%
+echo on
 cd /D C:\httpd-sdk
