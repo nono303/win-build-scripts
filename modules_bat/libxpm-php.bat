abbe4517a3bed7ce3c393418043ac1c79e43f7de
nono <nono@nono.com>
2018-12-12 11:21:35 +0100

    %PHPDEPS%

--- modules_bat/libxpm-php.bat
+++ modules_bat/libxpm-php.bat
@@ -1,4 +1,6 @@
 "C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" C:\src\libxpm\windows\vc15\libxpm.sln /nowarn:C4018 /nowarn:C4244 /nowarn:C4267 /nowarn:C4311 /nowarn:C4312 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libxpm /p:Configuration="Static Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
-copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.lib" C:\php72-sdk\phpmaster\vc15\%ARCH%\deps\lib\libxpm_a.lib
-copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.pdb" C:\php72-sdk\phpmaster\vc15\%ARCH%\deps\lib\libxpm_a.pdb
+copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.lib" %PHPDEPS%\lib\libxpm_a.lib
+copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.pdb" %PHPDEPS%\lib\libxpm_a.pdb
+mkdir %PHPDEPS%\include\X11
+copy /Y C:\httpd-sdk\src\libxpm\include\X11\xpm.h %PHPDEPS%\include\X11\xpm.h
 cd /D C:\httpd-sdk\modules_bat
