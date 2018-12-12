9aa2ca6a5bf816279d8bfefb8f574c41b64a8644
nono <nono@nono.com>
2018-12-12 11:21:17 +0100

    %PHPDEPS%

--- modules_bat/protobuf-php.bat
+++ modules_bat/protobuf-php.bat
@@ -2,8 +2,8 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\protobuf
 mkdir C:\httpd-sdk\build\protobuf
 cd /D C:\httpd-sdk\build\protobuf
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\php72-sdk\phpmaster\vc15\%ARCH%\deps -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DZLIB_LIBRARY=C:\httpd-sdk\install\lib\zlib.lib -DZLIB_INCLUDE_DIR=C:\httpd-sdk\install\include\ -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF ..\..\src\protobuf\cmake
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PHPDEPS% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DZLIB_LIBRARY=C:\httpd-sdk\install\lib\zlib.lib -DZLIB_INCLUDE_DIR=C:\httpd-sdk\install\include\ -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF ..\..\src\protobuf\cmake
 nmake %NMAKE_OPTS% clean install
-copy C:\src\protobuf\src\google\protobuf\stubs\strutil.h C:\php72-sdk\phpmaster\vc15\%ARCH%\deps\include\google\protobuf\stubs\strutil.h
-copy C:\src\protobuf\src\google\protobuf\parse_context.h C:\php72-sdk\phpmaster\vc15\%ARCH%\deps\include\google\protobuf\parse_context.h
+copy C:\src\protobuf\src\google\protobuf\stubs\strutil.h %PHPDEPS%\include\google\protobuf\stubs\strutil.h
+copy C:\src\protobuf\src\google\protobuf\parse_context.h %PHPDEPS%\include\google\protobuf\parse_context.h
 cd /D C:\httpd-sdk\modules_bat
