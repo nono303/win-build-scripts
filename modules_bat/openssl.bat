6ab0e6ccac4217661541053fe775820a02d8814e
nono <nono@nono.com>
2018-12-13 17:54:17 +0100

    reconfigure correctly for VC-WIN64A (avoid issue https://github.com/nono303/mod_md/issues/3)

--- modules_bat/openssl.bat
+++ modules_bat/openssl.bat
@@ -1,6 +1,5 @@
 cd /D C:\httpd-sdk\src\openssl
-REM https://wiki.openssl.org/index.php/Compilation_and_Installation#Configure_Options
-perl Configure %perlbuild% --prefix=C:\httpd-sdk\install --openssldir=C:\httpd-sdk\install\conf --with-zlib-include=C:/httpd-sdk/install/include --with-zlib-lib=C:/httpd-sdk/install/lib/zlib.lib -no-unit-test -no-external-tests -no-ssl3 -no-weak-ssl-ciphers -no-tests -threads -DOPENSSL_USE_IPV6=0 -DOPENSSL_NO_HEARTBEATS -L"/OPT:ICF /LTCG" +"/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %AVX%"
+perl Configure %perlbuild% --prefix=C:\httpd-sdk\install -threads --openssldir=C:\httpd-sdk\install\conf shared no-unit-test no-external-tests no-ssl3 no-weak-ssl-ciphers no-tests no-asm zlib zlib-dynamic --with-zlib-include=C:/httpd-sdk/install/include --with-zlib-lib=C:/httpd-sdk/install/lib/zlib.lib -DOPENSSL_USE_IPV6=1 -DOPENSSL_NO_HEARTBEATS -DOPENSSL_NO_DEPRECATED -L"/OPT:ICF /LTCG" +"/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %AVX%"
 if %ARCH% == x86 (
 	sed -i 's/\/O2 \/WX \-threads +/\/wd4267 \/wd4244 /g' /cygdrive/c/src/openssl/makefile
 )
@@ -9,11 +8,5 @@ if %ARCH% == x64 (
 )
 sed -i 's/\/nologo \/debug -L/\/nologo \/debug /g' /cygdrive/c/src/openssl/makefile
 sed -i 's/ARFLAGS= \/nologo/ARFLAGS= \/nologo \/LTCG/g' /cygdrive/c/src/openssl/makefile
-nmake %NMAKE_OPTS% clean install_sw
-if %perlbuild% == VC-WIN64A-masm (
-	move /Y C:\httpd-sdk\install\bin\libcrypto-1_1.dll C:\httpd-sdk\install\bin\libcrypto-1_1-x64.dll
-	move /Y C:\httpd-sdk\install\bin\libcrypto-1_1.pdb C:\httpd-sdk\install\bin\libcrypto-1_1-x64.pdb
-	move /Y C:\httpd-sdk\install\bin\libssl-1_1.dll C:\httpd-sdk\install\bin\libssl-1_1-x64.dll
-	move /Y C:\httpd-sdk\install\bin\libssl-1_1.pdb C:\httpd-sdk\install\bin\libssl-1_1-x64.pdb
-)
+nmake %NMAKE_OPTS% clean install
 cd /D C:\httpd-sdk\modules_bat
