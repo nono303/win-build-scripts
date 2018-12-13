295d52daaf7cff4464ca872ff4089fca0447e3a1
nono <nono@nono.com>
2018-12-13 17:52:20 +0100

    copy mod_md

--- httpd-build.bat
+++ httpd-build.bat
@@ -25,4 +25,13 @@ call %MODULE_BAT_DIR%lua.bat
 call %MODULE_BAT_DIR%httpd.bat
 call %MODULE_BAT_DIR%mod_h264_streaming.bat
 
+Copy /Y C:\httpd-sdk\install\modules\mod_md.pdb D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\mod_md.pdb
+Copy /Y C:\httpd-sdk\install\modules\mod_md.so D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\mod_md.so
+Copy /Y C:\httpd-sdk\install\modules\mod_ssl.pdb D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\mod_ssl.pdb
+Copy /Y C:\httpd-sdk\install\modules\mod_ssl.so D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\mod_ssl.so
+Copy /Y C:\httpd-sdk\install\bin\libcurl.dll D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\deps\libcurl.dll
+Copy /Y C:\httpd-sdk\install\bin\libcurl.pdb D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\deps\libcurl.pdb
+Copy /Y C:\httpd-sdk\install\bin\jansson.dll D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\deps\jansson.dll
+Copy /Y C:\httpd-sdk\install\bin\jansson.pdb D:\github\NONO_mod_md\vc15\%ARCH%%AVXB%\deps\jansson.pdb
+
 cd /D C:\httpd-sdk\
