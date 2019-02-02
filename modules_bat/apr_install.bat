760332ac5238ae331601567f239042885e1dec76
nono <nono@nono.com>
2019-02-02 09:11:36 +0100

    cut apr in compile & install for subversion pathed version build

--- modules_bat/apr_install.bat
+++ modules_bat/apr_install.bat
@@ -1,24 +1,3 @@
-rmdir /S /Q C:\src\apr-util\crypto\%ARCH%
-rmdir /S /Q C:\src\apr-util\dbd\%ARCH%
-rmdir /S /Q C:\src\apr-util\dbm\%ARCH%
-rmdir /S /Q C:\src\apr-util\ldap\%ARCH%
-rmdir /S /Q C:\src\apr-util\%ARCH%
-rmdir /S /Q C:\src\apr-iconv\build\preapriconv\
-rmdir /S /Q C:\src\apr-iconv\ccs\libapriconv_ccs_modules\
-rmdir /S /Q C:\src\apr-iconv\%ARCH%
-rmdir /S /Q C:\src\apr\build\%ARCH%
-rmdir /S /Q C:\src\apr\%ARCH%
-rm -f C:\src\apr\build\preaprapp.log
-
-cd /D C:\httpd-sdk\src\apr\tools
-cl gen_test_char.c
-gen_test_char.exe > C:\src\apr\include\apr_escape_test_char.h
-rm -f *.exe *.obj
-
-REM apr_crypto_nss;apr_dbd_mysql;apr_dbd_oracle;apr_dbd_pgsql;apr_dbd_sqlite2;apr_dbd_sqlite3;apr_dbm_db;apr_dbm_gdbm;
-cd /D C:\src\apr-util
-"C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" aprutil.sln /nowarn:msb4011 /nowarn:C4244 /nowarn:C4098 /nowarn:C4267 /nowarn:C4311 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:apr;apr_crypto_openssl;apr_dbd_odbc;apr_ldap;aprapp;apriconv;aprutil;libapr;libaprapp;libapriconv;libapriconv_ccs_modules;libapriconv_ces_modules;libaprutil;preaprapp;preapriconv;preaprutil;prelibaprapp /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%ARCH%"
-
 move /Y C:\src\apr\%ARCH%\Release\libapr-1.dll c:\httpd-sdk\install\bin\libapr-1.dll
 move /Y C:\src\apr\%ARCH%\Release\libapr-1.pdb c:\httpd-sdk\install\bin\libapr-1.pdb
 move /Y C:\src\apr\%ARCH%\LibR\apr-1.lib c:\httpd-sdk\install\lib\apr-1.lib
@@ -142,4 +121,3 @@ copy /Y C:\src\apr-util\include\apu_want.h C:\httpd-sdk\install\include\apu_want
 copy /Y C:\src\apr-util\include\apr_anylock.h C:\httpd-sdk\install\include\apr_anylock.h
 copy /Y C:\src\apr-util\include\apr_base64.h C:\httpd-sdk\install\include\apr_base64.h
 copy /Y C:\src\apr-util\include\apr_buckets.h C:\httpd-sdk\install\include\apr_buckets.h
-cd /D C:\httpd-sdk\modules_bat
