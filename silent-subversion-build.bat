18f4a242e1ca839cff337a416a6a990b194f4936
nono <nono@nono.com>
2019-02-02 10:57:12 +0100

    add avx on log filename

--- silent-subversion-build.bat
+++ silent-subversion-build.bat
@@ -1,3 +1,3 @@
 call ymdhis.bat
-SET LOGNAME=C:\httpd-sdk\_logs\subversion-build_%ARCH%_%ymdhis%.log
+SET LOGNAME=C:\httpd-sdk\_logs\subversion-build_%ARCH%%AVXB%_%ymdhis%.log
 call subversion-build.bat 2>&1 | tee %LOGNAME%
