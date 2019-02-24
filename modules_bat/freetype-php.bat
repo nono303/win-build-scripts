edb540aab5843fb52523f93fd5d1cac23e04fd3a
nono <nono@nono.com>
2019-02-24 08:11:13 +0100

    add dll build

--- modules_bat/freetype-php.bat
+++ modules_bat/freetype-php.bat
@@ -1,4 +1,5 @@
 "C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" C:\src\freetype\builds\windows\vc2017\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
+REM "C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" C:\src\freetype\builds\windows\vc2017\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration="DLL Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 copy /Y C:\src\freetype\objs\vc2017\%archmsbuild%\freetype_a.lib %PHPDEPS%\lib\freetype_a.lib
 copy /Y C:\src\freetype\objs\vc2017\%archmsbuild%\freetype_a.pdb %PHPDEPS%\lib\freetype_a.pdb
 mkdir %PHPDEPS%\include\freetype2
--- modules_bat/libxpm-php.bat
+++ modules_bat/libxpm-php.bat
@@ -1,6 +1,10 @@
 "C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" C:\src\libxpm\windows\vc15\libxpm.sln /nowarn:C4018 /nowarn:C4244 /nowarn:C4267 /nowarn:C4311 /nowarn:C4312 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libxpm /p:Configuration="Static Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
+"C:\msvc15\MSBuild\15.0\Bin\MSBuild.exe" C:\src\libxpm\windows\vc15\libxpm.sln /nowarn:C4018 /nowarn:C4244 /nowarn:C4267 /nowarn:C4311 /nowarn:C4312 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libxpm /p:Configuration="DLL Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.lib" %PHPDEPS%\lib\libxpm_a.lib
 copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.pdb" %PHPDEPS%\lib\libxpm_a.pdb
+copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\DLL Release\libxpm_a.dll" %PHPDEPS%\bin\libxpm.dll
+copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\DLL Release\libxpm_a.pdb" %PHPDEPS%\bin\libxpm.pdb
+
 mkdir %PHPDEPS%\include\X11
 copy /Y C:\httpd-sdk\src\libxpm\include\X11\xpm.h %PHPDEPS%\include\X11\xpm.h
 cd /D C:\httpd-sdk\modules_bat
