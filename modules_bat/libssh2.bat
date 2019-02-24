1b3d097990796492ec819ad26b91a33d19dd4890
nono <nono@nono.com>
2019-02-24 08:09:52 +0100

    don't copy apr-util patched openssl

--- modules_bat/libssh2.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-rmdir /S /Q C:\httpd-sdk\build\libssh2
-mkdir C:\httpd-sdk\build\libssh2
-cd /D C:\httpd-sdk\build\libssh2
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DCRYPTO_BACKEND=OpenSSL -DENABLE_ZLIB_COMPRESSION=ON ..\..\src\libssh2
-nmake %NMAKE_OPTS% clean install
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DCRYPTO_BACKEND=OpenSSL -DENABLE_ZLIB_COMPRESSION=ON ..\..\src\libssh2
-nmake %NMAKE_OPTS% clean install
-cd /D C:\httpd-sdk\modules_bat
