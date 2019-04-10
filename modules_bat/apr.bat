8ff960872191be83e830d9e94cf99c36e474cd89
nono <nono@nono.com>
2019-04-10 18:42:12 +0200

    apr 1.7.0

--- modules_bat/apr.bat
+++ modules_bat/apr.bat
@@ -1,23 +1,18 @@
-rmdir /S /Q C:\src\apr-util\crypto\%ARCH%
-rmdir /S /Q C:\src\apr-util\dbd\%ARCH%
-rmdir /S /Q C:\src\apr-util\dbm\%ARCH%
-rmdir /S /Q C:\src\apr-util\ldap\%ARCH%
-rmdir /S /Q C:\src\apr-util\%ARCH%
-rmdir /S /Q C:\src\apr-iconv\build\preapriconv\
-rmdir /S /Q C:\src\apr-iconv\ccs\libapriconv_ccs_modules\
-rmdir /S /Q C:\src\apr-iconv\%ARCH%
-rmdir /S /Q C:\src\apr\build\%ARCH%
-rmdir /S /Q C:\src\apr\%ARCH%
-rm -f C:\src\apr\build\preaprapp.log
+cd /D C:\src\apr\
+git clean -f -d
+cd /D C:\src\apr-util\
+git clean -f -d
+cd /D C:\src\apr-iconv\
+git clean -f -d
 
-cd /D C:\httpd-sdk\src\apr\tools
+cd /D C:\src\apr\tools
 cl gen_test_char.c
 gen_test_char.exe > C:\src\apr\include\apr_escape_test_char.h
-rm -f *.exe *.obj
+REM rm -f *.exe *.obj
 
 REM apr_crypto_nss;apr_dbd_mysql;apr_dbd_oracle;apr_dbd_pgsql;apr_dbd_sqlite2;apr_dbd_sqlite3;apr_dbm_db;apr_dbm_gdbm;
 cd /D C:\src\apr-util
-"C:\msvc%MSVC_VER%\MSBuild\15.0\Bin\MSBuild.exe" aprutil.sln /nowarn:msb4011 /nowarn:C4244 /nowarn:C4098 /nowarn:C4267 /nowarn:C4311 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:apr;apr_crypto_openssl;apr_dbd_odbc;apr_ldap;aprapp;apriconv;aprutil;libapr;libaprapp;libapriconv;libapriconv_ccs_modules;libapriconv_ces_modules;libaprutil;preaprapp;preapriconv;preaprutil;prelibaprapp /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%ARCH%"
+"C:\msvc%MSVC_VER%\MSBuild\15.0\Bin\MSBuild.exe" aprutil.sln /nowarn:msb4011 /nowarn:C4244 /nowarn:C4098 /nowarn:C4267 /nowarn:C4311 /nowarn:C4018 /nowarn:C4133 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:apr;apr_crypto_openssl;apr_dbd_odbc;apr_ldap;aprapp;apriconv;aprutil;libapr;libaprapp;libapriconv;libapriconv_ccs_modules;libapriconv_ces_modules;libaprutil;preaprapp;preapriconv;preaprutil;prelibaprapp /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%ARCH%"
 
 cd /D C:\httpd-sdk\modules_bat
 
