13d88eaec3497a0b8484c7111b58920b0d01f811
nono <nono@nono.com>
2019-04-11 17:08:24 +0200

    msbuild in path vc2017 => vc2019

--- modules_bat/freetype-php.bat
+++ modules_bat/freetype-php.bat
@@ -1,7 +1,9 @@
-"C:\msvc%MSVC_VER%\MSBuild\15.0\Bin\MSBuild.exe" C:\src\freetype\builds\windows\vc2017\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
-REM "C:\msvc%MSVC_VER%\MSBuild\15.0\Bin\MSBuild.exe" C:\src\freetype\builds\windows\vc2017\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration="DLL Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
-copy /Y C:\src\freetype\objs\vc2017\%archmsbuild%\freetype_a.lib %PHPDEPS%\lib\freetype_a.lib
-copy /Y C:\src\freetype\objs\vc2017\%archmsbuild%\freetype_a.pdb %PHPDEPS%\lib\freetype_a.pdb
-mkdir %PHPDEPS%\include\freetype2
-copy /Y C:\httpd-sdk\src\freetype\include\ft2build.h %PHPDEPS%\include\freetype2\ft2build.h
+MSBuild.exe C:\src\freetype\builds\windows\vc2019\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
+REM MSBuild.exe C:\src\freetype\builds\windows\vc2019\freetype.sln /nowarn:C4267 /nowarn:C4244 /nowarn:C4702 /nowarn:C4312 /nowarn:C4701 /nowarn:LNK4006 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,freetype /p:Configuration="DLL Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
+copy /Y C:\src\freetype\objs\vc2019\%archmsbuild%\freetype_a.lib %PHPDEPS%\lib\freetype_a.lib
+copy /Y C:\src\freetype\objs\vc2019\%archmsbuild%\freetype_a.pdb %PHPDEPS%\lib\freetype_a.pdb
+copy /Y C:\httpd-sdk\src\freetype\include\ft2build.h %PHPDEPS%\include\ft2build.h
+mkdir %PHPDEPS%\include\freetype\config
+copy /Y C:\httpd-sdk\src\freetype\include\freetype\config\*.h %PHPDEPS%\include\freetype\config\
+copy /Y C:\httpd-sdk\src\freetype\include\freetype\*.h %PHPDEPS%\include\freetype\
 cd /D C:\httpd-sdk\modules_bat
