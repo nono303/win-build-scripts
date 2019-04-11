fb4ba212a1328595891e51ddc492b8a40fa49330
nono <nono@nono.com>
2019-04-11 17:10:05 +0200

    msbuild in path

--- modules_bat/libmaxminddb.bat
+++ modules_bat/libmaxminddb.bat
@@ -1,6 +1,6 @@
 rmdir /S /Q C:\httpd-sdk\src\libmaxminddb\projects\VS12\%ARCH%
 cd /D C:\httpd-sdk\src\libmaxminddb\projects\VS12
-"C:\msvc%MSVC_VER%\MSBuild\15.0\Bin\MSBuild.exe" libmaxminddb.sln /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libmaxminddb /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
+MSBuild.exe libmaxminddb.sln /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libmaxminddb /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 copy /Y C:\httpd-sdk\src\libmaxminddb\projects\VS12\%outmsbuild%\libmaxminddb.lib C:\httpd-sdk\install\lib\libmaxminddb.lib
 copy /Y C:\httpd-sdk\src\libmaxminddb\include\maxminddb.h C:\httpd-sdk\install\include\maxminddb.h 
 copy /Y C:\httpd-sdk\src\libmaxminddb\include\maxminddb_config.h C:\httpd-sdk\install\include\maxminddb_config.h
