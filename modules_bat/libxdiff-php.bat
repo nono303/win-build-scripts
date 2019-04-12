0d25025589019923034bbee6283a0c362f42d37d
nono <nono@nono.com>
2019-04-12 13:00:58 +0200

    disable deps copy github

--- modules_bat/libxdiff-php.bat
+++ modules_bat/libxdiff-php.bat
@@ -4,5 +4,4 @@ mkdir C:\httpd-sdk\build\libxdiff
 cd /D C:\httpd-sdk\build\libxdiff
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PHPDEPS% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DENABLE_TESTS=OFF -DENABLE_TOOLS=OFF ..\..\src\libxdiff
 nmake %NMAKE_OPTS% clean install
-copy /Y %PHPDEPS%\bin\xdiff.dll D:\github\NONO_phpwin-perfbuild\deps-%ARCH%\xdiff.dll
 cd /D C:\httpd-sdk\modules_bat
