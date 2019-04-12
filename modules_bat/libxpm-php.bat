c5203ff82b68cca837fdf0d411c7ed6926a8dcee
nono <nono@nono.com>
2019-04-12 13:01:26 +0200

    mklink X11\xpm.h

--- modules_bat/libxpm-php.bat
+++ modules_bat/libxpm-php.bat
@@ -1,3 +1,4 @@
+
 MSBuild.exe C:\src\libxpm\windows\vc%MSVC_VER%\libxpm.sln /nowarn:C4018 /nowarn:C4244 /nowarn:C4267 /nowarn:C4311 /nowarn:C4312 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libxpm /p:Configuration="Static Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 MSBuild.exe C:\src\libxpm\windows\vc%MSVC_VER%\libxpm.sln /nowarn:C4018 /nowarn:C4244 /nowarn:C4267 /nowarn:C4311 /nowarn:C4312 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libxpm /p:Configuration="DLL Release" /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\Static Release\libxpm_a.lib" %PHPDEPS%\lib\libxpm_a.lib
@@ -6,4 +7,6 @@ copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\DLL Release\libxpm_a.dll" %P
 copy /Y "C:\src\libxpm\windows\builds\%archmsbuild%\DLL Release\libxpm_a.pdb" %PHPDEPS%\bin\libxpm.pdb
 
 copy /Y C:\httpd-sdk\src\libxpm\include\X11\xpm.h %PHPDEPS%\include\xpm.h
+mkdir %PHPDEPS%\include\X11
+mklink /h %PHPDEPS%\include\X11\xpm.h %PHPDEPS%\include\xpm.h
 cd /D C:\httpd-sdk\modules_bat
