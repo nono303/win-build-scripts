4edb447ccb8e825eb8055d0f5d5c16df28097763
nono <nono@nono.com>
2019-04-15 15:50:08 +0200

    add compiler option & AVX contextual brotli

--- modules_bat/brotli.bat
+++ modules_bat/brotli.bat
@@ -3,6 +3,7 @@ rmdir /S /Q C:\httpd-sdk\build\brotli
 mkdir C:\httpd-sdk\build\brotli
 cd /D C:\httpd-sdk\build\brotli
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON ..\..\src\brotli
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/brotli.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 copy /Y C:\httpd-sdk\build\brotli\brotlicommon.pdb c:\httpd-sdk\install\bin\brotlicommon.pdb
 copy /Y C:\httpd-sdk\build\brotli\brotli.pdb c:\httpd-sdk\install\bin\brotli.pdb 
