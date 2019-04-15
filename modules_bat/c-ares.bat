7fd80581b8c6e21d956259731d10963b8f93ac90
nono <nono@nono.com>
2019-04-15 17:26:05 +0200

    c-ares : flags optim + avx contextuel

--- modules_bat/c-ares.bat
+++ modules_bat/c-ares.bat
@@ -4,6 +4,7 @@ rmdir /S /Q C:\httpd-sdk\build\c-ares
 mkdir C:\httpd-sdk\build\c-ares
 cd /D C:\httpd-sdk\build\c-ares
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DCARES_STATIC=ON -DCARES_SHARED=ON -DCARES_INSTALL=ON -DCARES_STATIC_PIC=OFF -DCARES_BUILD_TESTS=OFF -DCARES_MSVC_STATIC_RUNTIME=OFF ..\..\src\c-ares 
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/c-ares.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 copy /Y C:\httpd-sdk\build\c-ares\bin\cares.pdb C:\httpd-sdk\install\bin\cares.pdb
 copy /Y C:\httpd-sdk\build\c-ares\bin\acountry.pdb C:\httpd-sdk\install\bin\acountry.pdb
