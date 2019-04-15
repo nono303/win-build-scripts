0ec064685d5700a65abe0c409d8670951f8c1b35
nono <nono@nono.com>
2019-04-15 16:26:30 +0200

    improvbe flags & contextual AVX

--- modules_bat/libexpat.bat
+++ modules_bat/libexpat.bat
@@ -2,7 +2,8 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\libexpat
 mkdir C:\httpd-sdk\build\libexpat
 cd /D C:\httpd-sdk\build\libexpat
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_doc=OFF -DBUILD_tests=OFF -DBUILD_examples=OFF -DBUILD_tools=OFF -DEXTRA_COMPILE_FLAGS="/Zi" ..\..\src\libexpat\expat
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_doc=OFF -DBUILD_tests=OFF -DBUILD_examples=OFF -DBUILD_tools=OFF ..\..\src\libexpat\expat
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/libexpat.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 copy /Y C:\httpd-sdk\build\libexpat\expat.pdb C:\httpd-sdk\install\bin\expat.pdb
 cd /D C:\httpd-sdk\modules_bat
