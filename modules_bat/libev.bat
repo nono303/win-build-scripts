f1e537733688d46c03052d50a384f981a90555d3
nono <nono@nono.com>
2019-04-15 22:02:21 +0200

    libev : flag optim + AVX contextuel

--- modules_bat/libev.bat
+++ modules_bat/libev.bat
@@ -2,7 +2,8 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\libev-win
 mkdir C:\httpd-sdk\build\libev-win
 cd /D C:\httpd-sdk\build\libev-win
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ..\..\src\libev-win 
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ..\..\src\libev-win
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/libev.sh "%AVXSED%"
 nmake %NMAKE_OPTS%
 copy /Y C:\httpd-sdk\build\libev-win\libev_static.lib C:\httpd-sdk\install\lib\libev_static.lib
 copy /Y C:\httpd-sdk\build\libev-win\libev.dll C:\httpd-sdk\install\bin\libev.dll
