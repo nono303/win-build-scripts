f18879759333f52cbc828e8df7e859fbb52983d3
nono <nono@nono.com>
2019-04-15 16:27:12 +0200

    serf : improve flags and add contextual AVX

--- modules_bat/serf-svn.bat
+++ modules_bat/serf-svn.bat
@@ -2,7 +2,11 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\serf
 mkdir C:\httpd-sdk\build\serf
 cd /D C:\httpd-sdk\build\serf
-REM non implemente -DGSSAPI=C:\httpd-sdk\install\ -DBROTLI=C:\httpd-sdk\install\
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DDEBUG=OFF -DSKIP_SHARED=OFF -DSKIP_STATIC=OFF -DLIBDIR=C:\httpd-sdk\install\lib\ -DDISABLE_LOGGING=OFF -DSKIP_TESTS=ON -DENABLE_SLOW_TESTS=OFF -DAPR_STATIC=OFF -DEXPAT=C:\httpd-sdk\install\ -DAPR_ROOT=C:\httpd-sdk\install\ -DAPRUtil_ROOT=C:\httpd-sdk\install\ ..\..\src\serf 
+if %ARCH% == x64 (
+	set SERF64=-DSERF_WIN64=ON 
+)
+REM non implemente -DGSSAPI=C:\httpd-sdk\install\ -DBROTLI=C:\httpd-sdk\install\ -DLIBDIR=C:\httpd-sdk\install\lib\ 
+cmake -Wno-dev -G "NMake Makefiles" %SERF64%-DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DDEBUG=OFF -DSKIP_SHARED=OFF -DSKIP_STATIC=OFF -DDISABLE_LOGGING=OFF -DSKIP_TESTS=ON -DENABLE_SLOW_TESTS=OFF -DAPR_STATIC=OFF -DEXPAT=C:\httpd-sdk\install\ -DAPR_ROOT=C:\httpd-sdk\install\ -DAPRUtil_ROOT=C:\httpd-sdk\install\ ..\..\src\serf 
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/serf-svn.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 cd /D C:\httpd-sdk\modules_bat
---
+++ modules_bat/serf-svn.sh
@@ -0,0 +1,3 @@
+/usr/bin/find /cygdrive/c/httpd-sdk/build/serf -type f -name "build.make" -exec sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' {} \;
+/usr/bin/find /cygdrive/c/httpd-sdk/build/serf -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox \/wd4267 \/wd4702 \/wd4244 \/wd4018 \/wd4090 \/wd4189 '$1' /g' {} \;
+/usr/bin/find /cygdrive/c/httpd-sdk/build/serf -type f -name "flags.make" -exec sed -i 's/\/W3//g' {} \;
