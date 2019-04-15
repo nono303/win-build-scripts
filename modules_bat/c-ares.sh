032b3f81fd111f735e9620578ae495de13dea7f3
nono <nono@nono.com>
2019-04-15 17:40:57 +0200

    suppress warning

--- modules_bat/c-ares.sh
+++ modules_bat/c-ares.sh
@@ -1,2 +1,2 @@
 /usr/bin/find /cygdrive/c/httpd-sdk/build/c-ares -type f -name "build.make" -exec sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' {} \;
-/usr/bin/find /cygdrive/c/httpd-sdk/build/c-ares -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox '$1' /g' {} \;
+/usr/bin/find /cygdrive/c/httpd-sdk/build/c-ares -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox \/wd4996 \/wd4267 '$1' /g' {} \;
