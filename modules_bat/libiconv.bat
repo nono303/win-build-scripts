d90865849a02d9a98a27bc67386da820071012ce
nono <nono@nono.com>
2019-04-15 22:20:58 +0200

    libiconv & libxml2 : flag optimisation & AVX contextuel

--- modules_bat/libiconv.bat
+++ modules_bat/libiconv.bat
@@ -1,3 +1,7 @@
+cd /D C:\httpd-sdk\src\libiconv
+git clean -d -f
+git reset --hard
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/vcxproj.sh "/cygdrive/c/httpd-sdk/src/libiconv/MSVC16/" %AVXVCX%
 MSBuild.exe C:\src\libiconv\MSvc%MSVC_VER%\libiconv.sln /nowarn:C4090 /nowarn:C4244 /nowarn:C4267 /nowarn:c4018 /nowarn:MSB8012 /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libiconv_dll /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
 copy /Y C:\src\libiconv\MSvc%MSVC_VER%\%archmsbuild%\bin\libiconv.dll C:\httpd-sdk\install\bin\libiconv.dll 
 copy /Y C:\src\libiconv\MSvc%MSVC_VER%\%archmsbuild%\bin\libiconv.pdb C:\httpd-sdk\install\bin\libiconv.pdb 
--- modules_bat/libxml2.bat
+++ modules_bat/libxml2.bat
@@ -1,5 +1,10 @@
+cd /D C:\httpd-sdk\src\libxml2\
+git clean -d -f
+git reset --hard
 cd /D C:\httpd-sdk\src\libxml2\win32\
 cscript configure.js compiler=nmakemsvc prefix=C:\httpd-sdk\install include=C:\httpd-sdk\install\include lib=C:\httpd-sdk\install\lib debug=no zlib=yes
+sed -i 's/\/W3/\/W3 \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox \/wd4311 \/wd4090 \/wd4996 \/wd4789 %AVXSED%/g' /cygdrive/c/httpd-sdk/src/libxml2/win32/Makefile.msvc
+sed -i 's/\/debug \/nologo/\/debug \/nologo \/LTCG \/OPT:ICF/g' /cygdrive/c/httpd-sdk/src/libxml2/win32/Makefile.msvc
 nmake %NMAKE_OPTS% /f Makefile.msvc clean install-libs
 copy /Y C:\httpd-sdk\src\libxml2\win32\bin.msvc\libxml2.pdb C:\httpd-sdk\install\bin\libxml2.pdb
 nmake %NMAKE_OPTS% /f Makefile.msvc distclean
