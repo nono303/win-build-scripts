7b481cb63c246f0966f1a75a13810929b7610604
nono <nono@nono.com>
2019-04-16 15:13:12 +0200

    lua : flags optimize + AVX contextuel

--- modules_bat/lua.bat
+++ modules_bat/lua.bat
@@ -3,6 +3,8 @@ rmdir /S /Q C:\httpd-sdk\build\lua
 mkdir C:\httpd-sdk\build\lua
 cd /D C:\httpd-sdk\build\lua
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DLUA_BUILD_WLUA=OFF -DLUA_BUILD_AS_DLL=ON ..\..\src\lua
+pause
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/lua.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 copy /Y C:\httpd-sdk\build\lua\lua.pdb C:\httpd-sdk\install\bin\lua.pdb
 cd /D C:\httpd-sdk\modules_bat
