b80abedab0195befe91415053b1fd04d1a21464a
nono <nono@nono.com>
2019-06-19 11:21:41 +0200

    rename for cmd line launch accordinf to ARCH & MSVC_NAME

--- silent-php-build.bat
+++
@@ -1,4 +0,0 @@
-call C:\httpd-sdk\ymdhis.bat
-SET LOGNAME=C:\httpd-sdk\_logs\php-build_%1_%ymdhis%.log
-call C:\httpd-sdk\MSVC_DEPS.bat
-call C:\php72-sdk\phpsdk-%MSVC_DEPS%-%1.bat 2>&1 | tee %LOGNAME%
