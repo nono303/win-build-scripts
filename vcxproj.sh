d7c678068f224abeb265d0248bfcc8c8b4e4f5f0
nono <nono@nono.com>
2019-06-21 21:34:55 +0200

    add LTCG sur Lib

--- vcxproj.sh
+++ vcxproj.sh
@@ -1,2 +1,3 @@
 /usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/ClCompile>/<WholeProgramOptimization>true<\/WholeProgramOptimization><MultiProcessorCompilation>true<\/MultiProcessorCompilation>'$2'<\/ClCompile>/g' {} \;
 /usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/Link>/<LinkTimeCodeGeneration>UseLinkTimeCodeGeneration<\/LinkTimeCodeGeneration><\/Link>/g' {} \;
+/usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/Lib>/<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration><\/Lib>/g' {} \;
