6ad442e4c935b0390494c2748207a6e4621579c6
nono <nono@nono.com>
2019-06-22 17:26:20 +0200

    phpsdk_deps => php-getdeps.php 1/2

--- phpsdk-local.bat
+++ phpsdk-local.bat
@@ -14,15 +14,16 @@ SET AVXMSC=
 SET AVXVCX=
 
 	REM -1 : only deps
-set BUILDALL=0
+set BUILDALL=1
 set BUILDLIB=1
 set BUILDREQ=1
+set UPDATEDEPS=0
 
 	REM ** uniquement sur init: phpsdk_buildtree phpmaster
 
-	REM Gestion des deps auto : KO sur vs16 
-		REM call phpsdk_deps -u -b 7.3 -a x64 -d C:\php72-sdk\phpmaster\%MSVC_DEPS%\x64\deps -t %MSVC_DEPS% -s staging
-		REM Fatal error: Uncaught SDK\Exception: The passed CRT 'vs16' doesn't match any availbale for branch '7.3' in C:\php72-sdk\lib\php\libsdk\SDK\Config.php:287
+REM Gestion des deps auto : KO sur vs16 
+	REM call phpsdk_deps -u -b 7.3 -a x64 -d C:\php72-sdk\phpmaster\%MSVC_DEPS%\x64\deps -t %MSVC_DEPS% -s staging
+	REM Fatal error: Uncaught SDK\Exception: The passed CRT 'vs16' doesn't match any availbale for branch '7.3' in C:\php72-sdk\lib\php\libsdk\SDK\Config.php:287
 REM	set SDKVER=%PHPVER%
 REM	if %SDKVER% == 7.1 (
 REM		set SDKVER=7.2
@@ -32,6 +33,15 @@ REM		call phpsdk_deps -u -b %SDKVER% -a %PHP_SDK_ARCH% -d C:\php72-sdk\phpmaster
 REM		set BUILDLIB=0
 REM		set BUILDREQ=0
 REM	)
+
+if %UPDATEDEPS% == 1 (
+	rmdir /S /Q C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\deps
+	mkdir C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\deps
+	call C:\php72-sdk\bin\php\do_php c:\httpd-sdk\php-getdeps.php
+	cd /D C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\deps
+	call C:\php72-sdk\bin\7za.exe x -y *
+	rm -f *.zip
+)
 
 if %BUILDLIB% == 1 (
 	echo ON
@@ -49,8 +59,11 @@ if %BUILDREQ% == 1 (
 	call %MODULE_BAT_DIR%libxdiff-php.bat
 	call %MODULE_BAT_DIR%libzmq-php.bat
 )
+
+REM ## TODO completer
 copy /Y %PHPDEPS%\bin\*.* D:\github\NONO_phpwin-perfbuild\\%MSVC_DEPS%-%ARCH%_deps\
 copy /Y C:\httpd-sdk\install_%ARCH%\bin\brotli*.* D:\github\NONO_phpwin-perfbuild\\%MSVC_DEPS%-%ARCH%_deps\
+
 if NOT %BUILDALL% == -1 (
 	set ZTS=--disable-zts
 	set TSNTS=nts
