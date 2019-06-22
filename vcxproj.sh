b73e2d46d1a66bcc8dd785e167712e75abea754e
nono <nono@nono.com>
2019-06-22 10:37:59 +0200

    ltcg for Lib

--- vcxproj.sh
+++ vcxproj.sh
@@ -1,3 +1,4 @@
 /usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/ClCompile>/<WholeProgramOptimization>true<\/WholeProgramOptimization><MultiProcessorCompilation>true<\/MultiProcessorCompilation>'$2'<\/ClCompile>/g' {} \;
 /usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/Link>/<LinkTimeCodeGeneration>UseLinkTimeCodeGeneration<\/LinkTimeCodeGeneration><\/Link>/g' {} \;
 /usr/bin/find $1 -type f -name "*.vcxproj" -exec sed -i 's/<\/Lib>/<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration><\/Lib>/g' {} \;
+/usr/bin/find $1 -type f -name "*.props" -exec sed -i 's/<LinkTimeCodeGeneration>false<\/LinkTimeCodeGeneration>/<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration>/g' {} \;
