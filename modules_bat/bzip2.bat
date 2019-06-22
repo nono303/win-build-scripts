cedd4c570f8ac70d5d83902cb00ba5ae63a28f96
nono <nono@nono.com>
2019-06-22 16:13:05 +0200

    mklink for pcre & php (different name...)

--- modules_bat/bzip2.bat
+++ modules_bat/bzip2.bat
@@ -11,7 +11,10 @@ move /Y C:\httpd-sdk\src\bzip2-1.0.6\libbz2.pdb C:\httpd-sdk\install\lib\libbz2.
 move /Y C:\httpd-sdk\src\bzip2-1.0.6\bzip2recover.pdb c:\httpd-sdk\install\bin\bzip2recover.pdb
 move /Y C:\httpd-sdk\src\bzip2-1.0.6\bzip2.pdb c:\httpd-sdk\install\bin\bzip2.pdb
 copy /Y c:\httpd-sdk\src\bzip2-1.0.6\bzlib.h c:\httpd-sdk\install\include\bzlib.h
-REM libbz2 => bzip2
+	REM libbz2 => bzip2  [pcre]
 mklink /h c:\httpd-sdk\install\lib\bzip2.lib C:\httpd-sdk\install\lib\libbz2.lib
 mklink /h c:\httpd-sdk\install\lib\bzip2.pdb C:\httpd-sdk\install\lib\libbz2.pdb
+	REM libbz2 => bzip2 [PHP]
+mklink /h c:\httpd-sdk\install\lib\libbz2_a.lib C:\httpd-sdk\install\lib\libbz2.lib
+mklink /h c:\httpd-sdk\install\lib\libbz2_a.pdb C:\httpd-sdk\install\lib\libbz2.pdb
 cd /D C:\httpd-sdk\modules_bat
