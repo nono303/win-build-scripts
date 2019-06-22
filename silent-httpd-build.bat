685c825470e71da2206da77de0628ecb81445965
nono <nono@nono.com>
2019-06-22 10:38:29 +0200

    add msvc version to log

--- silent-httpd-build.bat
+++ silent-httpd-build.bat
@@ -1,3 +1,3 @@
 call ymdhis.bat
-SET LOGNAME=C:\httpd-sdk\_logs\httpd-build_%ARCH%%AVXB%_%ymdhis%.log
+SET LOGNAME=C:\httpd-sdk\_logs\httpd-build_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.log
 call httpd-build.bat 2>&1 | tee %LOGNAME%
--- silent-php-build.bat
+++ silent-php-build.bat
@@ -1,3 +1,3 @@
 call C:\httpd-sdk\ymdhis.bat
-SET LOGNAME=C:\httpd-sdk\_logs\php-build_%1_%ymdhis%.log
+SET LOGNAME=C:\httpd-sdk\_logs\php-build_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.log
 call C:\php72-sdk\phpsdk-%MSVC_DEPS%-%ARCH%.bat 2>&1 | tee %LOGNAME%
