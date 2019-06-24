27e835b136682435e604d60cdb09a9a057215383
nono <nono@nono.com>
2019-06-24 18:51:55 +0200

    pdb & ltcg for static

--- modules_bat/serf-svn.bat
+++ modules_bat/serf-svn.bat
@@ -9,4 +9,5 @@ REM non implemente -DGSSAPI=C:\httpd-sdk\install\ -DBROTLI=C:\httpd-sdk\install\
 cmake -Wno-dev -G "NMake Makefiles" %SERF64%-DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DDEBUG=OFF -DSKIP_SHARED=OFF -DSKIP_STATIC=OFF -DDISABLE_LOGGING=OFF -DSKIP_TESTS=ON -DENABLE_SLOW_TESTS=OFF -DAPR_STATIC=OFF -DEXPAT=C:\httpd-sdk\install\ -DAPR_ROOT=C:\httpd-sdk\install\ -DAPRUtil_ROOT=C:\httpd-sdk\install\ ..\..\src\serf 
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/serf-svn.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
+copy /Y C:\httpd-sdk\build\serf\CMakeFiles\serf_static.dir\serf_static.pdb C:\httpd-sdk\install\lib\serf_static.pdb
 cd /D C:\httpd-sdk\modules_bat
