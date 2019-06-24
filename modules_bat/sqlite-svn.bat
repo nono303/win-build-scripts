8051c38163f7aa10dae08f6d8859a97e1ad64b37
nono <nono@nono.com>
2019-06-24 18:52:22 +0200

    flags optim

--- modules_bat/sqlite-svn.bat
+++ modules_bat/sqlite-svn.bat
@@ -3,5 +3,6 @@ rmdir /S /Q C:\httpd-sdk\build\sqlite-amalgamation
 mkdir C:\httpd-sdk\build\sqlite-amalgamation
 cd /D C:\httpd-sdk\build\sqlite-amalgamation
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_ENABLE_FTS3=ON -DBUILD_ENABLE_DBSTAT_VTAB=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_SHELL=ON -DBUILD_ENABLE_RBU=ON -DBUILD_ENABLE_RTREE=ON -DBUILD_ENABLE_JSON1=ON -DBUILD_ENABLE_ICU=OFF -DBUILD_ENABLE_FTS5=ON ..\..\src\sqlite-amalgamation 
+C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/sqlite-svn.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 cd /D C:\httpd-sdk\modules_bat
