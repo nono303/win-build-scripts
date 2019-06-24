5c8397bcab765671bafa4b767df03ab9eaa31722
nono <nono@nono.com>
2019-06-24 18:51:20 +0200

    vc15 patch zlib pdb name

--- modules_bat/subversion-svn.bat
+++ modules_bat/subversion-svn.bat
@@ -1,13 +1,14 @@
 mklink /H C:\httpd-sdk\install\lib\xml.lib C:\httpd-sdk\install\lib\expat.lib
-mklink /H C:\httpd-sdk\install\lib\zlib.pdb C:\httpd-sdk\install\lib\zlibstatic.pdb
+mklink /H C:\httpd-sdk\install\lib\zlib_a.pdb C:\httpd-sdk\install\lib\zlibstatic.pdb
 cd /D C:\src\subversion
 git clean -fdx
 mkdir C:\httpd-sdk\src\subversion\Release-%ARCH%
 mklink /J C:\httpd-sdk\src\subversion\Release C:\httpd-sdk\src\subversion\Release-%ARCH%
 rmdir /S /Q C:\httpd-sdk\src\subversion\build\win32\vcnet-vcproj
 python gen-make.py --release -t vcproj --with-jdk=C:\jdk8\%ARCH% --with-serf=C:\httpd-sdk\install\include --with-sqlite=C:\httpd-sdk\src\sqlite-amalgamation --vsnet-version=2017 --with-openssl=C:\httpd-sdk\install --with-apr-util=C:\httpd-sdk\install --with-apr=C:\httpd-sdk\install --with-apr-iconv=C:\httpd-sdk\install --with-zlib=C:\httpd-sdk\install --with-apr_memcache=C:\httpd-sdk\install --with-httpd=C:\httpd-sdk\install 
-REM ;__ALL_TESTS__ 
+	REM ;__ALL_TESTS__ 
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/subversion-svn.sh
+if %MSVC_DEPS% == vc15 (C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/vcxproj2vc15.sh "subversion/build/win32/vcnet-vcproj")
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/vcxproj.sh "/cygdrive/c/httpd-sdk/src/subversion/build/win32/vcnet-vcproj/" %AVXVCX%
 MSBuild.exe subversion_vcnet.sln /nowarn:C4702 /nowarn:LNK4087 /nowarn:C4703 /nowarn:C4132 /nowarn:C4389 /nowarn:C4244 /nowarn:C4245 /nowarn:C4267 /nowarn:C4018 /nowarn:C4334 /nowarn:C4189 /nowarn:C4312 /nowarn:C4090 /nowarn:C4152 /nowarn:C4146 /nologo /m:8 /t:Clean;__ALL__;__JAVAHL__ /p:Configuration=Release /p:Platform=%archmsbuild%  /p:DebugSymbols=true /p:DebugType=None
 
