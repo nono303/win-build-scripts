afb67cc2ef9d332bf3e7d9584c564d3a7d68fa38
nono <nono@nono.com>
2019-06-24 17:22:04 +0200

    clean curl build

--- modules_bat/curl.bat
+++ modules_bat/curl.bat
@@ -1,8 +1,5 @@
 rmdir /S /Q C:\src\curl\builds
 cd /D C:\httpd-sdk\src\curl\winbuild
-	REM WITH_SSH2=%DLLSTATIC% ko / 
-SET rd=-release-dll-cares-dll-zlib-dll-ipv6-sspi-winssl-nghttp2-dll
-SET rs=-release-static-cares-static-zlib-static-ipv6-sspi-winssl-nghttp2-static
 git reset --hard
 git clean -fdx
 move /Y C:\httpd-sdk\src\curl\src\tool_hugehelp.c.cvs C:\httpd-sdk\src\curl\src\tool_hugehelp.c
@@ -10,10 +7,23 @@ move /Y C:\httpd-sdk\src\curl\src\tool_hugehelp.c.cvs C:\httpd-sdk\src\curl\src\
 sed -i 's/\/W4/\/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox \/W3 %AVXSED%/g' /cygdrive/c/httpd-sdk/src/curl/winbuild/MakefileBuild.vc
 sed -i 's/LFLAGS     = \/nologo/LFLAGS     = \/nologo \/LTCG \/OPT:ICF/g' /cygdrive/c/httpd-sdk/src/curl/winbuild/MakefileBuild.vc
 sed -i 's/LNKLIB     = lib.exe/LNKLIB     = lib.exe \/LTCG/g' /cygdrive/c/httpd-sdk/src/curl/winbuild/MakefileBuild.vc
-nmake /f Makefile.vc mode=static VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_NGHTTP2=static WITH_CARES=static WITH_ZLIB=static ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
-nmake /f Makefile.vc mode=dll VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_NGHTTP2=dll WITH_CARES=dll WITH_ZLIB=dll ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
+
+SET rd=-release-dll-ssl-dll-cares-dll-zlib-dll-ssh2-static-ipv6-sspi-winssl-nghttp2-dll
+SET rs=-release-static-ssl-static-cares-static-zlib-static-ssh2-static-ipv6-sspi-winssl-nghttp2-static
+
+	REM static
+nmake /f Makefile.vc mode=static	VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_CARES=static WITH_SSH2=static WITH_SSL=static WITH_NGHTTP2=static WITH_ZLIB=static ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
+
+	REM dll
+		REM KO tout static : cares en dll
+		REM KO tout dll : ssh2 en static
+		REM OK nmake /f Makefile.vc mode=dll VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_CARES=dll WITH_SSH2=static WITH_SSL=static WITH_NGHTTP2=static WITH_ZLIB=static ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
+nmake /f Makefile.vc mode=dll VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_CARES=dll WITH_SSH2=static WITH_SSL=dll WITH_NGHTTP2=dll WITH_ZLIB=dll ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
 
 mkdir C:\httpd-sdk\install\include\curl\
+Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.lib C:\httpd-sdk\install\lib\libcurl_a.lib
+Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.pdb C:\httpd-sdk\install\lib\libcurl_a.pdb
+
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\libcurl.dll C:\httpd-sdk\install\bin\libcurl.dll
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\curl\curl.h C:\httpd-sdk\install\include\curl\curl.h
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\curl\curlver.h C:\httpd-sdk\install\include\curl\curlver.h
@@ -26,11 +36,10 @@ Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\cur
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\curl\urlapi.h C:\httpd-sdk\install\include\curl\urlapi.h
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\lib\libcurl.lib C:\httpd-sdk\install\lib\libcurl.lib
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\lib\libcurl.pdb C:\httpd-sdk\install\lib\libcurl.pdb
-copy /Y C:\httpd-sdk\install\lib\libcurl.pdb C:\httpd-sdk\install\bin\libcurl.pdb
-Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.lib C:\httpd-sdk\install\lib\libcurl_a.lib
-Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.pdb C:\httpd-sdk\install\lib\libcurl_a.pdb
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\curl.exe C:\httpd-sdk\install\bin\curl.exe
 Copy /Y C:\httpd-sdk\src\curl\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\curl.pdb C:\httpd-sdk\install\bin\curl.pdb
+
+Copy /Y C:\httpd-sdk\install\lib\libcurl.pdb C:\httpd-sdk\install\bin\libcurl.pdb
 Copy /Y C:\httpd-sdk\src\curl\include\curl\*.h C:\httpd-sdk\install\include\curl
 
 cd /D C:\httpd-sdk\modules_bat
