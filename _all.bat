4488a215759df4590c07fe29b01ea12a333744b7
nono <nono@nono.com>
2019-06-25 11:45:19 +0200

    add !AVX

--- _all.bat
+++ _all.bat
@@ -1,18 +1,38 @@
+REM AVX
 call vc15
 call x86
 call silent-httpd-build.bat
-call silent-php-build.bat
+REM call silent-php-build.bat
 call silent-subversion-build.bat
 call x64
 call silent-httpd-build.bat
-call silent-php-build.bat
+REM call silent-php-build.bat
 call silent-subversion-build.bat
 call vs16
 call x86
 call silent-httpd-build.bat
-call silent-php-build.bat
+REM call silent-php-build.bat
 call silent-subversion-build.bat
 call x64
 call silent-httpd-build.bat
-call silent-php-build.bat
+REM call silent-php-build.bat
+call silent-subversion-build.bat
+REM !AVX
+call vc15
+call x86
+call avx 0
+call silent-httpd-build.bat
+call silent-subversion-build.bat
+call x64
+call avx 0
+call silent-httpd-build.bat
+call silent-subversion-build.bat
+call vs16
+call x86
+call avx 0
+call silent-httpd-build.bat
+call silent-subversion-build.bat
+call x64
+call avx 0
+call silent-httpd-build.bat
 call silent-subversion-build.bat
