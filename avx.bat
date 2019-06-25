e7564f07f4c319a0989bd65be5794449d0b84007
nono <nono@nono.com>
2019-06-25 14:32:02 +0200

    change link and var set location according to AVX

--- avx.bat
+++ avx.bat
@@ -1,3 +1,4 @@
+echo off
 if %1 == 1 (
 	set AVX=/arch:AVX
 	set AVXB=-avx
@@ -14,3 +15,13 @@ if %1 == 0 (
 	set AVXVCX=
 	echo ~~-~~ SSE2 ~~-~~
 )
+
+rmdir /S /Q C:\httpd-sdk\install
+mklink /J C:\httpd-sdk\install C:\httpd-sdk\%MSVC_DEPS%_%ARCH%%AVXB%
+
+REM /GL ko sur nghttp2. /GL ko si /Zi http://forums.codeguru.com/showthread.php?556135-ZI-and-GL-incompatible
+set EXTCFLAGSNGHTTP2=/GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP8 /LD /MD /Zi /Ox %AVX%
+set EXTCFLAGS=/GL %EXTCFLAGSNGHTTP2%
+
+call C:\msvc16\VC\Auxiliary\Build\vcvarsall.bat %ARCH% -vcvars_ver=%vcvars_ver%
+echo on
