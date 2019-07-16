a1e1dfc23d2dded01879d2acef1ae8bda245f32f
nono <nono@nono.com>
2019-07-16 13:40:42 +0200

    BUILDTS & raz nonodeps

--- phpsdk-local.bat
+++ phpsdk-local.bat
@@ -9,10 +9,12 @@ set PHPVER=7.3
 call C:\httpd-sdk\avx.bat 0
 
 	REM -1 : only deps
-set BUILDALL=0
+set BUILDALL=1
+set BUILDTS=0
+
 set BUILDLIB=1
 set BUILDREQ=1
-set UPDATEDEPS=1
+set UPDATEDEPS=0
 
 	REM ** uniquement sur init: phpsdk_buildtree phpmaster
 
@@ -25,10 +27,20 @@ if %UPDATEDEPS% == 1 (
 	call C:\php72-sdk\bin\7za.exe x -y *
 	rm -f *.zip
 )
+set raznono=0
+if %BUILDLIB% == 1 set raznono=1
+if %BUILDREQ% == 1 set raznono=1
+if %raznono% == 1 (
+	rmdir /S /Q C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\depsnono
+	powershell -command "Start-Sleep -s 1"
+	mkdir C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\depsnono
+	mkdir C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\depsnono\lib
+	mkdir C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\depsnono\bin
+	mkdir C:\php72-sdk\phpmaster\%MSVC_DEPS%\%ARCH%\depsnono\include
+)
 if %BUILDLIB% == 1 (
 	echo ON
 	REM en premier pour créer la structure depsnono
-	call %MODULE_BAT_DIR%libssh2-php.bat
 	call %MODULE_BAT_DIR%libxpm-php.bat
 	call %MODULE_BAT_DIR%freetype-php.bat
 	call %MODULE_BAT_DIR%libpng-php.bat
@@ -61,19 +73,20 @@ if NOT %BUILDALL% == -1 (
 		set intrinsics=
 			echo *** std nts  ***
 			call C:\httpd-sdk\phpsdk-config_make.bat
+		if %BUILDTS% == 1 (
+			set ZTS=
+			set TSNTS=ts
+			set BUILDDIR=Release_TS
 
-		set ZTS=
-		set TSNTS=ts
-		set BUILDDIR=Release_TS
+				echo *** std ts  ***
+				call C:\httpd-sdk\phpsdk-config_make.bat
 
-			echo *** std ts  ***
-			call C:\httpd-sdk\phpsdk-config_make.bat
-
-		set outdir=-avx
-		call C:\httpd-sdk\avx.bat 1
-		set intrinsics=,sse3,ssse3,sse4.1,sse4.2,avx
-			echo *** avx ts  ***
-			call C:\httpd-sdk\phpsdk-config_make.bat
+			set outdir=-avx
+			call C:\httpd-sdk\avx.bat 1
+			set intrinsics=,sse3,ssse3,sse4.1,sse4.2,avx
+				echo *** avx ts  ***
+				call C:\httpd-sdk\phpsdk-config_make.bat
+		)
 
 		copy /Y D:\github\NONO_phpwin-perfbuild\%MSVC_DEPS%-%PHP_SDK_ARCH%-avx-nts\php_memcache.dll D:\github\NONO_PHP7-memcache-dll\%MSVC_DEPS%\%PHP_SDK_ARCH%\nts\avx\php-%PHPVER%.x_memcache.dll
 		copy /Y D:\github\NONO_phpwin-perfbuild\%MSVC_DEPS%-%PHP_SDK_ARCH%-avx-ts\php_memcache.dll D:\github\NONO_PHP7-memcache-dll\%MSVC_DEPS%\%PHP_SDK_ARCH%\ts\avx\php-%PHPVER%.x_memcache.dll
