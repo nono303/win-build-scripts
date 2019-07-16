3a448dfa28b5ba2c21c6bd57e6799eb69e5c6b15
nono <nono@nono.com>
2019-07-16 13:37:16 +0200

    set PATH after xCommon with PATHORI

--- x64.bat
+++ x64.bat
@@ -1,10 +1,10 @@
-echo off
+@echo off
 set ARCH=x64
 set serfbuild=x86_64
 set perlbuild=VC-WIN64A
 set archmsbuild=x64
 set outmsbuild=x64\Release
 set CYGV=64
-set PATH=%PATH%;C:\msvc16\MSBuild\Current\Bin\amd64
 echo ~~-~~  x64 ~~-~~
 call C:\httpd-sdk\xCommon.bat
+set PATH=%PATHORI%;C:\msvc16\MSBuild\Current\Bin\amd64
--- x86.bat
+++ x86.bat
@@ -1,10 +1,10 @@
-echo off
+@echo off
 set ARCH=x86
 set serfbuild=x86
 set perlbuild=VC-WIN32
 set archmsbuild=Win32
 set outmsbuild=Release
 set CYGV=32
-set PATH=%PATH%;C:\msvc16\MSBuild\Current\Bin
 echo ~~-~~  x86 ~~-~~
 call C:\httpd-sdk\xCommon.bat
+set PATH=%PATHORI%;C:\msvc16\MSBuild\Current\Bin
