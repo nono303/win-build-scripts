8f1bde9cce6f573f9cc01ddd8a8e571edd52afaa
nono <nono@nono.com>
2019-07-16 13:38:54 +0200

    static mklink 4_3_1 => 4_3_2

--- modules_bat/libzmq-php.bat
+++ modules_bat/libzmq-php.bat
@@ -4,5 +4,5 @@ cd /D C:\httpd-sdk\build\libzmq
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PHPDEPS% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DWITH_DOC=OFF -DBUILD_SHARED=ON -DBUILD_STATIC=OFF -DBUILD_TESTS=OFF -DENABLE_CPACK=OFF ..\..\src\libzmq 
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/libzmq-php.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
-mklink /h %PHPDEPS%\lib\libzmq.lib %PHPDEPS%\lib\libzmq-mt-4_3_1.lib
+mklink /h %PHPDEPS%\lib\libzmq.lib %PHPDEPS%\lib\libzmq-mt-4_3_2.lib
 cd /D C:\httpd-sdk\modules_bat
