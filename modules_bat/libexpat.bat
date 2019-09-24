0000375775f6a4105c92c9bf194113cb405373a8
nono <nono@nono.com>
2019-09-24 21:53:07 +0200

    conform option to 2.2.8

--- modules_bat/libexpat.bat
+++ modules_bat/libexpat.bat
@@ -2,7 +2,7 @@ cd ..
 rmdir /S /Q C:\httpd-sdk\build\libexpat
 mkdir C:\httpd-sdk\build\libexpat
 cd /D C:\httpd-sdk\build\libexpat
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_doc=OFF -DBUILD_tests=OFF -DBUILD_examples=OFF -DBUILD_tools=OFF ..\..\src\libexpat\expat
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DEXPAT_BUILD_DOCS=OFF -DEXPAT_BUILD_TESTS=OFF -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TOOLS=OFF ..\..\src\libexpat\expat
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/libexpat.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
 copy /Y C:\httpd-sdk\build\libexpat\expat.pdb C:\httpd-sdk\install\bin\expat.pdb
