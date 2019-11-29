785febff09bb25c068eb57b07d4ddc7657fef670
nono <nono@nono.com>
2019-11-29 13:15:27 +0100

    copy /lib pdb

--- modules_bat/nghttp2.bat
+++ modules_bat/nghttp2.bat
@@ -18,5 +18,6 @@ move /Y C:\httpd-sdk\install\lib\nghttp2.lib C:\httpd-sdk\install\lib\nghttp2_st
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=C:\httpd-sdk\install -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DENABLE_LIB_ONLY=ON -DENABLE_PYTHON_BINDINGS=OFF -DENABLE_EXAMPLES=OFF -DENABLE_STATIC_LIB=NO -DLIBCARES_LIBRARY=C:/httpd-sdk/install/lib/cares_static.lib -DLIBCARES_INCLUDE_DIR=C:/httpd-sdk/install/include -DLIBEV_LIBRARY=C:/httpd-sdk/install/lib/libev_static.lib -DLIBEVENT_INCLUDE_DIR=C:/httpd-sdk/install/include -DJANSSON_INCLUDE_DIR=C:/httpd-sdk/install/include -DJANSSON_LIBRARIES=C:/httpd-sdk/install/lib/jansson.lib -DJEMALLOC_LIBRARY=C:/httpd-sdk/install/lib/jemalloc.lib -DLIBEVENT_OPENSSL_LIBRARY=C:/httpd-sdk/install/lib/event_openssl.lib -DJEMALLOC_INCLUDE_DIR=C:/httpd-sdk/install/include -DENABLE_WERROR=OFF -DENABLE_DEBUG=OFF -DENABLE_THREADS=ON -DENABLE_FAILMALLOC=OFF -DWITH_LIBXML2=ON -DWITH_JEMALLOC=ON -DENABLE_HPACK_TOOLS=ON -DWITH_SPDYLAY=OFF -DWITH_MRUBY=OFF -DWITH_NEVERBLEED=OFF -DCYTHON_EXECUTABLE=C:\python27\Scripts ..\..\src\nghttp2
 C:\cyg64\bin\bash /cygdrive/c/httpd-sdk/modules_bat/nghttp2.sh "%AVXSED%"
 nmake %NMAKE_OPTS% clean install
+copy C:\httpd-sdk\build\nghttp2\lib\nghttp2.pdb C:\httpd-sdk\install\bin\nghttp2.pdb
 
 cd /D C:\httpd-sdk\modules_bat
