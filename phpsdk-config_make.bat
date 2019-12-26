24c1422a170b17ccb8ba47cc45595efe9122fd08
nono <nono@nono.com>
2019-12-26 07:39:29 +0100

    php < 7.4 for memcache only

--- phpsdk-config_make.bat
+++ phpsdk-config_make.bat
@@ -17,26 +17,27 @@ REM export config options
 	REM call configure --help > c:\configure_%PHPVER%.txt
 	REM exit /B
 
-if %PHPVER% == 7.1 (
-	call configure --with-mp=8 --enable-object-out-dir=../build/ --without-gd --disable-embed --disable-phpdbgs --disable-phpdbg --disable-cli-win32 --disable-test-ini --disable-debug --disable-debug-pack --disable-ipv6 --disable-phpdbg-webhelper --disable-crt-debug --disable-security-flags --without-enchant --without-imap --without-snmp --without-xmlrpc --without-xsl --without-gmp --without-wddx --without-libwebp --without-interbase --without-ldap --without-oci8 --without-pgsql --without-uncritical-warn-choke --with-boost=C:\src\boost --enable-memcache=shared --enable-fd-setsize=2048 --without-analyzer --with-extra-includes="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Include\um";"C:\Program Files (x86)\Windows Kits\10\Include\%WKITVER%\um";"..\depsnono\include";"C:\httpd-sdk\install\include" --with-extra-libs="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Lib\um\%PHP_SDK_ARCH%";"C:\Program Files (x86)\Windows Kits\10\Lib\%WKITVER%\um\%PHP_SDK_ARCH%";"..\depsnono\lib";"C:\httpd-sdk\install\lib" %ZTS% 
-)
-
 REM BUG batch if : portée variable phpveropts
 set native-intrinsics=0
 if %PHPVER% == 7.2 (
 	set phpveropts= --enable-sanitizer --without-wddx  --without-interbase
 )
 if %PHPVER% == 7.3 (
-	set phpveropts= --enable-sanitizer --enable-native-intrinsics=sse,sse2%intrinsics% --without-wddx  --without-interbase
-	set native-intrinsics=1
-)
-if %PHPVER% == 7.4 (
-	set phpveropts= --enable-sanitizer --enable-native-intrinsics=sse,sse2%intrinsics% --with-ffi --with-mhash
+	set phpveropts= --with-boost=C:\src\boost --enable-sanitizer --enable-native-intrinsics=sse,sse2%intrinsics% --without-wddx  --without-interbase
 	set native-intrinsics=1
 )
 
-if NOT %PHPVER% == 7.1 (
-	call configure --with-mp=8 --enable-object-out-dir=../build/ --disable-embed --disable-phpdbgs --disable-phpdbg --disable-cli-win32 --disable-test-ini --disable-debug --disable-debug-pack --disable-ipv6 --disable-phpdbg-webhelper --disable-crt-debug --disable-security-flags --without-enchant --without-imap --without-snmp --without-xmlrpc --without-xsl --without-gmp --without-libwebp --without-ldap --without-oci8 --without-pgsql --without-uncritical-warn-choke --enable-mysql-xdevapi=shared --with-xdiff=shared --with-boost=C:\src\boost --with-protobuf=..\depsnono\ --with-iconv=C:\httpd-sdk\install --enable-sockets --enable-mbstring --enable-exif --enable-memcache=shared --enable-brotli=shared --enable-pdo --enable-opcache --enable-soap --enable-fileinfo --enable-com-dotnet --enable-fd-setsize=2048 --enable-intl --enable-phar-native-ssl --without-analyzer --with-curl=shared --with-tidy=shared --with-openssl --with-mysqli --with-pdo-mysql --with-bz2 --with-sqlite3 --with-pdo-sqlite --with-extra-includes="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Include\um";"C:\Program Files (x86)\Windows Kits\10\Include\%WKITVER%\um";"..\depsnono\include";"C:\httpd-sdk\install\include" --with-extra-libs="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Lib\um\%PHP_SDK_ARCH%";"C:\Program Files (x86)\Windows Kits\10\Lib\%WKITVER%\um\%PHP_SDK_ARCH%";"..\depsnono\lib";"C:\httpd-sdk\install\lib" --with-xdebug=shared --with-zmq=shared %ZTS% %phpveropts%
+if NOT %PHPVER% == 7.4 (
+	call configure --with-mp=%MTPROC% --enable-object-out-dir=../build/ --without-gd --disable-embed --disable-phpdbgs --disable-phpdbg --disable-cli-win32 --disable-test-ini --disable-debug --disable-debug-pack --disable-ipv6 --disable-phpdbg-webhelper --disable-crt-debug --disable-security-flags --without-enchant --without-imap --without-snmp --without-xmlrpc --without-xsl --without-gmp --without-wddx --without-libwebp --without-interbase --without-ldap --without-oci8 --without-pgsql --without-uncritical-warn-choke --enable-memcache=shared --enable-fd-setsize=2048 --without-analyzer --with-extra-includes="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Include\um";"C:\Program Files (x86)\Windows Kits\10\Include\%WKITVER%\um";"..\depsnono\include";"C:\httpd-sdk\install\include" --with-extra-libs="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Lib\um\%PHP_SDK_ARCH%";"C:\Program Files (x86)\Windows Kits\10\Lib\%WKITVER%\um\%PHP_SDK_ARCH%";"..\depsnono\lib";"C:\httpd-sdk\install\lib" %ZTS% %phpveropts%
+)
+
+if %PHPVER% == 7.4 (
+	REM *** DISABLED ***
+		REM SET DISABLED=--with-zmq=shared --enable-mysql-xdevapi=shared 
+	set phpveropts= --enable-sanitizer --enable-native-intrinsics=sse,sse2%intrinsics% --with-mhash --with-ffi
+	set native-intrinsics=1
+
+	call configure --with-mp=%MTPROC% --enable-object-out-dir=../build/ --disable-embed --disable-phpdbgs --disable-phpdbg --disable-cli-win32 --disable-test-ini --disable-debug --disable-debug-pack --disable-ipv6 --disable-phpdbg-webhelper --disable-crt-debug --disable-security-flags --without-enchant --without-imap --without-snmp --without-xmlrpc --without-xsl --without-gmp --without-libwebp --without-ldap --without-oci8 --without-pgsql --without-uncritical-warn-choke --with-xdiff=shared --with-boost=C:\src\boost --with-protobuf=..\depsnono\ --with-iconv=C:\httpd-sdk\install --enable-sockets --enable-mbstring --enable-exif --enable-memcache=shared --enable-brotli=shared --enable-pdo --enable-opcache --enable-soap --enable-fileinfo --enable-com-dotnet --enable-fd-setsize=2048 --enable-intl --enable-phar-native-ssl --without-analyzer --with-curl=shared --with-tidy=shared --with-openssl --with-mysqli --with-pdo-mysql --with-bz2 --with-sqlite3 --with-pdo-sqlite --with-extra-includes="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Include\um";"C:\Program Files (x86)\Windows Kits\10\Include\%WKITVER%\um";"..\depsnono\include";"C:\httpd-sdk\install\include" --with-extra-libs="C:\Program Files (x86)\Windows Kits\NETFXSDK\4.7\Lib\um\%PHP_SDK_ARCH%";"C:\Program Files (x86)\Windows Kits\10\Lib\%WKITVER%\um\%PHP_SDK_ARCH%";"..\depsnono\lib";"C:\httpd-sdk\install\lib" --with-xdebug=shared %ZTS% %phpveropts%
 )
 
 REM pause
