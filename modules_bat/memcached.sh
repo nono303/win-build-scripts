ae7d7919b259ee064c6b1b44886d8ad7bf0307e8
nono <nono@nono.com>
2020-04-02 00:53:34 +0200

    finalize memcached install shell with - patch for external storage - patch for not build debug

--- modules_bat/memcached.sh
+++ modules_bat/memcached.sh
@@ -1,10 +1,9 @@
 MEMCACHE_ROOT=/cygdrive/c/src/memcached
-CUR=$(pwd)
 unameOut="$(uname -a)"
-mkdir -p $MEMCACHE_ROOT/release/
-mkdir -p $MEMCACHE_ROOT/release/x86/
-mkdir -p $MEMCACHE_ROOT/release/x64/
 cd $MEMCACHE_ROOT
+git reset --hard
+git clean -fdx
+git apply c:/httpd-sdk/modules_bat/memcached.patch
 ./version.sh
 ./autogen.sh
 case "${unameOut}" in
@@ -17,22 +16,23 @@ esac
 
 # https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
 # STD
-./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --disable-extstore --prefix=$MEMCACHE_ROOT/release/$ARCH_PRF/ $ARCH_PARAMS
+./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --prefix=$MEMCACHE_ROOT/release/$ARCH_PRF/ $ARCH_PARAMS
 #   SSE2
-make CFLAGS=' -O3 -s -msse2 ' clean install
-cp -rf $MEMCACHE_ROOT/release/$ARCH_PRF/bin/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached.exe
+make clean && make CFLAGS=' -O3 -s -msse2 ' 
+exit
+cp -rf $MEMCACHE_ROOT/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached.exe
 #   AVX
-make CFLAGS=' -O3 -s -march=sandybridge ' clean install
-cp -rf $MEMCACHE_ROOT/release/$ARCH_PRF/bin/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-avx.exe 
+make clean && make CFLAGS=' -O3 -s -march=sandybridge ' 
+cp -rf $MEMCACHE_ROOT/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-avx.exe 
 
 # TLS
-./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --disable-extstore --prefix=$MEMCACHE_ROOT/release/$ARCH_PRF/ $ARCH_PARAMS --enable-tls
+./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --prefix=$MEMCACHE_ROOT/release/$ARCH_PRF/ $ARCH_PARAMS --enable-tls
 #   SSE2
-make CFLAGS=' -O3 -s -msse2 ' clean install
-cp -rf $MEMCACHE_ROOT/release/$ARCH_PRF/bin/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-tls.exe
+make clean && make CFLAGS=' -O3 -s -msse2 '
+cp -rf $MEMCACHE_ROOT/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-tls.exe
 #   AVX
-make CFLAGS=' -O3 -s -march=sandybridge ' clean install
-cp -rf $MEMCACHE_ROOT/release/$ARCH_PRF/bin/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-avx-tls.exe 
+make clean && make CFLAGS=' -O3 -s -march=sandybridge ' 
+cp -rf $MEMCACHE_ROOT/memcached.exe /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/memcached-avx-tls.exe 
 
 cp -rf $CYG_BIN_PATH/cygcrypto-1.1.dll /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/
 cp -rf $CYG_BIN_PATH/cygevent_core-2-0-5.dll /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/
@@ -45,5 +45,3 @@ cp -rf $CYG_BIN_PATH/cygwin1.dll /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_
 cp -rf $CYG_BIN_PATH/cygz.dll /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/
 # x86
 cp -rf $CYG_BIN_PATH/cyggcc_s-1.dll /cygdrive/d/github/NONO_memcached/cygwin/$ARCH_PRF/
-
-cd $CUR
