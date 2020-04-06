93b2343f7cb553e674b1b13e5aac5fdd1db3cd18
nono303 <nono303@nono303.net>
2020-04-06 12:58:52 +1200

    v5

--- modules_bat/libmaxminddb.sh
+++ modules_bat/libmaxminddb.sh
@@ -1,1 +1,1 @@
-/usr/bin/find /cygdrive/c/httpd-sdk/src/libmaxminddb/projects/VS12 -type f -name "libmaxminddb.props" -exec sed -i 's/vc$(PlatformToolsetVersion).pdb/$(ProjectName).pdb/g' {} \;
+/usr/bin/find /cygdrive/c/src/libmaxminddb/projects/VS12 -type f -name "libmaxminddb.props" -exec sed -i 's/vc$(PlatformToolsetVersion).pdb/$(ProjectName).pdb/g' {} \;
