6962ca7031cac14f69dce77ce273096c96b00494
nono <nono@nono.com>
2020-04-08 05:43:20 +0200

    echo off big if

--- build.bat
+++ build.bat
@@ -1,14 +1,16 @@
+@echo off
 REM https://www.robvanderwoude.com/battech_inputvalidation_commandline.php
 REM https://stackoverflow.com/questions/9102422/windows-batch-set-inside-if-not-working
 
-call ymdhis.bat
+call %PATH_MODULES_COMMON%\ymdhis.bat
 IF /I "%~2"=="ALL" (
-	SET LOGNAME=%PATH_HTTPD_SDK%\_logs\%1_ALL_%ymdhis%.log
+	SET LOGNAME=%PATH_LOGS%\%1_ALL_%ymdhis%.log
 ) ELSE (
 	SET LOGNAME=%PATH_HTTPD_SDK%\_logs\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.log
 )
 
 IF /I "%~2"=="ALL" (
+	echo on
 	call %PATH_HTTPD_SDK%\vc15.bat 2>&1 | tee -a %LOGNAME%
 	call %PATH_HTTPD_SDK%\x86.bat 2>&1 | tee -a %LOGNAME%
 	call %PATH_HTTPD_SDK%\avx.bat 1 2>&1 | tee -a %LOGNAME%
@@ -50,7 +52,9 @@ IF /I "%~2"=="ALL" (
 	call %PATH_HTTPD_SDK%\avx.bat 0 2>&1 | tee -a %LOGNAME%
 	call %PATH_MODULES%\%1.bat %1 2>&1 | tee -a %LOGNAME%
 ) ELSE (
+	echo on
 	call %PATH_MODULES%\%1.bat %1 2>&1 | tee %LOGNAME%
 )
+@echo off
 dos2unix %LOGNAME%
 cd /D %PATH_HTTPD_SDK%
