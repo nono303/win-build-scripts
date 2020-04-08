a7e32d660a1a3bd0e13527871cfe80c7db984874
nono <nono@nono.com>
2020-04-08 09:26:20 +0200

    zlib with build.bat

--- modules/zlib.bat
+++ modules/zlib.bat
@@ -1,10 +1,9 @@
 call %PATH_MODULES_COMMON%/init.bat %1
 
 REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
-cd %PATH_SRC%\%1
-sed -i 's/ARFLAGS = -nologo/ARFLAGS = -nologo -ltcg/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
-sed -i 's/-incremental:no -opt:ref/-ltcg -opt:ICF/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
-sed -i 's/-O2/-GL -GS- -guard:cf- -FD -GF -Zc:inline -MP%NUMBER_OF_PROCESSORS% -LD -Ox %AVXMSC% -wd4267/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
+git apply %PATH_MODULES%\zlib.patch
+
+sed -i 's/Zc:inline/Zc:inline -MP%NUMBER_OF_PROCESSORS% %AVXMSC%/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
 nmake %NMAKE_OPTS% /f win32/Makefile.msc clean
 if %ARCH% == x86 (
 	nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj"
@@ -19,4 +18,3 @@ copy /Y %PATH_SRC%\%1\zlib1.pdb %PATH_INSTALL%\bin\zlib1.pdb
 copy /Y %PATH_SRC%\%1\zdll.lib %PATH_INSTALL%\lib\zlib.lib
 copy /Y %PATH_SRC%\%1\zlib.h %PATH_INSTALL%\include\zlib.h
 copy /Y %PATH_SRC%\%1\zconf.h %PATH_INSTALL%\include\zconf.h
-cd /D %PATH_MODULES%
