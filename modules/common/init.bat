d06d9f2c3ca5e2fe749ba958acda9946851ea66f
nono <nono@nono.com>
2020-04-08 23:32:01 +0200

    add cmake parameter to only create PATH_BUILD structure for cmake build

--- modules/common/init.bat
+++ modules/common/init.bat
@@ -1,6 +1,8 @@
 cd %PATH_SRC%\%1
 git reset --hard
 git clean -fdx
-if exist %PATH_BUILD%\%1\. rmdir /S /Q %PATH_BUILD%\%1
-mkdir %PATH_BUILD%\%1
-cd /D %PATH_BUILD%\%1
+IF /I "%~2"=="cmake" (
+	if exist %PATH_BUILD%\%1\. rmdir /S /Q %PATH_BUILD%\%1
+	mkdir %PATH_BUILD%\%1
+	cd /D %PATH_BUILD%\%1
+)
