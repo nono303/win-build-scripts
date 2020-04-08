ff737117e4997fa1e80ec333e9331c5905856d28
nono <nono@nono.com>
2020-04-08 23:31:15 +0200

    improve sed in loop

--- modules/common/flags.sh
+++ modules/common/flags.sh
@@ -1,4 +1,6 @@
-# c-ares, brotli
-/usr/bin/find $2 -type f -name "build.make" -exec sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' {} \;
-/usr/bin/find $2 -type f -name "build.make" -exec sed -i 's/static.lib @CMakeFiles/static.lib \/LTCG @CMakeFiles/g' {} \;
-/usr/bin/find $2 -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP'$3' \/LD \/MD \/Ox '$1' \/wd4334 \/wd4090 \/wd4996 \/wd4267 \/wd4244 /g' {} \;
+for i in `/usr/bin/find $2 -type f -name "build.make" 2>/dev/null` 
+do
+  sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' $i
+  sed -i 's/static.lib @CMakeFiles/static.lib \/LTCG @CMakeFiles/g' $i
+done
+/usr/bin/find $2 -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/LD \/MD \/Ox \/Zc:inline \/wd4334 \/wd4090 \/wd4996 \/wd4267 \/wd4244 \/MP'$3' '$1' /g' {} \;
