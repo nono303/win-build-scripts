08c557c485683dc52734e80c65da0f4105b8fc0c
nono <nono@nono.com>
2020-04-09 07:22:01 +0200

    correct %CYGPATH_MODULES_COMMON%/

--- modules/brotli.bat
+++ modules/brotli.bat
@@ -1,7 +1,7 @@
 call %PATH_MODULES_COMMON%/init.bat %1 cmake
 
 cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON %PATH_SRC%\%1
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES%/common/flags.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/flags.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 nmake %NMAKE_OPTS% clean install
 
 copy /Y %PATH_BUILD%\%1\brotlicommon.pdb %PATH_INSTALL%\bin\brotlicommon.pdb
