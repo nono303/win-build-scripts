a5462408f58c5715e6bec189da04590b78c5c0c6
nono <nono@nono.com>
2020-04-11 08:01:53 +0200

    correct git clean bug in loop

--- modules/curl-sub.bat
+++ modules/curl-sub.bat
@@ -1,27 +1,29 @@
-cd /D %PATH_SRC%\%4
+cd /D %PATH_SRC%\%1
 git clean -fdx
-cd /D %PATH_SRC%\%4\winbuild
-set CURL_CONFIGNAMELIB=%3_%1_deps-%2
-if %3 == openssl-ssh2 (set CURL_SSLNMAKE=ENABLE_WINSSL=no WITH_SSH2=%1 WITH_SSL=%2)
-if %3 == winssl (set CURL_SSLNMAKE=ENABLE_WINSSL=yes)
+	REM https://github.com/curl/curl/blob/master/GIT-INFO
+copy /Y %PATH_SRC%\%1\src\tool_hugehelp.c.cvs %PATH_SRC%\%1\src\tool_hugehelp.c
+cd /D %PATH_SRC%\%1\winbuild
+set CURL_CONFIGNAMELIB=%4_%2_deps-%3
+if %4 == openssl-ssh2 (set CURL_SSLNMAKE=ENABLE_WINSSL=no WITH_SSH2=%2 WITH_SSL=%3)
+if %4 == winssl (set CURL_SSLNMAKE=ENABLE_WINSSL=yes)
 
 nmake /f Makefile.vc ^
-mode=%1 ^
+mode=%2 ^
 VC=%MSVC_VER% ^
 WITH_DEVEL=%PATH_INSTALL% ^
-WITH_NGHTTP2=%2 ^
-WITH_CARES=%2 ^
-WITH_ZLIB=%2 ^
+WITH_NGHTTP2=%3 ^
+WITH_CARES=%3 ^
+WITH_ZLIB=%3 ^
 ENABLE_SSPI=yes ^
 ENABLE_IPV6=yes ^
 ENABLE_IDN=yes ^
 GEN_PDB=yes ^
 DEBUG=no ^
 MACHINE=%ARCH% ^
-BUILDDIR=%PATH_BUILD%\%4 ^
+BUILDDIR=%PATH_BUILD%\%1 ^
 CONFIGNAMELIB=%CURL_CONFIGNAMELIB% ^
 %CURL_SSLNMAKE%
 
-rmdir /S /Q %PATH_BUILD%\%4\%CURL_CONFIGNAMELIB%-obj-curl
-rmdir /S /Q %PATH_BUILD%\%4\%CURL_CONFIGNAMELIB%-obj-lib
+rmdir /S /Q %PATH_BUILD%\%1\%CURL_CONFIGNAMELIB%-obj-curl
+rmdir /S /Q %PATH_BUILD%\%1\%CURL_CONFIGNAMELIB%-obj-lib
 exit /B 0
--- modules/curl.bat
+++ modules/curl.bat
@@ -5,8 +5,6 @@ cd %PATH_SRC%\%1
 git apply %PATH_MODULES%\curl.patch
 sed -i 's/..\\\\builds/%SEDPATH_BUILD%\\\\%1/g' %CYGPATH_SRC%/%1/winbuild/MakefileBuild.vc
 
-	REM https://github.com/curl/curl/blob/master/GIT-INFO
-move /Y %PATH_SRC%\%1\src\tool_hugehelp.c.cvs %PATH_SRC%\%1\src\tool_hugehelp.c
 	REM https://curl.haxx.se/mail/lib-2012-10/0163.html
 sed -i 's/SSH2        = dll/SSH2        = dll\r\nWIN_LIBS     = $(WIN_LIBS) user32.lib/g' %CYGPATH_SRC%/curl/winbuild/MakefileBuild.vc
 sed -i 's/libcares/cares_static/g' %CYGPATH_SRC%/curl/winbuild/MakefileBuild.vc
@@ -26,40 +24,10 @@ for %%s in (%CURL_SSLENGINE%) do (
 	for %%m in (%CURL_MODE%) do (
 		for %%d in (%CURL_MODE%) do (
 			echo on
-			call %PATH_MODULES%\curl-sub.bat %%m %%d %%s %1
+			call %PATH_MODULES%\curl-sub.bat %1 %%m %%d %%s
 		)
 	)
 )
 echo on
-
-
-
-	REM dll
-		REM KO tout static : cares en dll
-		REM KO tout dll : ssh2 en static
-		REM OK nmake /f Makefile.vc mode=dll VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_CARES=dll WITH_SSH2=static WITH_SSL=static WITH_NGHTTP2=static WITH_ZLIB=static ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
-	REM WITH_SSH2=static WITH_SSL=dll // doesn't work on mod_md
-nmake /f Makefile.vc mode=dll VC=%MSVC_VER% WITH_DEVEL=c:/httpd-sdk/install WITH_CARES=dll WITH_NGHTTP2=dll WITH_ZLIB=dll ENABLE_SSPI=yes ENABLE_IPV6=yes ENABLE_IDN=yes ENABLE_WINSSL=yes GEN_PDB=yes DEBUG=no MACHINE=%ARCH%
-
-mkdir %PATH_INSTALL%\include\%1\
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.lib %PATH_INSTALL%\lib\libcurl_a.lib
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rs%\lib\libcurl_a.pdb %PATH_INSTALL%\lib\libcurl_a.pdb
-
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\libcurl.dll %PATH_INSTALL%\bin\libcurl.dll
-Copy /Y %PATH_INSTALL%\lib\libcurl.pdb %PATH_INSTALL%\bin\libcurl.pdb
-
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\lib\libcurl.lib %PATH_INSTALL%\lib\libcurl.lib
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\lib\libcurl.pdb %PATH_INSTALL%\lib\libcurl.pdb
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\curl.exe %PATH_INSTALL%\bin\curl.exe
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\bin\curl.pdb %PATH_INSTALL%\bin\curl.pdb
-
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\curl.h %PATH_INSTALL%\include\%1\curl.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\curlver.h %PATH_INSTALL%\include\%1\curlver.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\easy.h %PATH_INSTALL%\include\%1\easy.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\mprintf.h %PATH_INSTALL%\include\%1\mprintf.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\multi.h %PATH_INSTALL%\include\%1\multi.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\stdcheaders.h %PATH_INSTALL%\include\%1\stdcheaders.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\system.h %PATH_INSTALL%\include\%1\system.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\typecheck-gcc.h %PATH_INSTALL%\include\%1\typecheck-gcc.h
-Copy /Y %PATH_SRC%\%1\builds\libcurl-vc%MSVC_VER%-%ARCH%%rd%\include\%1\urlapi.h %PATH_INSTALL%\include\%1\urlapi.h
-Copy /Y %PATH_SRC%\%1\include\%1\*.h %PATH_INSTALL%\include\curl
+if exist %PATH_INSTALL%\%1\. rmdir /S /Q %PATH_INSTALL%\%1
+move /Y %PATH_BUILD%\%1 %PATH_INSTALL%
