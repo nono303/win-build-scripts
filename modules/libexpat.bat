21988a3d1e80cdf025b3838cb2a6bb95deb55ec7
nono <nono@nono.com>
2020-04-16 14:47:32 +0200

    use %1 param instead of hard-coded

--- modules/libexpat.bat
+++ modules/libexpat.bat
@@ -1,11 +1,8 @@
-cd ..
-rmdir /S /Q %PATH_BUILD%\libexpat
-mkdir %PATH_BUILD%\libexpat
-cd /D %PATH_BUILD%\libexpat
-cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DEXPAT_BUILD_DOCS=OFF -DEXPAT_BUILD_TESTS=OFF -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TOOLS=OFF %PATH_SRC%\libexpat\expat
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES%/libexpat.sh "%AVXSED%"
+call %PATH_MODULES_COMMON%\init.bat %1 cmake
+
+cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DEXPAT_BUILD_DOCS=OFF -DEXPAT_BUILD_TESTS=OFF -DEXPAT_BUILD_EXAMPLES=OFF -DEXPAT_BUILD_TOOLS=OFF %PATH_SRC%\%1\expat
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/flags.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 nmake %NMAKE_OPTS% clean install
-copy /Y %PATH_BUILD%\libexpat\libexpat.pdb %PATH_INSTALL%\bin\libexpat.pdb
-REM APR
-mklink /H %PATH_INSTALL%\lib\expat.lib %PATH_INSTALL%\lib\libexpat.lib
-cd /D %PATH_MODULES%
+copy /Y %PATH_BUILD%\%1\libexpat.pdb %PATH_INSTALL%\bin\libexpat.pdb
+	REM APR
+REM mklink /H %PATH_INSTALL%\lib\expat.lib %PATH_INSTALL%\lib\libexpat.lib
--- modules/libmaxminddb.bat
+++ modules/libmaxminddb.bat
@@ -1,14 +1,13 @@
-rmdir /S /Q %PATH_SRC%\libmaxminddb\projects\VS12\%ARCH%
-cd /D %PATH_SRC%\libmaxminddb
-git reset --hard
-git clean -fdx
-cd /D %PATH_SRC%\libmaxminddb\projects\VS12
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/libmaxminddb/projects/VS12/" %AVXVCX% %PTFTS% %WKITVER%
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES%/libmaxminddb.sh
-if %MSVC_DEPS% == vc15 (%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES%/vcxproj2vc15.sh "libmaxminddb/projects/VS12")
+call %PATH_MODULES_COMMON%\init.bat %1
+
+if exist %PATH_SRC%\%1\projects\VS12\%ARCH%\. rmdir /S /Q %PATH_SRC%\%1\projects\VS12\%ARCH%
+cd /D %PATH_SRC%\%1\projects\VS12
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/projects/VS12/" %AVXVCX% %PTFTS% %WKITVER%
+sed -i 's/vc$(PlatformToolsetVersion).pdb/$(ProjectName).pdb/g' %CYGPATH_SRC%/%1/projects/VS12/libmaxminddb.props
+
 MSBuild.exe libmaxminddb.sln /p:Turbo=true /m:8 /p:CL_MPCount=8 /t:Clean,libmaxminddb /p:Configuration=Release /p:DebugSymbols=true /p:DebugType=None /p:Platform="%archmsbuild%"
-copy /Y %PATH_SRC%\libmaxminddb\projects\VS12\%outmsbuild%\libmaxminddb.lib %PATH_INSTALL%\lib\libmaxminddb.lib
-copy /Y %PATH_SRC%\libmaxminddb\projects\VS12\%outmsbuild%\libmaxminddb.pdb %PATH_INSTALL%\lib\libmaxminddb.pdb
-copy /Y %PATH_SRC%\libmaxminddb\include\maxminddb.h %PATH_INSTALL%\include\maxminddb.h 
-copy /Y %PATH_SRC%\libmaxminddb\include\maxminddb_config.h %PATH_INSTALL%\include\maxminddb_config.h
-cd /D %PATH_MODULES%
+
+copy /Y %PATH_SRC%\%1\projects\VS12\%outmsbuild%\%1.lib %PATH_INSTALL%\lib\%1.lib
+copy /Y %PATH_SRC%\%1\projects\VS12\%outmsbuild%\%1.pdb %PATH_INSTALL%\lib\%1.pdb
+copy /Y %PATH_SRC%\%1\include\maxminddb.h %PATH_INSTALL%\include\maxminddb.h 
+copy /Y %PATH_SRC%\%1\include\maxminddb_config.h %PATH_INSTALL%\include\maxminddb_config.h
