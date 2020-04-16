6cc1bf6e4bfff451345a3de0c726ac635915792b
nono <nono@nono.com>
2020-04-16 19:40:36 +0200

    add wd XXXX add RC nologo remove W3

--- modules/common/flags.sh
+++ modules/common/flags.sh
@@ -3,4 +3,9 @@ do
   sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' $i
   sed -i 's/link.exe \/lib/link.exe \/lib  \/LTCG/g' $i
 done
-/usr/bin/find $2 -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/LD \/MD \/Ox \/Zc:inline \/wd4334 \/wd4090 \/wd4996 \/wd4267 \/wd4244 \/wd4152 \/wd4706 \/wd4068 \/wd4701 \/wd4295 \/MP'$3' '$1' /g' {} \;
+for i in `/usr/bin/find $2 -type f -name "flags.make" 2>/dev/null` 
+do
+  sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/LD \/MD \/Ox \/Zc:inline \/wd4334 \/wd4090 \/wd4996 \/wd4267 \/wd4244 \/wd4152 \/wd4706 \/wd4068 \/wd4701 \/wd4295 \/wd4702 \/wd4018 \/wd4189 \/MP'$3' '$1' /g' $i
+  sed -i 's/RC_FLAGS = /RC_FLAGS = \/nologo /g' $i
+  sed -i 's/\/W3//g' $i
+done
