96fd795c56bc116080bbf196dc1c5ce4875863e3
nono <nono@nono.com>
2020-04-16 19:37:51 +0200

    lua for build.bat

--- modules/lua.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/find /cygdrive/c/httpd-sdk/build/lua -type f -name "build.make" -exec sed -i 's/\/INCREMENTAL/\/LTCG \/OPT:ICF/g' {} \;
-/usr/bin/find /cygdrive/c/httpd-sdk/build/lua -type f -name "flags.make" -exec sed -i 's/ \/O2 / \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Ox '$1' /g' {} \;
