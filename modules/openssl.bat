39037286fdce56a0e335901204894afdb16145d6
nono303 <nono303@nono303.net>
2020-04-17 11:16:25 +0200

    pretty & readable

--- modules/openssl.bat
+++ modules/openssl.bat
@@ -1,9 +1,19 @@
 cd /D %PATH_SRC%\%1
 
-perl Configure %perlbuild% --prefix=%PATH_INSTALL% -threads zlib-dynamic --openssldir=%PATH_INSTALL%\conf shared no-unit-test no-external-tests no-ssl3 no-weak-ssl-ciphers no-tests zlib zlib-dynamic --with-zlib-include=C:/httpd-sdk/install/include -DOPENSSL_USE_IPV6=1 -DOPENSSL_NO_HEARTBEATS -DOPENSSL_NO_DEPRECATED -L"/OPT:ICF /LTCG" +"/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP%NUMBER_OF_PROCESSORS% /LD /MD /Zi /Ox %AVX%"
+perl Configure %perlbuild% ^
+shared no-unit-test no-external-tests no-ssl3 no-weak-ssl-ciphers no-tests zlib zlib-dynamic ^
+-threads zlib-dynamic ^
+--prefix=%PATH_INSTALL% ^
+--with-zlib-include=%SLASHPATH_INSTALL%/include ^
+--openssldir=%PATH_INSTALL%\conf ^
+-DOPENSSL_USE_IPV6=1 ^
+-DOPENSSL_NO_HEARTBEATS ^
+-DOPENSSL_NO_DEPRECATED ^
+-L"/OPT:ICF /LTCG" +"/GL /GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP%NUMBER_OF_PROCESSORS% /LD /MD /Zi /Ox %AVX%"
 
 sed -i 's/\/W3 \/wd4090 \/nologo \/O2 -threads +/\/nologo \/wd4090 \/wd4267 \/wd4244 \/wd4133 \/wd4311 /g' %CYGPATH_SRC%/%1/makefile
 sed -i 's/\/nologo \/debug -L/\/nologo \/debug /g' %CYGPATH_SRC%/%1/makefile
 sed -i 's/ARFLAGS= \/nologo/ARFLAGS= \/nologo \/LTCG/g' %CYGPATH_SRC%/%1/makefile
 
+	REM install_sw > no docs
 nmake %NMAKE_OPTS% clean install_sw
--- modules/zlib.bat
+++ modules/zlib.bat
@@ -1,16 +1,13 @@
 call %PATH_MODULES_COMMON%\init.bat %1
 
-REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
+	REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
 git apply --verbose %PATH_MODULES%\%1.patch
 
 sed -i 's/Zc:inline/Zc:inline -MP%NUMBER_OF_PROCESSORS% %AVXMSC%/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
 nmake %NMAKE_OPTS% /f win32/Makefile.msc clean
-if %ARCH% == x86 (
-	nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj"
-)
-if %ARCH% == x64 (
-	nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" AS=ml64 
-)
+if %ARCH% == x86 (nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj")
+if %ARCH% == x64 (nmake -f win32/Makefile.msc LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" AS=ml64)
+
 copy /Y %PATH_SRC%\%1\zlib.lib %PATH_INSTALL%\lib\zlib_a.lib
 copy /Y %PATH_SRC%\%1\zlib.pdb %PATH_INSTALL%\lib\zlib_a.pdb
 copy /Y %PATH_SRC%\%1\zlib1.dll %PATH_INSTALL%\bin\zlib1.dll
