c77c3c9e44c3f2f42bcdc37e74c338d4984aaca8
nono303 <nono303@nono303.net>
2020-04-17 11:19:25 +0200

    supress hard coded path

--- modules/httpd_flags.sh
+++ modules/httpd_flags.sh
@@ -1,11 +1,11 @@
-for i in `/usr/bin/find /cygdrive/c/httpd-sdk/build/httpd/CMakeFiles -type f -name "build.make" 2>/dev/null` 
+for i in `/usr/bin/find $2/httpd/CMakeFiles -type f -name "build.make" 2>/dev/null` 
 do
   sed -i 's/diag.c.obj \/Fd/diag.c.obj \/D DIAG_BITS_'$1' \/Fd/g' $i
   sed -i 's/\/debug \/INCREMENTAL/\/debug \/LTCG \/OPT:ICF/g' $i
 done
-for i in `/usr/bin/find /cygdrive/c/httpd-sdk/build/httpd/CMakeFiles -type f -name "flags.make" 2>/dev/null` 
+for i in `/usr/bin/find $2/httpd/CMakeFiles -type f -name "flags.make" 2>/dev/null` 
 do
   sed -i 's/\/W3 \/MD \/Zi \/O2/\/w/g' $i
   sed -i 's/RC_FLAGS = /RC_FLAGS = \/nologo /g' $i
 done
-sed -i 's/\/w \/Ob1/\/w \/Ob1 \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Zi \/Ox/g' /cygdrive/c/httpd-sdk/build/httpd/CMakeFiles/gen_test_char.dir/flags.make
+sed -i 's/\/w \/Ob1/\/w \/Ob1 \/GL \/GS- \/Oy- \/guard:cf- \/FD \/GF \/Zc:inline \/MP8 \/LD \/MD \/Zi \/Ox/g' $2/httpd/CMakeFiles/gen_test_char.dir/flags.make
