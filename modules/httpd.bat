dbd78cb0125d7d28f2f9cce6e2dac44acac02f51
nono303 <nono303@nono303.net>
2020-04-17 17:44:23 +0200

    correct hardcoded path

--- modules/httpd.bat
+++ modules/httpd.bat
@@ -3,7 +3,7 @@ call %PATH_MODULES_COMMON%\init.bat %MODULE_EXTRA%
 	REM https://github.com/maxmind/mod_maxminddb/issues/84
 git apply --verbose %PATH_MODULES%\%MODULE_EXTRA%.patch
 
-	REM pour mod_md
+	REM pour mod_md : DLL OK / WinSSL mandatory ?
 set CURL_VER=winssl_dll_deps-static
 set CURL_LIB_NAME=libcurl
 	REM github
@@ -53,14 +53,14 @@ cd %PATH_SRC%\%MODULE_EXTRA%
 git apply --verbose %PATH_MODULES%\%MODULE_EXTRA%.patch
 
 cd %PATH_BUILD%\%MODULE_EXTRA%
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\mod_h264_streaming.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\output_mp4.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\output_bucket.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\moov.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\mp4_process.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\mp4_writer.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\mp4_reader.c
-cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo C:/src\%MODULE_EXTRA%\mp4_io.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\mod_h264_streaming.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\output_mp4.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\output_bucket.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\moov.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\mp4_process.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\mp4_writer.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\mp4_reader.c
+cl /nologo -D HAVE_CONFIG_H -D WIN32 %EXTCFLAGS% -I"%PATH_INSTALL%\include" -I"%PATH_INSTALL%\lib" -D BUILDING_H264_STREAMING /c /Fo %PATH_SRC%\%MODULE_EXTRA%\mp4_io.c
 link /nologo kernel32.lib "%PATH_INSTALL%\lib/libhttpd.lib" "%PATH_INSTALL%\lib/libapr-1.lib" "%PATH_INSTALL%\lib/libaprutil-1.lib" -Dll /machine:%ARCH% /LTCG /OPT:ICF /debug /out:mod_h264_streaming.so output_mp4.obj output_bucket.obj mp4_writer.obj mp4_reader.obj mp4_process.obj mp4_io.obj moov.obj mod_h264_streaming.obj
 copy /Y %PATH_BUILD%\%MODULE_EXTRA%\mod_h264_streaming.so %PATH_INSTALL%\modules\mod_h264_streaming.so
 copy /Y %PATH_BUILD%\%MODULE_EXTRA%\mod_h264_streaming.pdb %PATH_INSTALL%\modules\mod_h264_streaming.pdb
