a3dc461c4e66fc3c412cd9857626b8454fe24761
nono303 <nono303@nono303.net>
2020-04-17 11:21:10 +0200

    disable /install mklink %PATH_INSTALL% > /release/__VER__

--- avx.bat
+++ avx.bat
@@ -21,9 +21,11 @@ if %1 == 0 (
 	set intrinsics=
 	echo ~~-~~ SSE2 ~~-~~
 )
-
-if exist %PATH_INSTALL%\. rmdir /S /Q %PATH_INSTALL%
-mklink /J %PATH_INSTALL% %PATH_HTTPD_SDK%\release\%MSVC_DEPS%_%ARCH%%AVXB%
+set PATH_INSTALL=%PATH_HTTPD_SDK%\release\%MSVC_DEPS%_%ARCH%%AVXB%
+	REM SLASH WIN PATH
+FOR /F "tokens=* USEBACKQ" %%F IN (`%PATH_BIN_CYGWIN%\cygpath -m %PATH_INSTALL%`) DO (SET SLASHPATH_INSTALL=%%F)
+	REM PROTECTED SLASH WIN PATH
+set PROTECTEDSLASHPATH_INSTALL=%PATH_INSTALL:\=\/%
 if not exist %PATH_INSTALL%\lib\. mkdir %PATH_INSTALL%\lib
 if not exist %PATH_INSTALL%\bin\. mkdir %PATH_INSTALL%\bin
 if not exist %PATH_INSTALL%\include\. mkdir %PATH_INSTALL%\include
@@ -31,6 +33,5 @@ if not exist %PATH_INSTALL%\include\. mkdir %PATH_INSTALL%\include
 	REM /GL ko sur nghttp2. /GL ko si /Zi http://forums.codeguru.com/showthread.php?556135-ZI-and-GL-incompatible
 set EXTCFLAGSNGHTTP2=/GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP%NUMBER_OF_PROCESSORS% /LD /MD /Zi /Ox %AVX%
 set EXTCFLAGS=/GL %EXTCFLAGSNGHTTP2%
-
-call %PATH_VS%\VC\Auxiliary\Build\vcvarsall.bat %ARCH% -vcvars_ver=%vcvars_ver%
-echo on
+call %PATH_VS%\VC\Auxiliary\Build\vcvarsall.bat %ARCH% %WKITVER% -vcvars_ver=%vcvars_ver%
+@echo off
