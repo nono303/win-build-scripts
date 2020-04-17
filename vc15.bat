4bc936deb98cc88011a7e4d034ff8cf5c20d3129
nono303 <nono303@nono303.net>
2020-04-17 17:37:46 +0200

    MSVC_DEPS=vc%MSVC_VER%

--- vc15.bat
+++ vc15.bat
@@ -1,6 +1,6 @@
 @echo off
 set vcvars_ver=14.16
 set PTFTS=141
-set MSVC_DEPS=vc15
 set MSVC_VER=15
+set MSVC_DEPS=vc%MSVC_VER%
 echo ~~-~~ VC15 ~~-~~
--- vs16.bat
+++ vs16.bat
@@ -1,6 +1,6 @@
 @echo off
 set vcvars_ver=14.25
 set PTFTS=142
-set MSVC_DEPS=vs16
 set MSVC_VER=16
+set MSVC_DEPS=vs%MSVC_VER%
 echo ~~-~~ VS16 ~~-~~
