f43f4c919e7509b16d89b0a0d3108f4b4ba1fd18
nono303 <nono303@nono303.net>
2020-04-21 06:17:09 +0200

    php and req deps for build.bat

---
+++ modules/phpanddeps.bat
@@ -0,0 +1,7 @@
+call %PATH_BATCH%\build.bat libxpm
+call %PATH_BATCH%\build.bat freetype
+call %PATH_BATCH%\build.bat libpng
+call %PATH_BATCH%\build.bat jpeg
+call %PATH_BATCH%\build.bat tidy
+call %PATH_BATCH%\build.bat libxdiff
+call %PATH_BATCH%\build.bat php
