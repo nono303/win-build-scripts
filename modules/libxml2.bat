abdd2611986b699c2cc2d2e65128e12e1293260d
nono303 <nono303@nono303.net>
2020-04-21 06:25:09 +0200

    prettify & readable move mklink for php in php batch

--- modules/libxml2.bat
+++ modules/libxml2.bat
@@ -1,11 +1,16 @@
 call %PATH_MODULES_COMMON%\init.bat %1
 
 cd /D %PATH_SRC%/%1/win32
-cscript configure.js compiler=nmakemsvc prefix=%PATH_INSTALL% include=%PATH_INSTALL%\include lib=%PATH_INSTALL%\lib debug=no zlib=yes
+cscript configure.js ^
+compiler=nmakemsvc ^
+prefix=%PATH_INSTALL% ^
+include=%PATH_INSTALL%\include ^
+lib=%PATH_INSTALL%\lib ^
+debug=no ^
+zlib=yes
+
 git apply --verbose %PATH_MODULES%\%1.patch
 sed -i 's/Zc:inline/Zc:inline \/MP%NUMBER_OF_PROCESSORS% %AVXSED%/g' %CYGPATH_SRC%/%1/win32/Makefile.msvc
 nmake %NMAKE_OPTS% /f Makefile.msvc clean install-libs
 
 copy /Y %PATH_SRC%\%1\win32\bin.msvc\libxml2.pdb %PATH_INSTALL%\bin\libxml2.pdb
-	REM pour PHP
-if not exist %PATH_INSTALL%\include\libxml\. mklink /J %PATH_INSTALL%\include\libxml %PATH_INSTALL%\include\%1\libxml
