151159345f4c940f7cbf5684bf08dd92ed0f13d7
nono303 <nono303@nono303.net>
2020-04-27 11:13:27 +0200

    patch version for apr_dbd_odbc-1.dll (don't know why rc is empty...)

--- modules/apr.bat
+++ modules/apr.bat
@@ -37,3 +37,8 @@ if not exist %PATH_INSTALL%\include\arch\. mkdir %PATH_INSTALL%\include\arch
 copy /Y %PATH_SRC%\apr\include\arch\apr_private_common.h %PATH_INSTALL%\include\arch\apr_private_common.h
 if not exist %PATH_INSTALL%\include\arch\win32\. mkdir %PATH_INSTALL%\include\arch\win32
 copy /Y %PATH_SRC%\apr\include\arch\win32\*.h %PATH_INSTALL%\include\arch\win32
+
+	REM version apr_dbd_odbc-1.dll
+CD /D %PATH_SRC%\apr-util 
+for /F "tokens=* USEBACKQ" %%F in (`git describe --tags`) do (set VERSION=%%F)
+call %PATH_MODULES_COMMON%\version.bat %PATH_INSTALL%\bin\apr_dbd_odbc-1.dll "%VERSION%"
