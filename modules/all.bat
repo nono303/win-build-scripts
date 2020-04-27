081b8cfb2ac67416c30ed4edeb93e63e5e6ad137
nono303 <nono303@nono303.net>
2020-04-27 11:10:53 +0200

    add bininfo checkavx updaterc

--- modules/all.bat
+++ modules/all.bat
@@ -1,3 +1,4 @@
 call %PATH_BATCH%\build.bat httpdanddeps NOLOG
 call %PATH_BATCH%\build.bat subversionanddeps NOLOG
 call %PATH_BATCH%\build.bat phpanddeps NOLOG
+call %PATH_BATCH%\build.bat bininfo checkavx updaterc 
