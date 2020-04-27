a06ccd7fa38b77d3ed8f03630b27f09ebbbbbbbc
nono303 <nono303@nono303.net>
2020-04-27 12:17:24 +0200

    remove copyright and set product to finename

--- modules/common/version.bat
+++ modules/common/version.bat
@@ -1,4 +1,4 @@
 @echo off
 if "%~3"=="" (set PV=%2) else (set PV=%3)
-call %PATH_MODULES_COMMON%\ymdhis.bat
-%BIN_VERPATCH% %1 /va /fn /high %2 /pv %PV% /s product "%ARCH% %MSVC_DEPS% (%vcvars_ver%)%AVXB:-= % - %ymdhis:_= %" /s copyright "https://github.com/nono303/win-build-scripts"
+for /F %%i in ("%1") do set VER_PN=%%~ni
+call %BIN_VERPATCH% %1 %2 /va /fn /high /pv %PV% /s product "%VER_PN%"
