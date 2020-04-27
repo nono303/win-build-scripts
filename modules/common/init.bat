02e6f8479b488efed7daf65c6cc65ba63d0c1293
nono303 <nono303@nono303.net>
2020-04-27 12:15:48 +0200

    add svn init

--- modules/common/init.bat
+++ modules/common/init.bat
@@ -1,6 +1,12 @@
 cd %PATH_SRC%\%1
-git reset --hard
-git clean -fdx
+if exist %PATH_SRC%\%1\.git\. (
+	git reset --hard
+	git clean -fdx
+)
+if exist %PATH_SRC%\%1\.svn\. (
+	svn revert . -R
+	svn cleanup . --remove-unversioned --remove-ignored
+)
 if /I "%~2"=="cmake" (
 	if exist %PATH_BUILD%\%1\. rmdir /S /Q %PATH_BUILD%\%1
 	mkdir %PATH_BUILD%\%1
