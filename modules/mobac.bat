4cefb18191d6cf81ad22f87b6cea4d4edad359a9
nono303 <nono303@nono303.net>
2020-04-27 12:23:44 +0200

    patch src after init

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -1,2 +1,3 @@
-cd /D %PATH_SRC%\%1
+call %PATH_MODULES_COMMON%\init.bat %1
+call svn patch %PATH_MODULES%\mobac.patch .
 call "%ANT_HOME%\bin\ant.bat" clean_workspace, delete_build, build_mapsources, create_jar
