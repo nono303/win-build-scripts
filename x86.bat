918075b0e237d8b0c64d33beb000b32e5a530c54
nono303 <nono303@nono303.net>
2020-04-27 14:13:03 +0200

    env > env_sample

--- x86.bat
+++ x86.bat
@@ -6,5 +6,9 @@ set archmsbuild=Win32
 set outmsbuild=Release
 set CYGV=32
 echo ~~-~~  x86 ~~-~~
-call .\env.bat
+if exist .\env.bat (
+	call .\env.bat
+) else (
+	call .\env_sample.bat
+)
 set PATH=%PATH_WIN%;%PATH_VS%\MSBuild\Current\Bin
