80bf2ba0874f34e5123b70d070c7a2c91de6bca0
nono303 <nono303@nono303.net>
2020-05-03 11:48:43 +0200

    final review

--- modules/ddverpatch.bat
+++ modules/ddverpatch.bat
@@ -1,18 +1,14 @@
-call %PATH_MODULES_COMMON%\init.bat %1
-git apply --verbose %PATH_MODULES%\%1.patch
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1
+
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1" %AVXVCX% %PTFTS% %WKITVER%
 
-set VERPATCH_CONF=Release
+set OUTDIR_CONF=Release
+MSBuild.exe verpatch.sln ^
+	%MSBUILD_OPTS% ^
+	/nowarn:C4267;C4838;C4474 ^
+	/t:Clean,verpatch ^
+	/p:Configuration=%OUTDIR_CONF% ^
+	/p:Platform="Win32"
 
-MSBuild.exe %PATH_SRC%\%1\verpatch.sln ^
-/nowarn:MSB8012 ^
-/p:Turbo=true ^
-/m:%NUMBER_OF_PROCESSORS% ^
-/p:CL_MPCount=%NUMBER_OF_PROCESSORS% ^
-/t:Clean,verpatch ^
-/p:Configuration=%VERPATCH_CONF% ^
-/p:DebugSymbols=true ^
-/p:DebugType=None ^
-/p:Platform="Win32"
-
-xcopy /C /F /Y %PATH_SRC%\%1\%VERPATCH_CONF%\verpatch.exe %PATH_SOFTS%\*
+xcopy /C /F /Y %PATH_SRC%\%1\%OUTDIR_CONF%\verpatch.exe %PATH_SOFTS%\*
+call do_php %PATH_UTILS%\sub\bininfo.php %PATH_SOFTS%\verpatch.exe null checkavx
