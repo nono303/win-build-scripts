578b7b585adc4fb9353fc618a6d42b557bc038f2
nono303 <nono303@nono303.net>
2020-05-03 11:49:14 +0200

    prettify and readable

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -1,2 +1,6 @@
-call %PATH_MODULES_COMMON%\init.bat %1
-call "%ANT_HOME%\bin\ant.bat" clean_workspace, delete_build, build_mapsources, create_jar
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1
+call "%ANT_HOME%\bin\ant.bat" ^
+	clean_workspace, ^
+	delete_build, ^
+	build_mapsources, ^
+	create_jar
