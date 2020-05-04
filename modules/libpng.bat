b675d0122c34bf3f42bd354682aea72ce30fcc4e
nono303 <nono303@nono303.net>
2020-05-04 19:21:03 +0200

    final review

--- modules/libpng.bat
+++ modules/libpng.bat
@@ -13,28 +13,24 @@ if not exist %PATH_BUILD%\%LIBNAME%.tar.gz (
 	wget http://sourceforge.mirrorservice.org/l/li/libpng/libpng16/%LIBVER%/%LIBNAME%.tar.gz
 )
 tar xvf %LIBNAME%.tar.gz
-copy /Y %PATH_BUILD%\%LIBNAME%\*.* %PATH_SRC%\%1\
+xcopy /C /F /Y %PATH_BUILD%\%LIBNAME%\*.* %PATH_SRC%\%1\*
 rmdir /S /Q %PATH_BUILD%\%LIBNAME%
 
 for %%C in ("Release" "Release Library") do (
 	MSBuild.exe %PATH_SRC%\%1\%VCDIR%\vstudio.sln ^
+	%MSBUILD_OPTS% ^
 	/nowarn:MSB8012 ^
-	/p:Turbo=true ^
-	/m:%NUMBER_OF_PROCESSORS% ^
-	/p:CL_MPCount=%NUMBER_OF_PROCESSORS% ^
 	/t:Clean,libpng ^
 	/p:Configuration=%%C ^
-	/p:DebugSymbols=true ^
-	/p:DebugType=None ^
 	/p:Platform="%archmsbuild%"
 )
 
-for %%X in (lib pdb) do (copy /Y "%PATH_SRC%\%1\%VCDIR%\%archmsbuild%\Release Library\libpng_a.%%X" %PATH_INSTALL%\lib)
-for %%X in (dll pdb) do (copy /Y "%PATH_SRC%\%1\%VCDIR%\%archmsbuild%\Release\libpng.%%X" %PATH_INSTALL%\bin)
+if %ARCH% == x86 (set INTER_DIR=)
+if %ARCH% == x64 (set INTER_DIR=%archmsbuild%\)
 
-copy /Y %PATH_SRC%\%1\pnglibconf.h %PATH_INSTALL%\include\pnglibconf.h
-copy /Y %PATH_SRC%\%1\png.h %PATH_INSTALL%\include\png.h
-copy /Y %PATH_SRC%\%1\pngconf.h %PATH_INSTALL%\include\pngconf.h
+for %%X in (lib pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\%VCDIR%\%INTER_DIR%Release Library\libpng_a.%%X" %PATH_INSTALL%\lib\*)
+for %%X in (dll pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\%VCDIR%\%INTER_DIR%Release\libpng.%%X" %PATH_INSTALL%\bin\*)
+for %%X in (pnglibconf png pngconf) do (xcopy /C /F /Y %PATH_SRC%\%1\%%X.h %PATH_INSTALL%\include\*)
 
 	REM version
 call %PATH_MODULES_COMMON%\version.bat %PATH_INSTALL%\bin\libpng.dll "%LIBVER%"
