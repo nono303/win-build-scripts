af622af12da2b733dad7d1f44de3c19d97d4d641
nono303 <nono303@nono303.net>
2020-05-04 13:40:31 +0200

    remove unsed var CFLAGS

--- avx.bat
+++ avx.bat
@@ -6,7 +6,7 @@ if %1 == 1 (
 	set AVXDIR=\avx
 	set AVXSED=\/arch:AVX
 	set AVXMSC=-arch:AVX
-	set AVXVCX="<EnableEnhancedInstructionSet>AdvancedVectorExtensions<\/EnableEnhancedInstructionSet>"
+	set AVXVCX="<EnableEnhancedInstructionSet>AdvancedVectorExtensions<\/EnableEnhancedInstructionSet>\r\n"
 		REM php
 	set outdirphp=-avx
 	set intrinsics=,sse3,ssse3,sse4.1,sse4.2,avx
@@ -19,7 +19,7 @@ if %1 == 0 (
 	set AVXDIR=
 	set AVXSED=
 	set AVXMSC=
-	set AVXVCX="\n"
+	set AVXVCX="\r\n"
 		REM php
 	set outdirphp=
 	set intrinsics=
@@ -31,9 +31,5 @@ for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -u %PATH_BUILD%`) 
 set PATH_INSTALL=%PATH_RELEASE%\%MSVC_DEPS%_%ARCH%%AVXB%
 for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -m %PATH_INSTALL%`) do (set SLASHPATH_INSTALL=%%F)
 for %%s in (lib bin include) do (if not exist %PATH_INSTALL%\%%s\. mkdir %PATH_INSTALL%\%%s)
-
-	REM /GL ko sur nghttp2. /GL ko si /Zi http://forums.codeguru.com/showthread.php?556135-ZI-and-GL-incompatible
-set EXTCFLAGSNGHTTP2=/GS- /Oy- /guard:cf- /FD /GF /Zc:inline /MP%NUMBER_OF_PROCESSORS% /LD /MD /Zi /Ox %AVX%
-set EXTCFLAGS=/GL %EXTCFLAGSNGHTTP2%
 call %PATH_VS%\VC\Auxiliary\Build\vcvarsall.bat %ARCH% %WKITVER% -vcvars_ver=%vcvars_ver%
 @echo off
