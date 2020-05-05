4aab1c961c7df6c1f7122691b3688ced5397a388
nono303 <nono303@nono303.net>
2020-05-05 16:53:05 +0200

    remove /Ob1 to keep /ob2, default with /o2

--- modules/common/flags.sh
+++ modules/common/flags.sh
@@ -5,7 +5,8 @@ do
 done
 for i in `/usr/bin/find $2 -type f -name "flags.make" 2>/dev/null` 
 do
-  sed -i 's/ \/O2 / \/O2 \/GL \/MD \/Zi \/MP'$3' '$1' /g' $i
+  sed -i 's/ \/O2 \/Ob1 / \/O2 \/GL \/MD \/Zi \/MP'$3' '$1' /g' $i
+  # result /DWIN32 /D_WINDOWS /w /MD /Zi /O2 /GL /MD /Zi /MP16 /arch:AVX /DNDEBUG
   sed -i 's/RC_FLAGS = /RC_FLAGS = \/nologo /g' $i
   sed -i -E 's/\/W[0-4]/\/w/g' $i
 done
