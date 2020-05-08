ddba3fd04ad411f35b4f3399c773db9b5af995df
nono303 <nono303@nono303.net>
2020-05-08 18:50:39 +0200

    correct cl /c arg

--- modules/php-cgi-spawner.bat
+++ modules/php-cgi-spawner.bat
@@ -1,6 +1,6 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 cd /D %PATH_SRC%\%1\src
-cl /nologo /O2 /GL /MD /Zi /w %AVX% php-cgi-spawner.c
+cl /c /nologo /O2 /GL /MD /Zi /w %AVX% php-cgi-spawner.c
 rc -nologo -r php-cgi-spawner.rc
 link /nologo /debug /LTCG /OPT:ICF,REF /PDB:php-cgi-spawner.pdb php-cgi-spawner.obj php-cgi-spawner.res /subsystem:windows /MERGE:.rdata=.text 
 call do_php %PATH_UTILS%\sub\bininfo.php %PATH_SRC%\%1\src\php-cgi-spawner.exe null checkavx
