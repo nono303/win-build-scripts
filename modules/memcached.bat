83f1636ed7f2c73108148b93a708de44e62cf248
nono303 <nono303@nono303.net>
2020-05-15 19:34:06 +0200

    update version & bininfo

--- modules/memcached.bat
+++ modules/memcached.bat
@@ -1,8 +1,5 @@
 @echo off
 for %%X in (32 64) do (call %PATH_ROOT_CYGWIN%%%X\bin\bash --login -c '%CYGPATH_MODULES%/memcached.sh %CYGPATH_SRC%/memcached %CYGPATH_MODULES% %PATH_RELEASE_MEMCACHED%')
 for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -m %PATH_RELEASE_MEMCACHED%`) do (set WINPATH_RELEASE_MEMCACHED=%%F)
-	REM version
-CD /D %PATH_SRC%\%1 
-for /F "tokens=* USEBACKQ" %%F in (`git describe --tags`) do (set VERSION=%%F)
-for /f "tokens=*" %%G in ('dir "%WINPATH_RELEASE_MEMCACHED%\*.exe" /s/b') do (call %PATH_MODULES_COMMON%\version.bat %%G "%VERSION%")
-call do_php %PATH_UTILS%\sub\bininfo.php %WINPATH_RELEASE_MEMCACHED% null recurse
+for /f "tokens=*" %%G in ('dir "%WINPATH_RELEASE_MEMCACHED%\*.exe" /s/b') do (call do_php %PATH_UTILS%\sub\version.php %1 %%G norpdb)
+call do_php %PATH_UTILS%\sub\bininfo.php %WINPATH_RELEASE_MEMCACHED% null recurse checkavx updaterc ext:exe
