b4919287fa13e89c03439d1f42eab9014aedbe3d
nono303 <nono303@nono303.net>
2020-05-15 19:29:10 +0200

    change scm

--- modules/libpng.bat
+++ modules/libpng.bat
@@ -1,27 +1,27 @@
 call %PATH_MODULES_COMMON%\init.bat %1
-set VCDIR=projects/vstudio2017
 
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR%" %AVXVCX% %PTFTS% %WKITVER%
+set VCDIR=projects\vstudio\nono
+mkdir %PATH_SRC%\%1\%VCDIR%
+	REM adapted from https://github.com/winlibs/libpng
+xcopy /C /F /Y %PATH_MODULES%\%1.vcxproj %PATH_SRC%\%1\%VCDIR%\*
+xcopy /C /F /Y %PATH_MODULES%\%1.sln %PATH_SRC%\%1\%VCDIR%\*
 
-if not exist %PATH_SRC%\%1\%VCDIR%\zlib-1.2.11\. mklink /J "%PATH_SRC%\%1\%VCDIR%\zlib-1.2.11" %PATH_SRC%\zlib
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR:\=/%" %AVXVCX% %PTFTS% %WKITVER%
 
-	REM update src...
-cd /D %PATH_BUILD%
-set LIBVER=1.6.37
-set LIBNAME=libpng-%LIBVER%
-if not exist %PATH_BUILD%\%LIBNAME%.tar.gz (
-	wget http://sourceforge.mirrorservice.org/l/li/libpng/libpng16/%LIBVER%/%LIBNAME%.tar.gz
-)
-tar xvf %LIBNAME%.tar.gz
-xcopy /C /F /Y %PATH_BUILD%\%LIBNAME%\*.* %PATH_SRC%\%1\*
-rmdir /S /Q %PATH_BUILD%\%LIBNAME%
+MSBuild.exe %PATH_SRC%\%1\projects\vstudio\vstudio.sln ^
+%MSBUILD_OPTS% ^
+/t:pnglibconf ^
+/p:Configuration=Release ^
+/p:Platform="Win32"
 
 for %%C in ("Release" "Release Library") do (
-	MSBuild.exe %PATH_SRC%\%1\%VCDIR%\vstudio.sln ^
+	MSBuild.exe %PATH_SRC%\%1\%VCDIR%\%1.sln ^
 	%MSBUILD_OPTS% ^
 	/nowarn:MSB8012 ^
 	/t:Clean,libpng ^
 	/p:Configuration=%%C ^
+	/p:ZLibSrcDir=C:\sdk\src\zlib ^
+	/p:ZLibLib=%PATH_INSTALL%\lib\zlib.lib ^
 	/p:Platform="%archmsbuild%"
 )
 
@@ -31,6 +31,3 @@ if %ARCH% == x64 (set INTER_DIR=%archmsbuild%\)
 for %%X in (lib pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\%VCDIR%\%INTER_DIR%Release Library\libpng_a.%%X" %PATH_INSTALL%\lib\*)
 for %%X in (dll pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\%VCDIR%\%INTER_DIR%Release\libpng.%%X" %PATH_INSTALL%\bin\*)
 for %%X in (pnglibconf png pngconf) do (xcopy /C /F /Y %PATH_SRC%\%1\%%X.h %PATH_INSTALL%\include\*)
-
-	REM version
-call %PATH_MODULES_COMMON%\version.bat %PATH_INSTALL%\bin\libpng.dll "%LIBVER%"
--- modules/libxpm.bat
+++ modules/libxpm.bat
@@ -1,27 +1,9 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
-set VCDIR=windows/vc15
+	REM copied from https://github.com/winlibs/libxpm
+call %BIN_SEVENZ% x -bb -o"%PATH_SRC%\%1\" -y %PATH_MODULES%\%1.zip
 
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR%" %AVXVCX% %PTFTS% %WKITVER%
-
-	REM update src...
-cd /D %PATH_BUILD%
-set LIBVER=3.5.13
-set LIBNAME=libXpm-%LIBVER%
-if not exist %PATH_BUILD%\%LIBNAME%.tar.gz (
-	wget https://www.x.org/releases/individual/lib/%LIBNAME%.tar.gz
-)
-tar xvf %LIBNAME%.tar.gz
-xcopy /E /C /F /Y %PATH_BUILD%\%LIBNAME%\src %PATH_SRC%\%1\src\
-rmdir /S /Q %PATH_BUILD%\%LIBNAME%
-
-	REM version
-xcopy /C /F /Y %PATH_MODULES%\%1_version.h %PATH_SRC%\%1\%VCDIR:/=\%\version.h*
-xcopy /C /F /Y %PATH_MODULES_COMMON%\version.rc %PATH_SRC%\%1\%VCDIR:/=\%\version.rc*
-sed -i 's/^<\/Project^>/^<ItemGroup^>^<ResourceCompile Include="version.rc" \/^>^<\/ItemGroup^>^<\/Project^>/g' %CYGPATH_SRC%/%1/%VCDIR%/libxpm.vcxproj
-sed -i -E 's/Static Release(.)Win32(.)"^>/Static Release\1Win32\2"^>\r\n    ^<Midl^>\r\n      ^<TargetEnvironment^>X86^<\/TargetEnvironment^>\r\n    ^<\/Midl^>/g' %CYGPATH_SRC%/%1/%VCDIR%/libxpm.vcxproj
-	REM version (in sh cause &gt &lt...) https://www.zachburlingame.com/2011/02/versioning-a-native-cc-binary-with-visual-studio/
-	REM "Static Release" win32 : LINK : warning LNK4068: /MACHINE not specified; defaulting to X86 [C:\sdk\src\libxpm\windows\vc15\libxpm.vcxproj]
-sed -i 's/^<\/Lib^>/^<TargetMachine^>Machine%ARCH%^<\/TargetMachine^>^<\/Lib^>/g' %CYGPATH_SRC%/%1/%VCDIR%/libxpm.vcxproj
+set VCDIR=windows\nono
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR:\=/%" %AVXVCX% %PTFTS% %WKITVER%
 
 for %%C in ("Static Release" "DLL Release") do (
 	MSBuild.exe %PATH_SRC%\%1\%VCDIR%\libxpm.sln ^
@@ -36,15 +18,5 @@ for %%X in (lib pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\windows\builds\%archmsbui
 for %%X in (dll pdb) do (xcopy /C /F /Y "%PATH_SRC%\%1\windows\builds\%archmsbuild%\DLL Release\libxpm.%%X" %PATH_INSTALL%\bin\)
 
 if not exist %PATH_INSTALL%\include\X11\. mkdir %PATH_INSTALL%\include\X11
-	REM get xlib include
-cd /D %PATH_BUILD%
-set LIBVER=3.5.12-5
-set LIBNAME=libxpm-%LIBVER%-vs16-x64
-if not exist %PATH_BUILD%\%LIBNAME%.zip (
-	wget https://windows.php.net/downloadS/php-sdk/deps/vs16/x64/%LIBNAME%.zip
-)
-call %BIN_SEVENZ% x -o"%LIBNAME%" -y %LIBNAME%.zip
-xcopy /E /C /F /Y %PATH_BUILD%\%LIBNAME%\include\X11 %PATH_INSTALL%\include\X11\
-rmdir /S /Q %PATH_BUILD%\%LIBNAME%
 xcopy /E /C /F /Y %PATH_SRC%\%1\include\X11 %PATH_INSTALL%\include\X11\
-if not exist %PATH_INSTALL%\include\xpm.h mklink /h %PATH_INSTALL%\include\xpm.h %PATH_INSTALL%\include\X11\xpm.h
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libxpm.dll
