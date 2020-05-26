a94de0640721628b032394476286cc8cb2c4983c
nono303 <nono303@nono303.net>
2020-05-26 18:35:05 +0200

    change svn path for avx-version

--- env_sample.bat
+++ env_sample.bat
@@ -44,7 +44,7 @@ set PATH_BIN_NASM=%PATH_SOFTS%\nasm
 set PATH_BIN_PERL=%PATH_SOFTS%\perl\bin
 set PATH_BIN_PERL_SITE=%PATH_SOFTS%\perl\site\bin
 	REM svn for mobac https://github.com/nono303/win-svn
-set PATH_BIN_SVN=%PATH_RELEASE_SVN%\vs16\x64;D:\github\NONO_subversion\vs16\x64\deps
+set PATH_BIN_SVN=%PATH_RELEASE_SVN%\vs16\x64-avx;D:\github\NONO_subversion\vs16\x64-avx\deps
 	REM for version patch if not present at build - https://github.com/pavel-a/ddverpatch (old https://www.nuget.org/packages/verpatch/1.0.14 / https://www.codeproject.com/Articles/37133/Simple-Version-Resource-Tool-for-Windows)
 set BIN_VERPATCH=%PATH_SOFTS%\verpatch.exe
 	REM for testing matching pdb with exe and dll http://www.debuginfo.com/tools/chkmatch.html
