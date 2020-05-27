0076f5c5c96dbd511fe8abc8d0fb7e720045aa0d
nono303 <nono303@nono303.net>
2020-05-27 17:53:55 +0200

    r2473

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -1,6 +1,6 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 	REM patch revision
-FOR /F "tokens=* USEBACKQ" %%F in (`svn log -r HEAD ^| grep -o -E "^r[0-9]{1,}" ^| sed 's/r//g`) do (set MOBAC_REVISION=%%F)
+FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set MOBAC_REVISION=%%F)
 sed -i -E 's/Revision: [0-9]+/Revision: %MOBAC_REVISION%/g' %CYGPATH_SRC%/%1/src/main/resources/mobac/mobac.properties
 call "%ANT_HOME%\bin\ant.bat" ^
 	clean_workspace, ^
