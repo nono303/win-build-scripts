323ec70850de8ce2d8d4812037a0c0673d57368a
nono303 <nono303@nono303.net>
2020-08-07 14:35:01 +0200

    add reference to sqlite patch

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -2,7 +2,7 @@
 	REM patch revision
 FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set MOBAC_REVISION=%%F)
 
-	REM sqlite update
+	REM sqlite update - https://bitbucket.org/xerial/sqlite-jdbc/downloads/
 xcopy /C /F /Y C:\sdk\batch\modules\sqlite-jdbc-3.30.1.jar C:\sdk\src\mobac\lib\sqlite-jdbc-3.30.1.jar*
 
 sed -i -E 's/Revision: [0-9]+/Revision: %MOBAC_REVISION%/g' %CYGPATH_SRC%/%1/src/main/resources/mobac/mobac.properties
