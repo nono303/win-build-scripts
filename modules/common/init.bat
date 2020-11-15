cae3afea40a267ae0d9aba1dd9579eda18f0e818
nono303 <nono303@nono303.net>
2020-11-15 14:18:05 +0100

    set SCM_VERSION find & SCM_VERSION patch after global patch

--- modules/common/init.bat
+++ modules/common/init.bat
@@ -13,7 +13,12 @@ if "%CUR_DEBUG%"=="1" (
 if exist %PATH_SRC%\%1\. (
 	cd %PATH_SRC%\%1
 	echo ^> %PATH_SRC%\%1
+	REM dirty !! https://stackoverflow.com/questions/9556676/batch-file-how-to-replace-equal-signs-and-a-string-variable
+	set SCM_VERSION=
+	setlocal enabledelayedexpansion
 	if exist %PATH_SRC%\%1\.git\. (
+		FOR /F "tokens=* USEBACKQ" %%F in (`git rev-parse --short HEAD`) do (set SCM_VERSION=%%F)
+		echo # %1 git commit:!SCM_VERSION!
 		if %ARG_KEEPSRC% == 0 (
 			git reset --hard
 			git clean -fdx
@@ -21,15 +26,25 @@ if exist %PATH_SRC%\%1\. (
 				echo # apply %1.patch
 				git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\%1.patch
 			)
+			if exist %PATH_MODULES%\%1.!SCM_VERSION!.patch (
+				echo # apply %1.!SCM_VERSION!.patch
+				git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\%1.!SCM_VERSION!.patch
+			)
 		)
 	)
 	if exist %PATH_SRC%\%1\.svn\. (
+		FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_VERSION=%%F)
+		echo # %1 svn revision:!SCM_VERSION!
 		if %ARG_KEEPSRC% == 0 (
 			svn revert . -R
 			svn cleanup . --remove-unversioned --remove-ignored
 			if exist %PATH_MODULES%\%1.patch (
 				echo # apply %1.patch
 				svn patch %PATH_MODULES%\%1.patch .
+			)
+			if exist %PATH_MODULES%\%1.!SCM_VERSION!.patch (
+				echo # apply %1.!SCM_VERSION!.patch
+				svn patch %PATH_MODULES%\%1.!SCM_VERSION!.patch .
 			)
 		)
 	)
