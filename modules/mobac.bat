c0d8f850b2efe0047ceccfb1e05246ecbc151f28
nono303 <nono303@nono303.net>
2020-12-21 18:46:48 +0100

    update sqlite for mobac

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -1,7 +1,8 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 
 	REM sqlite update - https://bitbucket.org/xerial/sqlite-jdbc/downloads/
-xcopy /C /F /Y C:\sdk\batch\modules\sqlite-jdbc-3.30.1.jar C:\sdk\src\mobac\lib\sqlite-jdbc-3.30.1.jar*
+xcopy /C /F /Y %PATH_MODULES%\sqlite-jdbc-3.34.0.jar %PATH_SRC%\%1\lib\sqlite-jdbc-3.34.0.jar*
+rm -f %PATH_SRC%\%1\lib\sqlite-jdbc-3.16.1.jar
 
 sed -i -E 's/Revision: [0-9]+/Revision: %SCM_VERSION%/g' %CYGPATH_SRC%/%1/src/main/resources/mobac/mobac.properties
 call "%ANT_HOME%\bin\ant.bat" ^
