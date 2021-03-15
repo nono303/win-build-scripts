1d5dcb648e5e9f618378ad45f8c17a0160a0edf1
nono303 <nono303@nono303.net>
2021-03-15 12:51:30 +0100

    bugfix php deps for 8.0 vc15 with locale list

--- modules/php-getdeps.php
+++ modules/php-getdeps.php
@@ -24,9 +24,9 @@
 	$msvc = $_ENV["MSVC_DEPS"];
 	// https://windows.php.net/downloadS/php-sdk/deps/series/
 	if($msvc == "vs16"){
-		$package = "master";
+		$pckver = "master";
 	} else {
-		$package = $_ENV["PHPVER"];
+		$pckver = $_ENV["PHPVER"];
 	}
 	$basedir = $_ENV["PATH_PHP_SDK"]."/phpmaster/".$msvc."/".$arch."/";
 	$outdir = $basedir."deps/";
@@ -35,47 +35,47 @@
 
 	$depsreq = array(
 						// 2020-04-22
-		// "apache",	//2.4.39
+		// "apache",
 		"c-client",		//2007f
-		"fbclient",		//3.0
-		// "freetype",	//2.9.1-1
+		"fbclient",		//3.0.6
+		// "freetype",
 		"glib",			//2.53.3
-		"ICU",			//66.1
+		"ICU",			//68.1
 		"libargon2",	//20190702
-		// "libbzip2",	//1.0.8
-		// "libcurl",	//7.69.1
-		"libenchant",	//1.6.1
-		// "libffi",		//3.3-1
-		// "libiconv",	//1.16-2
-		"libintl",		//0.18.3-5
-		// "libjpeg",	//9c
-		"liblmdb",		//0.9.22-3
-		"libonig",		//6.9.5
-		// "libpng",	//1.6.34-4
+		// "libbzip2",
+		// "libcurl",
+		"libenchant",	//2.2.8
+		// "libffi",
+		// "libiconv",
+		"libintl",		//0.18.3-6
+		// "libjpeg",
+		"liblmdb",		//0.9.22-4
+		"libonig",		//6.9.6
+		// "libpng",
 		"libpq",		//11.4
 		"libqdbm",		//1.8.78
 		"libsasl",		//2.1.27-2
-		"libsodium",	//1.0.17
-		// "libssh2",	//1.9.0
-		// "libtidy",	//5.6.0-2
+		"libsodium",	//1.0.18
+		// "libssh2",
+		// "libtidy",
 		"libwebp",		//1.1.0
-		// "libxml2",	//2.9.10
-		// "libxpm",	//3.5.12-5
-		"libxslt",		//1.1.32
-		// "libzip",		//1.5.2
+		// "libxml2",
+		// "libxpm",
+		"libxslt",		//1.1.34
+		// "libzip",
 		"mpir",			//3.0.0
 		"net-snmp",		//5.7.3-1
-		// "nghttp2",	//1.40.0
+		// "nghttp2",
 		"openldap",		//2.4.47
-		// "openssl",	//1.1.1e
-		// "sqlite3",	//3.31.1
-		// "wineditline",	//2.205-1
-		// "zlib",		//1.2.11
+		// "openssl",
+		// "sqlite3",
+		// "wineditline",
+		// "zlib",
 	);
 
 	$urlbase = "https://windows.php.net/downloadS/php-sdk/deps/";
 	$repdl = $urlbase.$msvc."/".$arch."/";
-	$package = "packages-".$package."-".$msvc."-".$arch."-".$type.".txt";
+	$package = "packages-".$pckver."-".$msvc."-".$arch."-".$type.".txt";
 	$serie = $urlbase."series/".$package;
 	$stampfile = "lastupate.txt";
 
@@ -90,25 +90,32 @@
 		CURLOPT_SSL_VERIFYHOST	=> 0,
 		CURLOPT_FOLLOWLOCATION	=> 1,
 		CURLINFO_HEADER_OUT		=> 0,
-		CURLOPT_FAILONERROR		=> 1,
+		CURLOPT_FAILONERROR		=> 0,
 		CURLOPT_PRIVATE			=> 1,
 		CURLOPT_FILETIME		=> 1,
 	);
 	curl_setopt_array($ch,$opts);
-	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-	curl_setopt($ch, CURLOPT_URL, $serie);
-	$data = curl_exec ($ch);
-	$error = curl_error($ch); 
-	if(is_array($error)){
-		print_r($error);
-		echo "Unbale to retrieve '".$serie."' > aborting!".PHP_EOL;
-		exit(-1);
-	}
-	echo "retrieve '".$serie."' > ok".PHP_EOL;
-	$timestamp = curl_getinfo($ch, CURLINFO_FILETIME);
-	if ($timestamp == -1) {
-		echo "Unbale to retrieve datetime of '".$serie."' > aborting!".PHP_EOL;
-		exit(-1);
+	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0');
+
+	if($pckver == "8.0" && $msvc == "vc15"){
+		$data = file_get_contents(dirname(__FILE__)."/".$package);
+		$timestamp = filemtime(dirname(__FILE__)."/".$package);
+	} else {
+		curl_setopt($ch, CURLOPT_URL, $serie);
+		
+		$data = curl_exec ($ch);
+		$cgi = curl_getinfo($ch);
+		$error = curl_error($ch); 
+		if(is_array($error) || $cgi["http_code"] != "200"){
+			echo "Unbale to retrieve '".$serie."' > aborting!".PHP_EOL.print_r($cgi,true).PHP_EOL.print_r($error,true);
+			exit(1);
+		}
+		echo "retrieve '".$serie."' > ok".PHP_EOL;
+		$timestamp = curl_getinfo($ch, CURLINFO_FILETIME);
+		if ($timestamp == -1) {
+			echo "Unbale to retrieve datetime of '".$serie."' > aborting!".PHP_EOL;
+			exit(1);
+		}
 	}
 	if(is_file($basedir.$stampfile)){
 		$lastupate = intval(trim(file_get_contents($basedir.$stampfile)));
@@ -132,14 +139,14 @@
 	}
 	if(!mkdir($outdir)){
 		echo "Unbale to create '".$outdir."' > aborting!".PHP_EOL;
-		exit(-1);
+		exit(1);
 	} else {
 		echo "creating '".$outdir."'".PHP_EOL;
 	}
 	if(!is_dir($zipdir)){
 		if(!mkdir($zipdir)){
 			echo "Unbale to create '".$zipdir."' > aborting!".PHP_EOL;
-			exit(-1);
+			exit(1);
 		} else {
 			echo "creating '".$zipdir."'".PHP_EOL;
 		}
@@ -157,6 +164,7 @@
 	};
 	curl_setopt_array($ch, $opts);
 	foreach(explode("\n",$data) as $dep){
+		$dep = trim($dep);
 		foreach($depsreq as  $depreq){
 			if(is_int(strpos($dep,$depreq))){
 				$destination = $zipdir.$dep;
