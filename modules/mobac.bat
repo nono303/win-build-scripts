8f80af1df3453f4f18ab498c90becfaffed9684a
nono303 <nono303@nono303.net>
2021-03-24 11:16:37 +0100

    clean build dir

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -1,4 +1,5 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
+if exist %PATH_SRC%\%1\mobac\build\. rmdir /S /Q %PATH_SRC%\%1\mobac\build
 	REM https://docs.gradle.org/current/userguide/command_line_interface.html
 call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% mobac:jar 2>&1
 FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_VERSION=%%F)
