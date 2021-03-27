6ab341d4e1d69da96d1b4ccd4a7a1631456c3675
nono303 <nono303@nono303.net>
2021-03-27 18:15:24 +0100

    add proj and lintiff (deps)

---
+++ modules/proj.bat
@@ -0,0 +1,27 @@
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
+
+set CURL_VER=openssl
+if not exist %PATH_INSTALL%\proj\. mkdir %PATH_INSTALL%\proj
+
+cmake ^
+%CMAKE_OPTS% ^
+-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL%\proj ^
+-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+-DBUILD_SHARED_LIBS=ON ^
+-DCURL_LIBRARY=%SLASHPATH_INSTALL%/curl/%CURL_VER%/lib/libcurl_imp.lib ^
+-DCURL_INCLUDE_DIR=%SLASHPATH_INSTALL%/curl/%CURL_VER%/include ^
+-DSQLITE3_INCLUDE_DIR=%SLASHPATH_INSTALL%/include ^
+-DSQLITE3_LIBRARY=%SLASHPATH_INSTALL%/lib/sqlite3.lib ^
+-DSQLITE3_BINARY=%SLASHPATH_INSTALL%/bin ^
+-DTIFF_LIBRARY=%SLASHPATH_INSTALL%/lib/tiff.lib ^
+-DTIFF_INCLUDE_DIR=%SLASHPATH_INSTALL%/include ^
+-DBUILD_TESTING=OFF ^
+-DENABLE_IPO=ON ^
+-DUSE_THREAD=ON ^
+%PATH_SRC%\%1
+
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/flags.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+nmake %NMAKE_OPTS% clean install
+
+xcopy /C /F /Y %PATH_INSTALL%\%1\bin\*.* B:\serveur\_gis\proj8\apps\*
+xcopy /C /F /Y %PATH_INSTALL%\%1\share\proj\*.* B:\serveur\_gis\proj8\share\*
