d1473064407235fc0d444a6db8019b8a17f9babc
nono303 <nono303@nono303.net>
2021-04-22 09:53:35 +0200

    copy jar to exec dir

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -4,3 +4,4 @@ if exist %PATH_SRC%\%1\mobac\build\. rmdir /S /Q %PATH_SRC%\%1\mobac\build
 call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% mobac:jar 2>&1
 FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_VERSION=%%F)
 move /Y %PATH_SRC%\%1\mobac\build\libs\Mobile_Atlas_Creator.jar %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_VERSION%.jar
+xcopy /C /F /Y %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_VERSION%.jar C:\PROGRA~2\mobac\*
