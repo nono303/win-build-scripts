b709d893b4870dd7323a949d6f245a540312f5dd
nono303 <nono303@nono303.net>
2021-07-22 19:49:43 +0200

    disable FindHarfBuzz to avoid useless warning message

--- modules/freetype2.bat
+++ modules/freetype2.bat
@@ -1,5 +1,11 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
+REM	CMake Warning at builds/cmake/FindHarfBuzz.cmake:99 (message):
+REM	  Required version (2.0.0) is higher than found version ()
+REM	Call Stack (most recent call first):
+REM	  CMakeLists.txt:215 (find_package)
+sed -i 's/  find_package(HarfBuzz/# find_package(HarfBuzz/g' %CYGPATH_SRC%/%1/CMakeLists.txt
+
 for %%S in (OFF ON) do (
 	cmake %CMAKE_OPTS% ^
 	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
@@ -12,7 +18,6 @@ for %%S in (OFF ON) do (
 	-DFT_WITH_HARFBUZZ=OFF ^
 	-DFT_WITH_BROTLI=ON ^
 	%PATH_SRC%\%1
-
 		REM LNK4098: defaultlib 'MSVCRT' conflicts with use of other libs; use /NODEFAULTLIB:library
 	REM sed -i 's/LTCG/LTCG \/NODEFAULTLIB:libcmt.lib/g' %PATH_BUILD%/%1/CMakeFiles/freetype.dir/build.make
 	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
