bf7bb17a342912bec8565f36876eb110991abea4
nono303 <nono303@nono303.net>
2021-07-22 19:51:12 +0200

    nmake > ninja

--- modules/httpd.bat
+++ modules/httpd.bat
@@ -7,7 +7,6 @@ for %%M in (mod_maxminddb mod_fcgid mod_h2 mod_md mod_wku_bt mod_h264_streaming)
 set CURL_VER=openssl
 for %%X in (dll exe pdb) do (xcopy /C /F /Y %PATH_INSTALL%\%FOLDER_RELEASE_CURL%\%CURL_VER%\bin\*.%%X %PATH_INSTALL%\bin\*)
 
-	REM ~~~~~~~~~~~~ cmake / nmake
 call %PATH_MODULES_COMMON%\init.bat %1 cmake
 cmake %CMAKE_OPTS% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
@@ -31,8 +30,16 @@ cmake %CMAKE_OPTS% ^
 -DEXTRA_INCLUDES=%PATH_SRC%/openssl ^
 %PATH_SRC%\httpd 
 
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES%/httpd_flags.sh %CYGV% %CYGPATH_BUILD% "%NUMBER_OF_PROCESSORS%" "%AVXSED%"
-nmake %NMAKE_OPTS% install
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+REM mod_wku-bt
+sed -i 's/DBIN_NAME=mod_backtrace.so/DBIN_NAME=mod_backtrace.so -DDIAG_BITS_%CYGV%/g' %CYGPATH_BUILD%/%1/build.ninja
+sed -i 's/DBIN_NAME=mod_whatkilledus.so/DBIN_NAME=mod_whatkilledus.so -DDIAG_BITS_%CYGV%/g' %CYGPATH_BUILD%/%1/build.ninja
+REM \/DBUILDING_H264_STREAMING : http://h264.code-shop.com/trac#H264StreamingModule
+sed -i 's/DBIN_NAME=mod_h264_streaming.so/DBIN_NAME=mod_h264_streaming.so -DBUILDING_H264_STREAMING/g' %CYGPATH_BUILD%/%1/build.ninja
+REM C:\sdk\src\httpd\support\win32\ApacheMonitor.rc(21) : warning RC4005: 'LONG_NAME' : redefinition
+sed -i 's/-DLONG_NAME=ApacheMonitor //g' %CYGPATH_BUILD%/%1/build.ninja
+%NINJA% install
+
 	REM Targeting for Windows 10
 mt.exe -manifest %PATH_MODULES%\httpd.exe.manifest -outputresource:%PATH_INSTALL%\bin\httpd.exe;1
 
--- modules/nghttp2.bat
+++ modules/nghttp2.bat
@@ -2,15 +2,18 @@
 
 sed -i 's/_EVENT_/EVENT__/g' %CYGPATH_SRC%/%1/cmake/FindLibevent.cmake
 
-	REM YES : static / NO : shared
-for %%S in (YES NO) do (
-	cmake %CMAKE_OPTS% ^
+for %%C in ("-DENABLE_STATIC_LIB=YES -DENABLE_SHARED_LIB=NO" "-DENABLE_STATIC_LIB=NO -DENABLE_SHARED_LIB=YES") do (
+		REM dirty !! https://stackoverflow.com/questions/9556676/batch-file-how-to-replace-equal-signs-and-a-string-variable
+	set CUR=%%C
+	setlocal enabledelayedexpansion
+	set new=!CUR:"=!
+
+	cmake -Wno-dev %CMAKE_OPTS% ^
 	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
 	-DENABLE_LIB_ONLY=ON ^
 	-DENABLE_PYTHON_BINDINGS=OFF ^
 	-DENABLE_EXAMPLES=OFF ^
-	-DENABLE_STATIC_LIB=%%S ^
 	-DLIBCARES_LIBRARY=%SLASHPATH_INSTALL%/lib/cares_static.lib ^
 	-DLIBCARES_INCLUDE_DIR=%SLASHPATH_INSTALL%/include ^
 	-DLIBEV_LIBRARY=%SLASHPATH_INSTALL%/lib/libev_static.lib ^
@@ -31,13 +34,13 @@ for %%S in (YES NO) do (
 	-DWITH_MRUBY=OFF ^
 	-DWITH_NEVERBLEED=OFF ^
 	-DCYTHON_EXECUTABLE=%PATH_BIN_PYTHON%\Scripts ^
+	!new! ^
 	%PATH_SRC%\%1
 
-	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/flags.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-	if %%S == YES sed -i 's/out:nghttp2.lib/LTCG \/out:nghttp2.lib/g' %CYGPATH_BUILD%/%1/lib/CMakeFiles/nghttp2_static.dir/build.make
-	
-	nmake %NMAKE_OPTS% clean install
-	if %%S == YES (
+	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+	%NINJA% install
+
+	if %%C =="-DENABLE_STATIC_LIB=YES -DENABLE_SHARED_LIB=NO" (
 		xcopy /C /F /Y %PATH_BUILD%\%1\lib\CMakeFiles\nghttp2_static.dir\nghttp2_static.pdb %PATH_INSTALL%\lib\*
 		move /Y %PATH_INSTALL%\lib\nghttp2.lib %PATH_INSTALL%\lib\nghttp2_static.lib
 	) else (
