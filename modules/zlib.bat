9570e508f56307b3c157e82c884404cc7193428f
nono303 <nono303@nono303.net>
2021-07-22 19:50:21 +0200

    no change

--- modules/zlib.bat
+++ modules/zlib.bat
@@ -2,8 +2,8 @@
 
 	REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
 sed -i 's/-O2 -GL -MD/-O2 -GL -MD -MP%NUMBER_OF_PROCESSORS% %AVXMSC%/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
-if %ARCH% == x86 (nmake %NMAKE_OPTS% -f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj")
-if %ARCH% == x64 (nmake %NMAKE_OPTS% -f win32/Makefile.msc LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" AS=ml64)
+if %ARCH% == x86 (nmake %NMAKE_OPTS% /f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj")
+if %ARCH% == x64 (nmake %NMAKE_OPTS% /f win32/Makefile.msc LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" AS=ml64)
 
 for %%E in (zlib.lib zlib.pdb zdll.lib) do (xcopy /C /F /Y %PATH_SRC%\%1\%%E %PATH_INSTALL%\lib\*)
 for %%E in (zlib1.dll zlib1.pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%%E %PATH_INSTALL%\bin\*)
