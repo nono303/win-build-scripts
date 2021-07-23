06d3b49602cc827a62c15b8e2cd7925d7d5625d1
nono303 <nono303@nono303.net>
2021-07-23 08:19:41 +0200

    disable loop bugfix LTCG for x64

--- modules/common/ninja.sh
+++ modules/common/ninja.sh
@@ -1,17 +1,14 @@
-for i in `/usr/bin/find $2 -type f -name "build.ninja" 2>/dev/null` 
-do
-  # LINK FLAGS (dll exe)
-  sed -i 's/LINK_FLAGS = \/machine:X86 \/debug \/INCREMENTAL/LINK_FLAGS = \/LTCG \/OPT:REF,ICF \/DEBUG \/machine:X86/g' $i
-  # LINK FLAGS (lib)
-  sed -i 's/LINK_FLAGS = \/machine:X86/LINK_FLAGS = \/LTCG \/machine:X86/g' $i
-  sed -i 's/\/INCREMENTAL //g' $i
-  # RC FLAGS
-  sed -i 's/FLAGS = -DWIN32/FLAGS = -DWIN32 -nologo/g' $i
-  # C & C++ FLAGS
-    # warn
-  sed -i -E 's/[\/-]W[0-4]/\/w/g' $i
-    # opti
-  sed -i 's/ \/O2 \/Ob1 / \/O2 \/GL \/MD \/Zi \/MP'$3' '$1' /g' $i
-	# found		/DWIN32 /D_WINDOWS /W4 /MD /Zi /O2 /Ob1 /DNDEBUG
-	# result	/DWIN32 /D_WINDOWS /w /MD /Zi /O2 /GL /MD /Zi /MP16 /arch:AVX /DNDEBUG
-done
+# LINK FLAGS (dll exe)
+sed -i -E 's/LINK_FLAGS = \/machine:(...) \/debug \/INCREMENTAL/LINK_FLAGS = \/LTCG \/OPT:REF,ICF \/DEBUG \/machine:\1/g' $2/build.ninja
+# LINK FLAGS (lib)
+sed -i -E 's/LINK_FLAGS = \/machine:(...)/LINK_FLAGS = \/LTCG \/machine:\1/g' $2/build.ninja
+sed -i 's/\/INCREMENTAL //g' $2/build.ninja
+# RC FLAGS
+sed -i 's/FLAGS = -DWIN32/FLAGS = -DWIN32 -nologo/g' $2/build.ninja
+# C & C++ FLAGS
+# warn
+sed -i -E 's/[\/-]W[0-4]/\/w/g' $2/build.ninja
+# opti
+sed -i 's/ \/O2 \/Ob1 / \/O2 \/GL \/MD \/Zi \/MP'$3' '$1' /g' $2/build.ninja
+# found		/DWIN32 /D_WINDOWS /W4 /MD /Zi /O2 /Ob1 /DNDEBUG
+# result	/DWIN32 /D_WINDOWS /w /MD /Zi /O2 /GL /MD /Zi /MP16 /arch:AVX /DNDEBUG
