b954f9b8ea66313c3555e704f828b5e7288b0d96
nono303 <git@nono303.net>
2021-09-30 10:21:43 +0200

    src update

--- utils/srccreate.bat
+++ utils/srccreate.bat
@@ -142,11 +142,11 @@ svn update -r 2628
 cd /D ..
 svn co http://svn.apache.org/repos/asf/httpd/mod_fcgid/trunk mod_fcgid
 cd /D mod_fcgid
-svn update -r 1893727
+svn update -r 1893741
 cd /D ..
 git clone https://github.com/icing/mod_h2.git mod_h2
 cd /D mod_h2
-git checkout tags/v1.15.24
+git checkout tags/v2.0.0-rc4
 cd /D ..
 git clone https://github.com/traceypooh/mod_h264_streaming--intra-keyframes.git mod_h264_streaming
 cd /D mod_h264_streaming
