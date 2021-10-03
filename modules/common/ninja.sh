7d8ab6b9a43890d2c3393b7d4bc701f9175b558a
nono303 <git@nono303.net>
2021-10-03 16:46:50 +0200

    remove duplicate /Zi

--- modules/common/ninja.sh
+++ modules/common/ninja.sh
@@ -9,6 +9,6 @@ sed -i 's/FLAGS = -DWIN32/FLAGS = -DWIN32 -nologo/g' $2/build.ninja
 # warn
 sed -i -E 's/[\/-]W[0-4]/\/w/g' $2/build.ninja
 # opti
-sed -i 's/ \/O2 \/Ob1 / \/O2 \/GL \/MD \/Zi \/MP'$3' '$1' /g' $2/build.ninja
+sed -i 's/ \/O2 \/Ob1 / \/O2 \/GL \/MD \/MP'$3' '$1' /g' $2/build.ninja
 # found		/DWIN32 /D_WINDOWS /W4 /MD /Zi /O2 /Ob1 /DNDEBUG
-# result	/DWIN32 /D_WINDOWS /w /MD /Zi /O2 /GL /MD /Zi /MP16 /arch:AVX /DNDEBUG
+# result	/DWIN32 /D_WINDOWS /w /MD /Zi /O2 /GL /MD /MP16 /arch:AVX /DNDEBUG
