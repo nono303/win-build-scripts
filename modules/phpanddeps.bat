83e5085daa78a8e6a397edbbe907e92ce528d938
nono303 <git@nono303.net>
2021-10-03 16:50:01 +0200

    jpeg > libjpeg-turbo

--- modules/phpanddeps.bat
+++ modules/phpanddeps.bat
@@ -3,7 +3,7 @@ call %PATH_BATCH%\go.bat liblzma NOLOG
 call %PATH_BATCH%\go.bat libzip NOLOG
 call %PATH_BATCH%\go.bat libxpm NOLOG
 call %PATH_BATCH%\go.bat libpng NOLOG
-call %PATH_BATCH%\go.bat jpeg NOLOG
+call %PATH_BATCH%\go.bat libjpeg-turbo NOLOG
 call %PATH_BATCH%\go.bat tidy NOLOG
 call %PATH_BATCH%\go.bat libxdiff NOLOG
 call %PATH_BATCH%\go.bat freetype2 NOLOG
