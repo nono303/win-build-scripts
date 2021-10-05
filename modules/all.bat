be0b239675db433ee00ff36a5206a2619f7601ea
nono303 <git@nono303.net>
2021-10-05 14:48:15 +0200

    refactor usage & remove unused comment in all.bat

--- modules/all.bat
+++ modules/all.bat
@@ -3,12 +3,5 @@ call %PATH_BATCH%\go.bat httpdanddeps NOLOG
 call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat phpanddeps NOLOG
 call %PATH_BATCH%\go.bat projgeolibanddeps NOLOG
-REM call %PATH_BATCH%\go.bat verpatch NOLOG
-REM call %PATH_BATCH%\go.bat sdlna NOLOG
-REM call %PATH_BATCH%\go.bat dependencies NOLOG
-REM call %PATH_BATCH%\go.bat mobac NOLOG
-REM call %PATH_BATCH%\go.bat mapsforgesrv NOLOG
-REM call %PATH_BATCH%\go.bat nssm NOLOG
-REM call %PATH_BATCH%\go.bat memcached NOLOG
 call %PATH_BATCH%\go.bat bininfo checkavx updaterc 
 if exist %PATH_BUILD%\. rmdir /S /Q %PATH_BUILD%\
