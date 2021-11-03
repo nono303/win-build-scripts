006104b355fc3934b829d3eb0449408df5231e22
nono303 <git@nono303.net>
2021-11-03 14:12:12 +0100

    proj 8.2 : 16 > 20

--- utils/bininfo.bat
+++ utils/bininfo.bat
@@ -8,6 +8,6 @@ if exist %2\. (
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB% %BININFO_LOG:\=/% %2 %3 nb:61 %4 %5 %6 %7
 		REM !! TMP (nts ts)
 	for %%E in (nts) do (call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_php-%%E %BININFO_LOG:\=/% %2 %3 nb:30 %4 %5 %6 %7)
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_proj\bin %BININFO_LOG:\=/% %2 %3 nb:16 %4 %5 %6 %7
+	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_proj\bin %BININFO_LOG:\=/% %2 %3 nb:20 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_geographiclib\bin %BININFO_LOG:\=/% %2 %3 nb:22 %4 %5 %6 %7
 )
