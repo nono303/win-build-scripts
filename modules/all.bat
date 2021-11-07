19c9244ef418ba5b6b9f505cb92c21f00bcd2394
nono303 <git@nono303.net>
2021-11-07 17:53:34 +0100

    do clean on httpdanddeps.bat

--- modules/all.bat
+++ modules/all.bat
@@ -1,4 +1,3 @@
-call %PATH_BATCH%\go.bat clean NOLOG
 call %PATH_BATCH%\go.bat httpdanddeps NOLOG
 call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat phpanddeps NOLOG
--- modules/httpdanddeps.bat
+++ modules/httpdanddeps.bat
@@ -1,3 +1,4 @@
+call %PATH_BATCH%\go.bat clean NOLOG
 call %PATH_BATCH%\go.bat brotli NOLOG
 call %PATH_BATCH%\go.bat jansson NOLOG
 call %PATH_BATCH%\go.bat bzip2 NOLOG
