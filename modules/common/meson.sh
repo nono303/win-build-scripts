0f33b4a067761b343c3aba4e59753dec1a49216a
nono303 <git@nono303.net>
2021-11-16 18:57:41 +0100

    fix !avx opts

--- modules/common/meson.sh
+++ modules/common/meson.sh
@@ -1,4 +1,3 @@
-
 # LINK FLAGS (dll exe)
 sed -i -E 's/release" "\/nologo/OPT:REF,ICF/g' $2/build.ninja
 # LINK FLAGS (lib)
@@ -7,9 +6,9 @@ sed -i -E 's/LINK_ARGS =/LINK_ARGS = "\/LTCG"/g' $2/build.ninja
 # warn
 sed -i -E 's/ "-wd[0-9]{4}"//g' $2/build.ninja
 sed -i 's/\/W3/\/w/g' $2/build.ninja
-# opti
-# https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+# add quote for AVX - https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
 [ ! -z "$1" ] && avx="\"$1\""
+# opti
 sed -i 's/ "\/Zi" / "\/Zi" "\/GL" "\/MP'$3'" '$avx' "\/DWIN32" "\/D_WINDOWS" "\/DNDEBUG" /g' $2/build.ninja
 # found		"/MD" "/nologo" "/showIncludes" "/W3" "/O2" "/Zi"
 # result	"/MD" "/nologo" "/showIncludes" "/W3" "/O2" "/Zi" "/GL" "/MP16" "/arch:AVX" "/DWIN32" "/D_WINDOWS" "/DNDEBUG"
