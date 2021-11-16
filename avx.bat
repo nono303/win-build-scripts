ca3f063b4c1299382c03ae69971488f394853eac
nono303 <git@nono303.net>
2021-11-16 10:22:06 +0100

    disable VS telemetry

--- avx.bat
+++ avx.bat
@@ -31,5 +31,12 @@ set PATH_BUILD=%PATH_BUILDROOT%\%MSVC_DEPS%_%ARCH%%AVXB%
 for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -u %PATH_BUILD%`) do (set CYGPATH_BUILD=%%F)
 set PATH_INSTALL=%PATH_RELEASE%\%MSVC_DEPS%_%ARCH%%AVXB%
 for %%s in (lib bin include) do (if not exist %PATH_INSTALL%\%%s\. mkdir %PATH_INSTALL%\%%s)
+
+	REM https://github.com/microsoft/vcpkg/issues/8756
+set __VSCMD_ARG_NO_LOGO=1
+set VSCMD_SKIP_SENDTELEMETRY=1
+	REM https://github.com/microsoft/vcpkg/blob/master/docs/users/config-environment.md
+set VCPKG_KEEP_ENV_VARS=VSCMD_SKIP_SENDTELEMETRY
+
 call %PATH_VS%\VC\Auxiliary\Build\vcvarsall.bat %ARCH% %WKITVER% -vcvars_ver=%vcvars_ver%
 @echo off
