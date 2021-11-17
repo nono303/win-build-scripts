84bea3d5f606ed613ca886c6bedc056123e4822b
nono303 <git@nono303.net>
2021-11-17 18:21:56 +0100

    refactor init and var from git with SCM_*

--- modules/php.bat
+++ modules/php.bat
@@ -1,15 +1,20 @@
-	REM ~~~~~~~~~~~~ init src
-for %%M in (php-geos php-src pecl-memcache pecl-text-xdiff php-ext-brotli xdebug php-sdk) do (call %PATH_MODULES_COMMON%\init.bat %%M)
+@echo off
+	REM ~~~~~~~~~~~~ current full build
+set PHP_FULLBUILD=8.1
 
 	REM revision & version for pecl_memcache
-cd /D %PATH_SRC%\pecl-memcache
+call %PATH_MODULES_COMMON%\init.bat pecl-memcache
+set PECLMEMCACHEGITCOMMIT=%SCM_VERSION%
+set PECLMEMCACHEGITBRANCH=%SCM_BRANCH%
+set PECLMEMCACHEGITDATE=%SCM_VERSION_DATE%
 FOR /F "tokens=* USEBACKQ" %%F in (`c:\sdk\softs\cyg64\bin\find -name 'memcache.c' -type f -exec dirname {} +`) do (set PECLMEMCACHECYGSRCDIR=%%F)
-FOR /F "tokens=* USEBACKQ" %%F in (`git rev-parse --short HEAD`) do (set PECLMEMCACHEGITCOMMIT=%%F)
-FOR /F "tokens=* USEBACKQ" %%F in (`git rev-parse --abbrev-ref HEAD`) do (set PECLMEMCACHEGITBRANCH=%%F)
-FOR /F "tokens=* USEBACKQ" %%F in (`git show -s --format^=%%cd --date=short %PECLMEMCACHEGITCOMMIT%`) do (set PECLMEMCACHEGITDATE=%%F)
 FOR /F "tokens=* USEBACKQ" %%F in (`grep PHP_MEMCACHE_VERSION %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/php_memcache.h ^| cut -d^'^"^' -f2`) do (set PECLMEMCACHEVERSION=%%F)
 FOR /F "tokens=* USEBACKQ" %%F in (`type C:\sdk\batch\modules\pecl-memcache.patch.version`) do (set PECLMEMCACHEPATCHVERSION=%%F)
 sed -i -E 's/, PHP_MEMCACHE_VERSION/, "%PECLMEMCACHEVERSION% | branch: %PECLMEMCACHEGITBRANCH% | commit: %PECLMEMCACHEGITCOMMIT%%PECLMEMCACHEPATCHVERSION% | date: %PECLMEMCACHEGITDATE% | https:\/\/github.com\/nono303\/PHP-memcache-dll"/g' %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/memcache.c
+
+	REM init sdk, pecl, src
+for %%M in (php-sdk php-geos pecl-text-xdiff php-ext-brotli xdebug php-src) do (call %PATH_MODULES_COMMON%\init.bat %%M)
+set PHPVER=%SCM_TAG:~4,3%
 
 	REM other way only first time 'phpsdk_buildtree phpmaster'
 if not exist %PATH_SRC%\php-sdk\phpmaster\. mklink /J %PATH_SRC%\php-sdk\phpmaster %PATH_SDK_ROOT%\phpmaster 
@@ -18,20 +23,12 @@ if not exist %PATH_SRC%\php-sdk\phpmaster\. mklink /J %PATH_SRC%\php-sdk\phpmast
 set LIB=
 set INCLUDE=
 
-	REM ~~~~~~~~~~~~ current full build
-set PHP_FULLBUILD=8.1
-
-	REM ~~~~~~~~~~~~ TS &&// NTS 
+	REM ~~~~~~~~~~~~ TS - NTS 
 set PHP_BUILDTS=0
 set PHP_BUILDNTS=1
 
 	REM ~~~~~~~~~~~~ curl
 set PHP_CURL=%PATH_INSTALL%\%FOLDER_RELEASE_CURL%\openssl
-
-	REM ~~~~~~~~~~~~ php version
-cd /D %PATH_SRC%\php-src
-FOR /F "tokens=* USEBACKQ" %%F in (`git describe --tags`) do ( set PHPGITVER=%%F )
-set PHPVER=%PHPGITVER:~4,3%
 
 	REM ~~~~~~~~~~~~ precompiled sdk deps
 REM call %PATH_PHP_SDK%\bin\php\do_php %PATH_MODULES%\php-getdeps.php
