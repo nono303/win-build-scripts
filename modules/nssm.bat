dce7c4d95d45cd64e052a25758e6774b875c1982
nono303 <git@nono303.net>
2021-11-17 18:18:52 +0100

    version and dirty hack for setted value

--- modules/nssm.bat
+++ modules/nssm.bat
@@ -1,6 +1,10 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
-
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
+
+	REM * branch master	e8211e9	10 Apr 2020	2.25 in readme.md	2.24 in ChangeLog.txt	no tag
+	REM   branch 2.x	4951a78	13 Apr 2020	2.22 in readme.md	2.24 in ChangeLog.txt	tag 2.25.0
+set NSSM_VER=2.25.0
+sed -i -E 's/for \/f.*/set description=%NSSM_VER:.=-%-0/g' %CYGPATH_SRC%/%1/version.cmd
 
 set OUTDIR_CONF=Release
 MSBuild.exe nssm.sln ^
@@ -10,6 +14,7 @@ MSBuild.exe nssm.sln ^
 	/p:Configuration=%OUTDIR_CONF% ^
 	/p:Platform="%archmsbuild%"
 
-for /F "tokens=* USEBACKQ" %%F in (`git describe --tags`) do (set VERSION=%%F)
-for %%E in (exe pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\out\%OUTDIR_CONF%\%archmsbuild%\nssm.%%E %PATH_INSTALL%\bin\nssm-%VERSION%.%%E*)
-call do_php %PATH_UTILS%\sub\bininfo.php %PATH_INSTALL%\bin\nssm-%VERSION%.exe null checkavx
+for %%E in (exe pdb) do (
+	xcopy /C /F /Y %PATH_SRC%\%1\out\%OUTDIR_CONF%\%archmsbuild%\nssm.%%E %PATH_INSTALL%\bin\nssm-%NSSM_VER%.%%E*
+)
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\nssm-%NSSM_VER%.exe
