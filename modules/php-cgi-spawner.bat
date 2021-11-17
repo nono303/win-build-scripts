46ecee0497988ef97ce31834623784099a57b2c0
nono303 <git@nono303.net>
2021-11-17 18:19:22 +0100

    copy exe & pdb to %PATH_INSTALL%\bin\

--- modules/php-cgi-spawner.bat
+++ modules/php-cgi-spawner.bat
@@ -4,4 +4,4 @@ cl /c /nologo /O2 /GL /MD /Zi /w %AVX% php-cgi-spawner.c
 rc -nologo -r php-cgi-spawner.rc
 link /nologo /debug /LTCG /OPT:ICF,REF /PDB:php-cgi-spawner.pdb php-cgi-spawner.obj php-cgi-spawner.res /subsystem:windows /MERGE:.rdata=.text 
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\src\php-cgi-spawner.exe
-call do_php %PATH_UTILS%\sub\bininfo.php %PATH_SRC%\%1\src\php-cgi-spawner.exe updaterc checkavx
+for %%X in (exe pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\src\php-cgi-spawner.%%X %PATH_INSTALL%\bin\*)
