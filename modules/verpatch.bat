cc707367001f46c18f595877558fb6327a8da784
nono303 <git@nono303.net>
2021-11-18 11:09:09 +0100

    version & x64 build (patched)

--- modules/verpatch.bat
+++ modules/verpatch.bat
@@ -2,13 +2,13 @@
 
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
 
-set OUTDIR_CONF=Release
 MSBuild.exe verpatch.sln ^
 	%MSBUILD_OPTS% ^
-	/nowarn:C4267;C4838;C4474 ^
+	/nowarn:C4267;C4838;C4474,C4311,C4302,C4312 ^
 	/t:Clean,verpatch ^
-	/p:Configuration=%OUTDIR_CONF% ^
-	/p:Platform="Win32"
+	/p:Configuration=Release ^
+	/p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_SRC%\%1\%OUTDIR_CONF%\verpatch.exe %PATH_SOFTS%\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\%outmsbuild%\verpatch.exe
+for %%X in (exe pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%outmsbuild%\verpatch.%%X %PATH_SOFTS%\*)
 call do_php %PATH_UTILS%\sub\bininfo.php %PATH_SOFTS%\verpatch.exe null checkavx
