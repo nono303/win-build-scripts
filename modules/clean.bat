c913a94a37f25776fc035667f40d737a88e79d7e
nono303 <git@nono303.net>
2021-11-18 11:07:32 +0100

    remove unnecessary calls

--- modules/clean.bat
+++ modules/clean.bat
@@ -1,6 +1,6 @@
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1
+@echo off
 for %%D in (%PATH_BUILD% %PATH_INSTALL%) do (
 	if exist %%D\. rmdir /S /Q %%D
 	mkdir %%D
 )
-for %%s in (lib bin include) do (if not exist %PATH_INSTALL%\%%s\. mkdir %PATH_INSTALL%\%%s)
+for %%s in (lib bin include) do (mkdir %PATH_INSTALL%\%%s)
