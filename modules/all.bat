4ba739ad397e26dcc145ae072a633b7b092136f4
nono303 <git@nono303.net>
2021-11-18 09:53:37 +0100

    nssm & php-cgi-spawner in "all"

--- modules/all.bat
+++ modules/all.bat
@@ -2,5 +2,7 @@ call %PATH_BATCH%\go.bat httpdanddeps NOLOG
 call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat phpanddeps NOLOG
 call %PATH_BATCH%\go.bat projgeolibanddeps NOLOG
+call %PATH_BATCH%\go.bat nssm NOLOG
+call %PATH_BATCH%\go.bat php-cgi-spawner NOLOG
 call %PATH_BATCH%\go.bat bininfo checkavx updaterc 
 if exist %PATH_BUILD%\. rmdir /S /Q %PATH_BUILD%\
