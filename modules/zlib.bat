ded070ba5e10fdc51e1883c770b2a38a66bc82e1
nono303 <git@nono303.net>
2022-01-18 15:32:27 +0100

    build zlib with cmake/ninja (instead of nmake)

--- modules/zlib.bat
+++ modules/zlib.bat
@@ -1,11 +1,10 @@
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1
-
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 	REM https://stackoverflow.com/questions/29505121/cmake-zlib-build-on-windows
-sed -i 's/-O2 -GL -MD/-O2 -GL -MD -MP%NUMBER_OF_PROCESSORS% %AVXMSC%/g' %CYGPATH_SRC%/%1/win32/Makefile.msc
-if %ARCH% == x86 (nmake %NMAKE_OPTS% /f win32/Makefile.msc LOC="-DASMV -DASMINF"     OBJA="inffas32.obj match686.obj")
-if %ARCH% == x64 (nmake %NMAKE_OPTS% /f win32/Makefile.msc LOC="-DASMV -DASMINF -I." OBJA="inffasx64.obj gvmat64.obj inffas8664.obj" AS=ml64)
-
-for %%E in (zlib.lib zlib.pdb zdll.lib) do (xcopy /C /F /Y %PATH_SRC%\%1\%%E %PATH_INSTALL%\lib\*)
-for %%E in (zlib1.dll zlib1.pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%%E %PATH_INSTALL%\bin\*)
-for %%E in (zlib.h zconf.h) do (xcopy /C /F /Y %PATH_SRC%\%1\%%E %PATH_INSTALL%\include\*)
+if %ARCH% == x86 (cmake %CMAKE_OPTS% -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -DASM686=ON %PATH_SRC%\%1)
+if %ARCH% == x64 (cmake %CMAKE_OPTS% -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -DAMD64=ON %PATH_SRC%\%1)
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+%NINJA% install
+xcopy /C /F /Y %PATH_BUILD%\%1\zlib.pdb %PATH_INSTALL%\lib\zlib.pdb*
+xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\zlibstatic.dir\zlibstatic.pdb %PATH_INSTALL%\lib\zlibstatic.pdb*
+xcopy /C /F /Y %PATH_BUILD%\%1\zlib.pdb %PATH_INSTALL%\bin\zlib1.pdb*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\zlib1.dll
