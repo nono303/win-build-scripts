a763a5a40b4ad03685f286d0c082b4b36e0e71b8
nono303 <git@nono303.net>
2022-01-19 11:44:40 +0100

    single build for static & shared build crypto (openssl) fix install, pdb & version

--- modules/ngtcp2.bat
+++ modules/ngtcp2.bat
@@ -1,36 +1,29 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
-for %%C in ("-DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF" "-DENABLE_STATIC_LIB=OFF -DENABLE_SHARED_LIB=ON") do (
-		REM dirty !! https://stackoverflow.com/questions/9556676/batch-file-how-to-replace-equal-signs-and-a-string-variable
-	set CUR=%%C
-	setlocal enabledelayedexpansion
-	set new=!CUR:"=!
+cmake %CMAKE_OPTS% ^
+-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+-DENABLE_WERROR=OFF ^
+-DENABLE_DEBUG=OFF ^
+-DENABLE_ASAN=OFF ^
+-DENABLE_GNUTLS=OFF ^
+-DENABLE_OPENSSL=ON ^
+-DENABLE_BORINGSSL=OFF ^
+-DLIBEV_LIBRARY=%PATH_INSTALL%\lib\libev_static.lib ^
+-DENABLE_STATIC_LIB=ON ^
+-DENABLE_SHARED_LIB=ON ^
+%PATH_SRC%\%1
 
-	cmake %CMAKE_OPTS% ^
-	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
-	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
-	-DENABLE_WERROR=OFF ^
-	-DENABLE_DEBUG=OFF ^
-	-DENABLE_ASAN=OFF ^
-	-DENABLE_GNUTLS=OFF ^
-	-DENABLE_OPENSSL=ON ^
-	-DENABLE_BORINGSSL=OFF ^
-	-DLIBEV_LIBRARY=%PATH_INSTALL%\lib\libev_static.lib ^
-	!new! ^
-	%PATH_SRC%\%1
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+%NINJA% lib/install crypto/install
 
-	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-	
+xcopy /C /F /Y %PATH_BUILD%\%1\lib\CMakeFiles\ngtcp2_static.dir\ngtcp2_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\crypto\openssl\CMakeFiles\ngtcp2_crypto_openssl_static.dir\ngtcp2_crypto_openssl_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\crypto\openssl\ngtcp2_crypto_openssl.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\lib\ngtcp2.pdb %PATH_INSTALL%\lib\*
 
-	if %%C =="-DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF" (
-		%NINJA% ngtcp2_static
-		xcopy /C /F /Y %PATH_BUILD%\%1\lib\CMakeFiles\ngtcp2_static.dir\ngtcp2_static.pdb %PATH_INSTALL%\lib\*
-		xcopy /Y %PATH_BUILD%\%1\lib\ngtcp2_static.lib %PATH_INSTALL%\lib\*
-	) else (
-		%NINJA% ngtcp2
-		for %%X in (dll pdb lib) do (
-			xcopy /C /F /Y %PATH_BUILD%\%1\lib\ngtcp2.%%X %PATH_INSTALL%\bin\*
-		)
-		call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\ngtcp2.dll
-	)
+for %%X in (ngtcp2 ngtcp2_crypto_openssl) do (
+	move /Y %PATH_INSTALL%\lib\%%X.dll %PATH_INSTALL%\bin\%%X.dll
+	xcopy /Y %PATH_INSTALL%\lib\%%X.pdb %PATH_INSTALL%\bin\*
+	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X.dll
 )
