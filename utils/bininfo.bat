e4c64d48f3bc2ee3d8b7f56281d6d64479e8eaec
nono303 <git@nono303.net>
2022-01-19 11:41:04 +0100

    set NBBIN for QUIC and !QUIC build

--- utils/bininfo.bat
+++ utils/bininfo.bat
@@ -1,10 +1,16 @@
 @echo off
 set BININFO_LOG=%PATH_LOGS%\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.csv
+if %QUIC_BUILD% == 1 (
+	set NBBIN=251
+) else (
+	set NBBIN=246
+)
 if exist %2\. (
 	call do_php %PATH_UTILS%\sub\%1.php %2 %BININFO_LOG:\=/% %2 %3 %4 %5 %6 %7 %8
 ) else (
+
 	set BININFO_LOG=%PATH_LOGS%\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.csv
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:245 %4 %5 %6 %7
+	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:%NBBIN% %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin\iconv %BININFO_LOG:\=/% %2 %3 nb:426 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\modules %BININFO_LOG:\=/% %2 %3 nb:256 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB% %BININFO_LOG:\=/% %2 %3 nb:61 %4 %5 %6 %7
