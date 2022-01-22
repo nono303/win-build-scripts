ba0773780f97e409209092641f28c252e70beeea
nono303 <git@nono303.net>
2022-01-22 23:52:30 +0100

    simplify revision & version for pecl_memcache in phpinfo()

--- modules/php.bat
+++ modules/php.bat
@@ -6,15 +6,11 @@ set PHP_FULLBUILD=8.1
 set PHP_BUILDTS=0
 set PHP_BUILDNTS=1
 
-	REM revision & version for pecl_memcache
 call %PATH_MODULES_COMMON%\init.bat pecl-memcache
-set PECLMEMCACHEGITCOMMIT=%SCM_VERSION%
-set PECLMEMCACHEGITBRANCH=%SCM_BRANCH%
-set PECLMEMCACHEGITDATE=%SCM_VERSION_DATE%
+	REM revision & version for pecl_memcache in phpinfo()
 FOR /F "tokens=* USEBACKQ" %%F in (`c:\sdk\softs\cyg64\bin\find -name 'memcache.c' -type f -exec dirname {} +`) do (set PECLMEMCACHECYGSRCDIR=%%F)
 FOR /F "tokens=* USEBACKQ" %%F in (`grep PHP_MEMCACHE_VERSION %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/php_memcache.h ^| cut -d^'^"^' -f2`) do (set PECLMEMCACHEVERSION=%%F)
-FOR /F "tokens=* USEBACKQ" %%F in (`type C:\sdk\batch\modules\pecl-memcache.patch.version`) do (set PECLMEMCACHEPATCHVERSION=%%F)
-sed -i -E 's/, PHP_MEMCACHE_VERSION/, "%PECLMEMCACHEVERSION% | branch: %PECLMEMCACHEGITBRANCH% | commit: %PECLMEMCACHEGITCOMMIT%%PECLMEMCACHEPATCHVERSION% | date: %PECLMEMCACHEGITDATE% | https:\/\/github.com\/nono303\/PHP-memcache-dll"/g' %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/memcache.c
+sed -i -E 's/, PHP_MEMCACHE_VERSION/, "%PECLMEMCACHEVERSION% | branch: %SCM_BRANCH% | commit: %SCM_VERSION% | date: %SCM_VERSION_DATE% | https:\/\/github.com\/nono303\/PHP-memcache-dll"/g' %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/memcache.c
 
 	REM init sdk, pecl, src
 for %%M in (php-sdk php-geos pecl-text-xdiff php-ext-brotli xdebug php-src) do (call %PATH_MODULES_COMMON%\init.bat %%M)
