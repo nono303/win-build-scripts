6adf2ba2a4c63c0e9092186162bfd09eff0c2808
nono303 <git@nono303.net>
2022-01-24 18:37:31 +0100

    SCM_VERSION > SCM_COMORREV

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -3,9 +3,9 @@ if exist %PATH_SRC%\%1\mobac\build\. rmdir /S /Q %PATH_SRC%\%1\mobac\build
 	REM https://docs.gradle.org/current/userguide/command_line_interface.html
 REM tilestore-util:jar
 call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% mobac:jar 2>&1
-FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_VERSION=%%F)
-move /Y %PATH_SRC%\%1\mobac\build\libs\Mobile_Atlas_Creator.jar %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_VERSION%.jar
+FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_COMORREV=%%F)
+move /Y %PATH_SRC%\%1\mobac\build\libs\Mobile_Atlas_Creator.jar %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_COMORREV%.jar
 
 if %LOCAL_COPY% == 1 if %LOCAL_COPY_AVXECHO% == %AVXECHO%  if %LOCAL_COPY_MSVC_VER% == %MSVC_VER% ( 
-	xcopy /C /F /Y %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_VERSION%.jar %LOCAL_PATH_MOBAC%\*
+	xcopy /C /F /Y %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_COMORREV%.jar %LOCAL_PATH_MOBAC%\*
 )
