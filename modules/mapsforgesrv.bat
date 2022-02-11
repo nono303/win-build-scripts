e8923ca667adc8ba3d0f45fadf7eb2046c4fb3e0
nono303 <git@nono303.net>
2022-02-11 18:33:25 +0100

    fix target

--- modules/mapsforgesrv.bat
+++ modules/mapsforgesrv.bat
@@ -1,3 +1,3 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 cd /D %PATH_SRC%\%1\%1
-call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% copyFatJar2jars_ready2use 2>&1
+call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar 2>&1
