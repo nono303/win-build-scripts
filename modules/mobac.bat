25d3cecbc84a9beac6effdc2e2b809184bfe8c3b
nono303 <git@nono303.net>
2022-02-13 18:53:25 +0100

    fix LOCAL_COPY

--- modules/mobac.bat
+++ modules/mobac.bat
@@ -6,6 +6,9 @@ call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-w
 FOR /F "tokens=* USEBACKQ" %%F in (`svn info --show-item revision`) do (set SCM_COMORREV=%%F)
 move /Y %PATH_SRC%\%1\mobac\build\libs\Mobile_Atlas_Creator.jar %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_COMORREV%.jar
 
-if %LOCAL_COPY% == 1 if %LOCAL_COPY_AVXECHO% == %AVXECHO%  if %LOCAL_COPY_MSVC_VER% == %MSVC_VER% ( 
+if %LOCAL_COPY% == 1 (
+	del /Q /F "%LOCAL_PATH_MOBAC%\Mobile_Atlas_Creator*.jar"
 	xcopy /C /F /Y %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_COMORREV%.jar %LOCAL_PATH_MOBAC%\*
+	del /Q /F "%LOCAL_PATH_MOBAC2%\Mobile_Atlas_Creator*.jar"
+	xcopy /C /F /Y %PATH_SRC%\%1\Mobile_Atlas_Creator_%SCM_COMORREV%.jar %LOCAL_PATH_MOBAC2%\*
 )
