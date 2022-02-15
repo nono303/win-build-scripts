4fba90fe0a6033254b6e62f01b9c33c3db2a756a
nono303 <git@nono303.net>
2022-02-15 17:42:46 +0100

    add libcheck

--- modules/all.bat
+++ modules/all.bat
@@ -5,4 +5,5 @@ call %PATH_BATCH%\go.bat projgeolibanddeps NOLOG
 call %PATH_BATCH%\go.bat nssm NOLOG
 call %PATH_BATCH%\go.bat php-cgi-spawner NOLOG
 call %PATH_BATCH%\go.bat bininfo checkavx
+call %PATH_BATCH%\go.bat libcheck
 if exist %PATH_BUILD%\. rmdir /S /Q %PATH_BUILD%\
