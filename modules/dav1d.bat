e4c3420390a8923569849a04894dc9f1c7dfd304
nono303 <git@nono303.net>
2022-02-15 17:46:14 +0100

    force one pdb for libdav1d

--- modules/dav1d.bat
+++ modules/dav1d.bat
@@ -14,8 +14,9 @@ meson ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/meson.sh "%AVXSED%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+sed -i -E 's/Fdsrc\\\libdav1d.*\.pdb"/Fdsrc\\\libdav1d.pdb" "\/FS"/g' %CYGPATH_BUILD%/%1/build.ninja
 %NINJA% install
 
 move /Y %PATH_INSTALL%\lib\libdav1d.a %PATH_INSTALL%\lib\libdav1d.lib
-xcopy /C /F /Y %PATH_BUILD%\%1\tools\dav1d.pdb %PATH_INSTALL%\lib\libdav1d.pdb*
+xcopy /C /F /Y %PATH_BUILD%\%1\src\libdav1d.pdb %PATH_INSTALL%\lib\libdav1d.pdb*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\dav1d.exe
