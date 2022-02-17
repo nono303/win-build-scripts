072a50df9790c04e6f5c01af4f692c13a11d2b11
nono303 <git@nono303.net>
2022-02-17 14:14:13 +0100

    fix libname to use shared

--- modules/phpsdk-local.bat
+++ modules/phpsdk-local.bat
@@ -17,14 +17,16 @@ sed -i 's/liblzma_a/liblzma/g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/libzip_a/zip/g' %CYGPATH_SRC%/php-src/configure.js
 	REM xpm
 sed -i 's/xpm.h/X11\\\\\\\\xpm.h/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libXpm_a/libxpm/g' %CYGPATH_SRC%/php-src/configure.js
 	REM bz2
-sed -i 's/libbz2_a/libbz2/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/;libbz2.lib//g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libbz2_a/bz2/g' %CYGPATH_SRC%/php-src/configure.js
 	REM jpeg > libjpeg-turbo
-sed -i 's/libjpeg_a.lib;libjpeg.lib/turbojpeg-static.lib/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libjpeg_a.lib;libjpeg.lib/jpeg.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM png
-sed -i 's/libpng_a.lib;libpng.lib/png_static.lib/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libpng_a.lib;libpng.lib/libpng16.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM sqlite3
-sed -i 's/libsqlite3_a.lib;libsqlite3.lib/sqlite3.lib/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libsqlite3_a.lib;//g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/sqlite3ext.h/sqlite3\/sqlite3ext.h/g' %CYGPATH_SRC%/php-src/configure.js
 	REM wineditline
 sed -i 's/edit_a/edit_static/g' %CYGPATH_SRC%/php-src/configure.js
@@ -37,7 +39,7 @@ sed -i 's/libcurl_a.lib;libcurl.lib/libcurl_imp.lib/g' %CYGPATH_SRC%/php-src/con
 sed -i 's/PHP_PHP_BUILD + "\/include\/curl/"%PHP_CURL:\=\/%" + "\/include\/curl/g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/EXTENSION("curl", "interface.c multi.c share.c curl_file.c");/EXTENSION("curl", "interface.c multi.c share.c curl_file.c"); CHECK_LIB("cares.lib", "curl", PHP_CURL);/g' %CYGPATH_SRC%/php-src/configure.js
 	REM freetype add lib bz2 et brotli
-sed -i -E 's/CHECK_LIB\("freetype_a.lib;freetype.lib", "gd", PHP_GD\) (..)/CHECK_LIB\("freetype.lib", "gd", PHP_GD\) \\1 CHECK_LIB\("libbz2.lib", "gd", PHP_GD\) \1 CHECK_LIB\("brotlidec.lib", "gd", PHP_GD\) \1/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i -E 's/CHECK_LIB\("freetype_a.lib;freetype.lib", "gd", PHP_GD\) (..)/CHECK_LIB\("freetype.lib", "gd", PHP_GD\) \\1 CHECK_LIB\("bz2.lib", "gd", PHP_GD\) \1 CHECK_LIB\("brotlidec.lib", "gd", PHP_GD\) \1/g' %CYGPATH_SRC%/php-src/configure.js
 
 	REM ~~~~~~~~~~~~ export config options
 call configure --help > %PATH_LOGS%\configure_%PHPVER%.txt
