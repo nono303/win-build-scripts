0070a9b779cf483f6fd8aacb6dcde4e0642a32de
nono303 <git@nono303.net>
2022-02-17 19:08:42 +0100

    change color display LIB WITH NO DLL & PDB

--- utils/sub/libcheck.php
+++ utils/sub/libcheck.php
@@ -5,6 +5,7 @@
 
 	if(is_dir($argv[1] = str_replace("\\","/",$argv[1]))){
 		echo ">>>>>> ".$argv[1]." <<<<<<".PHP_EOL;
+		$liblist = array_map('basename', glob($argv[1].'/*.lib', GLOB_BRACE));
 		foreach(["pdb" => "/","dll" => "/../bin/"] as $k => $v){
 			if(sizeof($entries = array_filter(explode("\n",trim(execnono("grep --binary-files=text --include=\*.lib -oRine '[0-9a-zA-Z_-]*\.".$k."'",NULL,$argv[1],NULL))))) == 0)
 				continue;
@@ -21,13 +22,15 @@
 				foreach($final[$k] as $lib => $tabresultat){
 					if(!is_array($final["pdb"]) || !($k == "dll" && array_key_exists($lib,$final["pdb"]))){
 						echo str_pad($lib,PAD);
+						$liblist = array_diff($liblist,[$lib]);
+						
 						$echres = "";
 						foreach($tabresultat as $pdbordll){
 							if(in_array($pdbordll,$pdbordlllist[$k])){
 								if(explode(".",$pdbordll)[0] == explode(".",$lib)[0] || $k == "dll"){
-									$echres .= str_pad("\033[32m".$pdbordll."\033[39m",PAD);
+									$echres .= str_pad("\033[92m".$pdbordll."\033[39m",PAD);
 								} else {
-									$echres .= str_pad("\033[35m".$pdbordll."\033[39m",PAD);
+									$echres .= str_pad("\033[95m".$pdbordll."\033[39m",PAD);
 								}
 								$pdbordlllist[$k] = array_diff($pdbordlllist[$k],[$pdbordll]);
 							} elseif($pdbordll == "vc140.pdb"){
@@ -41,12 +44,15 @@
 				}
 			}
 			if(sizeof($pdbordlllist[$k]) > 0){
-				echo "# UNUSED".PHP_EOL;
+				echo "\033[96m# ".strtoupper($k)." WITH NO LIB\033[39m".PHP_EOL;
 				foreach($pdbordlllist[$k] as $unused)
-					echo "\t\033[36m".$unused."\033[39m".PHP_EOL;
-			} else {
-				// echo "\033[36m# NO UNUSED\033[39m".PHP_EOL;
+					echo "\t\033[96m".$unused."\033[39m".PHP_EOL;
 			}
+		}
+		if(sizeof($liblist) > 0){
+			echo "\033[93m# LIB WITH NO DLL & PDB\033[39m".PHP_EOL;
+			foreach($liblist as $unused)
+				echo "\t\033[93m".$unused."\033[39m".PHP_EOL;
 		}
 	}
 ?>
