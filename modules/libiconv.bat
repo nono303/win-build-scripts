51e784ec30e065bb713650b821b3d77b8c61a304
nono303 <git@nono303.net>
2022-02-18 19:13:11 +0100

    rewrite bat fix unresolved _libiconv_version for shared use in php build

--- modules/libiconv.bat
+++ modules/libiconv.bat
@@ -1,22 +1,20 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 
-REM /******** > 2021-07-21 TMP !! vs17 non disponible ********/
-if not exist %PATH_SRC%\libiconv\MSVC%MSVC_VER%\. mklink /J %PATH_SRC%\libiconv\MSVC%MSVC_VER% %PATH_SRC%\libiconv\MSVC16
+set VCDIR=MSVC%MSVC_VER%
+if not exist %PATH_SRC%\%1\%VCDIR%\. mklink /J %PATH_SRC%\%1\%VCDIR% %PATH_SRC%\%1\MSVC16
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR%/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
 
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/libiconv/MSVC%MSVC_VER%/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-
-MSBuild.exe %PATH_SRC%\libiconv\MSVC%MSVC_VER%\libiconv.sln ^
+MSBuild.exe %PATH_SRC%\%1\%VCDIR%\%1.sln ^
 	%MSBUILD_OPTS% ^
 	/nowarn:C4267;C4090;C4018 ^
-	/t:Clean,libiconv_static:Rebuild,libiconv_dll:Rebuild ^
+	/t:Clean,%1_static:Rebuild,%1_dll:Rebuild ^
 	/p:Configuration=Release ^
 	/p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_SRC%\libiconv\MSVC%MSVC_VER%\%archmsbuild%\bin\libiconv.dll %PATH_INSTALL%\bin\* 
-xcopy /C /F /Y %PATH_SRC%\libiconv\MSVC%MSVC_VER%\%archmsbuild%\bin\libiconv.pdb %PATH_INSTALL%\bin\*
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libiconv.dll
-	REM for libxml2 & httpd
-xcopy /C /F /Y %PATH_SRC%\libiconv\MSVC%MSVC_VER%\%archmsbuild%\lib\libiconv.lib %PATH_INSTALL%\lib\iconv.lib*
-xcopy /C /F /Y %PATH_SRC%\libiconv\MSVC%MSVC_VER%\%archmsbuild%\lib\libiconv_a.lib %PATH_INSTALL%\lib\*
-xcopy /C /F /Y %PATH_SRC%\libiconv\MSVC%MSVC_VER%\%archmsbuild%\lib\libiconv_a.pdb %PATH_INSTALL%\lib\*
-xcopy /C /F /Y %PATH_SRC%\libiconv\source\include\iconv.h %PATH_INSTALL%\include\*
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\%1.dll %PATH_INSTALL%\bin\* 
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\%1.pdb %PATH_INSTALL%\bin\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%1.dll
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1.lib %PATH_INSTALL%\lib\iconv.lib*
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1_a.lib %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1_a.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_SRC%\%1\source\include\iconv.h %PATH_INSTALL%\include\*
