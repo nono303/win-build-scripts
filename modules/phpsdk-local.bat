73e91b4cbed7a3f3452c84ee517b0412702cee28
nono303 <git@nono303.net>
2022-02-19 16:45:03 +0100

    refactor sed in configure.js for comprehensive change in shared & static lib names

--- modules/phpsdk-local.bat
+++ modules/phpsdk-local.bat
@@ -9,11 +9,20 @@ cd /D %PHP_SRC_DIR%
 call buildconf
 
 	REM ~~~~~~~~~~~~ Patch some stuff in this shity configure.js, accoring to self made deps
+
+REM STATIC
+
 	REM libxml2s
 sed -i -E 's/CHECK_LIB\("libxml2_a_dll.lib;libxml2_a.lib", "libxml"\) (..)/CHECK_LIB\("icuuc.lib", "libxml"\) \\1 CHECK_LIB\("libxml2s.lib", "libxml"\) \\1/g' %CYGPATH_SRC%/php-src/configure.js
-	REM liblzma_a
+	REM edit
+sed -i 's/edit_a.lib;edit.lib/edit_static.lib/g' %CYGPATH_SRC%/php-src/configure.js
+
+REM SHARED
+	REM libiconv - shared with patch ext/iconv/iconv.c : _libiconv_version > _LIBICONV_VERSION
+sed -i 's/libiconv_a.lib/iconv.lib/g' %CYGPATH_SRC%/php-src/configure.js
+	REM lzma
 sed -i 's/liblzma_a/liblzma/g' %CYGPATH_SRC%/php-src/configure.js
-	REM libzip_a
+	REM zip
 sed -i 's/libzip_a/zip/g' %CYGPATH_SRC%/php-src/configure.js
 	REM xpm
 sed -i 's/xpm.h/X11\\\\\\\\xpm.h/g' %CYGPATH_SRC%/php-src/configure.js
@@ -21,25 +30,21 @@ sed -i 's/libXpm_a/libxpm/g' %CYGPATH_SRC%/php-src/configure.js
 	REM bz2
 sed -i 's/;libbz2.lib//g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/libbz2_a/bz2/g' %CYGPATH_SRC%/php-src/configure.js
-	REM jpeg > libjpeg-turbo
+	REM jpeg [libjpeg-turbo]
 sed -i 's/libjpeg_a.lib;libjpeg.lib/jpeg.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM png
 sed -i 's/libpng_a.lib;libpng.lib/libpng16.lib/g' %CYGPATH_SRC%/php-src/configure.js
-	REM libiconv shared : KO php8.dll.def : error LNK2001: unresolved external symbol _libiconv_version
-REM sed -i 's/libiconv_a.lib/iconv.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM sqlite3
 sed -i 's/libsqlite3_a.lib;//g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/sqlite3ext.h/sqlite3\/sqlite3ext.h/g' %CYGPATH_SRC%/php-src/configure.js
-	REM wineditline
-sed -i 's/edit_a/edit_static/g' %CYGPATH_SRC%/php-src/configure.js
 	REM zlib
 sed -i 's/zlib_a/zlib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM avif
 sed -i 's/avif.h/avif\/avif.h/g' %CYGPATH_SRC%/php-src/configure.js
-	REM curl
+	REM curl - add lib: cares 
 sed -i 's/libcurl_a.lib;libcurl.lib/libcurl_imp.lib/g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/EXTENSION("curl", "interface.c multi.c share.c curl_file.c");/EXTENSION("curl", "interface.c multi.c share.c curl_file.c"); CHECK_LIB("cares.lib", "curl", PHP_CURL);/g' %CYGPATH_SRC%/php-src/configure.js
-	REM freetype add lib bz2 et brotli
+	REM freetype2 - add libs: bz2 et brotli
 sed -i -E 's/CHECK_LIB\("freetype_a.lib;freetype.lib", "gd", PHP_GD\) (..)/CHECK_LIB\("freetype.lib", "gd", PHP_GD\) \\1 CHECK_LIB\("bz2.lib", "gd", PHP_GD\) \1 CHECK_LIB\("brotlidec.lib", "gd", PHP_GD\) \1/g' %CYGPATH_SRC%/php-src/configure.js
 
 	REM ~~~~~~~~~~~~ export config options
