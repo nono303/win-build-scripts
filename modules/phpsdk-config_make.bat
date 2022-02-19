76acc1d013681d73c5e86670811ceadf41ed9b01
nono303 <git@nono303.net>
2022-02-19 16:48:20 +0100

    all php module as shared (as possible): 30 > 78 global config for --with-toolset=vs & --with-cygwin fix php_iconv.lib required for iconv shared add shared liblzma.lib for libxml

--- modules/phpsdk-config_make.bat
+++ modules/phpsdk-config_make.bat
@@ -75,41 +75,56 @@ set PHP_COMMON_CONFIGURE=^
 	--without-oci8 ^
 	--without-pgsql ^
 	--without-uncritical-warn-choke ^
+	--with-toolset=vs ^
+	--with-cygwin=%PATH_BIN_CYGWIN% ^
 	--with-mp=%NUMBER_OF_PROCESSORS%
 
 REM known bug for --with-ffi if php != 8.1 || 7.4
 if %PHPVER% == %PHP_FULLBUILD% (
 	call configure %PHP_COMMON_CONFIGURE% ^
-	--with-toolset=vs ^
-	--with-cygwin=%PATH_BIN_CYGWIN% ^
-	--enable-brotli=shared ^
-	--enable-com-dotnet ^
-	--enable-exif ^
-	--enable-fileinfo ^
-	--enable-ftp ^
-	--enable-intl ^
 	--enable-mbstring ^
-	--enable-opcache ^
-	--enable-pdo ^
 	--enable-phar-native-ssl ^
-	--enable-soap ^
-	--enable-sockets ^
-	--with-bz2 ^
+	--enable-brotli=shared ^
+	--enable-com-dotnet=shared ^
+	--enable-exif=shared ^
+	--enable-fileinfo=shared ^
+	--enable-ftp=shared ^
+	--enable-intl=shared ^
+	--enable-opcache=shared ^
+	--enable-pdo=shared ^
+	--enable-soap=shared ^
+	--enable-sockets=shared ^
+	--with-bz2=shared ^
 	--with-curl=shared ^
 	--with-iconv=%PATH_INSTALL% ^
-	--with-mysqli ^
-	--with-openssl ^
-	--with-pdo-mysql ^
-	--with-pdo-sqlite ^
-	--with-sqlite3 ^
+	--with-mysqli=shared ^
+	--with-openssl=shared ^
+	--with-pdo-mysql=shared ^
+	--with-pdo-sqlite=shared ^
+	--with-sqlite3=shared ^
 	--with-tidy=shared ^
 	--with-xdebug=shared ^
 	--with-xdebug-compression ^
 	--with-xdiff=shared ^
 	--enable-memcache=shared ^
 	--with-geos=shared ^
-	--with-ffi ^
-	--with-sodium ^
+	--with-ffi=shared ^
+	--with-sodium=shared ^
+	--with-xml=shared ^
+	--enable-xmlreader=shared ^
+	--enable-xmlwriter=shared ^
+	--with-simplexml=shared ^
+	--with-libxml=shared ^
+	--with-dom=shared ^
+	--with-iconv=shared ^
+	--enable-zip=shared ^
+	--enable-zlib=shared ^
+	--with-readline=shared ^
+	--enable-pdo=shared ^
+	--enable-calendar=shared ^
+	--with-mhash=shared ^
+	--enable-ctype=shared ^
+	--enable-bcmath=shared ^
 	%ZTS% %phpveropts% %phparchopts%
 ) else (
 	call configure %PHP_COMMON_CONFIGURE% ^
@@ -146,8 +161,12 @@ sed -i -E 's/incremental:no/incremental:no \/LTCG \/NODEFAULTLIB:libcmt.lib/g' %
 sed -i 's/\/Ox/\/O2 \/GL \/Zf \/D PHP_ICONV_PREFIX=%PATH_INSTALL:\=\/%/g' %CYGPATH_SRC%/php-src/Makefile
 	REM no warn
 sed -i 's/d4996//g' %CYGPATH_SRC%/php-src/Makefile
-	REM 'warning U4004: too many rules' - https://docs.microsoft.com/en-us/cpp/error-messages/tool-errors/nmake-warning-u4004
+	REM PHP 7.X 'warning U4004: too many rules' - https://docs.microsoft.com/en-us/cpp/error-messages/tool-errors/nmake-warning-u4004
 REM sed -i 's/phpdbg_win.obj: /phpdbg_win.obj:: /g' %CYGPATH_SRC%/php-src/Makefile
+	REM fix for iconv shared : LINK : fatal error LNK1181: cannot open input file 'php_iconv.lib'
+	REM add shared liblzma.lib for libxml 
+		REM static way patch: [libxml2/xzlib.c  #ifdef LIBXML_LZMA_ENABLED +#define LZMA_API_STATIC]
+sed -i 's/LIBS_LIBXML=icuuc.lib libxml2s.lib iconv.lib php_iconv.lib/LIBS_LIBXML=icuuc.lib libxml2s.lib iconv.lib liblzma.lib/g' %CYGPATH_SRC%/php-src/Makefile
 
 	REM after sed:
 REM LDFLAGS=/nologo /incremental:no /LTCG /NODEFAULTLIB:libcmt.lib /debug /opt:ref,icf
@@ -168,7 +187,7 @@ for %%A in (exe dll) do (
 )
 if %PHPVER% == %PHP_FULLBUILD% (
 	call %PATH_MODULES_COMMON%\init.bat php-src varonly
-	for %%X in (php-cgi.exe php.exe php8.dll php_curl.dll php_fileinfo.dll php_gd.dll php_intl.dll php_opcache.dll php_openssl.dll php_tidy.dll) do (
+	for %%X in (php-cgi.exe php.exe php8.dll php_curl.dll php_fileinfo.dll php_gd.dll php_intl.dll php_opcache.dll php_openssl.dll php_tidy.dll php_bcmath.dll php_bz2.dll php_calendar.dll php_com_dotnet.dll php_ctype.dll php_dom.dll php_exif.dll php_ffi.dll php_ftp.dll php_iconv.dll php_mysqli.dll php_pdo_mysql.dll php_pdo_sqlite.dll php_readline.dll php_simplexml.dll php_soap.dll php_sockets.dll php_sodium.dll php_sqlite3.dll php_xml.dll php_xmlreader.dll php_xmlwriter.dll php_zip.dll php_zlib.dll) do (
 		call do_php %PATH_UTILS%\sub\version.php php-src %PATH_RELEASE%\%MSVC_DEPS%_%PHP_SDK_ARCH%%AVXB%\_php-%TSNTS%\%%X "build:%TSNTS%"
 	)
 	call %PATH_MODULES_COMMON%\init.bat pecl-text-xdiff varonly
