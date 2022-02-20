d46d0a5948f0fbd3450ae1e3d354c4408b4d4dd6
nono303 <git@nono303.net>
2022-02-20 17:14:37 +0100

    phpanddeps after httpd

--- modules/all.bat
+++ modules/all.bat
@@ -1,6 +1,6 @@
 call %PATH_BATCH%\go.bat httpdanddeps NOLOG
-call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat phpanddeps NOLOG
+call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat projgeolibanddeps NOLOG
 call %PATH_BATCH%\go.bat nssm NOLOG
 call %PATH_BATCH%\go.bat php-cgi-spawner NOLOG
