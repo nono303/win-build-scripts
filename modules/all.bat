d55f7dd7c9fd3ac574520869df410049eb8843f6
nono303 <git@nono303.net>
2022-02-22 13:13:59 +0100

    dependencies in all

--- modules/all.bat
+++ modules/all.bat
@@ -4,6 +4,7 @@ call %PATH_BATCH%\go.bat subversionanddeps NOLOG
 call %PATH_BATCH%\go.bat projgeolibanddeps NOLOG
 call %PATH_BATCH%\go.bat nssm NOLOG
 call %PATH_BATCH%\go.bat php-cgi-spawner NOLOG
+call %PATH_BATCH%\go.bat dependencies NOLOG
 call %PATH_BATCH%\go.bat bininfo checkavx
 call %PATH_BATCH%\go.bat libcheck
 if exist %PATH_BUILD%\. rmdir /S /Q %PATH_BUILD%\
