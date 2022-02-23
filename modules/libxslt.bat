c732fb0f359c7b133ff7f1be893abfc0229e51ff
nono303 <git@nono303.net>
2022-02-23 16:05:31 +0100

    build static libs

--- modules/libxslt.bat
+++ modules/libxslt.bat
@@ -4,28 +4,31 @@
 xcopy /C /F /Y %PATH_MODULES%\libxlst.FindLibXml2.cmake %PATH_SRC%\%1\FindLibXml2.cmake*
 sed -i 's/LibXml2 CONFIG/LibXml2/g' %CYGPATH_SRC%/%1/CMakeLists.txt
 
-cmake %CMAKE_OPTS% ^
-	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
-	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
-	-DBUILD_SHARED_LIBS=ON ^
-	-DLIBXSLT_WITH_DEBUGGER=ON ^
-	-DLIBXSLT_WITH_CRYPTO=OFF ^
-	-DLIBXSLT_WITH_MEM_DEBUG=ON ^
-	-DLIBXSLT_WITH_MODULES=ON ^
-	-DLIBXSLT_WITH_PROFILER=ON ^
-	-DLIBXSLT_WITH_PYTHON=OFF ^
-	-DLIBXSLT_WITH_TESTS=OFF ^
-	-DLIBXSLT_WITH_THREADS=ON ^
-	-DLIBXSLT_WITH_XSLT_DEBUG=ON ^
-	-DLIBXML2_INCLUDE_DIR=%PATH_INSTALL%\include\libxml2;%PATH_INSTALL%\include ^
-%PATH_SRC%\%1 
+	REM OFF : static ON : shared
+for %%X in (ON OFF) do (
+	cmake %CMAKE_OPTS% ^
+		-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+		-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+		-DBUILD_SHARED_LIBS=%%X ^
+		-DLIBXSLT_WITH_DEBUGGER=ON ^
+		-DLIBXSLT_WITH_CRYPTO=OFF ^
+		-DLIBXSLT_WITH_MEM_DEBUG=ON ^
+		-DLIBXSLT_WITH_MODULES=ON ^
+		-DLIBXSLT_WITH_PROFILER=ON ^
+		-DLIBXSLT_WITH_PYTHON=OFF ^
+		-DLIBXSLT_WITH_TESTS=OFF ^
+		-DLIBXSLT_WITH_THREADS=ON ^
+		-DLIBXSLT_WITH_XSLT_DEBUG=ON ^
+		-DLIBXML2_INCLUDE_DIR=%PATH_INSTALL%\include\libxml2;%PATH_INSTALL%\include ^
+	%PATH_SRC%\%1
 
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-for %%X in (build.ninja cmake_install.cmake) do (sed -i 's/libexslts\.lib/libexslt_static\.lib/g' %CYGPATH_BUILD%/%1/%%X)
-for %%X in (build.ninja cmake_install.cmake) do (sed -i 's/libxslts\.lib/libxslt_static\.lib/g' %CYGPATH_BUILD%/%1/%%X)
-sed -i 's/LibExslt\.pdb/libexslt_static\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-sed -i 's/LibXslt\.pdb/libxslt_static\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-%NINJA% install
+	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+	for %%X in (build.ninja cmake_install.cmake) do (sed -i 's/libexslts\.lib/libexslt_static\.lib/g' %CYGPATH_BUILD%/%1/%%X)
+	for %%X in (build.ninja cmake_install.cmake) do (sed -i 's/libxslts\.lib/libxslt_static\.lib/g' %CYGPATH_BUILD%/%1/%%X)
+	sed -i 's/LibExslt\.pdb/libexslt_static\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
+	sed -i 's/LibXslt\.pdb/libxslt_static\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
+	%NINJA% install
+)
 
 xcopy /C /F /Y %PATH_BUILD%\%1\xsltproc.pdb %PATH_INSTALL%\bin\*
 for %%X in (libexslt.dll libxslt.dll xsltproc.exe) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X)
