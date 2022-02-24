28e299628543328797bb9d81ffdef3ad61b457fe
nono303 <git@nono303.net>
2022-02-24 12:06:20 +0100

    rename libdeflatestatic > libdeflate_static

--- modules/libdeflate.bat
+++ modules/libdeflate.bat
@@ -1,14 +1,15 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 
 if not [%AVX%] == [] set AVXDEF=%AVX:/=\/%
-sed -i 's/\/MD \/O2/\/DWIN32 \/D_WINDOWS \/w \/DNDEBUG \/nologo \/O2 \/GL \/MD \/Zi \/w \/MP%NUMBER_OF_PROCESSORS% %AVXDEF% \/Fdlibdeflatestatic.pdb \/FS/g' %CYGPATH_SRC%/%1/makefile.msc
+sed -i 's/libdeflatestatic/libdeflate_static/g' %CYGPATH_SRC%/%1/makefile.msc
+sed -i 's/\/MD \/O2/\/DWIN32 \/D_WINDOWS \/w \/DNDEBUG \/nologo \/O2 \/GL \/MD \/Zi \/w \/MP%NUMBER_OF_PROCESSORS% %AVXDEF% \/Fdlibdeflate_static.pdb \/FS/g' %CYGPATH_SRC%/%1/makefile.msc
 sed -i 's/LDFLAGS =/LDFLAGS =\/DEBUG\ \/NOLOGO \/LTCG \/OPT:REF,ICF/g' %CYGPATH_SRC%/%1/makefile.msc
 sed -i 's/AR = lib/AR = lib \/NOLOGO \/LTCG /g' %CYGPATH_SRC%/%1/makefile.msc
 
-nmake %NMAKE_OPTS% /f Makefile.msc libdeflatestatic.lib libdeflate.dll libdeflate.lib
+nmake %NMAKE_OPTS% /f Makefile.msc libdeflate_static.lib libdeflate.dll libdeflate.lib
 
 for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\libdeflate.%%X %PATH_INSTALL%\bin\*)
-for %%X in (libdeflate.lib libdeflatestatic.lib libdeflatestatic.pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%%X %PATH_INSTALL%\lib\*)
+for %%X in (libdeflate.lib libdeflate_static.lib libdeflate_static.pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%%X %PATH_INSTALL%\lib\*)
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libdeflate.dll
 
 xcopy /C /F /Y %PATH_SRC%\%1\libdeflate.h %PATH_INSTALL%\include\*
