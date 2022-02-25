6831bfe9175733bff88f918244827d40b8bea8e8
nono303 <git@nono303.net>
2022-02-25 17:27:42 +0100

    shared without asm

--- modules/dav1d.bat
+++ modules/dav1d.bat
@@ -1,22 +1,20 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
+REM -Denable_asm=true + --default-library=shared == KO
 meson ^
 --prefix %PATH_INSTALL% ^
 --buildtype debugoptimized ^
---default-library=static ^
+--default-library=shared ^
 -Dfuzzing_engine=none ^
 -Denable_tests=false ^
 -Dtestdata_tests=false ^
 -Denable_examples=false ^
 -Denable_tools=true ^
 -Denable_avx512=false ^
--Denable_asm=true ^
+-Denable_asm=false ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/meson.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-sed -i -E 's/Fdsrc\\\libdav1d.*\.pdb"/Fdsrc\\\libdav1d.pdb" "\/FS"/g' %CYGPATH_BUILD%/%1/build.ninja
 %NINJA% install
 
-move /Y %PATH_INSTALL%\lib\libdav1d.a %PATH_INSTALL%\lib\libdav1d.lib
-xcopy /C /F /Y %PATH_BUILD%\%1\src\libdav1d.pdb %PATH_INSTALL%\lib\libdav1d.pdb*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\dav1d.exe
