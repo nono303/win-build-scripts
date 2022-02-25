4e0c365b3d37545014e881b1b465b5832ba04adc
nono303 <git@nono303.net>
2022-02-25 17:35:15 +0100

    accord imp lib to dll name: yuv > libyuv

--- modules/libyuv.bat
+++ modules/libyuv.bat
@@ -14,11 +14,13 @@ cmake %CMAKE_OPTS% ^
 sed -i 's/LINK_LIBRARIES = kernel32.lib/LINK_LIBRARIES = kernel32.lib %PATH_INSTALL:\=\\\%\\\lib\\\jpeg.lib/g' %CYGPATH_BUILD%/%1/build.ninja
 	REM shared lib for dll: include/libyuv/basic_types.h:48:#define LIBYUV_API __declspec(dllexport)
 sed -i 's/DWIN32/DWIN32 \/DLIBYUV_BUILDING_SHARED_LIBRARY=1/g' %CYGPATH_BUILD%/%1/build.ninja
+	REM accord imp lib to dll name
+for %%Y in (cmake_install.cmake build.ninja) do (sed -i 's/ yuv\.lib/ libyuv\.lib/g' %CYGPATH_BUILD%/%1/%%Y)
 	REM !install : CMake Error at cmake_install.cmake:36 (file):  file INSTALL cannot find "C:/sdk/build/vs17_x64-avx/libyuv/yuvconvert": No
 %NINJA%
 
 
-xcopy /C /F /Y %PATH_BUILD%\%1\yuv.lib %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\libyuv.lib %PATH_INSTALL%\lib\*
 for %%X in (CMakeFiles\yuv_static.dir\yuv_static.pdb yuv_static.lib) do (xcopy /C /F /Y %PATH_BUILD%\%1\%%X %PATH_INSTALL%\lib\*)
 for %%X in (libyuv.dll yuvconvert.exe) do (
 	xcopy /C /F /Y %PATH_BUILD%\%1\%%X %PATH_INSTALL%\bin\*
