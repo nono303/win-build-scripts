e96119b0719ed79d2790e091061b8fd6d1d3df8c
nono303 <git@nono303.net>
2022-02-25 15:11:42 +0100

    fix libname disable add deps to libs (shared build)

--- modules/phpsdk-local.bat
+++ modules/phpsdk-local.bat
@@ -15,16 +15,14 @@ REM STATIC
 	REM edit
 sed -i 's/edit_a.lib;edit.lib/edit_static.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM webp
-sed -i 's/libwebp.lib/webp.lib/g' %CYGPATH_SRC%/php-src/configure.js
-	REM webp
-sed -i 's/libwebp.lib/webp.lib/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libwebp.lib/webp_static.lib/g' %CYGPATH_SRC%/php-src/configure.js
 
 REM SHARED
 
 	REM gmp - shared
 sed -i 's/mpir_a.lib/mpir_%AVXMPIR%.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM libiconv - shared with patch ext/iconv/iconv.c : _libiconv_version > _LIBICONV_VERSION
-sed -i 's/libiconv_a.lib/iconv.lib/g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libiconv_a.lib/libiconv.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM lzma
 sed -i 's/liblzma_a/liblzma/g' %CYGPATH_SRC%/php-src/configure.js
 	REM zip
@@ -40,17 +38,20 @@ sed -i 's/libjpeg_a.lib;libjpeg.lib/jpeg.lib/g' %CYGPATH_SRC%/php-src/configure.
 	REM png
 sed -i 's/libpng_a.lib;libpng.lib/libpng16.lib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM sqlite3
-sed -i 's/libsqlite3_a.lib;//g' %CYGPATH_SRC%/php-src/configure.js
+sed -i 's/libsqlite3_a/sqlite3/g' %CYGPATH_SRC%/php-src/configure.js
 sed -i 's/sqlite3ext.h/sqlite3\/sqlite3ext.h/g' %CYGPATH_SRC%/php-src/configure.js
 	REM zlib
 sed -i 's/zlib_a/zlib/g' %CYGPATH_SRC%/php-src/configure.js
 	REM avif
 sed -i 's/avif.h/avif\/avif.h/g' %CYGPATH_SRC%/php-src/configure.js
+
+REM libs with STATIC deps
+
 	REM curl - add lib: cares 
-sed -i 's/libcurl_a.lib;libcurl.lib/libcurl_imp.lib/g' %CYGPATH_SRC%/php-src/configure.js
-sed -i 's/EXTENSION("curl", "interface.c multi.c share.c curl_file.c");/EXTENSION("curl", "interface.c multi.c share.c curl_file.c"); CHECK_LIB("cares.lib", "curl", PHP_CURL);/g' %CYGPATH_SRC%/php-src/configure.js
+REM sed -i 's/libcurl_a.lib;libcurl.lib/libcurl.lib/g' %CYGPATH_SRC%/php-src/configure.js
+REM sed -i 's/EXTENSION("curl", "interface.c multi.c share.c curl_file.c");/EXTENSION("curl", "interface.c multi.c share.c curl_file.c"); CHECK_LIB("cares.lib", "curl", PHP_CURL);/g' %CYGPATH_SRC%/php-src/configure.js
 	REM freetype2 - add libs: bz2 et brotli
-sed -i -E 's/CHECK_LIB\("freetype_a.lib;freetype.lib", "gd", PHP_GD\) (..)/CHECK_LIB\("freetype.lib", "gd", PHP_GD\) \\1 CHECK_LIB\("bz2.lib", "gd", PHP_GD\) \1 CHECK_LIB\("brotlidec.lib", "gd", PHP_GD\) \1/g' %CYGPATH_SRC%/php-src/configure.js
+REM sed -i -E 's/CHECK_LIB\("freetype_a.lib;freetype.lib", "gd", PHP_GD\) (..)/CHECK_LIB\("freetype.lib", "gd", PHP_GD\) \\1 CHECK_LIB\("bz2.lib", "gd", PHP_GD\) \1 CHECK_LIB\("brotlidec.lib", "gd", PHP_GD\) \1/g' %CYGPATH_SRC%/php-src/configure.js
 
 	REM ~~~~~~~~~~~~ export config options
 call configure --help > %PATH_LOGS%\configure_%PHPVER%.txt
