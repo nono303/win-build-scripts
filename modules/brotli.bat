191848b3a773993abed873eb234dc25f093235d1
nono303 <git@nono303.net>
2022-02-25 14:45:38 +0100

    *-static.lib > *._static

--- modules/brotli.bat
+++ modules/brotli.bat
@@ -8,11 +8,12 @@ cmake ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+for %%Y in (cmake_install.cmake build.ninja) do (sed -i 's/-static\./_static\./g' %CYGPATH_BUILD%/%1/%%Y)
 %NINJA% install
 
 for %%X in (brotlicommon brotli brotlidec brotlienc) do (xcopy /C /F /Y %PATH_BUILD%\%1\%%X.pdb %PATH_INSTALL%\bin\*)
-xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlicommon-static.dir\brotlicommon-static.pdb %PATH_INSTALL%\lib\*
-xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlidec-static.dir\brotlidec-static.pdb %PATH_INSTALL%\lib\*
-xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlienc-static.dir\brotlienc-static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlicommon_static.dir\brotlicommon_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlidec_static.dir\brotlidec_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\brotlienc_static.dir\brotlienc_static.pdb %PATH_INSTALL%\lib\*
 
 for %%X in (brotlicommon.dll brotli.exe brotlidec.dll brotlienc.dll) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X)
