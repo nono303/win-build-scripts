0ed2cad1b4050123aa1496d8e058fea3eca44a58
nono303 <git@nono303.net>
2022-02-25 15:02:41 +0100

    static: libiconv_a > libiconv_static shared (iconv.lib): libiconv.dll > iconv.dll

--- modules/libiconv.bat
+++ modules/libiconv.bat
@@ -6,6 +6,8 @@ REM https://www.codeproject.com/Articles/302012/How-to-Build-libiconv-with-Micro
 set VCDIR=MSVC%MSVC_VER%
 if not exist %PATH_SRC%\%1\%VCDIR%\. mklink /J %PATH_SRC%\%1\%VCDIR% %PATH_SRC%\%1\MSVC16
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR%/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
+sed -i 's/libiconv_a/libiconv_static/g' %CYGPATH_SRC%/%1/%VCDIR%/libiconv_static/libiconv_static.vcxproj
+for %%X in (dll pdb lib) do (sed -i 's/libiconv\.%%X/iconv\.%%X/g' %CYGPATH_SRC%/%1/%VCDIR%/libiconv_dll/libiconv_dll.vcxproj)
 
 MSBuild.exe %PATH_SRC%\%1\%VCDIR%\%1.sln ^
 	%MSBUILD_OPTS% ^
@@ -14,10 +16,7 @@ MSBuild.exe %PATH_SRC%\%1\%VCDIR%\%1.sln ^
 	/p:Configuration=Release ^
 	/p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\%1.dll %PATH_INSTALL%\bin\* 
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\%1.pdb %PATH_INSTALL%\bin\*
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%1.dll
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1.lib %PATH_INSTALL%\lib\iconv.lib*
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1_a.lib %PATH_INSTALL%\lib\*
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%1_a.pdb %PATH_INSTALL%\lib\*
+for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\iconv.%%X %PATH_INSTALL%\bin\*)
+for %%X in (iconv.lib %1_static.lib %1_static.pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\%%X %PATH_INSTALL%\lib\*)
 xcopy /C /F /Y %PATH_SRC%\%1\source\include\iconv.h %PATH_INSTALL%\include\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\iconv.dll
