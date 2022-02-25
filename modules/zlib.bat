800c9476762ee4538fc9c64256621be24d2d6e7f
nono303 <git@nono303.net>
2022-02-25 14:44:23 +0100

    zlibstatic.pdb > zlib_static.pdb zlib1.dll > zlib.dll

--- modules/zlib.bat
+++ modules/zlib.bat
@@ -3,7 +3,9 @@
 if %ARCH% == x86 (cmake %CMAKE_OPTS% -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -DASM686=ON %PATH_SRC%\%1)
 if %ARCH% == x64 (cmake %CMAKE_OPTS% -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% -DBUILD_SHARED_LIBS=ON -DAMD64=ON %PATH_SRC%\%1)
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+for %%Y in (cmake_install.cmake build.ninja) do (sed -i 's/zlibstatic\./zlib_static\./g' %CYGPATH_BUILD%/%1/%%Y)
+for %%Y in (cmake_install.cmake build.ninja) do (sed -i 's/zlib1\.dll/zlib\.dll/g' %CYGPATH_BUILD%/%1/%%Y)
 %NINJA% install
-xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\zlibstatic.dir\zlibstatic.pdb %PATH_INSTALL%\lib\zlibstatic.pdb*
-xcopy /C /F /Y %PATH_BUILD%\%1\zlib.pdb %PATH_INSTALL%\bin\zlib1.pdb*
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\zlib1.dll
+xcopy /C /F /Y %PATH_BUILD%\%1\CMakeFiles\zlib_static.dir\zlib_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\zlib.pdb %PATH_INSTALL%\bin\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\zlib.dll
