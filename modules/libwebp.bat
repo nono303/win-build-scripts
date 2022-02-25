6d484ac479313d01364346c1c48091d8cd3039f1
nono303 <git@nono303.net>
2022-02-25 16:52:02 +0100

    static > shared

--- modules/libwebp.bat
+++ modules/libwebp.bat
@@ -1,10 +1,11 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
-
+REM goto shared
 cmake %CMAKE_OPTS% ^
 	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
 	-DCMAKE_DISABLE_FIND_PACKAGE_PkgConfig=ON ^
 	-DGIF_LIBRARY=%PATH_INSTALL%\lib\giflib_static.lib ^
+	-DBUILD_SHARED_LIBS=ON ^
 	-DWEBP_ENABLE_SIMD=ON ^
 	-DWEBP_BUILD_ANIM_UTILS=ON ^
 	-DWEBP_BUILD_CWEBP=ON ^
@@ -23,23 +24,29 @@ cmake %CMAKE_OPTS% ^
 	-DWEBP_UNICODE=ON ^
 %PATH_SRC%\%1
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-%NINJA% install
+sed -i -E 's/WEBP_EXTERN extern/WEBP_EXTERN __declspec^(dllexport^)/g' %CYGPATH_SRC%/%1/src/webp/types.h
+%NINJA% webp.dll
+for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_BUILD%\%1\webp.%%X %PATH_INSTALL%\bin\*)
+xcopy /C /F /Y %PATH_BUILD%\%1\webp.lib %PATH_INSTALL%\lib\*
+if not exist %PATH_INSTALL%\include\webp\. mkdir %PATH_INSTALL%\include\webp
+for %%X in (decode.h types.h decode.h encode.h types.h decode.h demux.h mux_types.h types.h mux.h mux_types.h types.h) do (xcopy /C /F /Y %PATH_SRC%\%1\src\webp\%%X %PATH_INSTALL%\include\webp\*)
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\webp.dll
 
-for %%X in (cwebp dwebp gif2webp img2webp webpinfo webpmux vwebp) do (
-	xcopy /C /F /Y %PATH_BUILD%\%1\%%X.pdb %PATH_INSTALL%\bin\*
-	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X.exe
-)
+exit /B
+
+:cmakestatic
 	REM fix /lib pdb: webp.lib > vc140.pdb / webpdecoder.lib > vc140.pdb / webpmux.lib > libwebpmux.pdb
 for %%X in (webp webpdecoder webpdemux webpmux) do (
-	sed -i -E 's/TARGET_COMPILE_PDB.*/TARGET_COMPILE_PDB = %%~nX\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-	REM sed -i -E 's/TARGET_PDB.*/TARGET_PDB = %%~nX\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-	%NINJA% %%X.lib
-	move /Y %PATH_BUILD%\%1\%%X.lib %PATH_INSTALL%\lib\%%X.lib
-	move /Y %PATH_BUILD%\%1\%%X.pdb %PATH_INSTALL%\lib\%%X.pdb
+	sed -i -E 's/%%X\.lib/%%X_static\.lib/g' %CYGPATH_BUILD%/%1/build.ninja
+	sed -i -E 's/TARGET_COMPILE_PDB.*/TARGET_COMPILE_PDB = %%~nX_static\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
+	%NINJA% %%X.dll
+	move /Y %PATH_BUILD%\%1\%%X_static.lib %PATH_INSTALL%\lib\%%X_static.lib
+	move /Y %PATH_BUILD%\%1\%%X_static.pdb %PATH_INSTALL%\lib\%%X_static.pdb
 	del /S %PATH_BUILD%\%1\*.obj
 )
-exit /B
-	REM release-dynamic
+
+:nmakeshared
+	REM release-dynamic : pdb issue
 cd /D %PATH_SRC%\%1
 
 if not [%AVX%] == [] set AVXDEF=%AVX:/=\/%
