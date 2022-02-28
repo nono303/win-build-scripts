a13fa599401c480bb0808def1d4da0fe80257a62
nono303 <git@nono303.net>
2022-02-28 16:04:39 +0100

    disable tools/dav1d.exe (unused & conflict pdb with dll) manual copy of include

--- modules/dav1d.bat
+++ modules/dav1d.bat
@@ -15,6 +15,11 @@ meson ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/meson.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-%NINJA% install
+%NINJA% src/dav1d.dll
 
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\dav1d.exe
+for %%X in (dll pdb) do (xcopy /C /F /Y  %PATH_BUILD%\%1\src\dav1d.%%X %PATH_INSTALL%\bin\*)
+xcopy /C /F /Y  %PATH_BUILD%\%1\src\dav1d.lib %PATH_INSTALL%\lib\*
+if not exist %PATH_INSTALL%\include\dav1d\. mkdir %PATH_INSTALL%\include\dav1d
+xcopy /C /F /Y %PATH_SRC%\%1\include\dav1d\*.h %PATH_INSTALL%\include\dav1d\*
+xcopy /C /F /Y %PATH_BUILD%\%1\include\dav1d\version.h %PATH_INSTALL%\include\dav1d\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\dav1d.dll
