28c7211ae582a85fd68fb84d4c779bcae8e9fc4c
nono303 <git@nono303.net>
2022-03-03 17:08:35 +0100

    move from STATIC to SHARED build

--- modules/giflib.bat
+++ modules/giflib.bat
@@ -1,7 +1,6 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
 set VCDIR=5.2.1
-sed -i 's/GifLib/giflib_static/g' %CYGPATH_SRC%/%1/%VCDIR%/CMakeLists.txt
 cmake %CMAKE_OPTS% ^
 	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
@@ -10,5 +9,7 @@ cmake %CMAKE_OPTS% ^
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA%
 
-for %%X in (%PATH_BUILD%\%1\CMakeFiles\giflib_static.dir\giflib_static.pdb %PATH_SRC%\%1\%VCDIR%\lib\giflib_static.lib) do (xcopy /C /F /Y %%X %PATH_INSTALL%\lib\*)
-for %%X in (gif_lib.h gif_hash.h) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\include\%%X %PATH_INSTALL%\include\*)
+for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\lib\giflib.%%X %PATH_INSTALL%\bin\*)
+xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\lib\giflib.lib %PATH_INSTALL%\lib\*
+for %%X in (gif_lib.h gif_hash.h) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\source\%%X %PATH_INSTALL%\include\*)
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\giflib.dll
--- modules/wineditline.bat
+++ modules/wineditline.bat
@@ -6,18 +6,12 @@ cmake %CMAKE_OPTS% ^
 	%PATH_SRC%\%1 
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-%NINJA% install
+sed -i 's/\/DWIN32/\/DWIN32 \/DLIBRARY_EXPORTS=1/g' %CYGPATH_BUILD%/%1/build.ninja
+%NINJA% src\edit.dll
 
-for /f "tokens=*" %%G in ('dir %PATH_SRC%\%1\bin%CYGV%\*.dll %PATH_SRC%\%1\bin%CYGV%\*.exe /b') do (
-	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\bin%CYGV%\%%G
-	xcopy /C /F /Y %PATH_SRC%\%1\bin%CYGV%\%%G %PATH_INSTALL%\bin\*
-)
-for %%D in (bin lib) do (xcopy /C /F /Y %PATH_SRC%\%1\%%D%CYGV%\*.* %PATH_INSTALL%\%%D\*)
 if not exist %PATH_INSTALL%\include\editline\. mkdir %PATH_INSTALL%\include\editline
-xcopy /C /F /Y %PATH_SRC%\%1\include\editline\readline.h %PATH_INSTALL%\include\editline\*
-
-move /Y %PATH_INSTALL%\bin\edit.lib %PATH_INSTALL%\lib\edit.lib 
-for %%X in (edit_test.exe edit_test_dll.exe) do (del /Q /F %PATH_INSTALL%\bin\%%X)
+xcopy /C /F /Y %PATH_SRC%\%1\src\editline\readline.h %PATH_INSTALL%\include\editline\*
+xcopy /C /F /Y %PATH_BUILD%\%1\src\edit.dll %PATH_INSTALL%\bin\*
 xcopy /C /F /Y %PATH_BUILD%\%1\src\edit.pdb %PATH_INSTALL%\bin\*
-xcopy /C /F /Y %PATH_BUILD%\%1\src\CMakeFiles\edit_static.dir\edit_static.pdb %PATH_INSTALL%\lib\*
+xcopy /C /F /Y %PATH_BUILD%\%1\src\edit.lib %PATH_INSTALL%\lib\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\edit.dll
