beb3463d666160a88ba825b6ceccf8f6f25a639a
nono303 <git@nono303.net>
2022-03-09 15:05:54 +0100

    build STATIC > SHARED

--- modules/libmaxminddb.bat
+++ modules/libmaxminddb.bat
@@ -1,16 +1,20 @@
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
+REM goto shared
+cmake -Wno-dev -G "Visual Studio 17 2022" ^
+	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+	-DBUILD_SHARED_LIBS=ON ^
+	-DBUILD_TESTING=OFF ^
+%PATH_SRC%\%1
 
-cd /D %PATH_SRC%\%1\projects\VS12
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/projects/VS12/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-sed -i 's/vc$(PlatformToolsetVersion).pdb/$(ProjectName)_static.pdb/g' %CYGPATH_SRC%/%1/projects/VS12/libmaxminddb.props
-sed -i 's/Label="Configuration"^>/Label="Configuration"^>^<TargetName^>$(ProjectName)_static^<\/TargetName^>/g' %CYGPATH_SRC%/%1/projects/VS12/libmaxminddb.vcxproj
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES%/%1.sh "%PATH_BUILD:\=/%/%1/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
+set MAXMINDCONF=RelWithDebInfo
+MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\maxminddb.sln ^
+	%MSBUILD_OPTS% ^
+	/t:maxminddb ^
+	/p:Configuration=%MAXMINDCONF% ^
+	/p:Platform="%archmsbuild%"
 
-MSBuild.exe libmaxminddb.sln ^
-%MSBUILD_OPTS% ^
-/t:libmaxminddb ^
-/p:Configuration=Release ^
-/p:Platform="%archmsbuild%"
-
-for %%E in (lib pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\projects\VS12\%outmsbuild%\libmaxminddb_static.%%E %PATH_INSTALL%\lib\*)
-xcopy /C /F /Y %PATH_SRC%\%1\include\maxminddb.h %PATH_INSTALL%\include\*
-xcopy /C /F /Y %PATH_SRC%\%1\include\maxminddb_config.h %PATH_INSTALL%\include\*
+xcopy /C /F /Y %PATH_BUILD%\%1\%MAXMINDCONF%\maxminddb.lib %PATH_INSTALL%\lib\*
+for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%MAXMINDCONF%\maxminddb.%%X %PATH_INSTALL%\bin\*)
+for %%X in (maxminddb.h maxminddb_config.h) do (xcopy /C /F /Y  %PATH_SRC%\%1\include\%%X %PATH_INSTALL%\include\*)
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\maxminddb.dll
---
+++ modules/libmaxminddb.sh
@@ -0,0 +1,14 @@
+link="<EnableCOMDATFolding>true<\/EnableCOMDATFolding><OptimizeReferences>true<\/OptimizeReferences>"
+for i in `/usr/bin/find $1 -type f -name "*.vcxproj" -o -name "*.config" -o -name "*.csproj" -o -name "*.props" 2>/dev/null` 
+do
+  # MSVC
+  sed -i -E 's/<PlatformToolset>[<]+/<VCToolsVersion>'$5'<\/VCToolsVersion><PlatformToolset>v'$3'/g' $i
+  # Windows Kit
+  sed -i -E 's/<WindowsTargetPlatformVersion>[<]+/<WindowsTargetPlatformVersion>'$4'/g' $i
+  # .NET
+  sed -i -E 's/<TargetFrameworkVersion>[<]+/<TargetFrameworkVersion>v'$6'/g' $i
+  sed -i -E 's/NETFramework,Version=["]+/NETFramework,Version=v'$6'/g' $i
+  # C CXX Flags
+  sed -i -E 's/<Optimization>[<]+/<Optimization>MaxSpeed/g' $i
+  sed -i 's/<\/ClCompile>/<Optimization>MaxSpeed<\/Optimization><MultiProcessorCompilation>true<\/MultiProcessorCompilation>'$2'<\/ClCompile>/g' $i
+done
