53fa33c3965ad3a470b4807a429a13b1c6fcaddf
nono303 <git@nono303.net>
2022-03-18 08:58:09 +0100

    71.1: refactor & limit target

--- modules/icu.bat
+++ modules/icu.bat
@@ -1,3 +1,4 @@
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1
 REM ?icutu         # tool util
 REM ?icudt         # stub data
 REM +icuuc         # Common and Data libraries
@@ -6,8 +7,6 @@ REM +icuio         # Stream and I/O Library
 REM -icule         # Layout library
 REM -iculx         # Paragraph Layout library
 
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1
-
 FOR /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\grep 'U_ICU_VERSION_SHORT' /cygdrive/c/sdk/src/icu/icu4c/source/common/unicode/uvernum.h ^| %PATH_BIN_CYGWIN%\tr -dc '0-9'`) do (set ICUV=%%F)
 set VCDIR=icu4c\source\allinone
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/icu4c" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
@@ -15,15 +14,15 @@ set VCDIR=icu4c\source\allinone
 REM icudtXX.pdb
 sed -i 's/NXCOMPAT/NXCOMPAT \/OPT:REF,ICF \/DEBUG/g' %CYGPATH_SRC%/%1/icu4c/source/tools/pkgdata/pkgdata.cpp
 
-REM https://unicode-org.github.io/icu/userguide/icu4c/build.html#skipping-the-uwp-projects-on-the-command-line
-REM 	/t:cal,cintltst,common,ctestfw,date,derb,genbrk,genccode,gencmn,gencnval,genrb,gentest,i18n,intltest,makeconv,makedata,pkgdata,stubdata,toolutil,uconv,io,gensprep,iotest,icupkg,gendict,gencfu,gennorm2,icuinfo,testplug,makedata_uwp,i18n_uwp,common_uwp, ^
-	MSBuild.exe %PATH_SRC%\%1\%VCDIR%\allinone.sln ^
-	%MSBUILD_OPTS% ^
-	/t:cal,cintltst,common,date,derb,genbrk,genccode,gencmn,gencnval,genrb,i18n,makeconv,makedata,pkgdata,stubdata,toolutil,uconv,io,gensprep,icupkg,gendict,gencfu,gennorm2,icuinfo ^
-	/nowarn:C4018;C4116;C4267;C4312;C4146;MSB8028 ^
-	/p:Configuration=Release ^
-	/p:SkipUWP=true ^
-	/p:Platform=%archmsbuild%
+	REM	https://unicode-org.github.io/icu/userguide/icu4c/build.html#skipping-the-uwp-projects-on-the-command-line
+	REM 	derb,genbrk,genccode,gencfu,gencmn,gencnval,gendict,gennorm2,genrb,gensprep,icuinfo,icupkg,makeconv,uconv
+MSBuild.exe %PATH_SRC%\%1\%VCDIR%\allinone.sln ^
+%MSBUILD_OPTS% ^
+/t:cal,cintltst,common,date,i18n,icuexportdata,io,makedata,pkgdata,stubdata,toolutil ^
+/nowarn:C4018;C4116;C4267;C4312;C4146;MSB8028 ^
+/p:Configuration=Release ^
+/p:SkipUWP=true ^
+/p:Platform=%archmsbuild%
 
 for %%C in (dt in io tu uc) do (
 	xcopy /C /F /Y %PATH_SRC%\icu\icu4c\bin64\icu%%C%ICUV%.dll %PATH_INSTALL%\bin\*
