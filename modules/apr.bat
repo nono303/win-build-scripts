ef25bfd65cd06701950cc722989c75870805c454
nono303 <git@nono303.net>
2022-04-06 15:59:33 +0200

    some rewite and fix OpenSSL lib path

--- modules/apr.bat
+++ modules/apr.bat
@@ -11,26 +11,31 @@ set INCLUDE=%INCLUDE%;%PATH_INSTALL%\include;%PATH_INSTALL%\include\sqlite3
 
 	REM https://www.apachelounge.com/viewtopic.php?t=8260 / https://docs.microsoft.com/fr-fr/cpp/porting/modifying-winver-and-win32-winnt?view=vs-2019
 sed -i 's/_WIN32_WINNT 0x0501/_WIN32_WINNT 0x0601/g' %PATH_SRC%/apr/include/apr.hw
-sed -i 's/..\\\\openssl\\\\libcrypto.lib/%PATH_INSTALL:\=\\\\%\\\\lib\\\\libcrypto.lib/g' %PATH_SRC%/apr-util/Makefile.win
+
 cd /D %PATH_SRC%\apr-util
-echo # apply apr_post.patch && git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\apr_post.patch
+echo # apply apr_post.patch 
+git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\apr_post.patch
+
 sed -i 's/MAKEOPT=-nologo/MAKEOPT=%NMAKE_OPTS:/=\/%/g' %PATH_SRC%/apr-util/Makefile.win
 
 	REM ICU shared sqlite3
 for %%X in (mak dsp) do (sed -i 's/sqlite3.lib/libsqlite3.lib %PATH_INSTALL:\=\\/%\/lib\/icuuc.lib %PATH_INSTALL:\=\\/%\/lib\/icuin.lib/g' %PATH_SRC%/apr-util/dbd/apr_dbd_sqlite3.%%X)
+	REM OpenSSL lib path
+sed -i 's/..\\\\openssl\\\\libcrypto.lib/%PATH_INSTALL:\=\\\\%\\\\lib\\\\libcrypto.lib/g' %PATH_SRC%/apr-util/Makefile.win
+for %%X in (mak dsp) do (sed -i 's/\$^(SSLCRP^)\.lib \$^(SSLLIB^)\.lib/%PATH_INSTALL:\=\\/%\/lib\/libssl.lib %PATH_INSTALL:\=\\/%\/lib\/libcrypto.lib/g' %PATH_SRC%/apr-util/crypto/apr_crypto_openssl.%%X)
 
 nmake %NMAKE_OPTS% /f Makefile.win ^
-	USEMAK=1 ^
-	ARCH="%archmsbuild% Release" ^
-	APU_PATH=%PATH_SRC%\apr-util ^
-	API_PATH=%PATH_SRC%\apr-iconv ^
-	APR_PATH=%PATH_SRC%\apr ^
-	DBD_LIST="sqlite3" ^
-	XML_PARSER="libexpat" ^
-	CRYPTO_LIST="openssl" ^
-	PREFIX=%PATH_INSTALL% ^
-	SystemRoot=%SystemRoot% ^
-	buildall install
+USEMAK=1 ^
+ARCH="%archmsbuild% Release" ^
+APU_PATH=%PATH_SRC%\apr-util ^
+API_PATH=%PATH_SRC%\apr-iconv ^
+APR_PATH=%PATH_SRC%\apr ^
+DBD_LIST="sqlite3" ^
+XML_PARSER="libexpat" ^
+CRYPTO_LIST="openssl" ^
+PREFIX=%PATH_INSTALL% ^
+SystemRoot=%SystemRoot% ^
+buildall install
 
 REM pdb & lib are not build in same folder according to ARCH...
 for %%D in (apr\apr-1.pdb apr-util\aprutil-1.pdb apr-iconv\apriconv-1.pdb apr-iconv\apriconv-1.lib apr-util\crypto\apr_crypto_openssl-1.lib apr-util\dbd\apr_dbd_odbc-1.lib apr-util\dbd\apr_dbd_sqlite3-1.lib apr-util\ldap\apr_ldap-1.lib apr-iconv\libapriconv-1.lib) do (
