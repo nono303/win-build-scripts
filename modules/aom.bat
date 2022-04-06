8efb7c689285c4bd13b5a33ff0eaeae7dd5f96a4
nono303 <git@nono303.net>
2022-04-06 15:55:26 +0200

    rewrite cmake target VS

--- modules/aom.bat
+++ modules/aom.bat
@@ -1,7 +1,7 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
 	REM ninja: error: build.ninja:288: multiple rules generate aom.lib [-w dupbuild=err]
-cmake -Wno-dev -G "Visual Studio 17 2022" ^
+cmake %CMAKE_OPTS% -G %CMAKE_TGT_VS% -A %archmsbuild% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
 -DBUILD_SHARED_LIBS=1 ^
@@ -13,15 +13,13 @@ cmake -Wno-dev -G "Visual Studio 17 2022" ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%PATH_BUILD:\=/%/%1/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-set AOMCONF=RelWithDebInfo
-MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\AOM.sln ^
-	%MSBUILD_OPTS% ^
-	/t:aom ^
-	/p:Configuration=%AOMCONF% ^
-	/p:Platform="%archmsbuild%"
+MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\AOM.sln %MSBUILD_OPTS% ^
+/t:aom ^
+/p:Configuration=%CMAKE_BUILD_TYPE% ^
+/p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_BUILD%\%1\%AOMCONF%\aom.lib %PATH_INSTALL%\lib\*
-for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%AOMCONF%\aom.%%X %PATH_INSTALL%\bin\*)
+xcopy /C /F /Y %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\aom.lib %PATH_INSTALL%\lib\*
+for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\aom.%%X %PATH_INSTALL%\bin\*)
 if not exist %PATH_INSTALL%\include\aom\. mkdir %PATH_INSTALL%\include\aom
 xcopy /C /F /Y  %PATH_SRC%\%1\aom\*.h %PATH_INSTALL%\include\aom\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\aom.dll
--- modules/libmaxminddb.bat
+++ modules/libmaxminddb.bat
@@ -1,20 +1,19 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 REM goto shared
-cmake -Wno-dev -G "Visual Studio 17 2022" ^
-	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
-	-DBUILD_SHARED_LIBS=ON ^
-	-DBUILD_TESTING=OFF ^
+cmake %CMAKE_OPTS% -G %CMAKE_TGT_VS% -A %archmsbuild% ^
+-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+-DBUILD_SHARED_LIBS=ON ^
+-DBUILD_TESTING=OFF ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES%/%1.sh "%PATH_BUILD:\=/%/%1/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-set MAXMINDCONF=RelWithDebInfo
-MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\maxminddb.sln ^
-	%MSBUILD_OPTS% ^
-	/t:maxminddb ^
-	/p:Configuration=%MAXMINDCONF% ^
-	/p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_BUILD%\%1\%MAXMINDCONF%\maxminddb.lib %PATH_INSTALL%\lib\*
-for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%MAXMINDCONF%\maxminddb.%%X %PATH_INSTALL%\bin\*)
+MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\maxminddb.sln %MSBUILD_OPTS% ^
+/t:maxminddb ^
+/p:Configuration=%CMAKE_BUILD_TYPE% ^
+/p:Platform="%archmsbuild%"
+
+xcopy /C /F /Y %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\maxminddb.lib %PATH_INSTALL%\lib\*
+for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\maxminddb.%%X %PATH_INSTALL%\bin\*)
 for %%X in (maxminddb.h maxminddb_config.h) do (xcopy /C /F /Y  %PATH_SRC%\%1\include\%%X %PATH_INSTALL%\include\*)
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\maxminddb.dll
