4f270f60e4addfc097631545c96d5dd7d74a0fa3
nono303 <git@nono303.net>
2022-05-21 09:06:57 +0200

    update configexport to include python

--- utils/configexport.bat
+++ utils/configexport.bat
@@ -2,4 +2,5 @@ for %%A in (32 64) do (
 	REM https://gist.github.com/erikvip/63b236078d4ff4163fd3
 	call %PATH_ROOT_CYGWIN%%%A\bin\cygcheck -cd | tail -n+3 | cut -d' ' -f1 > %PATH_BATCH%\config\cyg%%A.packagelist 
 )
-call %BIN_VSINSTALLER% export --installPath "%PATH_VS%" --config "%PATH_BATCH%\config\.vsconfig"
+REM call %BIN_VSINSTALLER% export --installPath "%PATH_VS%" --config "%PATH_BATCH%\config\.vsconfig"
+call pip list > %PATH_BATCH%\config\python.pip
