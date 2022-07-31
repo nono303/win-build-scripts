e083722cbeb6ca75bef205fb0ef078ad0e3d8971
nono303 <git@nono303.net>
2022-07-31 17:26:45 +0200

    change scm (1.17)

--- modules/libiconv.bat
+++ modules/libiconv.bat
@@ -3,19 +3,21 @@
 REM https://bugs.php.net/bug.php?id=29362
 REM https://www.codeproject.com/Articles/302012/How-to-Build-libiconv-with-Microsoft-Visual-Studio
 
-set VCDIR=MSVC%MSVC_VER%
 set OUTDIR_CONF=Release
 
-if not exist %PATH_SRC%\%1\%VCDIR%\. mklink /J %PATH_SRC%\%1\%VCDIR% %PATH_SRC%\%1\MSVC16
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/%VCDIR%/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
+if not exist %PATH_SRC%\%1\. mklink /J %PATH_SRC%\%1 %PATH_SRC%\%1\MSVC16
+%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
 
-MSBuild.exe %PATH_SRC%\%1\%VCDIR%\%1.sln %MSBUILD_OPTS% ^
-/t:Clean,%1_dll:Rebuild ^
-/nowarn:C4267;C4090;C4018 ^
+MSBuild.exe %PATH_SRC%\%1\%1.sln %MSBUILD_OPTS% ^
+/t:Clean,%1 ^
+/nowarn:C4311,C4117,C4267,C4141,C4090 ^
 /p:Configuration=%OUTDIR_CONF% ^
 /p:Platform="%archmsbuild%"
 
-xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\lib\libiconv.lib %PATH_INSTALL%\lib\*
-for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%VCDIR%\%archmsbuild%\bin\libiconv.%%X %PATH_INSTALL%\bin\*)
-xcopy /C /F /Y %PATH_SRC%\%1\source\include\iconv.h %PATH_INSTALL%\include\*
+set LIBDIR=lib
+if %CYGV% == 64 (set LIBDIR=lib64)
+
+xcopy /C /F /Y %PATH_SRC%\%1\%LIBDIR%\libiconv.lib %PATH_INSTALL%\lib\*
+for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\%LIBDIR%\libiconv.%%X %PATH_INSTALL%\bin\*)
+xcopy /C /F /Y %PATH_SRC%\%1\include\iconv.h %PATH_INSTALL%\include\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libiconv.dll
