980d3c1dbd7d6fb644ec4e074a921397755ac7ec
nono303 <git@nono303.net>
2022-09-20 16:55:36 +0200

    REM: big message when building openssl add HTTPD_VERSION in so version

--- modules/mod_md.bat
+++ modules/mod_md.bat
@@ -1,7 +1,18 @@
 @echo off
 	REM ensure openssl native is set & build
 call quic 0
-if not exist %PATH_INSTALL_OSSL%\lib\libssl.lib call go openssl
+if not exist %PATH_INSTALL_OSSL%\lib\libssl.lib (
+	@echo on
+	echo ###########################
+	echo # %PATH_INSTALL_OSSL%\lib\libssl.lib not found
+	echo # building openssl
+	echo ###########################
+	@echo off
+	call go openssl
+)
+
+call %PATH_MODULES_COMMON%\init.bat httpd varonly
+set HTTPD_VERSION=%SCM_TAG%
 
 call %PATH_MODULES_COMMON%\init.bat %1 cmake
 cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
@@ -21,7 +32,7 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 
 if not "%PATH_GITHUB_MODMD%"=="" (
 	for %%X in (so pdb) do (xcopy /C /F /Y %PATH_BUILD%\%1\modules\mod_md.%%X %PATH_GITHUB_MODMD%\%MSVC_DEPS%\%ARCH%%AVXB%\*)
-	call do_php %PATH_UTILS%\sub\version.php mod_md %PATH_GITHUB_MODMD%\%MSVC_DEPS%\%ARCH%%AVXB%\mod_md.so
+	call do_php %PATH_UTILS%\sub\version.php mod_md %PATH_GITHUB_MODMD%\%MSVC_DEPS%\%ARCH%%AVXB%\mod_md.so "httpd:%HTTPD_VERSION%"
 	xcopy /C /F /Y %PATH_INSTALL%\bin\libcurl.* %PATH_GITHUB_MODMD%\%MSVC_DEPS%\%ARCH%%AVXB%\*
 )
 FOR /F "tokens=* USEBACKQ" %%F in (`dumpbin /DEPENDENTS D:\github\NONO_mod_md\vs17\x64-avx\mod_md.so ^| grep crypto`) do (set MD_CRYPTO=%%F)
