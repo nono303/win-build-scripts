dc6cc1e067a93c3ce029e5a290110bb8bd0df454
nono303 <git@nono303.net>
2022-10-24 11:35:20 +0200

    fix date format

--- modules/common/ymdhis.bat
+++ modules/common/ymdhis.bat
@@ -1,3 +1,4 @@
 @echo off
-set ymdhis=%Date:~-4%-%Date:~-7,-5%-%Date:~-10,-8%_%TIME:~-11,-9%-%TIME:~-8,-6%-%TIME:~-5,-3%
+set ymdhis=%Date%_%TIME:~-11,-9%-%TIME:~-8,-6%-%TIME:~-5,-3%
 set ymdhis=%ymdhis: =0%
+pause
