62751a3980883a2774ea3a0711cdaa50215c8826
nono303 <git@nono303.net>
2022-12-02 08:50:52 +0100

    junction for local .gradle to %userprofile%

--- modules/mapsforgesrv.bat
+++ modules/mapsforgesrv.bat
@@ -1,3 +1,4 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
+if exist %userprofile%\.gradle if not exist %PATH_SRC%\%1\%1\.gradle\. mklink /J %PATH_SRC%\%1\%1\.gradle %userprofile%\.gradle
 cd /D %PATH_SRC%\%1\%1
 call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar 2>&1
