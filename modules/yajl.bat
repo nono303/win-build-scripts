3195394e417770193b58635e0060a50cd350e2c6
nono303 <git@nono303.net>
2023-01-05 19:06:36 +0100

    fix yajl_static path issue

--- modules/yajl.bat
+++ modules/yajl.bat
@@ -8,5 +8,6 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA% install
 
-for %%X in (%PATH_INSTALL%\lib\yajl_static.lib %PATH_BUILD%\%1\src\CMakeFiles\yajl_static.dir\yajl_static.pdb) do (move /Y %%X %PATH_INSTALL%\lib_unused)
+if exist %PATH_INSTALL%\lib\yajl_static.lib del /Q /F %PATH_INSTALL%\lib\yajl_static.lib
+for %%X in (%PATH_BUILD%\%1\yajl-2.1.1\lib\yajl_static.lib %PATH_BUILD%\%1\src\CMakeFiles\yajl_static.dir\yajl_static.pdb) do (xcopy /C /F /Y %%X %PATH_INSTALL%\lib_unused\*)
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\yajl.dll
