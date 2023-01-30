c525b168585b6448e934d9a656d670e5d5b97397
nono303 <git@nono303.net>
2023-01-30 20:45:37 +0100

    CARES_BUILD_TOOLS: ON > OFF

--- modules/cares.bat
+++ modules/cares.bat
@@ -12,11 +12,13 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCARES_INSTALL=ON ^
 -DCARES_STATIC_PIC=OFF ^
 -DCARES_BUILD_TESTS=OFF ^
+-DCARES_BUILD_CONTAINER_TESTS=OFF ^
+-DCARES_BUILD_TOOLS=OFF ^
 -DCARES_MSVC_STATIC_RUNTIME=OFF ^
-%PATH_SRC%\%1 
+%PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA% install
 
-for %%X in (cares acountry adig ahost) do (xcopy /C /F /Y %PATH_BUILD%\%1\bin\%%X.pdb %PATH_INSTALL%\bin\*)
-for %%X in (cares.dll acountry.exe adig.exe ahost.exe) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X)
+xcopy /C /F /Y %PATH_BUILD%\%1\bin\cares.pdb %PATH_INSTALL%\bin\*
+call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\cares.dll
