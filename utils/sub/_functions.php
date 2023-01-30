20e288dd14386784cada1e6c8ddc70b4d65a706e
nono303 <git@nono303.net>
2023-01-30 22:15:17 +0100

    php bin in php-sdk: 7 > 8

--- utils/sub/_functions.php
+++ utils/sub/_functions.php
@@ -1,5 +1,5 @@
 <?php
-	error_reporting(E_ALL & ~E_NOTICE);
+	error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 	function replace_extension($filename, $new_extension) {
 		return preg_replace('/\.[^\.]+$/', '.' . $new_extension, $filename);
@@ -16,34 +16,7 @@
 	}
 
 	function execnono($cmd,$parts,$cwd,$env){
-		$args = '';
-		if($parts){
-			foreach ($parts as $k => $part) {
-				if (is_string($k)) {
-					$args .= ' ' . escapeshellarg($k) . ' ' . escapeshellarg($part);
-				} else {
-					$args .= ' ' . escapeshellarg($part);
-				}
-			}
-		}
-		$process_cmd = '"' . $cmd . '"' . ' ' . $args;
-		$options = array(
-			"suppress_errors" => false,
-			"bypass_shell" => false
-		);
-		$descriptorspec = array(
-		   0 => array("pipe", "r"), // stdin is a pipe that the child will read from
-		   1 => array("pipe", "w"), // stdout is a pipe that the child will write to
-		   2 => array("pipe", "w")  // stderr is a file to write to
-		);
-		$process = proc_open($process_cmd, $descriptorspec, $pipes, $cwd, $env, $options);
-		if (is_resource($process)) {
-			$out = stream_get_contents($pipes[1]);
-			fclose($pipes[1]);
-			$return_value = proc_close($process);
-			return trim($out);
-		}
-		return null;
+		return trim(shell_exec("cd /D ".$cwd." && ".$cmd));
 	}
 
 	function getLastModifiedFile($dir){
