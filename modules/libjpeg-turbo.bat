fed5c1434db9f0f7810a013dbc12da8291fef220
nono303 <git@nono303.net>
2023-01-31 11:37:12 +0100

    rollback

--- modules/libjpeg-turbo.bat
+++ modules/libjpeg-turbo.bat
@@ -6,41 +6,6 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DENABLE_SHARED=1 ^
 -DENABLE_STATIC=0 ^
 -DREQUIRE_SIMD=0 ^
--DWITH_12BIT=1 ^
--DWITH_ARITH_DEC=0 ^
--DWITH_ARITH_ENC=0 ^
--DWITH_JAVA=0 ^
--DWITH_JPEG7=1 ^
--DWITH_JPEG8=1 ^
--DWITH_MEM_SRCDST=1 ^
--DWITH_SIMD=0 ^
--DWITH_TURBOJPEG=0 ^
--DWITH_FUZZ=0 ^
--DWITH_CRT_DLL=0 ^
--DFORCE_INLINE=1 ^
--DWIN32=1 ^
-%PATH_SRC%\%1
-
-%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-for %%X in (dll pdb) do (for %%Y in (sharedlib/cmake_install.cmake build.ninja) do (sed -i 's/jpeg8\.%%X/jpeg\.%%X/g' %CYGPATH_BUILD%/%1/%%Y))
-sed -i -E 's/TARGET_COMPILE_PDB.*/TARGET_COMPILE_PDB = %%X\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-set JPEG_TARGET=jpeg
-%NINJA% %JPEG_TARGET%
-
-for %%X in (%PATH_BUILD%\%1\jconfig.h %PATH_SRC%\%1\jerror.h %PATH_SRC%\%1\jmorecfg.h %PATH_SRC%\%1\jpeglib.h %PATH_SRC%\%1\turbojpeg.h) do (
-	xcopy /C /F /Y %%X %PATH_INSTALL%\include\*
-)
-move /Y %PATH_BUILD%\%1\%JPEG_TARGET%.lib %PATH_INSTALL%\lib\%JPEG_TARGET%.lib
-for %%Y in (pdb dll) do (move /Y %PATH_BUILD%\%1\%JPEG_TARGET%.%%Y %PATH_INSTALL%\bin\%JPEG_TARGET%.%%Y)
-del /S %PATH_BUILD%\%1\*.obj
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%JPEG_TARGET%.dll
-
-cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
--DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
--DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
--DENABLE_SHARED=1 ^
--DENABLE_STATIC=0 ^
--DREQUIRE_SIMD=0 ^
 -DWITH_12BIT=0 ^
 -DWITH_ARITH_DEC=1 ^
 -DWITH_ARITH_ENC=1 ^
@@ -53,16 +18,22 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DWITH_FUZZ=0 ^
 -DWITH_CRT_DLL=0 ^
 -DFORCE_INLINE=1 ^
--DNEON_INTRINSICS=0 ^
--DWIN32=1 ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-sed -i -E 's/TARGET_COMPILE_PDB.*/TARGET_COMPILE_PDB = %%X\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
-set JPEG_TARGET=turbojpeg
-%NINJA% %JPEG_TARGET%
+for %%Y in (cmake_install.cmake build.ninja) do (sed -i 's/-static\./_static\./g' %CYGPATH_BUILD%/%1/%%Y)
+for %%X in (dll pdb) do (
+	for %%Y in (sharedlib/cmake_install.cmake build.ninja) do (sed -i 's/jpeg8\.%%X/jpeg\.%%X/g' %CYGPATH_BUILD%/%1/%%Y)
+)
 
-move /Y %PATH_BUILD%\%1\%JPEG_TARGET%.lib %PATH_INSTALL%\lib\%JPEG_TARGET%.lib
-for %%Y in (pdb dll) do (move /Y %PATH_BUILD%\%1\%JPEG_TARGET%.%%Y %PATH_INSTALL%\bin\%JPEG_TARGET%.%%Y)
-del /S %PATH_BUILD%\%1\*.obj
-call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%JPEG_TARGET%.dll
+for %%X in (turbojpeg jpeg) do (
+	sed -i -E 's/TARGET_COMPILE_PDB.*/TARGET_COMPILE_PDB = %%X\.pdb/g' %CYGPATH_BUILD%/%1/build.ninja
+	%NINJA% %%X
+	move /Y %PATH_BUILD%\%1\%%X.lib %PATH_INSTALL%\lib\%%X.lib
+	for %%Y in (pdb dll) do (move /Y %PATH_BUILD%\%1\%%X.%%Y %PATH_INSTALL%\bin\%%X.%%Y)
+	del /S %PATH_BUILD%\%1\*.obj
+	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X.dll
+)
+for %%X in (%PATH_BUILD%\%1\jconfig.h %PATH_SRC%\%1\jerror.h %PATH_SRC%\%1\jmorecfg.h %PATH_SRC%\%1\jpeglib.h %PATH_SRC%\%1\turbojpeg.h) do (
+	xcopy /C /F /Y %%X %PATH_INSTALL%\include\*
+)
