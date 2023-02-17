0c2e889327a80f5db69b1f01d5928f3f2ce49096
nono303 <git@nono303.net>
2023-02-17 16:10:22 +0100

    fix tmp shortcut

--- modules/qhull.bat
+++ modules/qhull.bat
@@ -1,5 +1,5 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
-goto tmp
+
 cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
@@ -13,6 +13,6 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA% install
-:tmp
+
 xcopy /C /F /Y %PATH_BUILD%\%1\qhull_r.pdb %PATH_INSTALL%\bin\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\qhull_r.dll
