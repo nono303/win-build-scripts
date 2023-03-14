faebabc8fce04abff669668f9cb772cf7b41e39b
nono303 <git@nono303.net>
2023-03-14 20:10:46 +0100

    fix & clean

--- modules/sdlna.bat
+++ modules/sdlna.bat
@@ -3,12 +3,10 @@
 set OUTDIR_CONF=Release
 
 MSBuild.exe sdlna.sln %MSBUILD_OPTS% ^
-/t:clean,Build ^
+/t:clean,restore,Build ^
 /p:Configuration=%OUTDIR_CONF% ^
-/p:Platform="Any CPU"
+/p:Platform="Any CPU" ^
+/p:RestorePackagesConfig=true
 
-for %%X in (x64 x86) do (xcopy /C /F /Y %PATH_SRC%\%1\packages\System.Data.SQLite.Core.1.0.112.0\build\net46\%%X\ %PATH_SRC%\%1\build)
-del /Q /F %PATH_SRC%\%1\build\SQLite.Interop.dll
 xcopy /C /F /Y %PATH_SRC%\%1\assets\nono\ico.ico %PATH_SRC%\%1\build\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\build\sdlna.exe
-call do_php %PATH_UTILS%\sub\bininfo.php %PATH_SRC%\%1\build\sdlna.exe checkavx
