4598150d452d3029e1a1036191ffaf13ff04b3c8
nono303 <git@nono303.net>
2023-06-05 10:39:42 +0200

    tidy: 5.9.14 > 5.9.20

--- utils/sub/version.php
+++ utils/sub/version.php
@@ -14,6 +14,8 @@
 	if(in_array($proot,["pecl-memcache","php-geos","pecl-text-xdiff","php-ext-brotli","xdebug","php-src"]))
 		$proot = "php";
 	$nogit = array(
+		"tidy"				=> ["/([0-9]{1,3}\.[0-9]+\.[0-9]+)/s",
+							pathenv("PATH_SRC")."/".$argv[1]."/version.txt"],
 		"libssh2"			=> ["/#define LIBSSH2_VERSION_MAJOR *([0-9]+).*#define LIBSSH2_VERSION_MINOR *([0-9]+).*#define LIBSSH2_VERSION_PATCH *([0-9]+)/s",
 							pathenv("PATH_SRC")."/".$argv[1]."/include/libssh2.h"],
 		"mod_fcgid"			=> ["/#define MODFCGID_VERSION_MAJOR *([0-9]+).*#define MODFCGID_VERSION_MINOR *([0-9]+).*#define MODFCGID_VERSION_SUBVER *([0-9]+).*#define MODFCGID_VERSION_DEV *([0-9]+)/s",
