4d4537402c8eec498f41e1e4ed432726208dcd75
nono303 <git@nono303.net>
2023-07-07 18:29:58 +0200

    typo

--- modules/tzdataservice.bat
+++ modules/tzdataservice.bat
@@ -1,5 +1,9 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
 if exist %userprofile%\.gradle if not exist %PATH_SRC%\%1\.gradle\. mklink /J %PATH_SRC%\%1\.gradle %userprofile%\.gradle
 cd /D %PATH_SRC%\%1
+
+echo ^>^>^> gradlew wrapper --gradle-version %GRADLEVER%
 call gradlew wrapper --gradle-version %GRADLEVER%
+echo ^<^<^< gradlew wrapper --gradle-version %GRADLEVER%
+
 call gradlew --warning-mode all --no-daemon --console=verbose --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar 2>&1
