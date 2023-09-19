b54190cbf23551f8d743636dc15963ad2f54c263
nono303 <git@nono303.net>
2023-09-19 07:14:13 +0200

    3.7.0 msbuild > ninja fix https://aomedia-review.googlesource.com/q/I36a4db36fb67237e94f1db3b0042ce219446e559 fix ninja: error: build.ninja:288: multiple rules generate aom.lib [-w dupbuild=err]

--- modules/aom.bat
+++ modules/aom.bat
@@ -1,10 +1,9 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
-	REM ninja: error: build.ninja:288: multiple rules generate aom.lib [-w dupbuild=err]
-cmake %CMAKE_OPTS% -G %CMAKE_TGT_VS% -A %archmsbuild% ^
+cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
--DBUILD_SHARED_LIBS=1 ^
+-DBUILD_SHARED_LIBS=ON ^
 -DENABLE_CCACHE=OFF ^
 -DENABLE_DECODE_PERF_TESTS=OFF ^
 -DENABLE_DISTCC=OFF ^
@@ -30,14 +29,8 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_VS% -A %archmsbuild% ^
 -DENABLE_AVX2=OFF ^
 %PATH_SRC%\%1
 
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%PATH_BUILD:\=/%/%1/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\AOM.sln %MSBUILD_OPTS% ^
-/t:aom ^
-/p:Configuration=%CMAKE_BUILD_TYPE% ^
-/p:Platform="%archmsbuild%"
+%NINJA% install
 
-xcopy /C /F /Y %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\aom.lib %PATH_INSTALL%\lib\*
-for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\aom.%%X %PATH_INSTALL%\bin\*)
-if not exist %PATH_INSTALL%\include\aom\. mkdir %PATH_INSTALL%\include\aom
-xcopy /C /F /Y  %PATH_SRC%\%1\aom\*.h %PATH_INSTALL%\include\aom\*
+xcopy /C /F /Y  %PATH_BUILD%\%1\aom.pdb %PATH_INSTALL%\bin\*
+move /Y %PATH_INSTALL%\lib\*_static.* %PATH_INSTALL%\%DIR_LIB_UNUSED%
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\aom.dll
