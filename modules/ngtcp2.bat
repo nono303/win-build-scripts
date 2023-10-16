2b431a4380e2941a7da4993765ea88d5da5509d2
nono303 <git@nono303.net>
2023-10-16 10:48:40 +0200

    typo

--- modules/ngtcp2.bat
+++ modules/ngtcp2.bat
@@ -26,6 +26,4 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 %NINJA% lib/install crypto/install
 
 for %%X in (lib\ngtcp2.pdb crypto\quictls\ngtcp2_crypto_quictls.pdb) do (xcopy /C /F /Y %PATH_BUILD%\%1\%%X %PATH_INSTALL%\bin\*)
-for %%X in (ngtcp2 ngtcp2_crypto_quictls) do (
-	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X.dll
-)
+for %%X in (ngtcp2 ngtcp2_crypto_quictls) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X.dll)
