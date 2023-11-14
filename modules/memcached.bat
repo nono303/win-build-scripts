7d1e5557966addd266892862097c62cb668f1b1b
nono303 <git@nono303.net>
2023-11-14 14:47:48 +0100

    rewrite & simplify

--- modules/memcached.bat
+++ modules/memcached.bat
@@ -1,8 +1,18 @@
+REM 1>	tr606
+REM	q: <> \\jupiter8.lan\c$
+REM	/cygdrive/q/sdk/batch/modules/memcached.sh /cygdrive/q/sdk/src/memcached /cygdrive/q/sdk/batch/modules /cygdrive/d/github/NONO_memcached/libevent-2.0
+REM old>	tb303
+REM	q: <> \\jupiter8.vpnu\c$
+REM	r: <> \\tr606.vpnu\d$
+REM	fecth / checkout / clean / reset D:\git\github.memcached
+REM	/cygdrive/q/sdk/batch/modules/memcached.sh /cygdrive/d/git/github.memcached/ /cygdrive/q/sdk/batch/modules /cygdrive/r/github/NONO_memcached/libevent-2.0
+REM 2>	jupiter8
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
-	REM 32 if need to rebuild x86
-for %%X in (64) do (call %PATH_ROOT_CYGWIN%%%X\bin\bash --login -c '%CYGPATH_MODULES%/%1.sh %CYGPATH_SRC%/%1 %CYGPATH_MODULES% %PATH_RELEASE_MEMCACHED%/libevent-2.1')
+set SSLH_TARGET_ARCH=64
+
+call %PATH_ROOT_CYGWIN%%SSLH_TARGET_ARCH%\bin\bash --login -c '%CYGPATH_MODULES%/%1.sh %CYGPATH_SRC%/%1 %CYGPATH_MODULES% %PATH_RELEASE_MEMCACHED%/libevent-2.1'
 for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -m %PATH_RELEASE_MEMCACHED%`) do (set WINPATH_RELEASE_MEMCACHED=%%F)
 for %%X in (0 1) do (
-	for /f "tokens=*" %%G in ('dir "%WINPATH_RELEASE_MEMCACHED%\libevent-2.%%X\x64\memcached*.exe" /s/b') do (call do_php %PATH_UTILS%\sub\version.php %1 %%G memcached)
-	call do_php %PATH_UTILS%\sub\bininfo.php %WINPATH_RELEASE_MEMCACHED%/libevent-2.%%X/x64 null recurse checkavx updaterc ext:exe
+	for /f "tokens=*" %%G in ('dir "%WINPATH_RELEASE_MEMCACHED%\libevent-2.%%X\x%SSLH_TARGET_ARCH%\memcached*.exe" /s/b') do (call do_php %PATH_UTILS%\sub\version.php %1 %%G memcached)
+	call do_php %PATH_UTILS%\sub\bininfo.php %WINPATH_RELEASE_MEMCACHED%/libevent-2.%%X/x%SSLH_TARGET_ARCH% null recurse checkavx updaterc ext:exe
 )
--- modules/memcached.sh
+++ modules/memcached.sh
@@ -1,45 +1,41 @@
-unameOut="$(uname -a)"
 cd $1
 git reset --hard
 git clean -fdx
-# disable debug
 git apply --verbose `cygpath -m $2/memcached.patch`
 ./version.sh
+autoupdate -f
 ./autogen.sh
+
+unameOut="$(uname -a)"
 case "${unameOut}" in
     *i686*)     ARCH_PRF=x86
-    		# only for x86..??
 		cp --verbose -rf /bin/cyggcc_s-1.dll $3/$ARCH_PRF/;;
     *x86_64*)   ARCH_PRF=x64
 		ARCH_PARAMS=--enable-64bit;;
 esac
 
+case "${3}" in
+    *-2.1)   EVENT_VER=2-1-7;;
+    *-2.0)   EVENT_VER=2-0-5;;
+esac
+
 # https://gcc.gnu.org/onlinedocs/gcc/x86-Options.html
-CFCOM="-Wno-char-subscripts -O3 -s"
-# STD
+	# STD
 ./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --prefix=$1/release/$ARCH_PRF/ $ARCH_PARAMS
 #   SSE2
-make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -msse2 '
-cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached.exe
-#   AVX
-make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -march=sandybridge '
-cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-avx.exe 
+make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -msse2 '			&& cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached.exe
 
-# TLS
+#   AVX
+make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -march=sandybridge '	&& cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-avx.exe 
+
+	# TLS
 ./configure --cache-file=$ARCH_PRF.configure.cache --disable-dependency-tracking --enable-silent-rules${machine} --disable-dtrace --disable-sasl --disable-docs --disable-coverage --prefix=$1/release/$ARCH_PRF/ $ARCH_PARAMS --enable-tls
 #   SSE2
-make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -msse2 '
-cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-tls.exe
+make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -msse2 '			&& cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-tls.exe
 #   AVX
-make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -march=sandybridge '
-cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-avx-tls.exe
+make -j$(nproc) clean && make -j$(nproc) CFLAGS+=' -Wno-char-subscripts -O3 -s -march=sandybridge '	&& cp --verbose -rf $1/memcached.exe $3/$ARCH_PRF/memcached-avx-tls.exe
 
-cp --verbose -rf /bin/cygcrypto-1.1.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygevent_core-2-1-7.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygevent_extra-2-1-7.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygevent_openssl-2-1-7.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygevent_pthreads-2-1-7.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygevent-2-1-7.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygssl-1.1.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygwin1.dll $3/$ARCH_PRF/
-cp --verbose -rf /bin/cygz.dll $3/$ARCH_PRF/
+for i in cygcrypto-1.1.dll cygevent_core-$EVENT_VER.dll cygevent_extra-$EVENT_VER.dll cygevent_openssl-$EVENT_VER.dll cygevent_pthreads-$EVENT_VER.dll cygevent-$EVENT_VER.dll cygssl-1.1.dll cygwin1.dll cygz.dll
+do
+	cp --verbose -rf /bin/$i $3/$ARCH_PRF/
+done
