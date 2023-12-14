3946827bf5d3909c7e5b755cdb5ed3953cf335ef
nono303 <git@nono303.net>
2023-12-14 18:02:55 +0100

    add 'svn' param to build specific and light release for subversion  - sqlite3: -ICU deps, -SHELL  - httpd: -MODULES, only zlib & openssl

--- modules/httpd.bat
+++ modules/httpd.bat
@@ -5,27 +5,40 @@ for %%M in (mod_maxminddb mod_fcgid mod_h2 mod_md mod_wku_bt mod_h264_streaming 
 call %PATH_MODULES_COMMON%\init.bat %1 cmake
 set HTTPD_VERSION=%SCM_TAG%
 
-cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
--DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
--DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
--DINSTALL_PDB=ON ^
--DINSTALL_MANUAL=OFF ^
--DENABLE_MODULES=A ^
--DLUA_LIBRARIES=%PATH_INSTALL:\=/%/lib/liblua.lib ^
--DLUA_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
--DLIBXML2_INCLUDE_DIR=%PATH_INSTALL:\=/%/include/libxml2 ^
--DLIBXML2_LIBRARIES=%PATH_INSTALL:\=/%/lib/libxml2.lib ^
--DLIBXML2_ICONV_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
--DLIBXML2_ICONV_LIBRARIES=%PATH_INSTALL:\=/%/lib/libiconv.lib ^
--DZLIB_LIBRARIES=%PATH_INSTALL:\=/%/lib/zlib.lib ^
--DJANSSON_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
--DJANSSON_LIBRARIES=%PATH_INSTALL:\=/%/lib/jansson.lib ^
--DMAXMIND_LIBRARIES=%PATH_INSTALL%/lib/maxminddb.lib ^
--DOPENSSL_ROOT_DIR=%PATH_INSTALL_OSSL:\=/% ^
--DCURL_LIBRARY=%PATH_INSTALL:\=/%/lib/libcurl.lib ^
--DCURL_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
--DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM% ^
-%PATH_SRC%\%1
+if "%2"=="svn" (
+	cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
+	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+	-DINSTALL_PDB=ON ^
+	-DINSTALL_MANUAL=OFF ^
+	-DENABLE_MODULES=O ^
+	-DZLIB_LIBRARIES=%PATH_INSTALL:\=/%/lib/zlib.lib ^
+	-DOPENSSL_ROOT_DIR=%PATH_INSTALL_OSSL:\=/% ^
+	-DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM% ^
+	%PATH_SRC%\%1
+) else (
+	cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
+	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+	-DINSTALL_PDB=ON ^
+	-DINSTALL_MANUAL=OFF ^
+	-DENABLE_MODULES=A ^
+	-DLUA_LIBRARIES=%PATH_INSTALL:\=/%/lib/liblua.lib ^
+	-DLUA_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
+	-DLIBXML2_INCLUDE_DIR=%PATH_INSTALL:\=/%/include/libxml2 ^
+	-DLIBXML2_LIBRARIES=%PATH_INSTALL:\=/%/lib/libxml2.lib ^
+	-DLIBXML2_ICONV_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
+	-DLIBXML2_ICONV_LIBRARIES=%PATH_INSTALL:\=/%/lib/libiconv.lib ^
+	-DZLIB_LIBRARIES=%PATH_INSTALL:\=/%/lib/zlib.lib ^
+	-DJANSSON_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
+	-DJANSSON_LIBRARIES=%PATH_INSTALL:\=/%/lib/jansson.lib ^
+	-DMAXMIND_LIBRARIES=%PATH_INSTALL%/lib/maxminddb.lib ^
+	-DOPENSSL_ROOT_DIR=%PATH_INSTALL_OSSL:\=/% ^
+	-DCURL_LIBRARY=%PATH_INSTALL:\=/%/lib/libcurl.lib ^
+	-DCURL_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
+	-DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM% ^
+	%PATH_SRC%\%1
+)
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 REM mod_wku-bt
@@ -51,15 +64,16 @@ for %%X in (ab.exe abs.exe ApacheMonitor.exe htcacheclean.exe htdbm.exe htdigest
 for /f "tokens=*" %%G in ('dir %PATH_INSTALL%\modules\*.so /b') do (
 	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\modules\%%G
 )
-
-	REM ~~~~~~~~~~~ external modules -   mod_wku_bt 
-for %%X in (mod_maxminddb mod_fcgid mod_h264_streaming mod_md mod_qos) do (
-	call %PATH_MODULES_COMMON%\init.bat %%X varonly
-	call do_php %PATH_UTILS%\sub\version.php %%X %PATH_INSTALL%\modules\%%X.so "httpd:%HTTPD_VERSION%"
+if NOT "%2"=="svn" (
+		REM ~~~~~~~~~~~ external modules -   mod_wku_bt 
+	for %%X in (mod_maxminddb mod_fcgid mod_h264_streaming mod_md mod_qos) do (
+		call %PATH_MODULES_COMMON%\init.bat %%X varonly
+		call do_php %PATH_UTILS%\sub\version.php %%X %PATH_INSTALL%\modules\%%X.so "httpd:%HTTPD_VERSION%"
+	)
+	call %PATH_MODULES_COMMON%\init.bat mod_h2 varonly
+	call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_http2.so "httpd:%HTTPD_VERSION%"
+	call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_proxy_http2.so "httpd:%HTTPD_VERSION%"
+	call %PATH_MODULES_COMMON%\init.bat mod_wku_bt varonly
+	call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_backtrace.so "httpd:%HTTPD_VERSION%"
+	call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_whatkilledus.so "httpd:%HTTPD_VERSION%"
 )
-call %PATH_MODULES_COMMON%\init.bat mod_h2 varonly
-call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_http2.so "httpd:%HTTPD_VERSION%"
-call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_proxy_http2.so "httpd:%HTTPD_VERSION%"
-call %PATH_MODULES_COMMON%\init.bat mod_wku_bt varonly
-call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_backtrace.so "httpd:%HTTPD_VERSION%"
-call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_whatkilledus.so "httpd:%HTTPD_VERSION%"
--- modules/sqlite.bat
+++ modules/sqlite.bat
@@ -1,21 +1,29 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
-REM -DSQLITE_ENABLE_COLUMN_METADATA=ON -DSQLITE_OMIT_DECLTYPE=OFF 
-	REM https://github.com/storesafe/cordova-sqlite-storage/issues/906
-	REM PHP: sqlite_statement.obj : error LNK2001: unresolved external symbol sqlite3_column_table_name / sqlite3_column_decltype
-REM -DICU_ROOT=%PATH_INSTALL% ^
-REM	without KO	"C:\Windows Kits\10\Lib\10.0.22621.0\um\x64\icuuc.lib"  "C:\Windows Kits\10\Lib\10.0.22621.0\um\x64\icuin.Lib"
-REM	with OK		C:\sdk\release\vs17_x64-avx\lib\icuuc.lib  C:\sdk\release\vs17_x64-avx\lib\icuin.lib
-REM		warn	Policy CMP0074 is not set
+REM	SQLITE_ENABLE_COLUMN_METADATA=ON	SQLITE_OMIT_DECLTYPE=OFF 
+REM		https://github.com/storesafe/cordova-sqlite-storage/issues/906
+REM		PHP: sqlite_statement.obj : error LNK2001: unresolved external symbol sqlite3_column_table_name / sqlite3_column_decltype
+REM	SQLITE_THREADSAFE=ON	> gdal
+REM	USE_SEH=ON		> subversion
+REM		error C4013: sqlite3PagerWalSystemErrno: undefined;
+REM	ICU_ROOT=%PATH_INSTALL%
+REM		without	KO	"C:\Windows Kits\10\Lib\10.0.22621.0\um\x64\icuuc.lib"	"C:\Windows Kits\10\Lib\10.0.22621.0\um\x64\icuin.Lib"
+REM		with	OK	"C:\sdk\release\vs17_x64-avx\lib\icuuc.lib"		"C:\sdk\release\vs17_x64-avx\lib\icuin.lib"
+REM			warn	Policy CMP0074 is not set
 
-REM SQLITE_THREADSAFE=ON > gdal
+set SQLITEICU=-DBUILD_SHELL=ON -DENABLE_ICU=ON -DICU_ROOT=%PATH_INSTALL%
+set SQLITEOUT=sqlite3.exe libsqlite3.dll
+if "%2"=="svn" (
+	set SQLITEICU=-DENABLE_ICU=OFF -DBUILD_SHELL=OFF
+	set SQLITEOUT=libsqlite3.dll
+)
+
 cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
--DENABLE_SHARED=ON ^
 -DSHAREDLIB_PREFIX=lib ^
+-DENABLE_SHARED=ON ^
 -DENABLE_STATIC=OFF ^
--DBUILD_SHELL=ON ^
 -DENABLE_STATIC_SHELL=OFF ^
 -DENABLE_EDITLINE=OFF ^
 -DENABLE_READLINE=OFF ^
@@ -31,22 +39,21 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DRECOMMENDED_OPTIONS=ON ^
 -DENABLE_COLUMN_METADATA=ON ^
 -DENABLE_DBSTAT_VTAB=ON ^
--DENABLE_ICU=ON ^
--DICU_ROOT=%PATH_INSTALL% ^
 -DENABLE_RBU=ON ^
 -DENABLE_STAT4=ON ^
 -DOMIT_JSON=OFF ^
 -DOMIT_DECLTYPE=OFF ^
 -DOMIT_AUTOINIT=OFF ^
 -DUSE_URI=OFF ^
+-DUSE_SEH=ON ^
 -DWIN32_MALLOC=ON ^
 -DWIN32_HEAP_CREATE=ON ^
 -DBUILD_WITH_XPSDK=OFF ^
-%PATH_SRC%\%1
+%SQLITEICU% %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA% install
 
-for %%Y in (sqlite3.exe libsqlite3.dll) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%Y)
+for %%Y in (%SQLITEOUT%) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%Y)
 	REM for GDAL / php must have sqlite3ext.h in /sqlite3
 if not exist %PATH_INSTALL%\include\sqlite3ext.h mklink /h %PATH_INSTALL%\include\sqlite3ext.h %PATH_INSTALL%\include\sqlite3\sqlite3ext.h
