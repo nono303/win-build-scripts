12759e0f0f205177fd011376b34b3b4326b2e8c4
nono303 <git@nono303.net>
2023-12-14 19:07:29 +0100

    fix  - sqlite: build with shared lib  - python 3.12 regexp  - force quic 0  - serf lib naming for tests improve  - typo  - test launch command  - nowarn  - clean folder release

--- modules/subversion.bat
+++ modules/subversion.bat
@@ -1,5 +1,7 @@
-@echo off 
-call %PATH_BATCH%\go.bat serf
+@echo off
+	REM force native openssl
+call quic 0
+
 call %PATH_MODULES_COMMON%\init.bat %1
 
 	REM ~~~~~~~~~~~~ Create Outdir
@@ -11,30 +13,33 @@ if not exist %PATH_SRC%\%1\%OUTDIR_CONF%\. mklink /J %PATH_SRC%\%1\%OUTDIR_CONF%
 python gen-make.py ^
 --release ^
 -t vcproj ^
---vsnet-version=2022 ^
+--vsnet-version=2019 ^
 --with-jdk=%JAVA_HOME% ^
 --with-serf=%PATH_INSTALL%\include ^
---with-sqlite=%PATH_SRC%\sqlite ^
+--with-sqlite=%PATH_INSTALL% ^
 --with-openssl=%PATH_INSTALL_OSSL% ^
 --with-apr=%PATH_INSTALL% ^
 --with-apr-iconv=%PATH_INSTALL% ^
 --with-apr_memcache=%PATH_INSTALL% ^
 --with-apr-util=%PATH_INSTALL% ^
 --with-zlib=%PATH_INSTALL% ^
---with-httpd=%PATH_INSTALL% 
+--with-httpd=%PATH_INSTALL%
 
 %PATH_BIN_CYGWIN%\bash %PATH_MODULES_COMMON%/vcxproj.sh "%CYGPATH_SRC%/%1/build/win32/vcnet-vcproj/" %AVXVCX% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
-sed -i 's/serf-2\.lib/libserf-2\.lib/g'  %CYGPATH_SRC%/%1/build/win32/vcnet-vcproj/libsvn_ra_dll.vcxproj
+	REM ~~~~~~~~~~~~ fix serf lib name
+for %%V in (conflict-data-test pristine-store-test client-test db-test wc-test entries-compat-test op-depth-test filesize-test conflicts-test libsvn_ra_dll) do (
+	sed -i 's/serf-2\.lib/libserf-2\.lib/g'  %CYGPATH_SRC%/%1/build/win32/vcnet-vcproj/%%V.vcxproj
+)
 
-	REM ~~~~~~~~~~~~ Make
-		REM /t:__ALL_TESTS__;
+	REM ~~~~~~~~~~~~ Make (;__ALL_TESTS__)
 MSBuild.exe subversion_vcnet.sln %MSBUILD_OPTS% ^
 /t:Clean;__ALL__:Rebuild;__JAVAHL__:Rebuild ^
-/nowarn:C4702;LNK4087;MSB8065;C4703;C4132;C4389;C4244;C4245;C4267;C4018;C4334;C4189;C4312;C4090;C4152;C4146 ^
+/nowarn:C4018;C4090;C4132;C4146;C4152;C4189;C4245;C4267;C4389;C4702;C4703;LNK4087 ^
 /p:Configuration=%OUTDIR_CONF% ^
 /p:Platform=%archmsbuild%
 
 	REM ~~~~~~~~~~~~ Copy Release
+del /Q /F "%PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%\*.*"
 for %%X in (exe dll so) do (
 	for /f "tokens=*" %%G in ('dir %PATH_SRC%\%1\%OUTDIR_CONF%\*.%%X /s/b') do (
 		call do_php %PATH_UTILS%\sub\version.php %1 %%~pG%%~nG.%%X
@@ -43,21 +48,8 @@ for %%X in (exe dll so) do (
 	)
 )
 
-	REM ~~~~~~~~~~~~ Copy Deps
-set DEPS=\deps
-if not exist %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%%DEPS%\. mkdir %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%%DEPS%\
-if %ARCH% == x64 (set sslarch=-x64)
-if %ARCH% == x86 (set sslarch=)
-for %%F in (libexpat libapr-1 libapriconv-1 libaprutil-1 brotlienc brotlicommon brotlidec libserf-2 zlib) do (
-	for %%X in (dll pdb) do (
-		xcopy /C /F /Y %PATH_INSTALL%\bin\%%F.%%X  %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%%DEPS%\*
-	)
-)
-for %%F in (libssl-%OPENSSL_SUF%%sslarch% libcrypto-%OPENSSL_SUF%%sslarch%) do (
-	for %%X in (dll pdb) do (
-		xcopy /C /F /Y %PATH_INSTALL_OSSL%\bin\%%F.%%X  %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%%DEPS%\*
-	)
-)
-	REM Launch TESTS
-REM cd /D C:\sdk\src\subversion
-REM win-tests.py --release --cleanup --parallel
+	REM Launch TESTS from a terminal
+REM cd %PATH_SRC%\%1
+REM set PATH=%PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%%DEPS%
+REM set PYTHONPATH=%PYTHONPATH%;%PATH_SRC%\%1\subversion\tests\cmdline
+REM python win-tests.py --release --cleanup --parallel --bin=%PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB%
