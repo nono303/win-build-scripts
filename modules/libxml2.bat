f798b16e5fd695a955d3b529aacab2b0334594f9
nono303 <git@nono303.net>
2023-12-18 15:41:09 +0100

    move to CMake build

--- modules/libxml2.bat
+++ modules/libxml2.bat
@@ -1,71 +1,56 @@
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
 	REM for PHP 
 	REM - libxml2_a_dll MUST be used 
 	REM   - https://wiki.php.net/internals/windows/libs/libxml2
 	REM   - https://gist.github.com/auroraeosrose/3452993 (support libxml PHP extension shared and against shared dll on windows)
 	REM - legacy MUST be yes
+for %%X in (OFF ON) do (
+	cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
+	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+	-DBUILD_SHARED_LIBS=%%X ^
+	-DBUILD_STATIC_FOR_DLL=ON ^
+	-DLIBXML2_WITH_C14N=ON ^
+	-DLIBXML2_WITH_CATALOG=ON ^
+	-DLIBXML2_WITH_DEBUG=ON ^
+	-DLIBXML2_WITH_FTP=OFF ^
+	-DLIBXML2_WITH_HTML=ON ^
+	-DLIBXML2_WITH_HTTP=ON ^
+	-DLIBXML2_WITH_ICONV=ON ^
+	-DLIBXML2_WITH_ICU=OFF ^
+	-DLIBXML2_WITH_ISO8859X=ON ^
+	-DLIBXML2_WITH_LEGACY=ON ^
+	-DLIBXML2_WITH_LZMA=ON ^
+	-DLIBXML2_WITH_MEM_DEBUG=OFF ^
+	-DLIBXML2_WITH_MODULES=ON ^
+	-DLIBXML2_WITH_OUTPUT=ON ^
+	-DLIBXML2_WITH_PATTERN=ON ^
+	-DLIBXML2_WITH_PROGRAMS=OFF ^
+	-DLIBXML2_WITH_PUSH=ON ^
+	-DLIBXML2_WITH_PYTHON=OFF ^
+	-DLIBXML2_WITH_READER=ON ^
+	-DLIBXML2_WITH_REGEXPS=ON ^
+	-DLIBXML2_WITH_SAX1=ON ^
+	-DLIBXML2_WITH_SCHEMAS=ON ^
+	-DLIBXML2_WITH_SCHEMATRON=ON ^
+	-DLIBXML2_WITH_TESTS=OFF ^
+	-DLIBXML2_WITH_THREADS=ON ^
+	-DLIBXML2_WITH_THREAD_ALLOC=ON ^
+	-DLIBXML2_WITH_TREE=ON ^
+	-DLIBXML2_WITH_VALID=ON ^
+	-DLIBXML2_WITH_WRITER=ON ^
+	-DLIBXML2_WITH_XINCLUDE=ON ^
+	-DLIBXML2_WITH_XPATH=ON ^
+	-DLIBXML2_WITH_XPTR=ON ^
+	-DLIBXML2_WITH_XPTR_LOCS=ON ^
+	-DLIBXML2_WITH_ZLIB=ON ^
+	%PATH_SRC%\%1
 
-sed -i 's/iconv.lib/libiconv\.lib/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/LDFLAGS = \/nologo/LDFLAGS = \/nologo \/LTCG \/OPT:ICF,REF \/DEBUG/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/ARFLAGS = \/nologo/ARFLAGS = \/nologo \/LTCG/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/Z7/Zi/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/rc -Fo/rc -nologo -Fo/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/libxmladll/libxml2_a_dll/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-sed -i 's/libxmla/libxml2_a/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-	REM https://externals.io/message/25641
-REM sed -i -E 's/\/Fo(.)\(XML_INTDIR\)/\/D "LIBXML_STATIC_FOR_DLL" \/Fo\\1\(XML_INTDIR\)/g'  %CYGPATH_SRC%/%1/win32/Makefile.msvc
-
-cd /D win32
-for %%X in (libxml2_a_dll libxml) do (
-	cscript /nologo /e:jscript configure.js ^
-	trio=no ^
-	threads=native ^
-	ftp=yes ^
-	http=yes ^
-	html=yes ^
-	c14n=yes ^
-	catalog=yes ^
-	xpath=yes ^
-	xptr=yes ^
-	xptr_locs=yes ^
-	xinclude=yes ^
-	iconv=yes ^
-	icu=yes ^
-	iso8859x=yes ^
-	zlib=yes ^
-	lzma=yes ^
-	xml_debug=yes ^
-	mem_debug=no ^
-	regexps=yes ^
-	modules=yes ^
-	tree=yes ^
-	reader=yes ^
-	writer=yes ^
-	walker=yes ^
-	pattern=yes ^
-	push=yes ^
-	valid=yes ^
-	sax1=yes ^
-	legacy=yes ^
-	output=yes ^
-	schemas=yes ^
-	schematron=yes ^
-	python=no ^
-	compiler=msvc ^
-	cruntime="/wd4018 /wd4090 /wd4723 /wd4013 /D_WINSOCK_DEPRECATED_NO_WARNINGS /Zf /GL /MD /MP%NUMBER_OF_PROCESSORS% %AVX% /Fd%%X.pdb" ^
-	vcmanifest=yes ^
-	debug=yes ^
-	static=no ^
-	prefix=%PATH_INSTALL% ^
-	include=%PATH_INSTALL%\include ^
-	lib=%PATH_INSTALL%\lib
-
-	nmake %NMAKE_OPTS% /f Makefile.msvc %%X
+	%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+	%NINJA% install
+	
+	if %%X == OFF (xcopy /C /F /Y %PATH_BUILD%\%1\libxml2_a_dll.pdb %PATH_INSTALL%\lib\*)
 )
 
-if not exist %PATH_INSTALL%\include\libxml2\libxml\. md %PATH_INSTALL%\include\libxml2\libxml\
-xcopy /C /F /Y %PATH_SRC%\%1\include\libxml\*.h %PATH_INSTALL%\include\libxml2\libxml\*
-for %%X in (dll pdb) do (xcopy /C /F /Y %PATH_SRC%\%1\win32\bin.msvc\libxml2.%%X %PATH_INSTALL%\bin\*)
-for %%X in (win32\libxml2_a_dll.pdb win32\bin.msvc\libxml2.lib win32\bin.msvc\libxml2_a_dll.lib) do (xcopy /C /F /Y %PATH_SRC%\%1\%%X %PATH_INSTALL%\lib\*)
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libxml2.dll
