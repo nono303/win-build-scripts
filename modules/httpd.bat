8152fe6d73716d76cf9b8c7771aa1d9079350b9d
nono303 <git@nono303.net>
2023-12-19 10:54:26 +0100

    Refactor httpd build: - remove mt.exe -manifest %PATH_MODULES%\httpd.exe.manifest - factorize CMAKE_HTTPD_COMMON - cmake  - define diag_bits (mod_whatkilledus)  - transform dirty cflags sed to cmake extra_defines (mod_h264_streaming, mod_whatkilledus, mod_backtrace, mod_qos)  - CMAKE_MINIMUM_REQUIRED: 2.8 > 3.5   - CMAKE_MINIMUM_REQUIRED in first   - GET_MOD_ENABLE_RANK: fix quotes   - GEN_TEST_CHAR_EXE: https://lists.apache.org/thread/w4k1gmcynjw3l5z88mq1mfgd7g097s0f   - remove useless extra_sources for external modules

--- modules/httpd.bat
+++ modules/httpd.bat
@@ -4,9 +4,7 @@ for %%M in (mod_maxminddb mod_fcgid mod_h2 mod_md mod_wku_bt mod_h264_streaming 
 
 call %PATH_MODULES_COMMON%\init.bat %1 cmake
 set HTTPD_VERSION=%SCM_TAG%
-
-if "%2"=="svn" (
-	cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
+set CMAKE_HTTPD_COMMON=%CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
 	-DINSTALL_PDB=ON ^
@@ -14,14 +12,13 @@ if "%2"=="svn" (
 	-DENABLE_MODULES=O ^
 	-DZLIB_LIBRARIES=%PATH_INSTALL:\=/%/lib/zlib.lib ^
 	-DOPENSSL_ROOT_DIR=%PATH_INSTALL_OSSL:\=/% ^
-	-DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM% ^
+	-DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM%
+if "%2"=="svn" (
+	cmake %CMAKE_HTTPD_COMMON% ^
+	-DENABLE_MODULES=O ^	
 	%PATH_SRC%\%1
 ) else (
-	cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
-	-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
-	-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
-	-DINSTALL_PDB=ON ^
-	-DINSTALL_MANUAL=OFF ^
+	cmake %CMAKE_HTTPD_COMMON% ^
 	-DENABLE_MODULES=A ^
 	-DLUA_LIBRARIES=%PATH_INSTALL:\=/%/lib/liblua.lib ^
 	-DLUA_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
@@ -29,51 +26,30 @@ if "%2"=="svn" (
 	-DLIBXML2_LIBRARIES=%PATH_INSTALL:\=/%/lib/libxml2.lib ^
 	-DLIBXML2_ICONV_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
 	-DLIBXML2_ICONV_LIBRARIES=%PATH_INSTALL:\=/%/lib/libiconv.lib ^
-	-DZLIB_LIBRARIES=%PATH_INSTALL:\=/%/lib/zlib.lib ^
 	-DJANSSON_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
 	-DJANSSON_LIBRARIES=%PATH_INSTALL:\=/%/lib/jansson.lib ^
 	-DMAXMIND_LIBRARIES=%PATH_INSTALL%/lib/maxminddb.lib ^
-	-DOPENSSL_ROOT_DIR=%PATH_INSTALL_OSSL:\=/% ^
 	-DCURL_LIBRARY=%PATH_INSTALL:\=/%/lib/libcurl.lib ^
 	-DCURL_INCLUDE_DIR=%PATH_INSTALL:\=/%/include ^
-	-DEXTRA_INCLUDES=%PATH_SRC:\=/%/%OPENSSL_SCM% ^
 	%PATH_SRC%\%1
 )
-
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-REM mod_wku-bt
-sed -i 's/DBIN_NAME=mod_backtrace.so/DBIN_NAME=mod_backtrace.so -DDIAG_BITS_%CYGV%/g' %CYGPATH_BUILD%/%1/build.ninja
-sed -i 's/DBIN_NAME=mod_whatkilledus.so/DBIN_NAME=mod_whatkilledus.so -DDIAG_BITS_%CYGV%/g' %CYGPATH_BUILD%/%1/build.ninja
-REM \/DBUILDING_H264_STREAMING : http://h264.code-shop.com/trac#H264StreamingModule
-sed -i 's/DBIN_NAME=mod_h264_streaming.so/DBIN_NAME=mod_h264_streaming.so -DBUILDING_H264_STREAMING/g' %CYGPATH_BUILD%/%1/build.ninja
-	REM http://mod-qos.sourceforge.net/#statusviewer
-sed -i 's/DBIN_NAME=mod_qos.so/DBIN_NAME=mod_qos.so -DQS_NO_STATUS_HOOK/g' %CYGPATH_BUILD%/%1/build.ninja
-REM /support/win32/ApacheMonitor.rc(21) : warning RC4005: 'LONG_NAME' : redefinition
-sed -i 's/-DLONG_NAME=ApacheMonitor //g' %CYGPATH_BUILD%/%1/build.ninja
 %NINJA% install
 
-REM delete unused exp in /lib
+	REM delete unused exp in /lib
 del /Q /F "%PATH_INSTALL%\lib\*.exp"
 
-	REM Targeting for Windows 10
-mt.exe -manifest %PATH_MODULES%\httpd.exe.manifest -outputresource:%PATH_INSTALL%\bin\httpd.exe;1
+for %%X in (ab.exe abs.exe ApacheMonitor.exe htcacheclean.exe htdbm.exe htdigest.exe htpasswd.exe httpd.exe httxt2dbm.exe libhttpd.dll logresolve.exe rotatelogs.exe) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X)
+for /f "tokens=*" %%G in ('dir %PATH_INSTALL%\modules\*.so /b') do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\modules\%%G)
 
-for %%X in (ab.exe abs.exe ApacheMonitor.exe htcacheclean.exe htdbm.exe htdigest.exe htpasswd.exe httpd.exe httxt2dbm.exe libhttpd.dll logresolve.exe rotatelogs.exe) do (
-	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X
-)
-for /f "tokens=*" %%G in ('dir %PATH_INSTALL%\modules\*.so /b') do (
-	call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\modules\%%G
-)
+	REM ~~~~~~~~~~~ external modules
 if NOT "%2"=="svn" (
-		REM ~~~~~~~~~~~ external modules -   mod_wku_bt 
 	for %%X in (mod_maxminddb mod_fcgid mod_h264_streaming mod_md mod_qos) do (
 		call %PATH_MODULES_COMMON%\init.bat %%X varonly
 		call do_php %PATH_UTILS%\sub\version.php %%X %PATH_INSTALL%\modules\%%X.so "httpd:%HTTPD_VERSION%"
 	)
 	call %PATH_MODULES_COMMON%\init.bat mod_h2 varonly
-	call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_http2.so "httpd:%HTTPD_VERSION%"
-	call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\mod_proxy_http2.so "httpd:%HTTPD_VERSION%"
+	for %%X in (mod_http2 mod_proxy_http2) do (call do_php %PATH_UTILS%\sub\version.php mod_h2 %PATH_INSTALL%\modules\%%X.so "httpd:%HTTPD_VERSION%")
 	call %PATH_MODULES_COMMON%\init.bat mod_wku_bt varonly
-	call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_backtrace.so "httpd:%HTTPD_VERSION%"
-	call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\mod_whatkilledus.so "httpd:%HTTPD_VERSION%"
+	for %%X in (mod_backtrace mod_whatkilledus) do (call do_php %PATH_UTILS%\sub\version.php mod_wku_bt %PATH_INSTALL%\modules\%%X.so "httpd:%HTTPD_VERSION%")
 )
