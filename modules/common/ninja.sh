e7deeb4dbe76f73628909257a1c880301ae1ce05
nono303 <git@nono303.net>
2024-03-14 11:09:59 +0100

    typo

--- modules/common/ninja.sh
+++ modules/common/ninja.sh
@@ -14,4 +14,4 @@ sed -i -E 's/[\/-]W[0-4]/\/w/g' $2/build.ninja
 # opti
 sed -i 's/ \/O2 \/Ob1/ \/Gw \/Gy \/Zc:inline \/O2 \/Zf \/Zi \/FS \/GL \/MP'$3' '$avx' \/w/gI' $2/build.ninja
 # found		/DWIN32 /D_WINDOWS /W4 /MD /Zi /ZF /O2 /Ob1 /DNDEBUG
-# result	/DWIN32 /D_WINDOWS /w /MD \/Gw \/Gy \/Zc:inline \/O2 /Zf /Zi /FS /GL /MD /MP16 /arch:AVX /DNDEBUG
+# result	/DWIN32 /D_WINDOWS /w /MD /Gw /Gy /Zc:inline /O2 /Zf /Zi /FS /GL /MD /MP16 /arch:AVX /DNDEBUG
