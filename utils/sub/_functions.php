a423613867045f9817908402fea1b44ebfb3d512
nono303 <git@nono303.net>
2024-03-15 09:38:57 +0100

    rewrite execnono to have stderr

--- utils/sub/_functions.php
+++ utils/sub/_functions.php
@@ -15,8 +15,45 @@
 		if(DEBUG) echo "#dbg: ".$msg.PHP_EOL;
 	}
 
-	function execnono($cmd,$parts,$cwd,$env){
-		return trim(shell_exec("cd /D ".$cwd." && ".$cmd));
+	function execnono($cmd,$parts,$cwd,$env,&$stderr = null){
+		$args = '';
+		if($parts) {
+			foreach ($parts as $k => $part)
+				is_string($k) ?  $args .= ' ' . escapeshellarg($k) . ' ' . escapeshellarg($part) : $args .= ' ' . escapeshellarg($part);
+			$process_cmd = '"' . $cmd . '" ' . $args;
+		} else {
+			$process_cmd = $cmd;
+		}
+		$options = [
+			"suppress_errors" => false,
+			"bypass_shell" => false,
+			"blocking_pipes" => false,
+			"create_process_group" => false,
+			"create_new_console" => false
+		];
+		$descriptorspec = [
+			0 => ["pipe", "r"],  // stdin is a pipe that the child will read from
+			1 => ["pipe", "w"],  // stdout is a pipe that the child will write to
+			2 => ["pipe", "w"]   // stderr is a file to write to
+		];
+		$process = proc_open($process_cmd, $descriptorspec, $pipes, $cwd, $env, $options);
+		stream_set_blocking($pipes[1], false);
+		if (is_resource($process)) {
+			if($stdin){
+				fwrite($pipes[0], $stdin);
+				fclose($pipes[0]);
+			}
+			$out = stream_get_contents($pipes[1]);
+			fclose($pipes[1]);
+			$stderr = stream_get_contents($pipes[2]);
+			fclose($pipes[2]);
+			// It is important that you close any pipes before calling
+			// proc_close in order to avoid a deadlock
+			$return_value = proc_close($process);
+			// echo "command returned $return_value\n";
+			return trim($out);
+		}
+		return null;		
 	}
 
 	function getLastModifiedFile($dir){
