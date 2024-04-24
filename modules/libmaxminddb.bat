166bd7f5ce066f2eb409e0617e929cb2c7a6c020
nono303 <git@nono303.net>
2024-04-24 18:51:34 +0200

    builder: msbuild > ninja (fixing /GL issue)

--- modules/libmaxminddb.bat
+++ modules/libmaxminddb.bat
@@ -1,21 +1,15 @@
-@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
-REM goto shared
-cmake %CMAKE_OPTS% -G %CMAKE_TGT_VS% -A %archmsbuild% ^
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake nocxx
+
+cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DBUILD_SHARED_LIBS=ON ^
 -DBUILD_TESTING=OFF ^
 %PATH_SRC%\%1
 
-%PATH_BIN_CYGWIN%\bash %PATH_MODULES%/%1.sh "%CYGPATH_BUILD:\=/%/%1/" %AVX_MSBUILD% %PTFTS% %WKITVER% %VCTOOLSVER% %DOTNETVER%
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+	REM fix issue "subcommand failed"
+sed -i 's/\/GL //g' %CYGPATH_BUILD%/%1/build.ninja
+%NINJA% install
 
-MSBuild.exe %PATH_BUILD%\%1\%VCDIR%\maxminddb.sln %MSBUILD_OPTS% ^
-/t:maxminddb ^
-/nowarn:C4068;C4090 ^
-/p:Configuration=%CMAKE_BUILD_TYPE% ^
-/p:Platform="%archmsbuild%"
-
-xcopy /C /F /Y %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\maxminddb.lib %PATH_INSTALL%\lib\*
-for %%X in (pdb dll) do (xcopy /C /F /Y  %PATH_BUILD%\%1\%CMAKE_BUILD_TYPE%\maxminddb.%%X %PATH_INSTALL%\bin\*)
-xcopy /C /F /Y  %PATH_SRC%\%1\include\maxminddb.h %PATH_INSTALL%\include\*
-xcopy /C /F /Y  %PATH_BUILD%\%1\generated\maxminddb_config.h %PATH_INSTALL%\include\*
+xcopy /C /F /Y  %PATH_BUILD%\%1\maxminddb.pdb %PATH_INSTALL%\bin\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\maxminddb.dll
