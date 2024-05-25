0681c392226b45dba3f69a6798e16699edc50e32
nono303 <git@nono303.net>
2024-05-25 09:15:37 +0200

    1.7.3 - https://github.com/OSGeo/libgeotiff/pull/115

--- modules/libgeotiff.bat
+++ modules/libgeotiff.bat
@@ -1,6 +1,5 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake
 
-sed -i 's/_i.lib/.lib/g' %CYGPATH_SRC%/%1/%1/CMakeLists.txt
 
 cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
@@ -8,15 +7,19 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DBUILD_SHARED_LIBS=ON ^
 -DPROJ_LIBRARY=%PATH_INSTALL:\=/%/_proj/lib/proj.lib ^
 -DPROJ_INCLUDE_DIR=%PATH_INSTALL:\=/%/_proj/include ^
+-DJPEG_LIBRARY_RELEASE=%PATH_INSTALL:\=/%/lib/jpeg.lib ^
+-DJPEG_DIR=%PATH_INSTALL:\=/% ^
 -DWITH_ZLIB=ON ^
 -DWITH_UTILITIES=OFF ^
 -DWITH_TIFF=ON ^
 -DWITH_JPEG=ON ^
 -DWITH_TOWGS84=ON ^
+-DBUILD_DOC=OFF ^
+-DBUILD_MAN=OFF ^
+-DLIB_SUFFIX= ^
 %PATH_SRC%\%1\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
 %NINJA% install
 
-xcopy /C /F /Y %PATH_BUILD%\%1\bin\geotiff.pdb %PATH_INSTALL%\bin\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\geotiff.dll
