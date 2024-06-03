44dd85bdda42b7331e2c136e07921ba74726f412
nono303 <git@nono303.net>
2024-06-03 17:35:58 +0200

    fix quote for avx 0

--- modules/memcached.bat
+++ modules/memcached.bat
@@ -2,7 +2,7 @@
 	REM 2024-02-29: libevent-2.0 discontinued
 set EVENT_VER=2.1
 
-call %PATH_BIN_CYGWIN%\bash --login -c '%CYGPATH_MODULES%/%1.sh %CYGPATH_SRC%/%1 %CYGPATH_MODULES% %PATH_RELEASE_MEMCACHED%/libevent-%EVENT_VER% %AVX_GCC% %AVXB% %CUR_DEBUG%'
+call %PATH_BIN_CYGWIN%\bash --login -c '%CYGPATH_MODULES%/%1.sh %CYGPATH_SRC%/%1 %CYGPATH_MODULES% %PATH_RELEASE_MEMCACHED%/libevent-%EVENT_VER% %AVX_GCC% "%AVXB%" %CUR_DEBUG%'
 for /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\cygpath -m %PATH_RELEASE_MEMCACHED%`) do (set WINPATH_RELEASE_MEMCACHED=%%F)
 
 for %%X in (memcached%AVXB%.exe memcached%AVXB%-tls.exe) do (
