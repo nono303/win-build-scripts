a5ebe89b47a0e2d350fb70e42b99a76a188ff539
nono303 <git@nono303.net>
2024-06-05 10:14:27 +0200

    https://github.com/telemaxx/mapsforgesrv/pull/65

--- modules/mapsforgesrv.bat
+++ modules/mapsforgesrv.bat
@@ -1,9 +1,10 @@
 @echo off && call %PATH_MODULES_COMMON%\init.bat %1
-if exist %userprofile%\.gradle if not exist %PATH_SRC%\%1\%1\.gradle\. mklink /J %PATH_SRC%\%1\%1\.gradle %userprofile%\.gradle
-cd /D %PATH_SRC%\%1\%1
+if exist %userprofile%\.gradle if not exist %PATH_SRC%\%1\.gradle\. mklink /J %PATH_SRC%\%1\.gradle %userprofile%\.gradle
+cd /D %PATH_SRC%\%1
 
 echo ^>^>^> gradlew wrapper --gradle-version %GRADLEVER%
 call gradlew wrapper --gradle-version %GRADLEVER%
 echo ^<^<^< gradlew wrapper --gradle-version %GRADLEVER%
 
-call gradlew --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar test 2>&1
+REM -Dorg.gradle.deprecation.trace=true 
+call gradlew -x test --warning-mode all --console=verbose --no-daemon --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar test 2>&1
