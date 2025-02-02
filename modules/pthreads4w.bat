125e26146a9d6fc3cfd6d65bbf99a7641167ead5
nono303 <git@nono303.net>
2025-02-02 12:39:56 +0100

    add pthreads4w

---
+++ modules/pthreads4w.bat
@@ -0,0 +1,14 @@
+@echo off && call %PATH_MODULES_COMMON%\init.bat %1 cmake nocxx
+
+cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
+-DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
+-DCMAKE_UNITY_BUILD=1 ^
+-DBUILD_SHARED=1 ^
+-DBUILD_STATIC=0 ^
+-DBUILD_TESTING=OFF ^
+%PATH_SRC%\%1
+
+%PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
+%NINJA% install
+
+for %%X in (pthreadVCE3.dll pthreadVSE3.dll pthreadVC3.dll) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\%%X)
