6fa2fccd63e03607c70c02e1a000140343d24a0b
nono303 <git@nono303.net>
2025-03-01 16:38:02 +0100

    +mod_zstd.bat

---
+++ modules/mod_zstd.bat
@@ -0,0 +1,68 @@
+@echo off
+set MOD_ZSTD_PATH=%PATH_SRC%\%1
+set MOD_ZSTD_RELEASE=%MSVC_DEPS%_%ARCH%%AVXB%
+
+REM call go zstd nolog
+REM xcopy /C /F /Y %PATH_INSTALL%\bin\zstd.pdb %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\*
+REM xcopy /C /F /Y %PATH_INSTALL%\bin\zstd.dll %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\*
+
+call %PATH_MODULES_COMMON%\init.bat httpd varonly
+set HTTPD_VERSION=%SCM_TAG%
+call %PATH_MODULES_COMMON%\init.bat %1 varonly
+
+cl.exe  ^
+/nologo ^
+-IC:\sdk\release\vs17_x64-avx2\include ^
+/std:clatest ^
+/DWIN32 ^
+/D_WINDOWS ^
+/w ^
+/MD ^
+/Zi ^
+/Gw ^
+/Gy ^
+/Zc:inline ^
+/O2 ^
+/Ob3 ^
+/Zf ^
+/FS ^
+/GL ^
+/MP16 ^
+/cgthreads8 ^
+/DNDEBUG ^
+%AVX% ^
+-DLONG_NAME="mod_zstd for Apache HTTP Server" ^
+-DBIN_NAME=mod_zstd.so ^
+/Fo%MOD_ZSTD_PATH%\releases\mod_zstd.c.obj ^
+/Fd%MOD_ZSTD_PATH%\releases\ ^
+-c %MOD_ZSTD_PATH%\mod_zstd.c
+
+link.exe ^
+%MOD_ZSTD_PATH%\releases\mod_zstd.c.obj ^
+/out:%MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.so ^
+/implib:%MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.lib ^
+/pdb:%MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.pdb ^
+/dll ^
+/machine:x64 ^
+/DEBUG ^
+/NOLOGO ^
+/LTCG ^
+/OPT:REF,ICF ^
+C:\sdk\release\vs17_x64-avx2\lib\zstd.lib ^
+C:\sdk\release\vs17_x64-avx2\lib\libhttpd.lib ^
+C:\sdk\release\vs17_x64-avx2\lib\libapr-1.lib ^
+C:\sdk\release\vs17_x64-avx2\lib\libaprutil-1.lib
+
+call do_php %PATH_UTILS%\sub\version.php %1 %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.so "httpd:%HTTPD_VERSION%"
+
+if exist %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%.zip rm -fv %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%.zip
+rm -fv %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.exp
+rm -fv %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.lib
+%BIN_SEVENZ% a -tzip %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%.zip %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\*.*
+
+if %MOD_ZSTD_RELEASE%==vs17_x64-avx2 (
+	xcopy /C /F /Y %PATH_INSTALL%\bin\zstd.dll F:\telecharger\*
+	xcopy /C /F /Y %PATH_INSTALL%\bin\zstd.pdb F:\telecharger\*
+	xcopy /C /F /Y %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.so F:\telecharger\*
+	xcopy /C /F /Y %MOD_ZSTD_PATH%\releases\%MOD_ZSTD_RELEASE%\mod_zstd.pdb F:\telecharger\*
+)
