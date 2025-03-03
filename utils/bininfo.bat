d11da58970d6dbdaa4efca61589c9f389ab0fd53
nono303 <git@nono303.net>
2025-03-03 11:30:45 +0100

    bin: +zstd.exe modules: +mod_zstd php: check ts 96

--- utils/bininfo.bat
+++ utils/bininfo.bat
@@ -1,9 +1,9 @@
 @echo off
 set BININFO_LOG=%PATH_LOGS%\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.csv
 if %QUIC_BUILD% == 1 (
-	set NBBIN=232
+	set NBBIN=234
 ) else (
-	set NBBIN=225
+	set NBBIN=227
 )
 if exist %2\. (
 	call do_php %PATH_UTILS%\sub\%1.php %2 %BININFO_LOG:\=/% %2 %3 %4 %5 %6 %7 %8
@@ -12,10 +12,10 @@ if exist %2\. (
 	
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL_OSSL%\bin %BININFO_LOG:\=/% %2 %3 nb:8 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:%NBBIN% %4 %5 %6 %7
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\modules %BININFO_LOG:\=/% %2 %3 nb:264 %4 %5 %6 %7
+	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\modules %BININFO_LOG:\=/% %2 %3 nb:266 %4 %5 %6 %7
 	REM call do_php %PATH_UTILS%\sub\%1.php %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB% %BININFO_LOG:\=/% %2 %3 nb:61 %4 %5 %6 %7
 		REM !! TMP (nts ts)
-	for %%E in (nts) do (call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_php-%%E %BININFO_LOG:\=/% %2 %3 nb:94 %4 %5 %6 %7)
+	for %%E in (ts) do (call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_php-%%E %BININFO_LOG:\=/% %2 %3 nb:96 %4 %5 %6 %7)
 	REM call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_geographiclib\bin %BININFO_LOG:\=/% %2 %3 nb:26 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_proj\bin %BININFO_LOG:\=/% %2 %3 nb:20 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_gdal\bin %BININFO_LOG:\=/% %2 %3 nb:60 %4 %5 %6 %7
