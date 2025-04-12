e87632a4ad7c768da0fb874f2037c553ac3d17e3
nono303 <git@nono303.net>
2025-04-12 11:26:37 +0200

    3.5.0 +enable-hqinterop fix libzstd refactor CFLAGS CXXFLAGS LDFLAGS

--- modules/openssl.bat
+++ modules/openssl.bat
@@ -12,18 +12,13 @@ call %PATH_MODULES_COMMON%\init.bat %OPENSSL_SCM%
 set CONFIGURE_OPENSSL=--with-brotli-include=%PATH_INSTALL:\=/%/include ^
 --with-brotli-lib=brotlicommon ^
 --with-zstd-include=%PATH_INSTALL:\=/%/include ^
---with-zstd-lib=zstd
+--with-zstd-lib=libzstd
 if "%2"=="svn" (set CONFIGURE_OPENSSL=no-brotli no-zstd)
-
-if NOT "%C_STD_VER%"=="" (
-	set __CNFC=/std:c%C_STD_VER%
-	set __CNFCXX=/std:c++%C_STD_VER%
-)
-set LDFLAGS=/nologo /debug /OPT:ICF,REF /LTCG 
-set CFLAGS=/nologo /w /Zi /Gs0 /GF /Gy /O2 /Ob3 /GL /Gw /Zc:inline /Zf /FS /MP%NUMBER_OF_PROCESSORS% /cgthreads8 %AVX% %__CNFC% 
 
 	REM !! --with-zlib-lib require the full path or name in PATH of zlib DLL without dll extension !!
 perl Configure %perlbuild% ^
+--prefix=%PATH_INSTALL_OSSL% ^
+--openssldir=%PATH_INSTALL_OSSL%\conf ^
 shared ^
 no-tests ^
 no-unit-test ^
@@ -32,12 +27,11 @@ no-ssl3 ^
 no-weak-ssl-ciphers ^
 no-docs ^
 threads ^
---prefix=%PATH_INSTALL_OSSL% ^
+enable-hqinterop ^
 zlib ^
 zlib-dynamic ^
 --with-zlib-include=%PATH_INSTALL:\=/%/include ^
 --with-zlib-lib=zlib ^
---openssldir=%PATH_INSTALL_OSSL%\conf ^
 -DOPENSSL_USE_IPV6=1 ^
 -DOPENSSL_NO_HEARTBEATS=1 ^
 -DMD5_ASM=1 ^
@@ -48,17 +42,20 @@ zlib-dynamic ^
 -DAES_ASM=1 ^
 %CONFIGURE_OPENSSL%
 
-	REM set ARFLAGS=/nologo /LTCG 'KO' must be change after configure by sed
+if NOT "%C_STD_VER%"=="" (
+	set __CNFC=\/std:c%C_STD_VER%
+	set __CNFCXX=\/std:c++%C_STD_VER%
+)
 sed -i 's/ARFLAGS= \/nologo/ARFLAGS= \/nologo \/LTCG/g' %CYGPATH_SRC%/%OPENSSL_SCM%/makefile
+sed -i 's/CFLAGS=\/W3 \/wd4090 \/nologo \/O2/CFLAGS=\/nologo \/DWIN32 \/D_WINDOWS \/w \/MD \/Zi \/Gs0 \/GF \/Gy \/O2 \/Ob3 \/GL \/Gw \/Zc:inline \/Zf \/FS \/MP%NUMBER_OF_PROCESSORS% \/cgthreads8 %__CNFC%%AVX_SED%/g' %CYGPATH_SRC%/%OPENSSL_SCM%/makefile
+sed -i 's/CNF_CFLAGS=\/Gs0 \/GF \/Gy \/MD/CNF_CFLAGS=/g' %CYGPATH_SRC%/%OPENSSL_SCM%/makefile
+sed -i 's/CNF_CXXFLAGS=/CNF_CXXFLAGS=\/nologo \/DWIN32 \/D_WINDOWS \/w \/MD \/Zi \/Gs0 \/GF \/Gy \/O2 \/Ob3 \/GL \/Gw \/Zc:inline \/Zf \/FS \/MP%NUMBER_OF_PROCESSORS% \/cgthreads8 %__CNFCXX%%AVX_SED%/g' %CYGPATH_SRC%/%OPENSSL_SCM%/makefile
+sed -i 's/LDFLAGS=\/nologo \/debug/LDFLAGS=\/nologo \/debug \/OPT:ICF,REF \/LTCG/g' %CYGPATH_SRC%/%OPENSSL_SCM%/makefile
 
 REM perl configdata.pm --dump
 
 	REM install_sw > no docs
 nmake %NMAKE_OPTS% install_sw
-
-set LDFLAGS=
-set CFLAGS=
-
  	REM move & version for engines - https://github.com/openssl/openssl/issues/7185
 for %%M in (engines-%OPENSSL_SUF% ossl-modules) do (
 	for /f "tokens=*" %%G in ('dir %PATH_INSTALL_OSSL%\lib\%%M\*.dll /b') do (
