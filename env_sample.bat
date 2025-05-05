dbb13eda65c552a93eb691a15cbd95c421fb1b3a
nono303 <git@nono303.net>
2025-05-05 15:41:06 +0200

    GRADLEVER: 8.13 > 8.14

--- env_sample.bat
+++ env_sample.bat
@@ -100,7 +100,7 @@ cd /D %PATH_BATCH%
 
 REM ########################## BUILD OPTION
 	REM https://services.gradle.org/distributions/
-set GRADLEVER=8.13
+set GRADLEVER=8.14
 	REM see dir in %PATH_ROOTWKIT%\Lib
 set WKITVER=10.0.26100.0
 	REM .NET installed SDK
