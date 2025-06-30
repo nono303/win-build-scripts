77e4087ba387bc03af8b710c76560388846d14eb
nono303 <git@nono303.net>
2025-06-30 18:35:30 +0200

    1.6.49

--- modules/libpng.bat
+++ modules/libpng.bat
@@ -4,11 +4,11 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DCMAKE_INSTALL_PREFIX=%PATH_INSTALL% ^
 -DPNG_BUILD_ZLIB=OFF ^
 -DPNG_SHARED=ON ^
+-DPNG_EXECUTABLES=OFF ^
 -DPNG_TOOLS=OFF ^
 -DPNG_STATIC=OFF ^
 -DPNG_TESTS=OFF ^
 -DPNG_FRAMEWORK=OFF ^
--DPNG_DEBUG=OFF ^
 -DPNG_HARDWARE_OPTIMIZATIONS=ON ^
 -Dld-version-script=OFF ^
 %PATH_SRC%\%1
@@ -20,4 +20,5 @@ sed -i 's/CMAKE_C_FLAGS \//CMAKE_C_FLAGS \/nologo \//g' %CYGPATH_BUILD%/%1/scrip
 
 xcopy /C /F /Y %PATH_BUILD%\%1\libpng16.pdb %PATH_INSTALL%\bin\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libpng16.dll
+if exist %PATH_INSTALL%\lib\cmake\%1\. rmdir /S /Q %PATH_INSTALL%\lib\cmake\%1
 move /Y %PATH_INSTALL%\lib\%1 %PATH_INSTALL%\lib\cmake\%1
