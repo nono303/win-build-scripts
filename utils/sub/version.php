16c3bbbc2c0252682229c5eb249cbe603f86d9bf
nono303 <git@nono303.net>
2025-07-02 17:47:37 +0200

    const PATH_SRC = pathenv("PATH_SRC")  - remove useless param update sub module list for php & httpd +brotli as nogit fix getVersion() if SCM_TAG is not present

--- utils/sub/version.php
+++ utils/sub/version.php
@@ -1,6 +1,7 @@
 <?php
 	include( dirname(__FILE__) . '/_functions.php');
 	define("DEBUG",false);
+	define("PATH_SRC",pathenv("PATH_SRC"));
 
 	global $letterpos;
 	global $nogit;
@@ -9,124 +10,126 @@
 	foreach (range('a', 'z') as $letter)
 		$letterpos[$letter] = $pos++;
 	$proot = $argv[1] = strtolower($argv[1]);
-	if(in_array($proot,["mod_bikeshed","mod_maxminddb","mod_fcgid","mod_h2","mod_md","mod_wku_bt","mod_h264_streaming","mod_qos","mod_security"]))
+	if(in_array($proot,["mod_bikeshed","mod_evasive","mod_fcgid","mod_h2","mod_h264_streaming","mod_maxminddb","mod_md","mod_qos","mod_security","mod_wku_bt","mod_zstd"]))
 		$proot = "httpd";
-	if(in_array($proot,["pecl-memcache","php-geos","php-ogr","php-proj","pecl-text-xdiff","php-ext-brotli","php-ext-zstd","xdebug","php-src"]))
+	if(in_array($proot,["pecl-datetime-timezonedb","pecl-igbinary","pecl-memcache","pecl-parallel","pecl-system-sync","pecl-text-xdiff","php-ext-brotli","php-ext-zstd","php-geos","php-ogr","php-proj","php-src"]))
 		$proot = "php";
 	$nogit = array(
+		"brotli"			=> ["/#define BROTLI_VERSION_MAJOR *([0-9]+).*#define BROTLI_VERSION_MINOR *([0-9]+).*#define BROTLI_VERSION_PATCH *([0-9]+)/s",
+							PATH_SRC."/".$argv[1]."/c/common/version.h"],
 		"pcre2"				=> ["/#define VERSION \"([^\"]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/config.h.generic"],
+							PATH_SRC."/".$argv[1]."/src/config.h.generic"],
 		"mod_zstd"			=> ["/#define MOD_ZSTD_VERSION \"([^\"]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/mod_zstd.h"],
+							PATH_SRC."/".$argv[1]."/mod_zstd.h"],
 		"pthreads4w"		=> ["/#define  __PTW32_VERSION ([0-9+]),([0-9+]),([0-9+]),([0-9+])/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/_ptw32.h"],
+							PATH_SRC."/".$argv[1]."/_ptw32.h"],
 		"pecl-parallel"		=> ["/# define PHP_PARALLEL_VERSION \"([^\"]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_parallel.h"],
+							PATH_SRC."/".$argv[1]."/php_parallel.h"],
 		"pecl-igbinary"		=> ["/#define PHP_IGBINARY_VERSION \"([^\"]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/php7/igbinary.h"],
+							PATH_SRC."/".$argv[1]."/src/php7/igbinary.h"],
 		"libgeotiff"		=> ["/#define LIBGEOTIFF_STRING_VERSION +\"([0-9\.]+)/s",
 							pathenv("PATH_INSTALL")."/include/geotiff.h"],
 		"sslh"				=> ["/#define VERSION \"v([0-9\.]+)-([^\"]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/version.h"],
+							PATH_SRC."/".$argv[1]."/version.h"],
 		"dependencies"		=> ["/AssemblyVersion\(\"([0-9\.]+)\"/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/DependenciesGui/Properties/AssemblyInfo.cs"],
+							PATH_SRC."/".$argv[1]."/DependenciesGui/Properties/AssemblyInfo.cs"],
 		"pecl-system-sync"	=> ["/PHP_SYNC_VERSION +\"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_sync.h"],
+							PATH_SRC."/".$argv[1]."/php_sync.h"],
 		"mod_maxminddb"		=> ["/\[mod_maxminddb\], ?\[([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/configure.ac"],
+							PATH_SRC."/".$argv[1]."/configure.ac"],
 		"subversion"		=> ["/#define SVN_VER_MAJOR *([0-9]+).*#define SVN_VER_MINOR *([0-9]+).*#define SVN_VER_PATCH *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/subversion/include/svn_version.h"],
+							PATH_SRC."/".$argv[1]."/subversion/include/svn_version.h"],
 		"tidy"				=> ["/([0-9]{1,3}\.[0-9]+\.[0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/version.txt"],
+							PATH_SRC."/".$argv[1]."/version.txt"],
 		"libssh2"			=> ["/#define LIBSSH2_VERSION_MAJOR *([0-9]+).*#define LIBSSH2_VERSION_MINOR *([0-9]+).*#define LIBSSH2_VERSION_PATCH *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/libssh2.h"],
+							PATH_SRC."/".$argv[1]."/include/libssh2.h"],
 		"mod_fcgid"			=> ["/#define MODFCGID_VERSION_MAJOR *([0-9]+).*#define MODFCGID_VERSION_MINOR *([0-9]+).*#define MODFCGID_VERSION_SUBVER *([0-9]+).*#define MODFCGID_VERSION_DEV *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/modules/fcgid/fcgid_conf.h"],
+							PATH_SRC."/".$argv[1]."/modules/fcgid/fcgid_conf.h"],
 		"libxml2"			=> ["/#define LIBXML_DOTTED_VERSION +\"([0-9\.]+)/s",
 							pathenv("PATH_INSTALL")."/include/libxml2/libxml/xmlversion.h"],
 		"python"			=> ["/#define PY_VERSION +\"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/Include/patchlevel.h"],
+							PATH_SRC."/".$argv[1]."/Include/patchlevel.h"],
 		"curl"				=> ["/#define LIBCURL_VERSION \"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/curl/curlver.h"],
+							PATH_SRC."/".$argv[1]."/include/curl/curlver.h"],
 		"nghttp2"			=> ["/nghttp2 VERSION ([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/CMakeLists.txt"],
+							PATH_SRC."/".$argv[1]."/CMakeLists.txt"],
 		"libjpeg-turbo"		=> ["/set\(VERSION ([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/CMakeLists.txt"],
+							PATH_SRC."/".$argv[1]."/CMakeLists.txt"],
 		"libgav1"			=> ["/LIBGAV1_MAJOR_VERSION *([0-9]+).*LIBGAV1_MINOR_VERSION *([0-9]+).*LIBGAV1_PATCH_VERSION *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/gav1/version.h"],
+							PATH_SRC."/".$argv[1]."/src/gav1/version.h"],
 		"php-ogr"			=> ['/PHP_OGR_VERSION "([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/php_ogr.h"],
+							PATH_SRC."/".$argv[1]."/php_ogr.h"],
 		"php-proj"			=> ['/PHP_PROJ_VERSION "([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/src/php_proj.h"],
+							PATH_SRC."/".$argv[1]."/src/php_proj.h"],
 		"libxpm"			=> ["/\[libXpm\], *\[([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/configure.ac"],
+							PATH_SRC."/".$argv[1]."/configure.ac"],
 		"yajl"				=> ["/SET \(YAJL_MAJOR ([0-9]+).*SET \(YAJL_MINOR ([0-9]+).*SET \(YAJL_MICRO ([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/CMakeLists.txt"],
+							PATH_SRC."/".$argv[1]."/CMakeLists.txt"],
 		"mod_security"		=> ["/MODSEC_VERSION_MAJOR *\"([0-9]+).*MODSEC_VERSION_MINOR *\"([0-9]+).*MODSEC_VERSION_MAINT *\"([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/apache2/msc_release.h"],
+							PATH_SRC."/".$argv[1]."/apache2/msc_release.h"],
 		"sqlite"			=> ["/#define SQLITE_VERSION +\"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/sqlite3.h"],
+							PATH_SRC."/".$argv[1]."/sqlite3.h"],
 		"php-ext-brotli"	=> ["/#define BROTLI_EXT_VERSION \"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_brotli.h"],
+							PATH_SRC."/".$argv[1]."/php_brotli.h"],
 		"php-ext-zstd"		=> ["/#define PHP_ZSTD_VERSION \"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_zstd.h"],
+							PATH_SRC."/".$argv[1]."/php_zstd.h"],
 		"libxslt"			=> ["/MAJOR_VERSION\], \[([0-9]+).*MINOR_VERSION\], \[([0-9]+).*MICRO_VERSION\], \[([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/configure.ac"],
+							PATH_SRC."/".$argv[1]."/configure.ac"],
 		"libsodium"			=> ["/#define LIBSODIUM_VERSION_STRING \"([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/builds/msvc/resource.rc"],
+							PATH_SRC."/".$argv[1]."/builds/msvc/resource.rc"],
 		"libpng"			=> ["/#define PNG_LIBPNG_VER_MAJOR *([0-9]+).*#define PNG_LIBPNG_VER_MINOR *([0-9]+).*#define PNG_LIBPNG_VER_RELEASE *([0-9]+).*/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/png.h"],
+							PATH_SRC."/".$argv[1]."/png.h"],
 		"libgd"				=> ["/#define GD_MAJOR_VERSION *([0-9]+).*#define GD_MINOR_VERSION *([0-9]+).*#define GD_RELEASE_VERSION *([0-9]+).*#define GD_EXTRA_VERSION *\"([^\"]+)\".*/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/gd.h"],
+							PATH_SRC."/".$argv[1]."/src/gd.h"],
 		"geographiclib"		=> ["/PROJECT_VERSION_MAJOR ([0-9]+).*PROJECT_VERSION_MINOR ([0-9]+).*PROJECT_VERSION_PATCH ([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/CMakeLists.txt"],
+							PATH_SRC."/".$argv[1]."/CMakeLists.txt"],
 		"libiconv"			=> ['/AC_INIT\(\[libiconv\], \[([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/libiconv/configure.ac"],
+							PATH_SRC."/".$argv[1]."/libiconv/configure.ac"],
 		"libev"				=> ['/AC_INIT\(\[libev\], \[([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/configure.ac"],
+							PATH_SRC."/".$argv[1]."/configure.ac"],
 		"mod_qos"			=> ['/g_revision\[\] = "([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/mod_qos.c"],
+							PATH_SRC."/".$argv[1]."/mod_qos.c"],
 		"giflib"			=> ['/giflib VERSION ([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/5.2.1/CMakeLists.txt"],
+							PATH_SRC."/".$argv[1]."/5.2.1/CMakeLists.txt"],
 		"mpir"				=> ['/#define _MSC_MPIR_VERSION "([0-9\.]+)"/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/gmp.h"],
+							PATH_SRC."/".$argv[1]."/gmp.h"],
 		"bzip2"				=> ['/VALUE "ProductVersion", "([0-9\.]+)/s',
-							pathenv("PATH_SRC")."/".$argv[1]."/version.rc"],
+							PATH_SRC."/".$argv[1]."/version.rc"],
 		"apr"				=> ["/#define APR_MAJOR_VERSION *([0-9]+).*#define APR_MINOR_VERSION *([0-9]+).*#define APR_PATCH_VERSION *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/apr_version.h"],
+							PATH_SRC."/".$argv[1]."/include/apr_version.h"],
 		"apr2"				=> ["/#define APR_MAJOR_VERSION *([0-9]+).*#define APR_MINOR_VERSION *([0-9]+).*#define APR_PATCH_VERSION *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/apr_version.h"],
+							PATH_SRC."/".$argv[1]."/include/apr_version.h"],
 		"apr-util"			=> ["/#define APU_MAJOR_VERSION *([0-9]+).*#define APU_MINOR_VERSION *([0-9]+).*#define APU_PATCH_VERSION *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/apu_version.h"],
+							PATH_SRC."/".$argv[1]."/include/apu_version.h"],
 		"openssl-quic"		=> ["/VERSION_NUMBER=([0-9\.]+)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/makefile"],
+							PATH_SRC."/".$argv[1]."/makefile"],
 		"verpatch"			=> ["/set _ver=\"([^ ]+)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/ver-self.cmd"],
+							PATH_SRC."/".$argv[1]."/ver-self.cmd"],
 		"php-geos"			=> ["/PHP_GEOS_VERSION \"([0-9\.]+)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_geos.h"],
+							PATH_SRC."/".$argv[1]."/php_geos.h"],
 		"pecl-text-xdiff"	=> ["/PHP_XDIFF_VERSION \"([0-9\.]+)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/php_xdiff.h"],
+							PATH_SRC."/".$argv[1]."/php_xdiff.h"],
 		"pecl-memcache"		=> ["/PHP_MEMCACHE_VERSION \"([0-9\.]+)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/php_memcache.h"],
+							PATH_SRC."/".$argv[1]."/src/php_memcache.h"],
 		"icu"				=> ["/U_ICU_VERSION \"(.*)\"/",
-							pathenv("PATH_SRC")."/".$argv[1]."/icu4c/source/common/unicode/uvernum.h"],
+							PATH_SRC."/".$argv[1]."/icu4c/source/common/unicode/uvernum.h"],
 		"nssm"				=> ["/NSSM_VERSIONINFO (.*),(.*),(.*),(.*)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/version.h"],
+							PATH_SRC."/".$argv[1]."/version.h"],
 		"libyuv"			=> ["/LIBYUV_VERSION (.*)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/include/libyuv/version.h"],
+							PATH_SRC."/".$argv[1]."/include/libyuv/version.h"],
 		"mobac"				=> ["/mobac.revision=(.*)/",
-							pathenv("PATH_SRC")."/".$argv[1]."/mobac/build/resources/main/mobac/mobac-rev.properties"],
+							PATH_SRC."/".$argv[1]."/mobac/build/resources/main/mobac/mobac-rev.properties"],
 		"mod_h264_streaming"=> ["/#define VERSION \"([^\"]+)\"/",
-							pathenv("PATH_SRC")."/".$argv[1]."/config.h"],
+							PATH_SRC."/".$argv[1]."/config.h"],
 		"serf"				=> ["/#define SERF_MAJOR_VERSION *([0-9]+).*#define SERF_MINOR_VERSION *([0-9]+).*#define SERF_PATCH_VERSION *([0-9]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/serf.h"],
+							PATH_SRC."/".$argv[1]."/serf.h"],
 		"wineditline"		=> ["/RL_READLINE_VERSION *0x([0-9]{2})([0-9]{2})([0-9]{2})/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/src/editline/readline.h"],
+							PATH_SRC."/".$argv[1]."/src/editline/readline.h"],
 		"libconfig"		=> ["/AC_INIT\(\[libconfig\],\[([0-9\.]+)/s",
-							pathenv("PATH_SRC")."/".$argv[1]."/configure.ac"],
+							PATH_SRC."/".$argv[1]."/configure.ac"],
 	);
 
-	function getVersion($cur,$src){
+	function getVersion($src){
 		global $letterpos;
 		global $nogit;
 		$ver_product = "";
@@ -148,9 +151,9 @@
 			} elseif (is_string($nogit[$src])){
 				$from = $nogit[$src];
 				$ver_product = $nogit[$src];
-			} elseif (is_dir($cur."/".$src."/.git")){
+			} elseif (is_dir(PATH_SRC."/".$src."/.git")){
 				$from = "git";
-				$ver_product = pathenv("SCM_TAG");
+				$ver_product = pathenv("SCM_TAG") ?: execnono($cmd = "git tag --points-at HEAD",NULL,realpath(PATH_SRC."/".$src),NULL);
 				// echo "SCM_TAG: ".$ver_product.PHP_EOL;
 				// remove name
 				$ver_product = str_replace($src,"",$ver_product);
@@ -204,9 +207,9 @@
 	// print_r($argv);
 	if(sizeof($argv) == 1){
 		$md = "| src | version | ".PHP_EOL."| ---- | ---- |".PHP_EOL;
-		foreach(scandir($cur = pathenv("PATH_SRC")) as $src) {
+		foreach(scandir($cur = PATH_SRC) as $src) {
 			if(is_dir($cur."/".$src) & $src != "." && $src != ".."){
-				$current = getVersion($cur,$src);
+				$current = getVersion($src);
 				// $upstream = str_replace("git:","https:",execnono($cmd = "git config --get remote.origin.url",NULL,$cur."/".$src,NULL));
 				echo str_pad($src,20).str_pad($current["product"],12).str_pad($current["file"],12).PHP_EOL;
 				// $md .= "| [".$src."](".$upstream.") | ".$current["file"]." |".PHP_EOL;
@@ -214,10 +217,10 @@
 		}
 		// file_put_contents(dirname(__FILE__) . '/../../SRC_VERSION.md',$md);
 	} elseif(sizeof($argv) == 2){
-		echo getVersion($cur,$argv[1])["product"];
+		echo getVersion($argv[1])["product"];
 	} elseif(sizeof($argv) >= 3){
-		if(is_dir($cur = pathenv("PATH_SRC"))."/".$argv[1]){
-			$current = getVersion($cur,$argv[1]);
+		if(is_dir($cur = PATH_SRC)."/".$argv[1]){
+			$current = getVersion($argv[1]);
 			if($argv[2] == "veronly"){
 				echo $current["file"];
 				exit(1);
@@ -287,7 +290,7 @@
 			}
 			passthru($cmd);
 		} else {
-			echo "version.php '".$argv[1]."' '".$argv[2]."': '".pathenv("PATH_SRC")."/".$argv[1]."' doesn't exist";
+			echo "version.php '".$argv[1]."' '".$argv[2]."': '".PATH_SRC."/".$argv[1]."' doesn't exist";
 			exit(-1);
 		}
 	} else {
