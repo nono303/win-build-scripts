17e1a28994cc4aa5d6d7858ff6b9b9b05e1ad5ba
nono303 <git@nono303.net>
2025-07-08 10:16:31 +0200

    display runtime dependencies remove /out (deliver jar via github release)

--- modules/fit2gpx.bat
+++ modules/fit2gpx.bat
@@ -5,10 +5,12 @@ echo ^>^>^> gradlew --project-cache-dir %PATH_GRADLE_BUILD% wrapper --gradle-ver
 call gradlew --project-cache-dir %PATH_GRADLE_BUILD% wrapper --gradle-version %GRADLEVER%
 echo ^<^<^< gradlew --project-cache-dir %PATH_GRADLE_BUILD% wrapper --gradle-version %GRADLEVER%
 
+REM call gradlew --refresh-dependencies run
+call gradlew -q dependencies --configuration runtimeClasspath
+
 call gradlew --project-cache-dir %PATH_GRADLE_BUILD% --warning-mode all --no-daemon --console=verbose --parallel --max-workers %NUMBER_OF_PROCESSORS% shadowJar 2>&1
-move /Y %PATH_SRC%\%1\build\libs\fit2gpx-all.jar %PATH_SRC%\%1\out\jar\fit2gpx.jar
 
 if %LOCAL_COPY% == 1 (
 	rm -fv "%LOCAL_PATH_FIT2GPX%\fit2gpx.jar"
-	xcopy /C /F /Y %PATH_SRC%\%1\out\jar\fit2gpx.jar %LOCAL_PATH_FIT2GPX%\*
+	xcopy /C /F /Y %PATH_SRC%\%1\build\libs\fit2gpx-all.jar %LOCAL_PATH_FIT2GPX%\fit2gpx.jar*
 )
