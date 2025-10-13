d927a2bd1aeacbe85897423fae47f0129abc6302
nono303 <git@nono303.net>
2025-10-14 00:43:46 +0200

    +OPTION(BUILD_PROGRAMS) fix: BUILD_TEST > BUILD_TESTS fix: #ifdef _MSC_VER: #define ssize_t SSIZE_T > #include <stdint.h> fix: patch offset

--- modules/libgd.bat
+++ modules/libgd.bat
@@ -26,21 +26,16 @@ cmake %CMAKE_OPTS% -G %CMAKE_TGT_NINJA% ^
 -DVERBOSE_MAKEFILE=ON ^
 -DBUILD_SHARED_LIBS=ON ^
 -DBUILD_STATIC_LIBS=OFF ^
--DBUILD_TEST=OFF ^
 -DBUILD_TESTS=OFF ^
+-DBUILD_PROGRAMS=OFF ^
 -DBUILD_EXEMPLES=OFF ^
 -DBUILD_DOCS=OFF ^
 -DBUILD_CPACK=OFF ^
 %PATH_SRC%\%1
 
 %PATH_BIN_CYGWIN%\bash %CYGPATH_MODULES_COMMON%/ninja.sh "%AVX%" "%CYGPATH_BUILD%/%1" "%NUMBER_OF_PROCESSORS%"
-for %%Y in (cmake_install.cmake build.ninja) do (
-	sed -i 's/-static\./_static\./g' %CYGPATH_BUILD%/%1/%%Y
-	REM "LINK : fatal error LNK1181: cannot open input file 'dav1d.lib'" : useless but in LINK list without path...
-	sed -i 's/dav1d.lib//g' %CYGPATH_BUILD%/%1/%%Y
-)
+
 %NINJA% install
 
-for %%X in (bdftogd gd2copypal.exe gd2togif.exe gd2topng.exe gdcmpgif.exe gdparttopng.exe gdtopng.exe giftogd2.exe pngtogd.exe pngtogd2.exe) do (rm -fv %PATH_INSTALL%\bin\%%X)
 xcopy /C /F /Y %PATH_BUILD%\%1\Bin\libgd.pdb %PATH_INSTALL%\bin\*
 call do_php %PATH_UTILS%\sub\version.php %1 %PATH_INSTALL%\bin\libgd.dll
