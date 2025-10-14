e5b5bf5736b28e5faf72d83a73dafab6aa5d7f6a
nono303 <git@nono303.net>
2025-10-14 18:01:49 +0200

    global refactor * use gitapply factorized by patch type (version, ext, pecl) + core build_type + improve dependencies check for win7 build type + generate def files for libxml2 & ext_pcre

--- modules/php.bat
+++ modules/php.bat
@@ -14,8 +14,6 @@ call %PATH_MODULES_COMMON%\init.bat php-src
 set PHPVER=%GET_VERSION:~0,3%
 set PHPVERFULL=%GET_VERSION%
 
-call do_php %PATH_SRC%\php-src\Zend\zend_vm_gen.php
-
 	REM ~~~~~~~~~~~~ type of build : full / memcached / xdebug
 set PHP_BUILD_TYPE=%PHPVER%
 set PHP_XTRALIBS=
@@ -32,41 +30,61 @@ for /L %%i in (2,1,%argCount%) do (
 	if /I "!argVec[%%i]!"=="IGBINARY"	set PHP_BUILD_TYPE=igbinary
 	if /I "!argVec[%%i]!"=="PARALLEL"	set PHP_BUILD_TYPE=parallel
 	if /I "!argVec[%%i]!"=="FFI"		set PHP_BUILD_TYPE=ffi
+	if /I "!argVec[%%i]!"=="CORE" 		set PHP_BUILD_TYPE=core
 	if /I "!argVec[%%i]!"=="WIN7" (
+		REM TODO comment for full build
 		set PHP_BUILD_TYPE=win7
-		echo     # apply php-src_win7.patch
-		git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\php-src_win7.patch
+		if %ARG_KEEPSRC% == 0 (call %PATH_UTILS%\gitapply %PATH_PATCHES%\php-src\%PHPVER%\win7.patch)
 	)
 )
 
 	REM ~~~~~~~~~~~~ check prereq deps
-if !PHP_BUILD_TYPE! == memcache	(
+if %PHP_BUILD_TYPE% == memcache	(
 	if not exist %PATH_INSTALL%\lib\zlib.lib (call go.bat zlib NOLOG)
 )
 if %PHP_BUILD_TYPE% == brotli	(
 	if not exist %PATH_INSTALL%\lib_unused\libbrotlicommon.lib (call go.bat brotli static NOLOG)
 	set PHP_XTRALIBS=;%PATH_INSTALL%\lib_unused\
 )
+if %PHP_BUILD_TYPE% == win7	(
+	if not exist %PATH_INSTALL%\lib\zlib.lib (call go.bat zlib NOLOG)
+	if not exist %PATH_INSTALL%\lib\liblzma.lib (call go.bat liblzma NOLOG)
+	if not exist %PATH_INSTALL%\lib\icudt.lib (call go.bat icu NOLOG)
+	if not exist %PATH_INSTALL%\lib\libiconv.lib (call go.bat libiconv NOLOG)
+	if not exist %PATH_INSTALL%\lib\libxml2_a_dll.lib (call go.bat libxml2 NOLOG)
+)
 
-	REM ~~~~~~~~~~~~ PHP-SRC VERSION PATCH
-if exist %PATH_MODULES%\php-src_%PHPVER%.patch (
-	if %ARG_KEEPSRC% == 0 (
-		echo     # apply php-src_%PHPVER%.patch
-		git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\php-src_%PHPVER%.patch
-	)
+if %ARG_KEEPSRC% == 0 (
+	REM ~~~~~~~~~~~~ GENERATE zend_vm
+	echo    # generate zend_vm
+	call do_php %PATH_SRC%\php-src\Zend\zend_vm_gen.php | gawk '{print "     " $0}' 2>&1
+	
+	REM ~~~~~~~~~~~~ PHP-SRC version PATCH
+	REM if exist %PATH_PATCHES%\php-src\%PHPVER%\php-src.patch (call %PATH_UTILS%\gitapply %PATH_PATCHES%\php-src\%PHPVER%\php-src.patch)
+	REM ~~~~~~~~~~~~ PHP-SRC ext PATCH
+	for /f "tokens=*" %%P in ('dir %PATH_PATCHES%\php-src\%PHPVER%\ext-*.patch /s/b') do (call %PATH_UTILS%\gitapply %%P)
+	REM ~~~~~~~~~~~~ PHP-SRC pr PATCH
+	for /f "tokens=*" %%P in ('dir %PATH_PATCHES%\php-src\%PHPVER%\pr*.patch /s/b') do (call %PATH_UTILS%\gitapply %%P)
+	
+	REM ~~~~~~~~~~~~ DEF - Makefile: PHP_DLL_DEF_SOURCES
+	set DEF_EXPORTS=%PATH_SRC%\php-src\ext\libxml\php_libxml2.def
+	echo EXPORTS > !DEF_EXPORTS!
+	dumpbin /EXPORTS %PATH_INSTALL%\lib\libxml2.lib | grep -P '\s{18}' | sed 's/^ *//g' >> !DEF_EXPORTS!
+	echo    # rebuild !DEF_EXPORTS!
+	
+	set DEF_EXPORTS=%PATH_SRC%\php-src\ext\pcre\ext_pcre.def
+	echo EXPORTS > !DEF_EXPORTS!
+	dumpbin /EXPORTS %PATH_INSTALL%\lib\pcre2-8.lib | grep -P '\s{18}' | sed 's/^ *//g' >> !DEF_EXPORTS!
+	echo    # build !DEF_EXPORTS!
 )
-	REM ~~~~~~~~~~~~ PHP-SRC TMP PATCH
-for %%P in (php-src_pr17848 php-src_libxml2-def php-src_8.5_20110) do (
-	echo     # apply %%P.patch
-	git apply --verbose --ignore-space-change --ignore-whitespace %PATH_MODULES%\%%P.patch
-)
+
 	REM ~~~~~~~~~~~~ pecl-memcache
 call %PATH_MODULES_COMMON%\init.bat pecl-memcache
 	REM revision & version for pecl_memcache in phpinfo()
 FOR /F "tokens=* USEBACKQ" %%F in (`%PATH_BIN_CYGWIN%\find -name 'memcache.c' -type f -exec dirname {} +`) do (set PECLMEMCACHECYGSRCDIR=%%F)
 FOR /F "tokens=* USEBACKQ" %%F in (`grep PHP_MEMCACHE_VERSION %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/php_memcache.h ^| cut -d^'^"^' -f2`) do (set PECLMEMCACHEVERSION=%%F)
 sed -i -E 's/, PHP_MEMCACHE_VERSION/, "%PECLMEMCACHEVERSION% | branch: %SCM_BRANCH% | commit: %SCM_COMORREV% | date: %SCM_COMORREV_DATE% | https:\/\/github.com\/nono303\/PHP-memcache-dll"/g' %CYGPATH_SRC%/pecl-memcache/%PECLMEMCACHECYGSRCDIR%/memcache.c
-echo %PECLMEMCACHEVERSION% ^| branch: %SCM_BRANCH% ^| commit: %SCM_COMORREV% ^| date: %SCM_COMORREV_DATE%
+REM echo %PECLMEMCACHEVERSION% ^| branch: %SCM_BRANCH% ^| commit: %SCM_COMORREV% ^| date: %SCM_COMORREV_DATE%
 
 	REM ~~~~~~~~~~~~ pecl-text-xdiff : libxdiff version
 cd /D %PATH_SRC%\libxdiff
@@ -83,6 +101,7 @@ sed -i 's/AC_DEFINE..USE_BROTLI_DICTIONARY./AC_DEFINE\("BROTLI_LIB_VERSION", "%B
 	REM ~~~~~~~~~~~~ other PECL init
 for %%E in (php-sdk xdebug php-geos php-proj php-ogr php-ext-zstd pecl-datetime-timezonedb pecl-system-sync pecl-igbinary pecl-parallel) do (
 	call %PATH_MODULES_COMMON%\init.bat %%E
+	if exist %PATH_PATCHES%\php-src\%PHPVER%\pecl-%%E.patch (call %PATH_UTILS%\gitapply %PATH_PATCHES%\php-src\%PHPVER%\pecl-%%E.patch)
 )
 
 set PHP_SRC_DIR=%PATH_PHP_SDK%\phpmaster\%MSVC_DEPS%\%ARCH%\php-src
