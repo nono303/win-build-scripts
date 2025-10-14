1d513e6606309358ed001217e227a75a0f8357d9
nono303 <git@nono303.net>
2025-10-14 18:20:08 +0200

    * php 8.5 (-php_opcache) * PATH_INSTALL_OSSL >  PATH_INSTALL

--- utils/bininfo.bat
+++ utils/bininfo.bat
@@ -5,12 +5,11 @@ if exist %2\. (
 ) else (
 	set BININFO_LOG=%PATH_LOGS%\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.csv
 	
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL_OSSL%\bin %BININFO_LOG:\=/% %2 %3 nb:8 %4 %5 %6 %7
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:225 %4 %5 %6 %7
+	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:233 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\modules %BININFO_LOG:\=/% %2 %3 nb:266 %4 %5 %6 %7
 	REM call do_php %PATH_UTILS%\sub\%1.php %PATH_RELEASE_SVN%\%MSVC_DEPS%\%ARCH%%AVXB% %BININFO_LOG:\=/% %2 %3 nb:61 %4 %5 %6 %7
 		REM !! TMP (nts ts)
-	for %%E in (ts) do (call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_php-%%E %BININFO_LOG:\=/% %2 %3 nb:98 %4 %5 %6 %7)
+	for %%E in (ts) do (call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_php-%%E %BININFO_LOG:\=/% %2 %3 nb:96 %4 %5 %6 %7)
 	REM call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_geographiclib\bin %BININFO_LOG:\=/% %2 %3 nb:26 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_proj\bin %BININFO_LOG:\=/% %2 %3 nb:20 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\_gdal\bin %BININFO_LOG:\=/% %2 %3 nb:62 %4 %5 %6 %7
