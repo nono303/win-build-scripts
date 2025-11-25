0c66d54ff86bcb0234fe55fd4e79ed7a11f142d1
nono303 <git@nono303.net>
2025-11-25 13:17:45 +0100

    search dll in /bin subdirs & /_php-ts

--- utils/sub/libcheck.php
+++ utils/sub/libcheck.php
@@ -6,11 +6,13 @@
 	if(is_dir($argv[1] = str_replace("\\","/",$argv[1]))){
 		echo ">>>>>> ".$argv[1]." <<<<<<".PHP_EOL;
 		$liblist = array_map('basename', glob($argv[1].'/*.lib', GLOB_BRACE));
-		foreach(["pdb" => "/","dll" => "/../bin/"] as $k => $v){
+		foreach(["pdb" => ["/"],"dll" => ["/../bin/","/../bin/ossl-modules/","/../bin/engines-3/","/../_php-ts/php[0-9]"]] as $k => $v){
 			if(sizeof($entries = array_filter(explode("\n",trim(execnono("grep --binary-files=text --include=\*.lib -oRine '[0-9a-zA-Z_-]*\.".$k."'",NULL,$argv[1],NULL))))) == 0)
 				continue;
 			echo "\t****** ".$afftype[$k]." ******".PHP_EOL;
-			$pdbordlllist[$k] = array_map('basename', glob($argv[1].$v.'*.'.$k, GLOB_BRACE));
+			$pdbordlllist[$k] = [];
+			foreach($v as $pv)
+				$pdbordlllist[$k] = array_merge($pdbordlllist[$k],array_map('basename', glob($argv[1].$pv.'*.'.$k, GLOB_BRACE)));
 			foreach($entries as $entry){
 				$ret = explode(":",$entry);
 				if(!is_array($final[$k][$libname = end(explode("/",$ret[0]))]))
@@ -27,15 +29,19 @@
 						foreach($tabresultat as $pdbordll){
 							if(($pdbordlllist[$k] && in_array($pdbordll,$pdbordlllist[$k])) || ($pdbordllused[$k] && in_array($pdbordll,$pdbordllused[$k]))){
 								if(explode(".",$pdbordll)[0] == explode(".",$lib)[0]){
+									// same name lib & dll
 									$echres .= str_pad("\033[92m".$pdbordll."\033[39m",PAD);
 								} else {
+									// diff name lib & dll
 									$echres .= str_pad("\033[95m".$pdbordll."\033[39m",PAD);
 								}
 								$pdbordllused[$k][] = $pdbordll;
 								$pdbordlllist[$k] = array_diff($pdbordlllist[$k],[$pdbordll]);
 							} elseif($pdbordll == "vc140.pdb"){
+								// pdb name is vc140.pdb
 								$echres .= str_pad("\033[33m".$pdbordll."\033[39m",PAD);
 							} else {
+								// no dll found
 								$echres .= str_pad("\033[31m".$pdbordll."\033[39m",PAD);
 							}
 						}
