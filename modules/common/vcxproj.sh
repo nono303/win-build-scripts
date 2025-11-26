87f756dfa422e056d2c8e995f60c9b520e3b79be
nono303 <git@nono303.net>
2025-11-26 18:57:15 +0100

    fix & clean before moving it to php

--- modules/common/vcxproj.sh
+++ modules/common/vcxproj.sh
@@ -1,29 +1,64 @@
 link="<EnableCOMDATFolding>true<\/EnableCOMDATFolding><OptimizeReferences>true<\/OptimizeReferences>"
+
 # nssm jemalloc
 [[ $7 != nostd ]] && std="<LanguageStandard>stdcpplatest<\/LanguageStandard><LanguageStandard_C>stdclatest<\/LanguageStandard_C>"
-for i in `/usr/bin/find $1 -type f -name "*.vcxproj" -o -name "*.config" -o -name "*.csproj" -o -name "*.props" 2>/dev/null` 
+
+for i in `/usr/bin/find $1 -type f -name \*.vcxproj -o -name \*.config -o -name \*.csproj -o -name \*.props 2>/dev/null`
 do
+  echo $i
+  # TASK
+    # remove CodeAnalysisRuleSet
+    # https://github.com/dotnet/msbuild/issues/5692
+    sed -i -E 's/ *<CodeAnalysisRuleSet>[^<]+<\/CodeAnalysisRuleSet>//g' $i
+
   # MSVC
-  sed -i -E 's/<PlatformToolset>[^<]+/<VCToolsVersion>'$5'<\/VCToolsVersion><PlatformToolset>v'$3'/g' $i
+   sed -i -E 's/<PlatformToolset>[^<]+/<VCToolsVersion>'$5'<\/VCToolsVersion><PlatformToolset>v'$3'/g' $i
+
   # Windows Kit
-  sed -i -E 's/<WindowsTargetPlatformVersion>[^<]+/<WindowsTargetPlatformVersion>'$4'/g' $i
+   sed -i -E 's/<WindowsTargetPlatformVersion>[^<]+/<WindowsTargetPlatformVersion>'$4'/g' $i
+
   # .NET
-  sed -i -E 's/<TargetFrameworkVersion>[^<]+/<TargetFrameworkVersion>v'$6'/g' $i
-  sed -i -E 's/NETFramework,Version=[^"]+/NETFramework,Version=v'$6'/g' $i
+    sed -i -E 's/<TargetFrameworkVersion>[^<]+/<TargetFrameworkVersion>v'$6'/g' $i
+    sed -i -E 's/NETFramework,Version=[^"]+/NETFramework,Version=v'$6'/g' $i
+
   # C CXX Flags
-  sed -i -E 's/<Optimization>[^<]+/<Optimization>MaxSpeed/g' $i
-  sed -i 's/<\/ClCompile>/<Optimization>MaxSpeed<\/Optimization><WholeProgramOptimization>true<\/WholeProgramOptimization><MultiProcessorCompilation>true<\/MultiProcessorCompilation><FunctionLevelLinking>true<\/FunctionLevelLinking>'$std$2'<AdditionalOptions>\/Ob3 \/cgthreads8 %(AdditionalOptions)<\/AdditionalOptions><\/ClCompile>/g' $i
+    # remove
+    for remove in InlineFunctionExpansion Optimization WarningLevel WholeProgramOptimization FunctionLevelLinking DebugInformationFormat MultiProcessorCompilation; do
+      sed -i -E 's/<'$remove'>[^<]+<\/'$remove'>//g' $i
+    done
+    
+    sed -i 's/<\/ClCompile>/  '$2'\
+      '$std'\r\n\
+      <Optimization>MaxSpeed<\/Optimization>\r\n\
+      <WarningLevel>TurnOffAllWarnings<\/WarningLevel>\r\n\
+      <WholeProgramOptimization>true<\/WholeProgramOptimization>\r\n\
+      <MultiProcessorCompilation>true<\/MultiProcessorCompilation>\r\n\
+      <FunctionLevelLinking>true<\/FunctionLevelLinking>\r\n\
+      <DebugInformationFormat>ProgramDatabase<\/DebugInformationFormat>\r\n\
+      <RuntimeLibrary>MultiThreadedDLL<\/RuntimeLibrary>\r\n\
+      <AdditionalOptions>\/FS \/Zf \/Gw \/Ob3 \/cgthreads8 %(AdditionalOptions)<\/AdditionalOptions>\r\n\
+    <\/ClCompile>/g' $i
+
   # LINK Flags
-  sed -i -E 's/ *<.?LinkIncremental.*//g' $i
-  sed -i 's/<\/Link>/'$link'<LinkTimeCodeGeneration>UseLinkTimeCodeGeneration<\/LinkTimeCodeGeneration><\/Link>/g' $i
+    # remove
+    for remove in LinkIncremental LinkTimeCodeGeneration EnableCOMDATFolding TargetMachine GenerateDebugInformation; do
+      sed -i -E 's/<'$remove'>[^<]+<\/'$remove'>//g' $i
+    done
+
+    sed -i 's/<\/Link>/  '$link'\r\n\
+      <GenerateDebugInformation>true<\/GenerateDebugInformation>\r\n\
+      <LinkTimeCodeGeneration>UseLinkTimeCodeGeneration<\/LinkTimeCodeGeneration>\r\n\
+    <\/Link>/g' $i
+
   # LIB Flags
-  sed -i 's/<\/Lib>/'$link'<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration><\/Lib>/g' $i
-  # remove inlineExpansion (/Obx) as /Ob3 is not implemented
-  # https://learn.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.vcprojectengine.inlineexpansionoption?view=visualstudiosdk-2022 
-  # https://learn.microsoft.com/en-us/cpp/build/reference/ob-inline-function-expansion?view=msvc-170
-  sed -i -E 's/ *<InlineFunctionExpansion>[^<]+<\/InlineFunctionExpansion>//g' $i
-  # remove CodeAnalysisRuleSet 
-  # https://github.com/dotnet/msbuild/issues/5692
-  sed -i -E 's/ *<CodeAnalysisRuleSet>[^<]+<\/CodeAnalysisRuleSet>//g' $i
+    sed -i 's/<\/Lib>/  '$link'\r\n\
+      <LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration>\r\n\
+      <TargetMachine>MachineX64<\/TargetMachine>\r\n\
+    <\/Lib>/g' $i
+ 
+  # CLEAN
+    sed -i -r '/^\s*$/d' $i
+  # /cygdrive/C/sdk/release/vs18_x64-avx2/bin/tidy.exe --show-warnings false -xml -i -q "$(cygpath -w $i)" --output-file "$(cygpath -w $i)"
 done
-/usr/bin/find $1 -type f -name "*.props" -exec sed -i 's/<LinkTimeCodeGeneration>false<\/LinkTimeCodeGeneration>/'$link'<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration><EnableCOMDATFolding>true<\/EnableCOMDATFolding><OptimizeReferences>true<\/OptimizeReferences>/g' {} \;
+
+# /usr/bin/find $1 -type f -name "*.props" -exec sed -i 's/<LinkTimeCodeGeneration>false<\/LinkTimeCodeGeneration>/'$link'<LinkTimeCodeGeneration>true<\/LinkTimeCodeGeneration><EnableCOMDATFolding>true<\/EnableCOMDATFolding><OptimizeReferences>true<\/OptimizeReferences>/g' {} \;
