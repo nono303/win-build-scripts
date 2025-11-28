c83f396a9d2a3f19312ae401c7c2afe982d23f7d
nono303 <git@nono303.net>
2025-11-28 20:30:14 +0100

    - nssm serf

--- utils/bininfo.bat
+++ utils/bininfo.bat
@@ -5,7 +5,7 @@ if exist %2\. (
 ) else (
 	set BININFO_LOG=%PATH_LOGS%\%1_%MSVC_DEPS%-%ARCH%%AVXB%_%ymdhis%.csv
 	
-	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:229 %4 %5 %6 %7
+	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin %BININFO_LOG:\=/% %2 %3 nb:225 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin\engines-3 %BININFO_LOG:\=/% %2 %3 nb:8 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\bin\ossl-modules %BININFO_LOG:\=/% %2 %3 nb:2 %4 %5 %6 %7
 	call do_php %PATH_UTILS%\sub\%1.php %PATH_INSTALL%\modules %BININFO_LOG:\=/% %2 %3 nb:266 %4 %5 %6 %7
