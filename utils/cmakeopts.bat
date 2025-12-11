7426dd9252cb08383f87d3fe9a80a647c1a87482
nono303 <git@nono303.net>
2025-12-11 18:33:00 +0100

    remove useless

--- utils/cmakeopts.bat
+++
@@ -1,4 +0,0 @@
-REM aom option in macro
-for %%X in (apr-util apr brotli bzip2 cares curl dav1d freeglut freetype2 gdal geographiclib geos giflib jansson lerc libavif libdeflate libev libevent libexpat libffi libgav1 libgd libgeotiff libheif libjpeg-turbo liblzma libmaxminddb libpng libssh2 libtiff libwebp libxdiff libxslt libyuv libzip mod_fcgid nghttp2 nghttp3 ngtcp2 pcre2 proj pthread-win32 qhull serf sqlite tidy wineditline yajl zlib zstd) do (
-	call go %%X cmopts
-)
