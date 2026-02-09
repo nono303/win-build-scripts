8523a6dfd16286ab64a3d6b750b8d28696c11c41
nono303 <git@nono303.net>
2026-02-09 08:38:20 +0100

    fix sigcheck 2.91 2byte char encoded string exec sigcheck by folder (better perf than by file) global refator

--- utils/sub/bininfo.php
+++ utils/sub/bininfo.php
@@ -4,100 +4,86 @@
 	define("SCRIPT_DIR",dirname(__FILE__));
 	define("DEBUG",false);
 
+	function twoBtoOneBStr($data) {
+		foreach($result = str_split($data) as $key => $value) if($key&1) unset($result[$key]);
+		return implode($result);
+	}
+
+	// "Path", "Verified", "Date", "Publisher", "Company", "Description", "Product", "Product Version", "File Version", "Machine Type", "Binary Version", "Original Name", "Internal Name", "Copyright", "Comments", "Entropy"
 	function sigcheck($dir,$file=""){
 		global $data;
-		$sigcmd = pathenv("BIN_SYGCHECK")." -a -c -e -r -nobanner ".$dir."/".$file;
-		if(DEBUG) echo "###".$sigcmd.PHP_EOL;
-		$sig = execnono($sigcmd,NULL,$dir."/",NULL);
+		$sig = explode("\n",twoBtoOneBStr(execnono($sigcmd = pathenv("BIN_SYGCHECK")." -a -ct -e -r -nobanner -accepteula ".$dir."/".$file,NULL,$dir."/",NULL)));
 		debug("sigcheck() ".$sigcmd.": ".$sig);
-		$sig = explode("\n",$sig);
-		array_shift($sig);
-		foreach($sig as $v){
-			$res = explode('","',$v);
-			$ddate = explode(" ",$res[2]);
-			$ddate = $ddate[1]." ".$ddate[0];
-			$ressig[basename($res[0])] = array(
-				"date" => $ddate,
-				"publisher" => $res[3],
-				"company" => $res[4],
-				"description" => $res[5],
-				"product" => $res[6],
-				"productversion" => explode(" ",$res[7])[0],
-				"fileversion" => explode(" ",$res[8])[0],
-				"machinetype" => str_replace("-bit","",$res[9]),
-				"binaryversion" => $res[10],
-				"originalname" => $res[11],
-				"internalname" => $res[12],
-				"copyright" => $res[13],
-				"comments" => $res[14],
-			);
-		};
+		$ark = array_map('trim', explode("\t",array_shift($sig))); // cvs headers
+		foreach($sig as $line) {
+			$val = array_map('trim', explode("\t", $line));
+			if($key = basename($val[0]))
+				foreach($val as $i => $v)
+					$ressig[$key][$ark[$i]] = $v;
+		}
 		return $ressig;
 	}
 
 	function check($dirbase,$dir,$recurse=false){
 		if(is_dir($dir)){
+			$ressig = sigcheck($dir);
 			foreach(scandir($dir) as $file){
 				if(is_dir($dir."/".$file) && $file != "." && $file != ".." && $recurse){
 					check($dirbase, $dir."/".$file,$recurse);
 				} elseif (is_file($dir."/".$file)) {
-					fileCheck($file,$dirbase,$dir,$recurse);
+					fileCheck($file,$dirbase,$dir,$recurse,$ressig);
 				}
 			}
 		} elseif (is_file($dir)) {
 			$file = basename($dir);
 			$dir = str_replace("/".$file,"",$dir);
-			fileCheck($file,$dir,$dir,$recurse);
+			fileCheck($file,$dir,$dir,$recurse,$ressig);
 		}
 	}
 
-	function fileCheck($file,$dirbase,$dir,$recurse,){
+	function fileCheck($file,$dirbase,$dir,$recurse,$ressig){
 		global $col;
 		global $data;
 		global $cur;
 		global $ext;
 		if(is_file($dir."/".$file) && in_array(pathinfo($dir."/".$file)["extension"],$ext)) {
-			$ressig = sigcheck($dir,$file);
 			debug("fileCheck(1) ".$dir."/".$file);
-			if($recurse) {
-				$data[$cur][00] = str_replace($dirbase,"",$dir)."/".$file;
-				} else {
-				$data[$cur][00] = $file;
-			}
-
+			$data[$cur][00] = $recurse ?
+				str_replace($dirbase,"",$dir)."/".$file :
+				$file;
 			$copyright = $_ENV["RC_COPYRIGHT"];
 			$title = $_ENV["ARCH"]." ".$_ENV["AVXECHO"]." MSVC ".$_ENV["vcvars_ver"];
-			$data[$cur][50] =	$ressig[$file]["binaryversion"];
-			$data[$cur][60] =	$ressig[$file]["productversion"];
-			$data[$cur][5] =	$ressig[$file]["date"];
-			$data[$cur][88] =	$ressig[$file]["product"];
-			$data[$cur][110] =	$ressig[$file]["company"];
-			$data[$cur][120] =	strlen($ressig[$file]["description"]) > 140 ? substr($ressig[$file]["description"],0,140)."..." : $ressig[$file]["description"];
-			$data[$cur][65] =	$ressig[$file]["fileversion"];
-			$data[$cur][10] =	$ressig[$file]["machinetype"];
-			$data[$cur][150] =	$ressig[$file]["originalname"];
-			$data[$cur][160] =	$ressig[$file]["internalname"];
-			$data[$cur][170] =	$ressig[$file]["copyright"];
-			$data[$cur][180] =	$ressig[$file]["comments"];
+			$data[$cur][50] =	$ressig[$file]["Binary Version"];
+			$data[$cur][60] =	$ressig[$file]["Product Version"];
+			$data[$cur][5] =	$ressig[$file]["Date"];
+			$data[$cur][88] =	$ressig[$file]["Product"];
+			$data[$cur][110] =	$ressig[$file]["Company"];
+			$data[$cur][120] =	strlen($ressig[$file]["Description"]) > 140 ?
+				substr($ressig[$file]["Description"],0,140)."..." :
+				$ressig[$file]["Description"];
+			$data[$cur][65] =	$ressig[$file]["File Version"];
+			$data[$cur][10] =	$ressig[$file]["Machine Type"];
+			$data[$cur][150] =	$ressig[$file]["Original Name"];
+			$data[$cur][160] =	$ressig[$file]["Internal Name"];
+			$data[$cur][170] =	$ressig[$file]["Copyright"];
+			$data[$cur][180] =	$ressig[$file]["Comments"];
 			if($data[$cur][88] == "n/a")
 				$data[$cur][88] = "\033[33m".$data[$cur][88]."\033[39m";
-			if($data[$cur][170] == $copyright){
-				$data[$cur][85] = "OK";
-			} else {
-				$data[$cur][85] = "\033[31mKO\033[39m";
-			}
+			$data[$cur][85] = $data[$cur][170] == $copyright ?
+				"OK" :
+				"\033[31mKO\033[39m";
 			$nbavx = 0;
 			$data[$cur][30] = "";
 			if(CHECK_AVX){
 				$obdcmd = pathenv("PATH_BIN_CYGWIN").'/sh.exe -c "objdump -M intel -d '.$dir."/".$file.' | ./opcode.sh -s AVX';
-				if(DEBUG) 
+				if(DEBUG)
 					echo "###".$obdcmd.PHP_EOL;
 				$stderr = null;
 				$obd  = execnono($obdcmd,NULL,SCRIPT_DIR,NULL,$stderr);
 				if($stderr){
 					// dumpbin fix issue with 'BFD: __FILE__: bad string table size X' but is really slow
 					$obdcmd = pathenv("PATH_BIN_CYGWIN").'/sh.exe -c "dumpbin /disasm '.$dir."/".$file.' | ./opcode.sh -s AVX';
-					if(DEBUG) 
+					if(DEBUG)
 						echo "###".$obdcmd.PHP_EOL;
 					$obd  = execnono($obdcmd,NULL,SCRIPT_DIR,NULL);
 				}
@@ -109,7 +95,7 @@
 					}
 				}
 				if($nbavx > 0)
-				$data[$cur][30]="avx (".$nbavx.")";
+					$data[$cur][30]="avx (".$nbavx.")";
 			}
 
 			$pdbfile = replace_extension($file,"pdb");
@@ -117,7 +103,8 @@
 			$data[$cur][80] = "\033[33mn/a\033[39m";
 			if(is_file($dir."/".$pdbfile)){
 				$chkmcmd = pathenv("BIN_CHKMATCH").' -c '.$dir."/".$file.' '.$dir."/".$pdbfile;
-				if(DEBUG) echo "###".$chkmcmd.PHP_EOL;
+				if(DEBUG)
+					echo "###".$chkmcmd.PHP_EOL;
 				$chkm = execnono($chkmcmd,NULL,SCRIPT_DIR,NULL);
 				debug("fileCheck(3) ".$chkmcmd.": ".$chkm);
 				preg_match("/Result: (.*)/",$chkm,$matches);
@@ -125,15 +112,14 @@
 				$data[$cur][80] = $matches[1] ?
 					str_replace(")","",str_replace("Unmatched (reason: ","",$matches[1])) :
 					"KO";
-				if($data[$cur][80] != "Matched"){
-					$data[$cur][80] = "\033[31m".$data[$cur][80]."\033[39m";
-				} else {
-					$data[$cur][80] = "OK";
-				}
+				$data[$cur][80] = $data[$cur][80] != "Matched" ?
+					"\033[31m".$data[$cur][80]."\033[39m" :
+					"OK";
 			}
 
 			$dbhcmd = "dumpbin /headers ".$dir."/".$file;
-			if(DEBUG) echo "###".$dbhcmd.PHP_EOL;
+			if(DEBUG)
+				echo "###".$dbhcmd.PHP_EOL;
 			$dbh = execnono($dbhcmd,NULL,$dir."/",NULL);
 			debug("fileCheck(4) ".$dbhcmd.": ".$dbh);
 
@@ -146,13 +132,8 @@
 
 			ksort($data[$cur],SORT_NUMERIC);
 			foreach($data[$cur] as $k => $v)
-				if($col[$k]["pad"] != -1) {
-					if(is_int(strpos($v ?: "","\033"))) {
-						echo str_pad($v,$col[$k]["pad"]+10," ");
-					} else {
-						echo str_pad($v,$col[$k]["pad"]," ");
-					}
-				}
+				if($col[$k]["pad"] != -1)
+					echo str_pad($v,$col[$k]["pad"]+ (is_int(strpos($v ?: "","\033")) ? 10 : 0)," ");
 			echo PHP_EOL;
 			$cur++;
 		}
@@ -172,12 +153,9 @@
 		if($matches[1])
 			$nbfile = $matches[1];
 	}
+
 	global $ext;
-	if($extarg){
-		$ext = explode("-",$extarg);
-	} else {
-		$ext = ["exe","dll","so"];
-	}
+	$ext = $extarg ? explode("-",$extarg) : ["exe","dll","so"];
 	echo "************************************".PHP_EOL;
 	echo "ext:      ".str_replace('"','',json_encode($ext)).PHP_EOL;
 	if($checkavx){
@@ -215,11 +193,11 @@
 			150 => array("name" =>	"originalname",	"pad" => -1),
 			160 => array("name" =>	"internalname",	"pad" => -1),
 			170 => array("name" =>	"copyright",	"pad" => -1),
-		
+
 			180 => array("name" =>	"comments",		"pad" => -1),
-		
+
 	);
-	if(!CHECK_AVX) 
+	if(!CHECK_AVX)
 		$col[30]["pad"] = -1;
 
 	global $data;
@@ -230,32 +208,27 @@
 
 	$argv[1] = str_replace("\\","/",$argv[1]);
 	$affile = " ";
-	if($nbfile && is_dir($argv[1])){
-		if(($scanfile = sizeof(scandir($argv[1]))-2) == $nbfile) {
-			$affile = ": \033[32m".$scanfile." (".$nbfile.")\033[39m ";
-		} else {
-			$affile = ": \033[31m".$scanfile." (".$nbfile.")\033[39m ";
-		}
-	}
+	if($nbfile && is_dir($argv[1]))
+		$affile = ($scanfile = sizeof(scandir($argv[1]))-2) == $nbfile ?
+			": \033[32m".$scanfile." (".$nbfile.")\033[39m " :
+			": \033[31m".$scanfile." (".$nbfile.")\033[39m ";
 	echo ">>>>>> ".$argv[1].$affile."<<<<<<".PHP_EOL;
 
 	$data[$cur] = $argv[1];
 	$cur++;
 	foreach($col as $k => $v){
 		$data[$cur][$k] = $v["name"];
-		if($v["pad"] != -1) echo str_pad($v["name"],$v["pad"]," ");
+		if($v["pad"] != -1)
+			echo str_pad($v["name"],$v["pad"]," ");
 	}
 	echo PHP_EOL;
 	$cur++;
 	check($argv[1],$argv[1],RECURSE);
 	$csv = "";
-	foreach($data as $v){
-		if(is_array($v)){
-			$csv .= implode(";",$v).PHP_EOL;
-		} else {
-			$csv .= $v.PHP_EOL;
-		}
-	}
+	foreach($data as $v)
+		$csv .= is_array($v) ?
+			implode(";",$v).PHP_EOL :
+			$v.PHP_EOL;
 	if($argv[2] != "null")
 		file_put_contents($argv[2],preg_replace("/\\033\[3[0-9]m/","",$csv), FILE_APPEND);
 ?>
