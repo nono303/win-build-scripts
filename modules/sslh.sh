231215f377f70adb5bb5738d3cf635d64a1c5ebc
nono303 <git@nono303.net>
2026-03-06 15:19:58 +0100

    set make opts only build exe

--- modules/sslh.sh
+++ modules/sslh.sh
@@ -8,4 +8,4 @@ fi
 cd $1
 ./configure
 make $makeargs -j$(nproc) clean
-make $makeargs -j$(nproc) CFLAGS+=" -Wno-error -O3 -s -march=${2} "
+make $makeargs -j$(nproc) USELIBEV=1 USELIBCONFIG=1 ENABLE_REGEX=1 CFLAGS+=" -Wno-error -O3 -s -march=${2} " sslh-fork sslh-select sslh-ev echosrv
