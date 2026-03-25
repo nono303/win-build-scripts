4d274b08f6629d3f6a867cc20e55ea6309a2b58e
nono303 <git@nono303.net>
2026-03-25 16:57:21 +0100

    build Dependencies.exe (cmd line)

--- modules/dependencies.bat
+++ modules/dependencies.bat
@@ -6,13 +6,13 @@ set OUTDIR_CONF=Release
 %PATH_SOFTS%\NuGet.exe restore Dependencies.sln
 
 MSBuild.exe Dependencies.sln %MSBUILD_OPTS% ^
-/t:clean,DependenciesGui ^
+/t:clean,DependenciesGui,Dependencies ^
 /nowarn:C4018;C4267;CS0169 ^
 /p:Configuration=%OUTDIR_CONF% ^
 /p:Platform="%ARCH%"
 
 if exist %PATH_INSTALL%\_%1\. rmdir /S /Q %PATH_INSTALL%\_%1
 mkdir %PATH_INSTALL%\_%1
-for %%X in (DependenciesGui.exe DependenciesLib.dll) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\bin\%OUTDIR_CONF%%ARCH%\%%X nopdb)
+for %%X in (Dependencies.exe DependenciesGui.exe DependenciesLib.dll) do (call do_php %PATH_UTILS%\sub\version.php %1 %PATH_SRC%\%1\bin\%OUTDIR_CONF%%ARCH%\%%X nopdb)
 xcopy /C /F /Y %PATH_SRC%\%1\bin\%OUTDIR_CONF%%ARCH%\* %PATH_INSTALL%\_%1\*
-call do_php %PATH_UTILS%\sub\bininfo.php %PATH_INSTALL%\_%1\ checkavx
+REM call do_php %PATH_UTILS%\sub\bininfo.php %PATH_INSTALL%\_%1\ checkavx
